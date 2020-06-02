24138a1e35cfdf74d3ff0aca66bda9bef3b62b08
PARTHSONI95 <58928487+PARTHSONI95@users.noreply.github.com>
2020-06-02 12:51:05 -0400

    Initial frontend React app (#2)
    Created initial frontend React app

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div><marquee>Hello World!</marquee><hr/></div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
