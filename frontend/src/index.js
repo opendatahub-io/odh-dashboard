5aa7660c2c4393ef62b00a156b7b98d2cbf5a2da
Parth Soni <parthsangita95@yahoo.in>
2020-06-09 13:50:39 -0400

    Updating with Card Layout using React hooks and Patternfly

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import App from './App.js'
 
 ReactDOM.render(
   <React.StrictMode>
-    <div><marquee>Hello World!</marquee><hr/></div>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
