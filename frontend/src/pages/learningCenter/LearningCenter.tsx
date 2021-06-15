4cc544b00ed6f9963f7640a7034d80668659230c
Jeffrey Phillips <jephilli@redhat.com>
2021-06-15 13:23:52 -0400

    Collapse filter panel when view gets too narrow
    Also adds "Favorites" category.

--- frontend/src/pages/learningCenter/LearningCenter.tsx
+++ frontend/src/pages/learningCenter/LearningCenter.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
-import { useHistory } from 'react-router';
-import classNames from 'classnames';
 import * as _ from 'lodash';
 import useDimensions from 'react-cool-dimensions';
-import {
-  Button,
-  EmptyState,
-  EmptyStateBody,
-  EmptyStateIcon,
-  EmptyStateSecondaryActions,
-  EmptyStateVariant,
-  Gallery,
-  PageSection,
-  PageSectionVariants,
-  Title,
-} from '@patternfly/react-core';
-import { SearchIcon } from '@patternfly/react-icons';
 import { QuickStartContext, QuickStartContextValues } from '@cloudmosaic/quickstarts';
 import { OdhDocument, OdhDocumentType } from '../../types';
 import { useWatchComponents } from '../../utilities/useWatchComponents';
 import { useWatchDocs } from '../../utilities/useWatchDocs';
 import { useLocalStorage } from '../../utilities/useLocalStorage';
 import { useQueryParams } from '../../utilities/useQueryParams';
-import { removeQueryArgument } from '../../utilities/router';
 import ApplicationsPage from '../ApplicationsPage';
 import QuickStarts from '../../app/QuickStarts';
-import OdhDocCard from '../../components/OdhDocCard';
-import OdhDocListItem from '../../components/OdhDocListItem';
 import {
   DOC_SORT_KEY,
   DOC_SORT_ORDER_KEY,
@@ -37,122 +19,18 @@ import {
   SORT_TYPE_NAME,
   SORT_TYPE_TYPE,
   CARD_VIEW,
-  LIST_VIEW,
   VIEW_TYPE,
-  ENABLED_FILTER_KEY,
-  SEARCH_FILTER_KEY,
-  DOC_TYPE_FILTER_KEY,
-  PROVIDER_FILTER_KEY,
-  CATEGORY_FILTER_KEY,
+  FAVORITE_RESOURCES,
 } from './const';
-import LearningCenterListHeader from './LearningCenterListHeader';
 import LearningCenterToolbar from './LearningCenterToolbar';
 import LearningCenterFilters from './LearningCenterFilters';
 import { useDocFilterer } from './useDocFilterer';
+import { combineCategoryAnnotations } from '../../utilities/utils';
+import LearningCenterDataView from './LearningCenterDataView';
 
 import './LearningCenter.scss';
-import { combineCategoryAnnotations } from '../../utilities/utils';
-
-export const FAVORITE_RESOURCES = 'ods.dashboard.resources.favorites';
 
 const description = `Access all learning resources for Open Data Hub and supported applications.`;
-
-type LearningCenterDataViewProps = {
-  filteredDocApps: OdhDocument[];
-  favorites: string[];
-  updateFavorite: (isFavorite: boolean, name: string) => void;
-  viewType: string;
-};
-
-const LearningCenterDataView: React.FC<LearningCenterDataViewProps> = React.memo(
-  ({ filteredDocApps, favorites, updateFavorite, viewType }) => {
-    const history = useHistory();
-    const [sizeClass, setSizeClass] = React.useState<string>('m-ods-size-lg');
-    const { observe } = useDimensions({
-      breakpoints: { sm: 0, md: 600, lg: 750 },
-      onResize: ({ currentBreakpoint }) => {
-        setSizeClass(`m-odh-size-${currentBreakpoint}`);
-      },
-    });
-
-    const onClearFilters = () => {
-      removeQueryArgument(history, SEARCH_FILTER_KEY);
-      removeQueryArgument(history, CATEGORY_FILTER_KEY);
-      removeQueryArgument(history, ENABLED_FILTER_KEY);
-      removeQueryArgument(history, DOC_TYPE_FILTER_KEY);
-      removeQueryArgument(history, PROVIDER_FILTER_KEY);
-    };
-
-    const renderContent = () => {
-      if (filteredDocApps.length === 0) {
-        return (
-          <EmptyState variant={EmptyStateVariant.full}>
-            <EmptyStateIcon icon={SearchIcon} />
-            <Title headingLevel="h2" size="lg">
-              No results match the filter criteria
-            </Title>
-            <EmptyStateBody>
-              No resources are being shown due to the filters being applied.
-            </EmptyStateBody>
-            <EmptyStateSecondaryActions>
-              <Button variant="link" onClick={onClearFilters}>
-                Clear all filters
-              </Button>
-            </EmptyStateSecondaryActions>
-          </EmptyState>
-        );
-      }
-
-      if (viewType !== LIST_VIEW) {
-        return (
-          <Gallery className="odh-learning-paths__gallery" hasGutter>
-            {filteredDocApps.map((doc) => (
-              <OdhDocCard
-                key={`${doc.metadata.name}`}
-                odhDoc={doc}
-                favorite={favorites.includes(doc.metadata.name)}
-                updateFavorite={(isFavorite) => updateFavorite(isFavorite, doc.metadata.name)}
-              />
-            ))}
-          </Gallery>
-        );
-      }
-
-      const listViewClasses = classNames('odh-learning-paths__list-view', sizeClass);
-      return (
-        <div className={listViewClasses} aria-label="Simple data list example">
-          <LearningCenterListHeader />
-          {filteredDocApps.map((doc) => (
-            <OdhDocListItem
-              key={`${doc.metadata.name}`}
-              odhDoc={doc}
-              favorite={favorites.includes(doc.metadata.name)}
-              updateFavorite={(isFavorite) => updateFavorite(isFavorite, doc.metadata.name)}
-            />
-          ))}
-        </div>
-      );
-    };
-
-    return (
-      <>
-        <PageSection
-          isFilled={true}
-          variant={viewType === LIST_VIEW ? PageSectionVariants.light : PageSectionVariants.default}
-          className={
-            viewType === LIST_VIEW
-              ? 'odh-learning-paths__view-panel__list-view'
-              : 'odh-learning-paths__view-panel__card-view'
-          }
-        >
-          {renderContent()}
-        </PageSection>
-        <div ref={observe} />
-      </>
-    );
-  },
-);
-LearningCenterDataView.displayName = 'LearningCenterInner';
 
 const LearningCenter: React.FC = () => {
   const { docs: odhDocs, loaded: docsLoaded, loadError: docsLoadError } = useWatchDocs();
@@ -161,12 +39,21 @@ const LearningCenter: React.FC = () => {
   const [docApps, setDocApps] = React.useState<OdhDocument[]>([]);
   const [filteredDocApps, setFilteredDocApps] = React.useState<OdhDocument[]>([]);
   const queryParams = useQueryParams();
-  const docFilterer = useDocFilterer();
   const sortType = queryParams.get(DOC_SORT_KEY) || SORT_TYPE_NAME;
   const sortOrder = queryParams.get(DOC_SORT_ORDER_KEY) || SORT_ASC;
   const [favorites, setFavorites] = useLocalStorage(FAVORITE_RESOURCES);
   const favoriteResources = React.useMemo(() => JSON.parse(favorites || '[]'), [favorites]);
+  const docFilterer = useDocFilterer(favoriteResources);
   const [viewType, setViewType] = useLocalStorage(VIEW_TYPE);
+  const [filtersCollapsed, setFiltersCollapsed] = React.useState<boolean>(false);
+  const [filtersCollapsible, setFiltersCollapsible] = React.useState<boolean>(false);
+  const { observe } = useDimensions({
+    breakpoints: { sm: 0, md: 600 },
+    onResize: ({ currentBreakpoint }) => {
+      setFiltersCollapsed(currentBreakpoint === 'sm');
+      setFiltersCollapsible(currentBreakpoint === 'sm');
+    },
+  });
 
   React.useEffect(() => {
     if (loaded && !loadError && docsLoaded && !docsLoadError) {
@@ -215,7 +102,6 @@ const LearningCenter: React.FC = () => {
           } else {
             updatedDoc.spec.appEnabled = false;
           }
-          console.log(`===== ${updatedDoc.metadata.name}: =>  ${updatedDoc.metadata.annotations}`);
           return updatedDoc;
         });
       setDocApps(updatedDocApps);
@@ -296,14 +182,22 @@ const LearningCenter: React.FC = () => {
       loadError={loadError || docsLoadError}
       empty={false}
     >
-      <div className="odh-learning-paths__content">
-        <LearningCenterFilters docApps={docApps} />
+      <div className="odh-learning-paths__content" ref={observe}>
+        <LearningCenterFilters
+          docApps={docApps}
+          collapsible={filtersCollapsible}
+          collapsed={filtersCollapsed}
+          onCollapse={() => setFiltersCollapsed(true)}
+          favorites={favoriteResources}
+        />
         <div className="odh-learning-paths__view-panel">
           <LearningCenterToolbar
             count={filteredDocApps.length}
             totalCount={docApps.length}
             viewType={viewType || CARD_VIEW}
             updateViewType={setViewType}
+            filtersCollapsible={filtersCollapsible}
+            onToggleFiltersCollapsed={() => setFiltersCollapsed(!filtersCollapsed)}
           />
           <LearningCenterDataView
             filteredDocApps={filteredDocApps}
