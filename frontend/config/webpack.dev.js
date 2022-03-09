901bdd67d0034ac03dff5275dc33d4aefc3c185b
Christopher Chase <cchase@redhat.com>
2022-03-09 16:57:47 -0500

    Update api-proxy to use BACKEND_PORT env var (#132)

--- frontend/config/webpack.dev.js
+++ frontend/config/webpack.dev.js
@@ -12,6 +12,7 @@ const COMMON_DIR = process.env._ODH_COMMON_DIR;
 const DIST_DIR = process.env._ODH_DIST_DIR;
 const HOST = process.env._ODH_HOST;
 const PORT = process.env._ODH_PORT;
+const BACKEND_PORT = process.env.BACKEND_PORT || 8080;
 
 module.exports = merge(
   {
@@ -35,7 +36,7 @@ module.exports = merge(
       open: true,
       stats: 'errors-only',
       proxy: {
-        '/api': 'http://localhost:8080',
+        '/api': `http://localhost:${BACKEND_PORT}`,
       },
     },
     module: {
