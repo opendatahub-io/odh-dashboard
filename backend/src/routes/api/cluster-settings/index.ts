b36738d013235ce278ab502e05fc4e004c6e2fa8
Chris Chase <cchase@redhat.com>
2022-03-09 16:41:26 -0500

    Update api-proxy to use BACKEND_PORT env var

--- backend/src/routes/api/cluster-settings/index.ts
+++ backend/src/routes/api/cluster-settings/index.ts
@@ -1,21 +1,14 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { DEV_MODE } from '../../../utils/constants';
-import { addCORSHeader } from '../../../utils/responseUtils';
 import { getClusterSettings, updateClusterSettings } from './clusterSettingsUtils';
 
 export default async (fastify: FastifyInstance): Promise<void> => {
   fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
     return getClusterSettings(fastify)
       .then((res) => {
-        if (DEV_MODE) {
-          addCORSHeader(request, reply);
-        }
         return res;
       })
       .catch((res) => {
-        if (DEV_MODE) {
-          addCORSHeader(request, reply);
-        }
         reply.send(res);
       });
   });
@@ -23,15 +16,9 @@ export default async (fastify: FastifyInstance): Promise<void> => {
   fastify.get('/update', async (request: FastifyRequest, reply: FastifyReply) => {
     return updateClusterSettings(fastify, request)
       .then((res) => {
-        if (DEV_MODE) {
-          addCORSHeader(request, reply);
-        }
         return res;
       })
       .catch((res) => {
-        if (DEV_MODE) {
-          addCORSHeader(request, reply);
-        }
         reply.send(res);
       });
   });
