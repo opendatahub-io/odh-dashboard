dcf080a926f3c477ab8e77555669645ddc20398d
Christopher Chase <cchase@redhat.com>
2022-03-09 15:04:31 -0500

    Use webpack dev server proxy instead of CORS utilities (#127)

--- backend/src/routes/api/docs/index.ts
+++ backend/src/routes/api/docs/index.ts
@@ -1,21 +1,13 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
-import { DEV_MODE } from '../../../utils/constants';
-import { addCORSHeader } from '../../../utils/responseUtils';
 import { listDocs } from './list';
 
 export default async (fastify: FastifyInstance): Promise<void> => {
   fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
     return listDocs(fastify, request)
       .then((res) => {
-        if (DEV_MODE) {
-          addCORSHeader(request, reply);
-        }
         return res;
       })
       .catch((res) => {
-        if (DEV_MODE) {
-          addCORSHeader(request, reply);
-        }
         reply.send(res);
       });
   });
