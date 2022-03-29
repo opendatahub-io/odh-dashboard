7c4965b68f3e6598fa423004015ba680afdbebb7
Tom Coufal <7453394+tumido@users.noreply.github.com>
2022-03-29 14:22:23 +0200

    [byon] Implement backend (#156)
    * feat(byon): List all notebooks
    
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    
    * feat(byon): Get single notebook
    
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    
    * fix(byon): Update api spec to include software and id
    
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    
    * feat(byon): Schedule new notebook import
    
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    
    * feat(byon): Delete notebook
    
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    
    * feat(byon): Update notebook
    
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -31,17 +31,23 @@ export declare type QuickStart = {
 };
 
 // Properties common to (almost) all Kubernetes resources.
-export type K8sResourceCommon = {
+export type K8sResourceBase = {
   apiVersion?: string;
   kind?: string;
+}
+
+export type K8sResourceCommon = {
   metadata?: {
     name?: string;
     namespace?: string;
+    generateName?: string;
     uid?: string;
     labels?: { [key: string]: string };
     annotations?: { [key: string]: string };
+    creationTimestamp?: Date;
   };
-};
+} & K8sResourceBase;
+
 
 export enum BUILD_PHASE {
   none = 'Not started',
@@ -228,18 +234,78 @@ export type NotebookError = {
 export type NotebookStatus = "Importing" | "Validating" | "Succeeded" | "Failed";
 
 export type Notebook = {
-  name: string;
-  url: string;
-  description?: string;
+  id: string;
   phase?: NotebookStatus;
   user?: string;
   uploaded?: Date;
+  error?: NotebookError;
+} & NotebookCreateRequest & NotebookUpdateRequest;
+
+export type NotebookCreateRequest = {
+  name: string;
+  url: string;
+  description?: string;
+}
+
+export type NotebookUpdateRequest = {
+  id: string;
   visible?: boolean;
   packages?: NotebookPackage[];
-  error?: NotebookError;
+  software?: NotebookPackage[];
 }
 
 export type NotebookPackage = {
   name: string;
   version: string;
 }
+
+
+export type ImageStreamTagSpec = {
+  name: string;
+  annotations?: { [key: string]: string };
+  from?: {
+    kind: string;
+    name: string;
+  }
+}
+export type ImageStreamKind = {
+  spec?: {
+    tags: ImageStreamTagSpec[];
+  }
+  status?: any
+} & K8sResourceCommon;
+
+export type ImageStreamListKind = {
+  items: ImageStreamKind[];
+} & K8sResourceBase;
+
+export type PipelineRunKind = {
+  spec: {
+    params: {
+      name: string;
+      value: string;
+    }[]
+    pipelineRef: {
+      name: string;
+    }
+    workspaces?: [
+      {
+        name: string
+        volumeClaimTemplate: {
+          spec: {
+            accessModes: string[]
+            resources: {
+              requests: {
+                storage: string
+              }
+            }
+          }
+        }
+      }
+    ]
+  }
+} & K8sResourceCommon;
+
+export type PipelineRunListKind = {
+  items: PipelineRunKind[];
+} & K8sResourceBase;
