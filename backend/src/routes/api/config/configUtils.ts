6b68109902c4c900547ab66b533763fa02ac9e58
maroroman <mroman@redhat.com>
2022-05-23 16:26:52 +0200

    Add config patch

---
+++ backend/src/routes/api/config/configUtils.ts
@@ -0,0 +1,21 @@
+import { PatchUtils } from "@kubernetes/client-node";
+import { KubeFastifyInstance, DashboardConfig } from "../../../types";
+
+export const setDashboardConfig = async (fastify: KubeFastifyInstance, request: any): Promise<DashboardConfig> => {
+    const options = {
+        headers: { 'Content-type': PatchUtils.PATCH_FORMAT_JSON_MERGE_PATCH },
+    };
+    const response = await fastify.kube.customObjectsApi.patchNamespacedCustomObject(
+        'opendatahub.io',
+        'v1alpha',
+        fastify.kube.namespace,
+        'odhdashboards',
+        'odh-dashboard-config',
+        request,
+        undefined,
+        undefined,
+        undefined,
+        options,
+    )
+    return response.body as DashboardConfig;
+}
