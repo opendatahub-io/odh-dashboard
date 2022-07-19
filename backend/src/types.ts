9fb91587933eb6f396dd9016b3002bb66a1a6e26
Lucas Fernandez <lferrnan@redhat.com>
2022-07-19 19:50:41 +0200

    Fix issue with user retrieval

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -115,9 +115,9 @@ export type BuildKind = {
     output: {
       to: {
         name: string;
-      }
-    }
-  }
+      };
+    };
+  };
   status: {
     phase: BUILD_PHASE;
     completionTimestamp: string;
