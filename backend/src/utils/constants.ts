3dcedd15af9e16c7fa3972071035a2de7cfcfbef
Juntao Wang <juntwang@redhat.com>
2022-07-21 11:00:26 -0400

    fix small issue

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,6 +1,5 @@
 import './dotenv';
 import { DashboardConfig } from '../types';
-import { V1ConfigMap } from '@kubernetes/client-node';
 
 export const PORT = process.env.PORT || process.env.BACKEND_PORT || 8080;
 export const IP = process.env.IP || '0.0.0.0';
@@ -40,12 +39,4 @@ export const blankDashboardCR: DashboardConfig = {
       disableUserManagement: false,
     },
   },
-};
-export const blankEnvCM: V1ConfigMap = {
-  apiVersion: 'v1',
-  kind: 'ConfigMap',
-  metadata: {
-    name: '',
-  },
-  data: {},
 };
