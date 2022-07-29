fe63794a5b89a5ea0afbb07bd054bd01c38672e3
maroroman <mroman@redhat.com>
2022-07-29 13:58:09 +0200

    Formatted

--- backend/src/routes/api/gpu/gpuUtils.ts
+++ backend/src/routes/api/gpu/gpuUtils.ts
@@ -35,32 +35,36 @@ export const getGPUNumber = async (fastify: KubeFastifyInstance): Promise<number
       .then((res) => res.body as V1Secret);
     const promToken = decodeB64(dashboardSASecret.data.token);
     for (let i = 0; i < gpuPodList.items.length; i++) {
-      const data = await getGPUData(fastify, gpuPodList.items[i].status.podIP, promToken)
-      console.log("Got data: " + JSON.stringify(data, null, 2))
-      const gpuNumber = Number(data["response"]["data"]["result"][0]['value'][1])
-      console.log("Got number: " + gpuNumber)
+      const data = await getGPUData(fastify, gpuPodList.items[i].status.podIP, promToken);
+      //console.log('Got data: ' + JSON.stringify(data, null, 2));
+      const gpuNumber = Number(data['response']['data']['result'][0]['value'][1]);
+      //console.log('Got number: ' + gpuNumber);
       if (gpuNumber > maxGpuNumber) {
         maxGpuNumber = gpuNumber;
       }
-      console.log("Current max: " + maxGpuNumber)
+      //console.log('Current max: ' + maxGpuNumber);
     }
-  };
+  }
   return maxGpuNumber;
 };
 
-export const getGPUData = async (fastify: KubeFastifyInstance, podIP: string, token: string): Promise<any> => {
+export const getGPUData = async (
+  fastify: KubeFastifyInstance,
+  podIP: string,
+  token: string,
+): Promise<any> => {
   return await new Promise((resolve, reject) => {
     const options = {
       hostname: 'thanos-querier.openshift-monitoring.svc.cluster.local',
       port: 9091,
-      path: `/api/v1/query?query=count+(count+by+(UUID,GPU_I_ID)(DCGM_FI_PROF_GR_ENGINE_ACTIVE{instance=\"${podIP}:8080\"})+or+vector(0))+-+count+(count+by+(UUID,GPU_I_ID)(DCGM_FI_PROF_GR_ENGINE_ACTIVE{instance=\"${podIP}:8080\",exported_pod=~\".+\"})+or+vector(0))`,
+      path: `/api/v1/query?query=count+(count+by+(UUID,GPU_I_ID)(DCGM_FI_PROF_GR_ENGINE_ACTIVE{instance="${podIP}:8080"})+or+vector(0))+-+count+(count+by+(UUID,GPU_I_ID)(DCGM_FI_PROF_GR_ENGINE_ACTIVE{instance="${podIP}:8080",exported_pod=~".+"})+or+vector(0))`,
       headers: {
         Authorization: `Bearer ${token}`,
       },
       protocol: 'https:',
-      rejectUnauthorized: false
+      rejectUnauthorized: false,
     };
-    console.log("Starting request...")
+    console.log('Starting request...');
     const httpsRequest = https.get(options, (res) => {
       res.setEncoding('utf8');
       let rawData = '';
@@ -70,17 +74,17 @@ export const getGPUData = async (fastify: KubeFastifyInstance, podIP: string, to
       res.on('end', () => {
         try {
           const parsedData = JSON.parse(rawData);
-          console.log("Parsed data: " + JSON.stringify(parsedData, null, 2))
-          resolve({ response: parsedData })
+          //console.log('Parsed data: ' + JSON.stringify(parsedData, null, 2));
+          resolve({ response: parsedData });
         } catch (e) {
-          console.error(e.message);
-          reject({ code: 500, response: rawData})
+          //console.error(e.message);
+          reject({ code: 500, response: rawData });
         }
       });
     });
-    console.log("Ending request...")
+    //console.log('Ending request...');
     httpsRequest.end();
-  })
+  });
 };
 
 const decodeB64 = (str: string): string => Buffer.from(str, 'base64').toString('binary');
