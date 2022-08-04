337e3f9665534b8fae04d60ac81f1fca40d488d3
maroroman <mroman@redhat.com>
2022-08-04 14:28:37 +0200

    Clean up

--- backend/src/routes/api/gpu/gpuUtils.ts
+++ backend/src/routes/api/gpu/gpuUtils.ts
@@ -1,18 +1,12 @@
 import { KubeFastifyInstance, PrometheusResponse } from '../../../types';
-import { V1PodList, V1Secret, V1ServiceAccount } from '@kubernetes/client-node';
+import { V1PodList } from '@kubernetes/client-node';
 import https from 'https';
 import * as fs from 'fs';
-import { resolve } from 'path';
 
 export const getGPUNumber = async (fastify: KubeFastifyInstance): Promise<number> => {
   let maxGpuNumber = 0;
   const gpuPodList = await fastify.kube.coreV1Api
-    .listPodForAllNamespaces(
-      undefined,
-      undefined,
-      undefined,
-      'app=nvidia-dcgm-exporter',
-    )
+    .listPodForAllNamespaces(undefined, undefined, undefined, 'app=nvidia-dcgm-exporter')
     .then((res) => {
       return res.body as V1PodList;
     })
@@ -21,33 +15,18 @@ export const getGPUNumber = async (fastify: KubeFastifyInstance): Promise<number
       return { items: [] } as V1PodList;
     });
   if (gpuPodList.items.length != 0) {
-    const dashboardSA = await fastify.kube.coreV1Api
-      .readNamespacedServiceAccount('odh-dashboard', fastify.kube.namespace)
-      .then((res) => res.body as V1ServiceAccount);
-    const token = await new Promise((resolve,reject) => {
-      fs.readFile("/var/run/secrets/kubernetes.io/serviceaccount/token", (err, data) => {
+    const token = await new Promise<string>((resolve, reject) => {
+      fs.readFile('/var/run/secrets/kubernetes.io/serviceaccount/token', (err, data) => {
         try {
-          resolve(String(data))
+          resolve(String(data));
+        } catch {
+          reject('');
+          fastify.log.error(err);
         }
-        catch {
-          reject("")
-          fastify.log.error(err)
-        }
-      })
-    })
-    console.log(`TOKEN: ${token}`)
-    let dashboardTokenName = '';
-    for (let i = 0; i < dashboardSA.secrets.length; i++) {
-      if (dashboardSA.secrets[i].name.includes('token')) {
-        dashboardTokenName = dashboardSA.secrets[i].name;
-      }
-    }
-    const dashboardSASecret = await fastify.kube.coreV1Api
-      .readNamespacedSecret(dashboardTokenName, fastify.kube.namespace)
-      .then((res) => res.body as V1Secret);
-    const promToken = decodeB64(dashboardSASecret.data.token);
+      });
+    });
     for (let i = 0; i < gpuPodList.items.length; i++) {
-      const data = await getGPUData(gpuPodList.items[i].status.podIP, promToken);
+      const data = await getGPUData(gpuPodList.items[i].status.podIP, token);
       if (data.code === 200) {
         const gpuNumber = data.response;
         if (gpuNumber > maxGpuNumber) {
@@ -84,9 +63,7 @@ export const getGPUData = async (
       });
       res.on('end', () => {
         try {
-          console.log("Raw Data: " + JSON.stringify(rawData, null, 2));
           const parsedData: PrometheusResponse = JSON.parse(rawData);
-          console.log('Parsed data: ' + JSON.stringify(parsedData, null, 2));
           resolve({ code: 200, response: Number(parsedData['data']['result'][0]['value'][1]) });
         } catch (e) {
           reject({ code: 500, response: rawData });
@@ -96,5 +73,3 @@ export const getGPUData = async (
     httpsRequest.end();
   });
 };
-
-const decodeB64 = (str: string): string => Buffer.from(str, 'base64').toString('binary');
