1068b54d08f6e4b888d724a4f501ce4e2c047bd1
Juntao Wang <juntwang@redhat.com>
2022-08-29 15:59:58 -0400

    Refine notebook controller routes

--- frontend/src/pages/notebookController/screens/server/NotebookServerRoutes.tsx
+++ frontend/src/pages/notebookController/screens/server/NotebookServerRoutes.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { Switch, Route, useRouteMatch, Redirect } from 'react-router-dom';
 import NotebookServer from './NotebookServer';
 import NotebookControlPanelRedirect from './NotebookControlPanelRedirect';
 import SpawnerPage from './SpawnerPage';
@@ -15,8 +15,11 @@ const NotebookServerRoutes: React.FC = () => {
       <Route exact path={`${path}/spawner`}>
         <SpawnerPage />
       </Route>
-      <Route exact path={`${path}/:username/home`}>
+      <Route exact path={`${path}/:username/(home|spawn)`}>
         <NotebookControlPanelRedirect />
+      </Route>
+      <Route>
+        <Redirect to={path} />
       </Route>
     </Switch>
   );
