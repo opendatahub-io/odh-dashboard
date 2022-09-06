c36910f220536f8adc5200ecca22691b3d95abde
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2022-09-06 21:44:03 +0200

    Fix issue with requesting kube resources (#501)

--- backend/src/routes/api/notebooks/notebookUtils.ts
+++ backend/src/routes/api/notebooks/notebookUtils.ts
@@ -85,12 +85,12 @@ export const createNotebook = async (
   fastify: KubeFastifyInstance,
   request: FastifyRequest<{
     Params: {
-      projectName: string;
+      namespace: string;
     };
     Body: Notebook;
   }>,
 ): Promise<Notebook> => {
-  const namespace = request.params.projectName;
+  const namespace = request.params.namespace;
   const notebookData = request.body;
   const notebookName = notebookData.metadata.name;
   notebookData.metadata.namespace = namespace;
@@ -186,7 +186,7 @@ export const createRBAC = async (
   fastify: KubeFastifyInstance,
   request: FastifyRequest<{
     Params: {
-      projectName: string;
+      namespace: string;
     };
     Body: Notebook;
   }>,
