10128f776ee761f806f48a12d466dc3f4fdbc4ee
Samuel Veloso <svelosol@redhat.com>
2022-09-07 14:08:50 +0200

    Create dedicated OAuthclient

--- install/deploy.sh
+++ install/deploy.sh
@@ -8,10 +8,15 @@ if [[ -z "${OC_PROJECT}" ]]; then
   exit 1
 fi
 
-oc project ${OC_PROJECT} 2> /dev/null || oc new-project ${PROJECT}
-oc label namespace ${OC_PROJECT} openshift.io/cluster-monitoring='true'
-oc project
+if [[ ! oc get project "${OC_PROJECT}" ]]; then
+  echo "ERROR: Project ${OC_PROJECT} does not exist"
+  exit 1
+fi
 
+# Allow Prometheus to scrape metrics from dashboard service monitors
+oc label namespace ${OC_PROJECT} openshift.io/cluster-monitoring='true'
+
+# Configure dashboard deployment
 pushd ${KUSTOMIZE_MANIFEST_DIR_OVERLAY_DEV}
 kustomize edit set namespace ${OC_PROJECT}
 kustomize edit set image quay.io/opendatahub/odh-dashboard=${IMAGE_REPOSITORY}
@@ -22,3 +27,17 @@ popd
 
 # Use kustomize to build the yaml objects so we get full support for all the kustomize standards
 kustomize build ${KUSTOMIZE_MANIFEST_DIR_OVERLAY_DEV} | oc apply -f -
+
+# OAuthClient requires a redirect URI with a route host name which the platform
+# assigns to the route.
+dashboard_url=$(oc get route odh-dashboard -o jsonpath='{$.spec.host}' -n ${OC_PROJECT})
+cat <<EOF | oc apply -f -
+apiVersion: oauth.openshift.io/v1
+kind: OAuthClient
+metadata:
+  name: odh-dashboard
+grantMethod: auto
+redirectURIs:
+- https://${dashboard_url}
+secret: zPI1OE6GuN2o_PfmjhN0itZk7VWLdmHEsh6LMRrOw1Q
+EOF
