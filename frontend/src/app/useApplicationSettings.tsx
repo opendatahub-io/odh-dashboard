8681fc5bd95b369e43d76bf257722417b5b97598
Andrew Ballantyne <8126518+andrewballantyne@users.noreply.github.com>
2022-09-15 09:10:56 -0400

    When we get an OAuth Failure, we logout (#552)

--- frontend/src/app/useApplicationSettings.tsx
+++ frontend/src/app/useApplicationSettings.tsx
@@ -3,6 +3,7 @@ import { DashboardConfig } from '../types';
 import { POLL_INTERVAL } from '../utilities/const';
 import { useDeepCompareMemoize } from '../utilities/useDeepCompareMemoize';
 import { fetchDashboardConfig } from '../services/dashboardConfigService';
+import { logout } from './appUtils';
 
 export const useApplicationSettings = (): {
   dashboardConfig: DashboardConfig | null;
@@ -27,6 +28,17 @@ export const useApplicationSettings = (): {
           setLoadError(undefined);
         })
         .catch((e) => {
+          if (e?.message?.includes('Error getting Oauth Info for user')) {
+            // NOTE: this endpoint only requests ouath because of the security layer, this is not an ironclad use-case
+            // Something went wrong on the server with the Oauth, let us just log them out and refresh for them
+            console.error(
+              'Something went wrong with the oauth token, logging out...',
+              e.message,
+              e,
+            );
+            logout().then(() => window.location.reload()); // note this is a bad side-effect, never do this
+            return;
+          }
           setLoadError(e);
         });
       watchHandle = setTimeout(watchDashboardConfig, POLL_INTERVAL);
