14034a91df62776041d59178ef5d828bc54ea1f9
Juntao Wang <juntwang@redhat.com>
2022-09-20 15:59:44 -0400

    make jumplink items generic

--- frontend/src/pages/projects/screens/detail/ProjectDetailsSidebar.tsx
+++ frontend/src/pages/projects/screens/detail/ProjectDetailsSidebar.tsx
@@ -9,19 +9,21 @@ import {
 import { ProjectSectionID, ProjectSectionTitle } from './types';
 
 const ProjectDetailsSidebar: React.FC = ({ children }) => {
+  const navItems = [
+    { id: ProjectSectionID.WORKSPACE, title: ProjectSectionTitle.WORKSPACE },
+    { id: ProjectSectionID.STORAGE, title: ProjectSectionTitle.STORAGE },
+    { id: ProjectSectionID.DATA_CONNECTIONS, title: ProjectSectionTitle.DATA_CONNECTIONS },
+  ];
+
   return (
     <Sidebar hasGutter>
       <SidebarPanel variant="sticky">
         <JumpLinks isVertical label="Jump to section" scrollableSelector="#project-details-list">
-          <JumpLinksItem href={`#${ProjectSectionID.WORKSPACE}`}>
-            {ProjectSectionTitle.WORKSPACE}
-          </JumpLinksItem>
-          <JumpLinksItem href={`#${ProjectSectionID.STORAGE}`}>
-            {ProjectSectionTitle.STORAGE}
-          </JumpLinksItem>
-          <JumpLinksItem href={`#${ProjectSectionID.DATA_CONNECTIONS}`}>
-            {ProjectSectionTitle.DATA_CONNECTIONS}
-          </JumpLinksItem>
+          {navItems.map(({ id, title }) => (
+            <JumpLinksItem key={`link-${id}`} href={`#${id}`}>
+              {title}
+            </JumpLinksItem>
+          ))}
         </JumpLinks>
       </SidebarPanel>
       <SidebarContent>{children}</SidebarContent>
