62d18b8b04cc6656818329bb16414c5f96ae4cdc
Juntao Wang <juntwang@redhat.com>
2022-12-08 16:28:34 -0500

    add notebook name in the success prompt

--- frontend/src/pages/projects/screens/detail/useCheckLogoutParams.ts
+++ frontend/src/pages/projects/screens/detail/useCheckLogoutParams.ts
@@ -1,18 +1,29 @@
 import * as React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import useNotification from '../../../../utilities/useNotification';
+import { ProjectDetailsContext } from '../../ProjectDetailsContext';
+import { getNotebookDisplayName } from '../../utils';
 
 const useCheckLogoutParams = (): void => {
   const [queryParams, setQueryParams] = useSearchParams();
   const notification = useNotification();
+  const {
+    notebooks: { data: notebooks, loaded },
+  } = React.useContext(ProjectDetailsContext);
 
   React.useEffect(() => {
-    if (queryParams.get('notebookLogout')) {
-      notification.success(`Logout workbench successfully`);
+    if (loaded) {
+      const notebookLogout = queryParams.get('notebookLogout');
+      const notebook = notebooks.find((n) => n.notebook.metadata.name === notebookLogout);
+      if (notebook?.notebook) {
+        notification.success(
+          `Logout workbench "${getNotebookDisplayName(notebook.notebook)}" successfully`,
+        );
+      }
       queryParams.delete('notebookLogout');
       setQueryParams(queryParams);
     }
-  }, [notification, queryParams, setQueryParams]);
+  }, [notification, queryParams, setQueryParams, notebooks, loaded]);
 };
 
 export default useCheckLogoutParams;
