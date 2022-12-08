abae5d967b74abc1cd24e7c3d4071b7bd10ccc03
Juntao Wang <juntwang@redhat.com>
2022-12-08 13:51:30 -0500

    remove comment

--- frontend/src/pages/projects/screens/detail/useCheckLogoutParams.ts
+++ frontend/src/pages/projects/screens/detail/useCheckLogoutParams.ts
@@ -6,7 +6,6 @@ const useCheckLogoutParams = (): void => {
   const [queryParams, setQueryParams] = useSearchParams();
   const notification = useNotification();
 
-  // TODO: this will be triggered twice, don't know why
   React.useEffect(() => {
     if (queryParams.get('notebookLogout')) {
       notification.success(`Logout workbench successfully`);
