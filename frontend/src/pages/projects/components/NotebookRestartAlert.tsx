58a124c47759115f09764ee619d76e9a73d8d705
Juntao Wang <juntwang@redhat.com>
2023-01-24 15:15:35 -0500

    fix linta

--- frontend/src/pages/projects/components/NotebookRestartAlert.tsx
+++ frontend/src/pages/projects/components/NotebookRestartAlert.tsx
@@ -6,16 +6,21 @@ import { getNotebookDisplayName } from '../utils';
 type NotebookRestartAlertProps = {
   notebooks: NotebookState[];
   isCurrent?: boolean;
-}
+};
 
 const NotebookRestartAlert: React.FC<NotebookRestartAlertProps> = ({ notebooks, isCurrent }) => {
-  const runningNotebooks = notebooks.filter(notebookState => notebookState.isRunning);
+  const runningNotebooks = notebooks.filter((notebookState) => notebookState.isRunning);
   return (
     <Alert variant="info" isInline title="Unsaved work will be lost">
-      {isCurrent ? 'Updating this workbench will cause it to restart. ' : 'Running workbenches will restart upon updating. '}
-      To avoid losing your work, save any recent data{
-        runningNotebooks.length === 0 || isCurrent ? '.' : ` in the following running workbenches: ${runningNotebooks.map(notebook => getNotebookDisplayName(notebook.notebook)).join(', ')}.`
-      } 
+      {isCurrent
+        ? 'Updating this workbench will cause it to restart. '
+        : 'Running workbenches will restart upon updating. '}
+      To avoid losing your work, save any recent data
+      {runningNotebooks.length === 0 || isCurrent
+        ? '.'
+        : ` in the following running workbenches: ${runningNotebooks
+            .map((notebook) => getNotebookDisplayName(notebook.notebook))
+            .join(', ')}.`}
     </Alert>
   );
 };
