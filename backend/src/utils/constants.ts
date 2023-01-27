31aff2f5202c6afd155f2e7dc8641cb02d14dd23
Landon LaSmith <2432396+LaVLaS@users.noreply.github.com>
2023-01-27 13:52:31 -0500

    Admin logging (#914)
    * Add admin activity logging
    
    Signed-off-by: Landon LaSmith <LLaSmith@redhat.com>
    
    * Reduce risk of perf issues with added logging
    
    ---------
    
    Signed-off-by: Landon LaSmith <LLaSmith@redhat.com>
    Co-authored-by: Maros Roman <mroman@redhat.com>
    Co-authored-by: Andrew Ballantyne <8126518+andrewballantyne@users.noreply.github.com>

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,9 +1,11 @@
+import * as path from 'path';
 import './dotenv';
 import { DashboardConfig, NotebookSize } from '../types';
 
 export const PORT = process.env.PORT || process.env.BACKEND_PORT || 8080;
 export const IP = process.env.IP || '0.0.0.0';
 export const LOG_LEVEL = process.env.FASTIFY_LOG_LEVEL || process.env.LOG_LEVEL || 'info';
+export const LOG_DIR = path.join(__dirname, '../../../logs');
 export const DEV_MODE = process.env.APP_ENV === 'development';
 export const APP_ENV = process.env.APP_ENV;
 
