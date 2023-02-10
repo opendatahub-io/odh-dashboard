76b6f22ef62287ffaa3ea25478329dded5cadcb3
Gage Krumbach <gkrumbach@gmail.com>
2023-02-10 15:40:13 -0600

    prevent supported icon from being alone on a line

--- frontend/src/components/SupportedAppTitle.tsx
+++ frontend/src/components/SupportedAppTitle.tsx
@@ -10,18 +10,34 @@ type SupportedAppTitleProps = {
 };
 
 const SupportedAppTitle: React.FC<SupportedAppTitleProps> = ({ odhApp, showProvider = false }) => {
+  const { title, icon } = React.useMemo(() => {
+    const split = odhApp.spec.displayName?.split(' ');
+
+    if (isRedHatSupported(odhApp)) {
+      return {
+        title: `${split.slice(0, -1).join(' ')} `,
+        icon: (
+          <span style={{ whiteSpace: 'nowrap' }}>
+            <span style={{ verticalAlign: 'text-bottom' }}>{split.pop()}</span>
+            <Tooltip removeFindDomNode content={`${ODH_PRODUCT_NAME} certified and supported`}>
+              <img
+                style={{ marginLeft: 'var(--pf-global--spacer--xs)' }}
+                src="../images/CheckStar.svg"
+                alt={`${ODH_PRODUCT_NAME} certified and supported`}
+              />
+            </Tooltip>
+          </span>
+        ),
+      };
+    } else {
+      return { title: odhApp.spec.displayName };
+    }
+  }, [odhApp]);
+
   return (
     <CardTitle>
-      <span style={{ verticalAlign: 'text-bottom' }}>{odhApp.spec.displayName}</span>
-      {isRedHatSupported(odhApp) && (
-        <Tooltip removeFindDomNode content={`${ODH_PRODUCT_NAME} certified and supported`}>
-          <img
-            style={{ marginLeft: 'var(--pf-global--spacer--xs)' }}
-            src="../images/CheckStar.svg"
-            alt={`${ODH_PRODUCT_NAME} certified and supported`}
-          />
-        </Tooltip>
-      )}
+      <span style={{ verticalAlign: 'text-bottom' }}>{title}</span>
+      {icon}
       {showProvider && odhApp.spec.provider && (
         <div>
           <span className="odh-card__provider">by {odhApp.spec.provider}</span>
