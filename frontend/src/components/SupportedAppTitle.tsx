a3a421d531e13bc92d6d5af677778c16fd3e65df
Gage Krumbach <gkrumbach@gmail.com>
2023-02-14 10:43:38 -0600

    Prevent RedHat supported icon on application card title from being alone on a line (#947)
    * prevent supported icon from being alone on a line
    
    * Update frontend/src/components/SupportedAppTitle.tsx
    
    Co-authored-by: Andrew Ballantyne <8126518+andrewballantyne@users.noreply.github.com>
    
    * removed function, added aria labels
    
    * removed aria labels, made icon accessible
    
    ---------
    
    Co-authored-by: Andrew Ballantyne <8126518+andrewballantyne@users.noreply.github.com>

--- frontend/src/components/SupportedAppTitle.tsx
+++ frontend/src/components/SupportedAppTitle.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CardTitle, Tooltip } from '@patternfly/react-core';
+import { Button, CardTitle, Tooltip } from '@patternfly/react-core';
 import { OdhApplication } from '../types';
 import { isRedHatSupported } from '../utilities/utils';
 import { ODH_PRODUCT_NAME } from 'utilities/const';
@@ -10,18 +10,32 @@ type SupportedAppTitleProps = {
 };
 
 const SupportedAppTitle: React.FC<SupportedAppTitleProps> = ({ odhApp, showProvider = false }) => {
+  let title = odhApp.spec.displayName;
+  let icon;
+
+  if (isRedHatSupported(odhApp)) {
+    const splitTitle = odhApp.spec.displayName.split(' ');
+    title = `${splitTitle.slice(0, -1).join(' ')} `;
+    icon = (
+      <span style={{ whiteSpace: 'nowrap' }}>
+        <span style={{ verticalAlign: 'text-bottom' }}>{splitTitle[splitTitle.length - 1]}</span>
+        <Tooltip removeFindDomNode content={`${ODH_PRODUCT_NAME} certified and supported`}>
+          <Button variant="plain" style={{ padding: 0 }}>
+            <img
+              style={{ marginLeft: 'var(--pf-global--spacer--xs)' }}
+              src="../images/CheckStar.svg"
+              alt={`${ODH_PRODUCT_NAME} certified and supported`}
+            />
+          </Button>
+        </Tooltip>
+      </span>
+    );
+  }
+
   return (
     <CardTitle>
-      <span style={{ verticalAlign: 'text-bottom' }}>{odhApp.spec.displayName}</span>
-      {isRedHatSupported(odhApp) && (
-        <Tooltip removeFindDomNode content={`${ODH_PRODUCT_NAME} certified and supported`}>
-          <img
-            style={{ marginLeft: 'var(--pf-global--spacer--xs)' }}
-            src="../images/CheckStar.svg"
-            alt={`${ODH_PRODUCT_NAME} certified and supported`}
-          />
-        </Tooltip>
-      )}
+      <span style={{ verticalAlign: 'text-bottom' }}>{title}</span>
+      {icon}
       {showProvider && odhApp.spec.provider && (
         <div>
           <span className="odh-card__provider">by {odhApp.spec.provider}</span>
