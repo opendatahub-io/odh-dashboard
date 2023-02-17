01da86e93d9a5af45f9cf6355d9419524888ba88
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2023-02-17 19:10:11 +0100

    Improve Model Server modal error handling with dynamic spec (#934)
    Add error handling for the dynamic spec of the ServingRuntime object.
    Fix https://issues.redhat.com/browse/RHODS-6538

--- frontend/src/api/network/servingRuntimes.ts
+++ frontend/src/api/network/servingRuntimes.ts
@@ -7,43 +7,14 @@ import {
   k8sUpdateResource,
 } from '@openshift/dynamic-plugin-sdk-utils';
 import { ServingRuntimeModel } from '../models';
-import { ServingRuntimeKind } from '../../k8sTypes';
+import { ConfigMapKind, ServingRuntimeKind } from '../../k8sTypes';
 import { CreatingServingRuntimeObject } from 'pages/modelServing/screens/types';
 import { getModelServingRuntimeName } from 'pages/modelServing/utils';
 import { getModelServingProjects } from './projects';
-import { getConfigMap } from './configMaps';
-import { DEFAULT_MODEL_SERVING_TEMPLATE } from 'pages/modelServing/screens/const';
-import YAML from 'yaml';
 import { assemblePodSpecOptions } from './utils';
 import { ContainerResources } from '../../types';
-
-const fetchServingRuntime = (
-  data: CreatingServingRuntimeObject,
-  configNamespace: string,
-  namespace: string,
-): Promise<ServingRuntimeKind> => {
-  //TODO: remove fetch servingruntimes-config here and pass in the previously fetched valued.
-  // Leaving it in for now to keep the original behavior when no GPU is configured.
-  return getConfigMap(configNamespace, 'servingruntimes-config')
-    .then((configmap) => {
-      const overrideServingRuntime = YAML.parse(configmap.data?.['override-config'] || '');
-      if (overrideServingRuntime) {
-        return assembleServingRuntime(data, namespace, overrideServingRuntime);
-      }
-      const defaultServingRuntime = YAML.parse(configmap.data?.['default-config'] || '');
-      if (defaultServingRuntime === null) {
-        throw new Error(
-          'servingruntimes-config is misconfigured or key might be missing from the ConfigMap',
-        );
-      }
-      return assembleServingRuntime(data, namespace, defaultServingRuntime);
-    })
-    .catch((e) => {
-      console.error(`${e}, using default config.`);
-      const servingRuntime = DEFAULT_MODEL_SERVING_TEMPLATE;
-      return assembleServingRuntime(data, namespace, servingRuntime);
-    });
-};
+import { getDefaultServingRuntime } from 'pages/modelServing/screens/projects/utils';
+import { DEFAULT_MODEL_SERVING_TEMPLATE } from 'pages/modelServing/screens/const';
 
 const assembleServingRuntime = (
   data: CreatingServingRuntimeObject,
@@ -163,16 +134,30 @@ export const updateServingRuntime = (
   });
 };
 
+const getAssembledServingRuntime = (
+  data: CreatingServingRuntimeObject,
+  servingRuntimeConfig: ConfigMapKind | undefined,
+  namespace: string,
+): ServingRuntimeKind => {
+  const servingRuntime = getDefaultServingRuntime(servingRuntimeConfig);
+
+  try {
+    return assembleServingRuntime(data, namespace, servingRuntime);
+  } catch {
+    return assembleServingRuntime(data, namespace, DEFAULT_MODEL_SERVING_TEMPLATE);
+  }
+};
+
 export const createServingRuntime = (
   data: CreatingServingRuntimeObject,
-  configNamespace: string,
+  servingRuntimeConfig: ConfigMapKind | undefined,
   namespace: string,
 ): Promise<ServingRuntimeKind> => {
-  return fetchServingRuntime(data, configNamespace, namespace).then((servingRuntime) => {
-    return k8sCreateResource<ServingRuntimeKind>({
-      model: ServingRuntimeModel,
-      resource: servingRuntime,
-    });
+  const assembledServingRuntime = getAssembledServingRuntime(data, servingRuntimeConfig, namespace);
+
+  return k8sCreateResource<ServingRuntimeKind>({
+    model: ServingRuntimeModel,
+    resource: assembledServingRuntime,
   });
 };
 
