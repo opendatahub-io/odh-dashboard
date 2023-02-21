9774a9db94386379ab3b6735dd53a15d16d0c5fd
Andrew Ballantyne <aballant@redhat.com>
2023-02-21 17:23:02 -0500

    Document source of kubeflow calls

--- frontend/src/api/pipelines/custom.ts
+++ frontend/src/api/pipelines/custom.ts
@@ -1,1 +1,4 @@
-// TODO: Kubeflow
+// TODO: Kubeflow endpoints
+
+// eg uploadPipeline: POST /apis/v1beta1/pipelines/upload
+// https://www.kubeflow.org/docs/components/pipelines/v1/reference/api/kubeflow-pipeline-api-spec/#operation--apis-v1beta1-pipelines-upload-post
