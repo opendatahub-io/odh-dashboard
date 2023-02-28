25bff4785ecbd988e0a13b848544c8a9b72ccdbb
Lucas Fernandez <lferrnan@redhat.com>
2023-02-28 11:21:57 +0100

    Fix lint issues

--- frontend/src/services/impersonateService.ts
+++ frontend/src/services/impersonateService.ts
@@ -4,9 +4,7 @@ export const updateImpersonateSettings = (impersonate: boolean): Promise<void> =
   const url = '/api/dev-impersonate';
   return axios
     .post(url, { impersonate })
-    .then((response) => {
-      return response.data;
-    })
+    .then((response) => response.data)
     .catch((e) => {
       throw new Error(e.response.data.message);
     });
