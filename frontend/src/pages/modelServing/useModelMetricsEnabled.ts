cabed93e7c8b66596f72a26105bf5d53c692f0e8
Andrew Ballantyne <aballant@redhat.com>
2023-03-09 16:12:21 -0500

    Disable Model Metrics

--- frontend/src/pages/modelServing/useModelMetricsEnabled.ts
+++ frontend/src/pages/modelServing/useModelMetricsEnabled.ts
@@ -1,14 +1,3 @@
-import { useAppContext } from 'app/AppContext';
-import { useDashboardNamespace } from 'redux/selectors';
-import { isModelMetricsEnabled } from './screens/metrics/utils';
-
-const useModelMetricsEnabled = (): [modelMetricsEnabled: boolean] => {
-  const { dashboardNamespace } = useDashboardNamespace();
-  const { dashboardConfig } = useAppContext();
-
-  const checkModelMetricsEnabled = () => isModelMetricsEnabled(dashboardNamespace, dashboardConfig);
-
-  return [checkModelMetricsEnabled()];
-};
+const useModelMetricsEnabled = (): [boolean] => [false];
 
 export default useModelMetricsEnabled;
