72371ae65fb8d97949988c323345cf799ad4ea9b
jenny-s51 <jshandel@redhat.com>
2023-03-09 09:34:14 -0500

    fix block error

--- frontend/src/pages/projects/screens/projects/EmptyProjects.tsx
+++ frontend/src/pages/projects/screens/projects/EmptyProjects.tsx
@@ -11,25 +11,23 @@ import { CubesIcon } from '@patternfly/react-icons';
 import { Link } from 'react-router-dom';
 import NewProjectButton from './NewProjectButton';
 
-const EmptyProjects: React.FC = () => {
-  return (
-    <EmptyState>
-      <EmptyStateIcon icon={CubesIcon} />
-      <Title headingLevel="h2" size="lg">
-        No data science projects yet.
-      </Title>
-      <EmptyStateBody>
-        To get started, create a data science project or launch a notebook with Jupyter.
-      </EmptyStateBody>
-      <NewProjectButton />
-      <EmptyStateSecondaryActions>
-        <Button
-          variant="link"
-          component={() => <Link to="/notebookController">Launch Jupyter</Link>}
-        />
-      </EmptyStateSecondaryActions>
-    </EmptyState>
-  );
-};
+const EmptyProjects: React.FC = () => (
+  <EmptyState>
+    <EmptyStateIcon icon={CubesIcon} />
+    <Title headingLevel="h2" size="lg">
+      No data science projects yet.
+    </Title>
+    <EmptyStateBody>
+      To get started, create a data science project or launch a notebook with Jupyter.
+    </EmptyStateBody>
+    <NewProjectButton />
+    <EmptyStateSecondaryActions>
+      <Button
+        variant="link"
+        component={() => <Link to="/notebookController">Launch Jupyter</Link>}
+      />
+    </EmptyStateSecondaryActions>
+  </EmptyState>
+);
 
 export default EmptyProjects;
