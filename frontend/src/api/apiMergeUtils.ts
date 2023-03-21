8378419c0a19c7252d830b4b1fda465de421ab95
Andrew Ballantyne <8126518+andrewballantyne@users.noreply.github.com>
2023-03-21 13:28:27 -0400

    Add useFetchState & rework all modern calls (#1036)

--- frontend/src/api/apiMergeUtils.ts
+++ frontend/src/api/apiMergeUtils.ts
@@ -1,10 +1,33 @@
-import { QueryParams } from '@openshift/dynamic-plugin-sdk-utils';
+import { K8sResourceBaseOptions, QueryParams } from '@openshift/dynamic-plugin-sdk-utils';
 import { K8sAPIOptions } from '~/k8sTypes';
 
-export const mergeK8sQueryParams = (
+const mergeK8sQueryParams = (
   opts: K8sAPIOptions = {},
   specificOpts: QueryParams = {},
 ): QueryParams => ({
   ...specificOpts,
   ...(opts.dryRun && { dryRun: 'All' }),
+});
+
+const mergeRequestInit = (
+  opts: K8sAPIOptions = {},
+  specificOpts: RequestInit = {},
+): RequestInit => ({
+  ...specificOpts,
+  ...(opts.signal && { signal: opts.signal }),
+});
+
+export const applyK8sAPIOptions = <T extends K8sResourceBaseOptions>(
+  opts: K8sAPIOptions = {},
+  apiData: T,
+): T => ({
+  ...apiData,
+  queryOptions: {
+    ...apiData.queryOptions,
+    queryParams: mergeK8sQueryParams(opts, apiData.queryOptions?.queryParams),
+  },
+  fetchOptions: {
+    ...apiData.fetchOptions,
+    requestInit: mergeRequestInit(opts, apiData.fetchOptions?.requestInit),
+  },
 });
---
+++ frontend/src/api/k8s/routes.ts
@@ -0,0 +1,16 @@
+import { k8sGetResource } from '@openshift/dynamic-plugin-sdk-utils';
+import { RouteModel } from '~/api/models';
+import { K8sAPIOptions, RouteKind } from '~/k8sTypes';
+import { applyK8sAPIOptions } from '~/api/apiMergeUtils';
+
+export const getRoute = (
+  name: string,
+  namespace: string,
+  opts?: K8sAPIOptions,
+): Promise<RouteKind> =>
+  k8sGetResource<RouteKind>(
+    applyK8sAPIOptions(opts, {
+      model: RouteModel,
+      queryOptions: { name, ns: namespace },
+    }),
+  );
