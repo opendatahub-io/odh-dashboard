c96a51279c0b1ea571256bab2dafccb70bbde444
Alex Creasy <acreasy@redhat.com>
2023-03-30 10:50:24 +0100

    WIP - skeleton chart

--- frontend/src/pages/modelServing/screens/metrics/BiasGraph.tsx
+++ frontend/src/pages/modelServing/screens/metrics/BiasGraph.tsx
@@ -1,15 +1,27 @@
 import { Card, CardBody, CardTitle, Stack, StackItem } from '@patternfly/react-core';
 import React from 'react';
+import {
+  InferenceMetricType,
+  ModelServingMetricsContext,
+} from '~/pages/modelServing/screens/metrics/ModelServingMetricsContext';
+import { per100 } from '~/pages/modelServing/screens/metrics/utils';
+import MetricsChart from '~/pages/modelServing/screens/metrics/MetricsChart';
 
-const BiasGraph = () => (
-  <Stack hasGutter>
-    <StackItem>
-      <Card>
-        <CardTitle>Bias Graph Placeholder</CardTitle>
-        <CardBody>Lorem Ipsum</CardBody>
-      </Card>
-    </StackItem>
-  </Stack>
-);
-
+const BiasGraph = () => {
+  const { data } = React.useContext(ModelServingMetricsContext);
+  return (
+    <Stack hasGutter>
+      <StackItem>
+        <MetricsChart
+          metrics={{
+            name: 'Bias',
+            metric: data[InferenceMetricType.REQUEST_COUNT_SUCCESS],
+            translatePoint: per100,
+          }}
+          title={`Bias`}
+        />
+      </StackItem>
+    </Stack>
+  );
+};
 export default BiasGraph;
