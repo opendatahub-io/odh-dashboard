f8ae34f18b76fdcbfe871df28fc7a0078c2573eb
Gage Krumbach <gkrumbach@gmail.com>
2023-04-12 07:14:12 -0500

    added deploy model test (#1084)

--- frontend/src/pages/modelServing/screens/global/ModelServingGlobal.stories.tsx
+++ frontend/src/pages/modelServing/screens/global/ModelServingGlobal.stories.tsx
@@ -143,3 +143,58 @@ DeleteModel.play = async ({ canvasElement }) => {
   await userEvent.type(retypeNameInput, 'Test Inference Service', { delay: 50 });
   expect(deleteButton).not.toBeDisabled();
 };
+
+export const DeployModel = Template.bind({});
+DeployModel.parameters = {
+  a11y: {
+    // need to select modal as root
+    element: '.pf-c-backdrop',
+  },
+};
+DeployModel.play = async ({ canvasElement }) => {
+  // load page and wait until settled
+  const canvas = within(canvasElement);
+  await canvas.findByText('Test Inference Service', undefined, { timeout: 5000 });
+
+  // user flow for editing a project
+  await userEvent.click(canvas.getByText('Deploy model', { selector: 'button' }));
+
+  // get modal
+  const body = within(canvasElement.ownerDocument.body);
+  const nameInput = body.getByRole('textbox', { name: 'Model Name' });
+  const updateButton = body.getByText('Deploy', { selector: 'button' });
+
+  // test that you can not submit on empty
+  await userEvent.clear(nameInput);
+  expect(updateButton).toBeDisabled();
+
+  // fill the form
+  await userEvent.type(nameInput, 'Model Name', { delay: 50 });
+  body.getAllByRole('button', { name: 'Options menu' });
+  await userEvent.click(body.getAllByRole('button', { name: 'Options menu' })[0]);
+  await userEvent.click(body.getByText('Test Project', { selector: 'button' }));
+  await body.findByText('Select a framework');
+  await userEvent.click(body.getAllByRole('button', { name: 'Options menu' })[1]);
+  await userEvent.click(body.getByText('onnx - 1'));
+  await body.findByText('Select...');
+  await userEvent.click(body.getAllByRole('button', { name: 'Options menu' })[2]);
+  await body.findByText('Test Secret');
+  await userEvent.click(body.getByText('Test Secret'));
+  expect(updateButton).not.toBeDisabled();
+
+  await userEvent.click(body.getByRole('radio', { name: 'New data connection' }));
+  expect(updateButton).toBeDisabled();
+
+  // test that adding required values validates submit
+  const showPassword = body.getByRole('button', { name: 'Show password' });
+  await userEvent.click(showPassword);
+
+  const secretName = body.getByRole('textbox', { name: 'AWS field Name' });
+  const secretKey = body.getByRole('textbox', { name: 'AWS field AWS_ACCESS_KEY_ID' });
+  const secretValue = body.getByRole('textbox', { name: 'AWS field AWS_SECRET_ACCESS_KEY' });
+
+  await userEvent.type(secretName, 'Test Secret', { delay: 50 });
+  await userEvent.type(secretKey, 'test-secret-key', { delay: 50 });
+  await userEvent.type(secretValue, 'test-secret-password', { delay: 50 });
+  expect(updateButton).not.toBeDisabled();
+};
