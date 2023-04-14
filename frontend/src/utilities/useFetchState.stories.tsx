3e46e43ba46c0ad452785521054e075d48c47876
Gage Krumbach <gkrumbach@gmail.com>
2023-04-13 23:46:46 -0500

    made test hook generic

--- frontend/src/utilities/useFetchState.stories.tsx
+++ frontend/src/utilities/useFetchState.stories.tsx
@@ -1,211 +1,138 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { within, userEvent } from '@storybook/testing-library';
+import { userEvent, within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
-import { Icon, Spinner } from '@patternfly/react-core';
-import { CloseIcon } from '@patternfly/react-icons';
 import useFetchState from './useFetchState';
-
-type TestComponentProps<T> = {
-  defaultState: T;
-  response: T;
-  refreshRate?: number;
-  responseFailed: boolean;
-  responseError?: Error;
-  responseDelay: number;
-  fetchCallbackPromise: () => Promise<T>;
-};
-
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-constraint
-const TestComponent = <T extends unknown>({
-  defaultState,
-  response,
-  refreshRate,
-  responseFailed = false,
-  responseError,
-  responseDelay = 0,
-  fetchCallbackPromise,
-}: TestComponentProps<T>) => {
-  // pick func to use and apply delay
-  const callback = useCallback(async () => {
-    await new Promise((resolve) => setTimeout(resolve, responseDelay));
-
-    if (fetchCallbackPromise) {
-      return fetchCallbackPromise();
-    }
-
-    if (responseFailed) {
-      throw responseError;
-    }
-    return Promise.resolve(response);
-  }, [fetchCallbackPromise, responseDelay, responseError, response, responseFailed]);
-  // hook to test
-  const [result, loaded, loadError, refresh] = useFetchState(callback, defaultState, refreshRate);
-
-  return (
-    <div>
-      <div>
-        <span>
-          <b>result: </b>
-          {loadError ? (
-            <Icon isInline size="md">
-              <CloseIcon color="red" />
-            </Icon>
-          ) : !loaded ? (
-            <Spinner isInline size="sm" />
-          ) : (
-            <span style={{ color: 'green' }}>{String(result)}</span>
-          )}
-        </span>
-      </div>
-      <div>
-        <b>loaded: </b>
-        {loaded ? 'TRUE' : 'FALSE'}
-      </div>
-      <div>
-        <p>
-          <b>error</b>
-        </p>
-        <div style={{ marginLeft: 8 }}>
-          <div>
-            <b>name: </b>
-            {loadError?.name}
-          </div>
-          <div>
-            <b>message: </b>
-            {loadError?.message}
-          </div>
-          <div>
-            <b>stack: </b>
-            {loadError?.stack}
-          </div>
-        </div>
-      </div>
-      <button onClick={refresh}>refresh</button>
-    </div>
-  );
-};
+import { TestHook } from './TestHook';
 
 export default {
   title: 'useFetchState',
-  component: TestComponent,
-} as ComponentMeta<typeof TestComponent>;
-
-const Template: ComponentStory<typeof TestComponent> = (args) => <TestComponent {...args} />;
-
-export const Default = Template.bind({});
-Default.args = {
-  defaultState: 'default-test-state',
-  response: 'success-test-state',
-  responseDelay: 0,
-  responseFailed: false,
-  refreshRate: 0,
-  responseError: {
-    name: 'error-test-state',
-    message: 'An unknown error occurred',
-    stack: undefined,
+  component: TestHook,
+  parameters: {
+    a11y: {
+      disable: true,
+    },
   },
+} as ComponentMeta<typeof TestHook>;
+
+const Template: ComponentStory<typeof TestHook> = (args) => <TestHook {...args} />;
+
+export const Success = Template.bind({});
+Success.args = {
+  hook: useFetchState,
+  defaultHookParams: [() => Promise.resolve('success-test-state'), 'default-test-state'],
 };
 
-export const Resolved = Template.bind({});
-Resolved.args = {
-  defaultState: 'default-test-state',
-  response: 'success-test-state',
-  responseFailed: false,
-  responseError: {
-    name: 'error-test-state',
-    message: 'test error message',
-  },
-};
-Resolved.play = async ({ canvasElement }) => {
+Success.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
 
-  // test that values from api are be displayed correctly
-  expect(
-    await canvas.findByText('success-test-state', undefined, { timeout: 5000 }),
-  ).toBeInTheDocument();
+  // wait 2 seconds to settle
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  expect(canvas.queryByText('error-test-state')).not.toBeInTheDocument();
+  // test that is loaded
+  expect((await canvas.findByTestId('result-value-1')).innerText).toBe('true');
+
+  // test that value is correct
+  expect(canvas.getByTestId('result-value-0').innerText).toBe('"success-test-state"');
 };
 
-export const Rejected = Template.bind({});
-Rejected.args = {
-  defaultState: 'default-test-state',
-  response: 'success-test-state',
-  responseFailed: true,
-  responseError: {
-    name: 'error-test-state',
-    message: 'test error message',
-  },
+export const Failure = Template.bind({});
+Failure.args = {
+  hook: useFetchState,
+  defaultHookParams: [() => Promise.reject('error-test-state'), 'default-test-state'],
 };
-Rejected.play = async ({ canvasElement }) => {
+
+Failure.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
 
-  // test that values from api are be displayed correctly
-  expect(
-    await canvas.findByText('error-test-state', undefined, { timeout: 5000 }),
-  ).toBeInTheDocument();
+  // wait 2 seconds to settle
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  expect(canvas.queryByText('success-test-state')).not.toBeInTheDocument();
+  // test that is loaded
+  expect((await canvas.findByTestId('result-value-1')).innerText).toBe('false');
+
+  // test that value is correct
+  expect(canvas.getByTestId('result-value-0').innerText).toBe('"default-test-state"');
+
+  // test that error is correct
+  expect(canvas.getByTestId('result-value-2').innerText).toBe('"error-test-state"');
 };
 
-export const Refresh = Template.bind({});
-Refresh.args = {
-  refreshRate: 100,
+export const Stable = Template.bind({});
+Stable.args = {
+  hook: useFetchState,
+  defaultHookParams: [() => Promise.resolve([1, 2, 3]), []],
+  hookParams: [
+    [() => Promise.resolve([1, 2, 4]), []],
+    [() => Promise.resolve([1, 2, 4]), []],
+    [() => Promise.resolve({ a: 1, b: { c: 2 } }), {}],
+    [() => Promise.reject('error-test-state'), {}],
+  ],
 };
-Refresh.decorators = [
-  (Story, context) => {
-    const [counter, setCounter] = React.useState(0);
-    const call = React.useCallback(() => {
-      setCounter((c) => c + 1);
-      return 'Counting';
-    }, [setCounter]);
-    return (
-      <div>
-        <b>call count: </b>
-        <span data-testid="call-count">{counter}</span>
-        <Story
-          args={{
-            ...context.args,
-            fetchCallbackPromise: call,
-          }}
-        />
-      </div>
-    );
-  },
-];
-Refresh.play = async ({ canvasElement }) => {
+
+Stable.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
 
-  // test that values from api are be displayed correctly
-  expect(await canvas.findByText('Counting', undefined, { timeout: 5000 })).toBeInTheDocument();
+  // wait 2 seconds to settle
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  // refresh every 0.1 seconds, expect call count to be above 5 after 1 second
-  await new Promise((r) => setTimeout(r, 1000));
-  expect(Number(canvas.getByTestId('call-count').innerText)).toBeGreaterThan(5);
+  // test that is loaded
+  expect((await canvas.findByTestId('result-value-1')).innerText).toBe('true');
+
+  // test that result is unstable and func is stable
+  // the result will always be unstable as it comes from a promise
+  expect(canvas.getByTestId('result-status-0').innerText).toBe('unstable');
+  expect(canvas.getByTestId('result-status-3').innerText).toBe('stable');
+
+  // switch to params-0
+  userEvent.click(canvas.getByTestId('params-0'));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  expect(canvas.getByTestId('result-status-0').innerText).toBe('unstable');
+  expect(canvas.getByTestId('result-status-3').innerText).toBe('stable');
+
+  // switch to params-1
+  userEvent.click(canvas.getByTestId('params-1'));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  expect(canvas.getByTestId('result-status-0').innerText).toBe('unstable');
+  expect(canvas.getByTestId('result-status-3').innerText).toBe('stable');
+
+  // switch to params-2 (object)
+  userEvent.click(canvas.getByTestId('params-2'));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  expect(canvas.getByTestId('result-status-0').innerText).toBe('unstable');
+  expect(canvas.getByTestId('result-status-3').innerText).toBe('stable');
+
+  // switch to params-3 (error)
+  userEvent.click(canvas.getByTestId('params-3'));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  expect(canvas.getByTestId('result-status-0').innerText).toBe('stable');
+  expect(canvas.getByTestId('result-value-2').innerText).toBe('"error-test-state"');
+  expect((await canvas.findByTestId('result-value-1')).innerText).toBe('true');
 };
 
-export const RefreshButton = Template.bind({});
-// reuse refresh decorator
-RefreshButton.decorators = Refresh.decorators;
-RefreshButton.play = async ({ canvasElement }) => {
+export const RefreshRate = Template.bind({});
+RefreshRate.args = {
+  hook: useFetchState,
+  defaultHookParams: [() => Promise.resolve([1, 2, 3]), [], 1000],
+  hookParams: [[() => Promise.resolve([1, 2, 4]), [], 1000]],
+};
+
+RefreshRate.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
 
-  // test that values from api are be displayed correctly
-  expect(await canvas.findByText('Counting', undefined, { timeout: 5000 })).toBeInTheDocument();
+  // wait 2 seconds to settle
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  const refreshButton = canvas.getByText('refresh', { selector: 'button' });
+  // test that is loaded
+  expect((await canvas.findByTestId('result-value-1')).innerText).toBe('true');
 
-  //refresh 5 times
-  userEvent.click(refreshButton);
-  userEvent.click(refreshButton);
-  userEvent.click(refreshButton);
-  userEvent.click(refreshButton);
-  userEvent.click(refreshButton);
+  // test that result is unstable and func is stable
+  // the result will always be unstable as it comes from a promise
+  expect(canvas.getByTestId('result-status-0').innerText).toBe('unstable');
+  expect(canvas.getByTestId('result-status-3').innerText).toBe('stable');
 
-  // wait to settle
-  await new Promise((r) => setTimeout(r, 1000));
-
-  // expect to be called once initially + 1 more time for each refresh done (5 refreshes)
-  expect(Number(canvas.getByTestId('call-count').innerText)).toBe(6);
+  // switch to params-0
+  userEvent.click(canvas.getByTestId('params-0'));
+  expect(canvas.getByTestId('result-status-0').innerText).toBe('unstable');
+  expect(canvas.getByTestId('result-status-3').innerText).toBe('stable');
 };
