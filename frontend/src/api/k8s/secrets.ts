1609fa408c50efe78a7fdcd5e10dfc6c376f9ebf
Gage Krumbach <gkrumbach@gmail.com>
2023-05-02 13:33:04 -0500

    fixed deleteSecret

--- frontend/src/api/k8s/secrets.ts
+++ frontend/src/api/k8s/secrets.ts
@@ -144,11 +144,8 @@ export const replaceSecret = (data: SecretKind, opts?: K8sAPIOptions): Promise<S
 export const deleteSecret = (
   projectName: string,
   secretName: string,
-  opts?: K8sAPIOptions,
 ): Promise<K8sStatus> =>
-  k8sDeleteResource<SecretKind, K8sStatus>(
-    applyK8sAPIOptions(opts, {
-      model: SecretModel,
-      queryOptions: { name: secretName, ns: projectName },
-    }),
-  );
+  k8sDeleteResource<SecretKind, K8sStatus>({
+    model: SecretModel,
+    queryOptions: { name: secretName, ns: projectName },
+  });
