2349adb5746c93d36e527ba19de657d34c6c2ea5
Juntao Wang <juntwang@redhat.com>
2023-05-04 17:37:29 +0800

    Control model serving related fetching based on the feature flags

--- frontend/src/pages/modelServing/customServingRuntimes/useTemplateOrder.ts
+++ frontend/src/pages/modelServing/customServingRuntimes/useTemplateOrder.ts
@@ -1,19 +1,27 @@
 import * as React from 'react';
 import { getDashboardConfigTemplateOrder } from '~/api';
-import useFetchState, { FetchState } from '~/utilities/useFetchState';
+import useCustomServingRuntimesEnabled from '~/pages/modelServing/customServingRuntimes/useCustomServingRuntimesEnabled';
+import useFetchState, { FetchState, NotReadyError } from '~/utilities/useFetchState';
 
 const useTemplateOrder = (namespace?: string): FetchState<string[]> => {
+  const customServingRuntimesEnabled = useCustomServingRuntimesEnabled();
+
   const getTemplateOrder = React.useCallback(() => {
     if (!namespace) {
       return Promise.reject(new Error('No namespace provided'));
     }
+
+    if (!customServingRuntimesEnabled) {
+      return Promise.reject(new NotReadyError('Custom serving runtime is not enabled'));
+    }
+
     return getDashboardConfigTemplateOrder(namespace).catch((e) => {
       if (e.statusObject?.code === 404) {
         throw new Error('Dashboard config template order is not configured.');
       }
       throw e;
     });
-  }, [namespace]);
+  }, [namespace, customServingRuntimesEnabled]);
 
   return useFetchState<string[]>(getTemplateOrder, []);
 };
--- frontend/src/pages/modelServing/useInferenceServices.ts
+++ frontend/src/pages/modelServing/useInferenceServices.ts
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import { getInferenceServiceContext } from '~/api';
 import { InferenceServiceKind } from '~/k8sTypes';
-import useFetchState, { FetchState } from '~/utilities/useFetchState';
+import useModelServingEnabled from '~/pages/modelServing/useModelServingEnabled';
+import useFetchState, { FetchState, NotReadyError } from '~/utilities/useFetchState';
 
 const useInferenceServices = (namespace?: string): FetchState<InferenceServiceKind[]> => {
-  const getServingInferences = React.useCallback(
-    () => getInferenceServiceContext(namespace, 'opendatahub.io/dashboard=true'),
-    [namespace],
-  );
+  const modelServingEnabled = useModelServingEnabled();
+
+  const getServingInferences = React.useCallback(() => {
+    if (!modelServingEnabled) {
+      return Promise.reject(new NotReadyError('Model serving is not enabled'));
+    }
+
+    return getInferenceServiceContext(namespace, 'opendatahub.io/dashboard=true');
+  }, [namespace, modelServingEnabled]);
 
   return useFetchState<InferenceServiceKind[]>(getServingInferences, []);
 };
