a39c51634a846bfade55131bfc558924d2c552f8
pnaik <pnaik@redhat.com>
2023-06-26 18:30:53 +0530

    Add 'Edit Permissions' Kebab to Project Row

---
+++ frontend/src/pages/projects/screens/projects/useProjectTableRowItems.ts
@@ -0,0 +1,57 @@
+import { useNavigate } from 'react-router-dom';
+import { useAccessReview } from '~/api';
+import { AccessReviewResourceAttributes, ProjectKind } from '~/k8sTypes';
+
+type Item = {
+  title?: string;
+  isDisabled?: boolean;
+  isSeparator?: boolean;
+  onClick?: () => void;
+};
+const accessReviewResource: AccessReviewResourceAttributes = {
+  group: 'rbac.authorization.k8s.io',
+  resource: 'rolebindings',
+  verb: 'create',
+};
+const useProjectTableRowItems = (
+  project: ProjectKind,
+  isRefreshing: boolean,
+  setEditData: (data: ProjectKind) => void,
+  setDeleteData: (data: ProjectKind) => void,
+): Item[] => {
+  const [allowCreate] = useAccessReview({
+    ...accessReviewResource,
+    namespace: project.metadata.name,
+  });
+  const navigate = useNavigate();
+  const item: Item[] = [
+    {
+      title: 'Edit project',
+      isDisabled: isRefreshing,
+      onClick: () => {
+        setEditData(project);
+      },
+    },
+    ...(allowCreate
+      ? [
+          {
+            title: 'Edit permissions',
+            onClick: () => {
+              navigate(`/projects/${project.metadata.name}`, { state: 'Permissions' });
+            },
+          },
+        ]
+      : []),
+    {
+      isSeparator: true,
+    },
+    {
+      title: 'Delete project',
+      onClick: () => {
+        setDeleteData(project);
+      },
+    },
+  ];
+  return item;
+};
+export default useProjectTableRowItems;
