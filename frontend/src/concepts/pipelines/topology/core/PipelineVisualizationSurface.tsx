ea532660c940690aa8272b853c580bcb6eb1d5c7
pnaik1 <pnaik@redhat.com>
2023-07-20 12:12:00 +0530

    Uploading a malformed pipeline yaml results in a blank page

--- frontend/src/concepts/pipelines/topology/core/PipelineVisualizationSurface.tsx
+++ frontend/src/concepts/pipelines/topology/core/PipelineVisualizationSurface.tsx
@@ -11,6 +11,8 @@ import {
   VisualizationSurface,
   getSpacerNodes,
 } from '@patternfly/react-topology';
+import { EmptyState, EmptyStateBody, EmptyStateIcon, Title } from '@patternfly/react-core';
+import { ExclamationCircleIcon } from '@patternfly/react-icons';
 
 type PipelineVisualizationSurfaceProps = {
   nodes: PipelineNodeModel[];
@@ -22,7 +24,7 @@ const PipelineVisualizationSurface: React.FC<PipelineVisualizationSurfaceProps> 
   selectedIds,
 }) => {
   const controller = useVisualizationController();
-
+  const [error, setError] = React.useState<Error | null>();
   React.useEffect(() => {
     // PF Bug
     // TODO: Pipeline Topology weirdly doesn't set a width and height on spacer nodes -- but they do when using finally spacer nodes
@@ -30,15 +32,34 @@ const PipelineVisualizationSurface: React.FC<PipelineVisualizationSurfaceProps> 
     const renderNodes = [...spacerNodes, ...nodes];
     // TODO: We can have a weird edge issue if the node is off by a few pixels vertically from the center
     const edges = getEdgesFromNodes(renderNodes);
-
-    controller.fromModel(
-      {
-        nodes: renderNodes,
-        edges,
-      },
-      true,
-    );
+    try {
+      controller.fromModel(
+        {
+          nodes: renderNodes,
+          edges,
+        },
+        true,
+      );
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error);
+      } else {
+        // eslint-disable-next-line no-console
+        console.error('Unknown error occurred rendering Pipeline Graph', error);
+      }
+    }
   }, [controller, nodes]);
+  if (error) {
+    return (
+      <EmptyState data-id="error-empty-state">
+        <EmptyStateIcon icon={ExclamationCircleIcon} />
+        <Title headingLevel="h4" size="lg">
+          Incorrect pipeline definition
+        </Title>
+        <EmptyStateBody>{error.message}</EmptyStateBody>
+      </EmptyState>
+    );
+  }
 
   return (
     <TopologyView
