9b9744bce692f7daff973eb6380376518d84276f
Lucas Fernandez <lferrnan@redhat.com>
2023-12-01 14:47:21 +0100

    Bring tooltips to kserve

--- frontend/src/pages/modelServing/screens/projects/kServeModal/ManageKServeModal.tsx
+++ frontend/src/pages/modelServing/screens/projects/kServeModal/ManageKServeModal.tsx
@@ -230,6 +230,8 @@ const ManageKServeModal: React.FC<ManageKServeModalProps> = ({
             <ServingRuntimeReplicaSection
               data={createDataServingRuntime}
               setData={setCreateDataServingRuntime}
+              infoContent="Consider network traffic and failover scenarios when specifying the number of model
+                server replicas."
             />
           </StackItem>
           <StackItem>
@@ -240,6 +242,7 @@ const ManageKServeModal: React.FC<ManageKServeModalProps> = ({
               servingRuntimeSelected={servingRuntimeSelected}
               acceleratorState={acceleratorState}
               setAcceleratorState={setAcceleratorState}
+              infoContent="Select a server size that will accommodate your largest model. See the product documentation for more information."
             />
           </StackItem>
           <StackItem>
