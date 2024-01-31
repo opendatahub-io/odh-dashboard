36b617227ce199a6a429cbf1b42a98bd0025a36f
Juntao Wang <juntwang@redhat.com>
2024-01-31 10:01:21 -0500

    fix pipeline run logs parsing issue

--- backend/src/routes/api/k8s/index.ts
+++ backend/src/routes/api/k8s/index.ts
@@ -49,7 +49,7 @@ module.exports = async (fastify: KubeFastifyInstance) => {
           promise = passThroughResource(fastify, req, passThroughData);
           break;
         default:
-          promise = passThroughText(fastify, req, passThroughData);
+          promise = passThroughText(fastify, req, passThroughData).then((d) => d[0]);
       }
 
       return promise.catch((error) => {
