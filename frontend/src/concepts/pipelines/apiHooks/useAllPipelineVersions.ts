68530feac612d22ccc0832a563e347e7a01782e0
Juntao Wang <juntwang@redhat.com>
2024-02-15 13:57:56 -0500

    Fix the issue of showing pipeline version on the runs table when there is no pipeline

--- frontend/src/concepts/pipelines/apiHooks/useAllPipelineVersions.ts
+++ frontend/src/concepts/pipelines/apiHooks/useAllPipelineVersions.ts
@@ -15,14 +15,14 @@ export const useAllPipelineVersions = (
   refreshRate = 0,
 ): FetchState<PipelineListPaged<PipelineVersionKF>> => {
   const { api } = usePipelinesAPI();
-  const [{ items: pipelines }] = usePipelines();
+  const [{ items: pipelines }, pipelinesLoaded] = usePipelines();
   const pipelineIds = useDeepCompareMemoize(pipelines.map((pipeline) => pipeline.id));
 
   return usePipelineQuery<PipelineVersionKF>(
     React.useCallback(
       async (opts, params) => {
-        if (pipelineIds.length === 0) {
-          return Promise.reject(new NotReadyError('No pipeline id'));
+        if (!pipelinesLoaded) {
+          return Promise.reject(new NotReadyError('Pipelines not loaded'));
         }
 
         const pipelineVersionRequests = pipelineIds.map((pipelineId) =>
@@ -42,7 +42,7 @@ export const useAllPipelineVersions = (
           { total_size: 0, items: [] },
         );
       },
-      [api, pipelineIds],
+      [api, pipelineIds, pipelinesLoaded],
     ),
     options,
     refreshRate,
