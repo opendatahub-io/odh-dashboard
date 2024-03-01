732d09095ed4514c872ad7ccd789e91761839916
Christian Vogt <cvogt@redhat.com>
2024-03-01 15:44:32 -0500

    remove duplicate visitWithLogin command

--- frontend/src/__tests__/cypress/cypress/support/commands/intercept-snapshots.ts
+++ frontend/src/__tests__/cypress/cypress/support/commands/intercept-snapshots.ts
@@ -14,29 +14,6 @@ declare global {
   }
 }
 
-Cypress.Commands.add('visitWithLogin', (url) => {
-  cy.intercept('GET', url).as('visitWithLogin');
-
-  cy.visit(url, { failOnStatusCode: false });
-
-  cy.wait('@visitWithLogin').then((interception) => {
-    if (interception.response?.statusCode === 403) {
-      // do login
-      cy.findByRole('button', { name: 'Log in with OpenShift' }).click();
-      cy.findByRole('link', { name: 'customadmins' }).click();
-      cy.findByLabelText('Username *').type(Cypress.env('USERNAME'));
-      cy.findByLabelText('Password *').type(Cypress.env('PASSWORD'));
-      cy.findByRole('button', { name: 'Log in' }).click();
-    } else if (interception.response?.statusCode !== 200) {
-      throw new Error(
-        `Failed to visit '${url}'. Status code: ${interception.response?.statusCode || 'unknown'}`,
-      );
-    } else {
-      cy.log('Already logged in');
-    }
-  });
-});
-
 Cypress.Commands.add('readSnapshot', (path) => cy.task('readJSON', path));
 
 Cypress.Commands.add('waitSnapshot', (alias) => waitSnapshot(alias));
