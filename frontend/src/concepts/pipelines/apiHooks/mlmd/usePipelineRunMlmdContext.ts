5016cf4901e9bb79ca203ae5f1abe5ad06327d53
Juntao Wang <juntwang@redhat.com>
2024-04-09 17:38:21 -0400

    Read pipeline run node details from mlmd context

---
+++ frontend/src/concepts/pipelines/apiHooks/mlmd/usePipelineRunMlmdContext.ts
@@ -0,0 +1,8 @@
+import { FetchState } from '~/utilities/useFetchState';
+import { MlmdContext, MlmdContextTypes } from '~/concepts/pipelines/apiHooks/mlmd/types';
+import { useMlmdContext } from '~/concepts/pipelines/apiHooks/mlmd/useMlmdContext';
+
+export const usePipelineRunMlmdContext = (
+  runID?: string,
+  refreshRate?: number,
+): FetchState<MlmdContext | null> => useMlmdContext(runID, MlmdContextTypes.RUN, refreshRate);
