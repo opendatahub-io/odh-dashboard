fb98d89959f8599c6bb42c3b748adf4b50104884
Yu Zhao <zyu0321@gmail.com>
2024-04-23 13:25:47 -0400

    nim-integration-demo

---
+++ frontend/src/pages/modelServing/screens/projects/NIMServiceModal/DeployNIMServiceModal.tsx
@@ -0,0 +1,283 @@
+import * as React from 'react';
+import {
+  Form,
+  FormSection,
+  Modal,
+  Stack,
+  StackItem,
+} from '@patternfly/react-core';
+import { EitherOrNone } from '@openshift/dynamic-plugin-sdk';
+import {
+  getSubmitInferenceServiceResourceFn,
+  getSubmitServingRuntimeResourcesFn,
+  useCreateInferenceServiceObject,
+  useCreateServingRuntimeObject,
+} from '~/pages/modelServing/screens/projects/utils';
+import {
+  TemplateKind,
+  ProjectKind,
+  InferenceServiceKind,
+  AccessReviewResourceAttributes,
+  SecretKind,
+} from '~/k8sTypes';
+import { requestsUnderLimits, resourcesArePositive } from '~/pages/modelServing/utils';
+import useCustomServingRuntimesEnabled from '~/pages/modelServing/customServingRuntimes/useCustomServingRuntimesEnabled';
+import { getServingRuntimeFromName } from '~/pages/modelServing/customServingRuntimes/utils';
+import useServingAcceleratorProfile from '~/pages/modelServing/screens/projects/useServingAcceleratorProfile';
+import DashboardModalFooter from '~/concepts/dashboard/DashboardModalFooter';
+import {
+  InferenceServiceStorageType,
+  ServingRuntimeEditInfo,
+} from '~/pages/modelServing/screens/types';
+import ServingRuntimeSizeSection from '~/pages/modelServing/screens/projects/ServingRuntimeModal/ServingRuntimeSizeSection';
+import NIMModelListSection from '~/pages/modelServing/screens/projects/NIMServiceModal/NIMModelListSection';
+import NIMModelDeploymentNameSection from '~/pages/modelServing/screens/projects/NIMServiceModal/NIMModelDeploymentNameSection';
+import ProjectSection from '~/pages/modelServing/screens/projects/InferenceServiceModal/ProjectSection';
+import { DataConnection, NamespaceApplicationCase } from '~/pages/projects/types';
+import { AwsKeys } from '~/pages/projects/dataConnections/const';
+import { isAWSValid } from '~/pages/projects/screens/spawner/spawnerUtils';
+import DataConnectionSection from '~/pages/modelServing/screens/projects/InferenceServiceModal/DataConnectionSection';
+import { getProjectDisplayName, translateDisplayNameForK8s } from '~/pages/projects/utils';
+import { containsOnlySlashes, isS3PathValid } from '~/utilities/string';
+import AuthServingRuntimeSection from '~/pages/modelServing/screens/projects/ServingRuntimeModal/AuthServingRuntimeSection';
+import { useAccessReview } from '~/api';
+import { SupportedArea, useIsAreaAvailable } from '~/concepts/areas';
+import KServeAutoscalerReplicaSection from './KServeAutoscalerReplicaSection';
+
+const accessReviewResource: AccessReviewResourceAttributes = {
+  group: 'rbac.authorization.k8s.io',
+  resource: 'rolebindings',
+  verb: 'create',
+};
+
+type DeployNIMServiceModalProps = {
+  isOpen: boolean;
+  onClose: (submit: boolean) => void;
+  servingRuntimeTemplates?: TemplateKind[];
+} & EitherOrNone<
+  {
+    projectContext?: {
+      currentProject: ProjectKind;
+      dataConnections: DataConnection[];
+    };
+  },
+  {
+    editInfo?: {
+      servingRuntimeEditInfo?: ServingRuntimeEditInfo;
+      inferenceServiceEditInfo?: InferenceServiceKind;
+      secrets?: SecretKind[];
+    };
+  }
+>;
+
+const DeployNIMServiceModal: React.FC<DeployNIMServiceModalProps> = ({
+  isOpen,
+  onClose,
+  servingRuntimeTemplates,
+  projectContext,
+  editInfo,
+}) => {
+  const [createDataServingRuntime, setCreateDataServingRuntime, resetDataServingRuntime, sizes] =
+    useCreateServingRuntimeObject(editInfo?.servingRuntimeEditInfo);
+  const [createDataInferenceService, setCreateDataInferenceService, resetDataInferenceService] =
+    useCreateInferenceServiceObject(
+      editInfo?.inferenceServiceEditInfo,
+      editInfo?.servingRuntimeEditInfo?.servingRuntime,
+      editInfo?.secrets,
+    );
+
+  const isAuthorinoEnabled = useIsAreaAvailable(SupportedArea.K_SERVE_AUTH).status;
+  const currentProjectName = projectContext?.currentProject.metadata.name;
+  const namespace = currentProjectName || createDataInferenceService.project;
+  const isInferenceServiceNameWithinLimit =
+    translateDisplayNameForK8s(createDataInferenceService.name).length <= 253;
+
+  const [acceleratorProfileState, setAcceleratorProfileState, resetAcceleratorProfileData] =
+    useServingAcceleratorProfile(
+      editInfo?.servingRuntimeEditInfo?.servingRuntime,
+      editInfo?.inferenceServiceEditInfo,
+    );
+  const customServingRuntimesEnabled = useCustomServingRuntimesEnabled();
+  const [allowCreate] = useAccessReview({
+    ...accessReviewResource,
+    namespace,
+  });
+
+  const [actionInProgress, setActionInProgress] = React.useState(false);
+  const [error, setError] = React.useState<Error | undefined>();
+  const [alertVisible, setAlertVisible] = React.useState(true);
+
+  React.useEffect(() => {
+    if (currentProjectName && isOpen) {
+      setCreateDataInferenceService('project', currentProjectName);
+    }
+  }, [currentProjectName, setCreateDataInferenceService, isOpen]);
+
+  // Serving Runtime Validation
+  const baseInputValueValid =
+    createDataServingRuntime.numReplicas >= 0 &&
+    resourcesArePositive(createDataServingRuntime.modelSize.resources) &&
+    requestsUnderLimits(createDataServingRuntime.modelSize.resources);
+
+  const isDisabledServingRuntime = namespace === '' || actionInProgress || !baseInputValueValid;
+
+  const isDisabledInferenceService =
+    actionInProgress ||
+    createDataInferenceService.name.trim() === '' ||
+    createDataInferenceService.project === '' ||
+    !isInferenceServiceNameWithinLimit 
+
+  const servingRuntimeSelected = React.useMemo(
+    () =>
+      editInfo?.servingRuntimeEditInfo?.servingRuntime ||
+      getServingRuntimeFromName(
+        createDataServingRuntime.servingRuntimeTemplateName,
+        servingRuntimeTemplates,
+      ),
+    [editInfo, servingRuntimeTemplates, createDataServingRuntime.servingRuntimeTemplateName],
+  );
+
+  const onBeforeClose = (submitted: boolean) => {
+    onClose(submitted);
+    setError(undefined);
+    setActionInProgress(false);
+    resetDataServingRuntime();
+    resetDataInferenceService();
+    resetAcceleratorProfileData();
+    setAlertVisible(true);
+  };
+
+  const setErrorModal = (e: Error) => {
+    setError(e);
+    setActionInProgress(false);
+  };
+
+  const onSuccess = () => {
+    setActionInProgress(false);
+    onBeforeClose(true);
+  };
+
+  const submit = () => {
+    setError(undefined);
+    setActionInProgress(true);
+    onSuccess();
+
+    // const servingRuntimeName = 'nim';
+
+    // const submitServingRuntimeResources = getSubmitServingRuntimeResourcesFn(
+    //   servingRuntimeSelected,
+    //   createDataServingRuntime,
+    //   customServingRuntimesEnabled,
+    //   namespace,
+    //   editInfo?.servingRuntimeEditInfo,
+    //   false,
+    //   acceleratorProfileState,
+    //   NamespaceApplicationCase.KSERVE_PROMOTION,
+    //   projectContext?.currentProject,
+    //   servingRuntimeName,
+    //   false,
+    // );
+
+    // const submitInferenceServiceResource = getSubmitInferenceServiceResourceFn(
+    //   createDataInferenceService,
+    //   editInfo?.inferenceServiceEditInfo,
+    //   servingRuntimeName,
+    //   false,
+    //   acceleratorProfileState,
+    //   allowCreate,
+    //   editInfo?.secrets,
+    // );
+
+    // Promise.all([
+    //   submitServingRuntimeResources({ dryRun: true }),
+    //   submitInferenceServiceResource({ dryRun: true }),
+    // ])
+    //   .then(() =>
+    //     Promise.all([
+    //       submitServingRuntimeResources({ dryRun: false }),
+    //       submitInferenceServiceResource({ dryRun: false }),
+    //     ]),
+    //   )
+    //   .then(() => onSuccess())
+    //   .catch((e) => {
+    //     setErrorModal(e);
+    //   });
+  };
+
+  return (
+    <Modal
+      title="Deploy model"
+      description="Configure properties for deploying your model"
+      variant="medium"
+      isOpen={isOpen}
+      onClose={() => onBeforeClose(false)}
+      footer={
+        <DashboardModalFooter
+          submitLabel="Deploy"
+          onSubmit={submit}
+          onCancel={() => onBeforeClose(false)}
+          isSubmitDisabled={isDisabledServingRuntime || isDisabledInferenceService}
+          error={error}
+          alertTitle="Error creating model server"
+        />
+      }
+      showClose
+    >
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          submit();
+        }}
+      >
+        <Stack hasGutter>
+          <StackItem>
+            <ProjectSection
+              projectName={
+                (projectContext?.currentProject &&
+                  getProjectDisplayName(projectContext.currentProject)) ||
+                editInfo?.inferenceServiceEditInfo?.metadata.namespace ||
+                ''
+              }
+            />
+          </StackItem>
+          <StackItem>
+            <NIMModelDeploymentNameSection
+              data={createDataInferenceService}
+              setData={setCreateDataInferenceService}
+            />
+          </StackItem>
+          <StackItem>
+            <StackItem>
+              <NIMModelListSection
+                data={createDataInferenceService}
+                setData={setCreateDataInferenceService}
+                isEditing={!!editInfo}
+              />
+            </StackItem>
+          </StackItem>
+          <StackItem>
+            <KServeAutoscalerReplicaSection
+              data={createDataInferenceService}
+              setData={setCreateDataInferenceService}
+              infoContent="Consider network traffic and failover scenarios when specifying the number of model
+                server replicas."
+            />
+          </StackItem>
+          <StackItem>
+            <ServingRuntimeSizeSection
+              data={createDataServingRuntime}
+              setData={setCreateDataServingRuntime}
+              sizes={sizes}
+              servingRuntimeSelected={servingRuntimeSelected}
+              acceleratorProfileState={acceleratorProfileState}
+              setAcceleratorProfileState={setAcceleratorProfileState}
+              infoContent="Select a server size that will accommodate your largest model. See the product documentation for more information."
+            />
+          </StackItem>
+        </Stack>
+      </Form>
+    </Modal>
+  );
+};
+
+export default DeployNIMServiceModal;
