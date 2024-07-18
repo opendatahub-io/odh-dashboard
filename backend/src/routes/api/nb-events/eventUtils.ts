3552d1a5b63b903a3c7837b7946115ca0e9e4d90
Robert Sun <107655677+rsun19@users.noreply.github.com>
2024-07-18 13:21:48 -0400

    disable access to notebook from other users

--- backend/src/routes/api/nb-events/eventUtils.ts
+++ backend/src/routes/api/nb-events/eventUtils.ts
@@ -1,5 +1,7 @@
-import { V1Event, V1EventList } from '@kubernetes/client-node';
-import { KubeFastifyInstance } from '../../../types';
+import { V1Event, V1SelfSubjectAccessReview } from '@kubernetes/client-node';
+import { K8sStatus, KubeFastifyInstance, OauthFastifyRequest } from '../../../types';
+import { createSelfSubjectAccessReview } from '../namespaces/namespaceUtils';
+import { createCustomError } from '../../../utils/requestUtils';
 
 export const getNotebookEvents = async (
   fastify: KubeFastifyInstance,
@@ -7,18 +9,57 @@ export const getNotebookEvents = async (
   notebookName: string,
   podUID: string | undefined,
 ): Promise<V1Event[]> => {
-  return fastify.kube.coreV1Api
-    .listNamespacedEvent(
+  if (podUID) {
+    const response = await fastify.kube.coreV1Api.listNamespacedPod(
       namespace,
       undefined,
       undefined,
       undefined,
-      podUID
-        ? `involvedObject.kind=Pod,involvedObject.uid=${podUID}`
-        : `involvedObject.kind=StatefulSet,involvedObject.name=${notebookName}`,
-    )
-    .then((res) => {
-      const body = res.body as V1EventList;
-      return body.items;
-    });
+      undefined,
+      `notebook-name=${notebookName}`,
+    );
+    for (const pod of response.body.items) {
+      if (pod.metadata.uid === podUID) {
+        return fastify.kube.coreV1Api
+          .listNamespacedEvent(
+            namespace,
+            undefined,
+            undefined,
+            undefined,
+            `involvedObject.kind=Pod,involvedObject.uid=${podUID}`,
+          )
+          .then((res) => res.body.items);
+      }
+    }
+    throw createCustomError(
+      '404 Referenced pod not found for the notebook',
+      'Referenced pod not found for the notebook',
+      404,
+    );
+  } else {
+    return fastify.kube.coreV1Api
+      .listNamespacedEvent(
+        namespace,
+        undefined,
+        undefined,
+        undefined,
+        `involvedObject.kind=StatefulSet,involvedObject.name=${notebookName}`,
+      )
+      .then((res) => res.body.items);
+  }
 };
+
+export const checkUserNotebookPermissions = (
+  fastify: KubeFastifyInstance,
+  request: OauthFastifyRequest,
+  name: string,
+  namespace: string,
+): Promise<V1SelfSubjectAccessReview | K8sStatus> =>
+  createSelfSubjectAccessReview(fastify, request, {
+    group: 'kubeflow.org',
+    resource: 'notebooks',
+    subresource: '',
+    verb: 'get',
+    name,
+    namespace,
+  });
