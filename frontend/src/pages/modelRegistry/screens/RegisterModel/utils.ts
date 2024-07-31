2b3afccb199378d8ae645da1ea7315ad589d3e8e
Mike Turley <mike.turley@alum.cs.umass.edu>
2024-07-31 14:55:54 -0400

    Construct URI for object storage mode
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- frontend/src/pages/modelRegistry/screens/RegisterModel/utils.ts
+++ frontend/src/pages/modelRegistry/screens/RegisterModel/utils.ts
@@ -6,7 +6,11 @@ import {
   RegisteredModel,
 } from '~/concepts/modelRegistry/types';
 import { ModelRegistryAPIState } from '~/concepts/modelRegistry/context/useModelRegistryAPIState';
-import { RegisterModelFormData, RegisterVersionFormData } from './useRegisterModelData';
+import {
+  ModelLocationType,
+  RegisterModelFormData,
+  RegisterVersionFormData,
+} from './useRegisterModelData';
 
 export type RegisterModelCreatedResources = RegisterVersionCreatedResources & {
   registeredModel: RegisteredModel;
@@ -67,9 +71,61 @@ export const registerVersion = async (
     modelFormatName: formData.sourceModelFormat, // TODO change to sourceModelFormatName
     modelFormatVersion: '1', // TODO add new formData.sourceModelFormatVersion
     // storageKey: 'TODO', // TODO fill in the name of the data connection we used to prefill if we used one - reference ticket
-    // TODO construct this URI from formData according to agreed-upon structure
-    uri: 's3://rhods-public/demo-models/flan-t5-small-caikit?endpoint=http%3A%2F%2Fs3.amazonaws.com%2F&defaultRegion=us-east-1',
+    uri:
+      formData.modelLocationType === ModelLocationType.ObjectStorage
+        ? objectStorageFieldsToUri({
+            endpoint: formData.modelLocationEndpoint,
+            bucket: formData.modelLocationBucket,
+            region: formData.modelLocationRegion,
+            path: formData.modelLocationPath,
+          })
+        : formData.modelLocationURI,
     artifactType: 'model-artifact',
   });
   return { modelVersion, modelArtifact };
+};
+
+type ObjectStorageFields = {
+  endpoint: string;
+  bucket: string;
+  region?: string;
+  path: string;
+};
+
+// TODO unit tests for the below using:
+/*
+s3://rhods-public/demo-models/flan-t5-small-caikit?endpoint=http%3A%2F%2Fs3.amazonaws.com%2F&defaultRegion=us-east-1
+{
+  endpoint: 'http://s3.amazonaws.com/',
+  bucket: 'rhods-public',
+  region: 'us-east-1',
+  path: 'demo-models/flan-t5-small-caikit',
+}
+*/
+
+export const objectStorageFieldsToUri = (fields: ObjectStorageFields): string => {
+  const { endpoint, bucket, region, path } = fields;
+  const searchParams = new URLSearchParams();
+  searchParams.set('endpoint', endpoint);
+  if (region) {
+    searchParams.set('defaultRegion', region);
+  }
+  return `s3://${bucket}/${path}?${searchParams.toString()}`;
+};
+
+export const uriToObjectStorageFields = (uri: string): ObjectStorageFields | null => {
+  try {
+    const urlObj = new URL(uri);
+    const [bucket, ...pathSplit] = urlObj.pathname.split('/').filter(Boolean);
+    const path = pathSplit.join('/');
+    const searchParams = new URLSearchParams(urlObj.search);
+    const endpoint = searchParams.get('endpoint');
+    const region = searchParams.get('defaultRegion');
+    if (endpoint && bucket && path) {
+      return { endpoint, bucket, region: region || undefined, path };
+    }
+    return null;
+  } catch {
+    return null;
+  }
 };
