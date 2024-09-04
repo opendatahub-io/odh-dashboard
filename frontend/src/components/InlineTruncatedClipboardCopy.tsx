83dc0793411460d0e1724ce77c958803bb754ac1
gitdallas <5322142+gitdallas@users.noreply.github.com>
2024-09-04 13:40:35 -0500

    remove width from inlinetruncatedclipcopy
    Signed-off-by: gitdallas <5322142+gitdallas@users.noreply.github.com>

--- frontend/src/components/InlineTruncatedClipboardCopy.tsx
+++ frontend/src/components/InlineTruncatedClipboardCopy.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 
 type Props = {
   textToCopy: string;
-  width?: string;
   testId?: string;
 };
 
@@ -12,21 +11,19 @@ type Props = {
  * https://github.com/patternfly/patternfly-react/issues/10890
  **/
 
-const InlineTruncatedClipboardCopy: React.FC<Props> = ({ textToCopy, width = '100%', testId }) => (
-  <div style={{ width }}>
-    <ClipboardCopy
-      variant="inline-compact"
-      style={{ display: 'inline-flex' }}
-      hoverTip="Copy"
-      clickTip="Copied"
-      onCopy={() => {
-        navigator.clipboard.writeText(textToCopy);
-      }}
-      data-testid={testId}
-    >
-      <Truncate content={textToCopy} />
-    </ClipboardCopy>
-  </div>
+const InlineTruncatedClipboardCopy: React.FC<Props> = ({ textToCopy, testId }) => (
+  <ClipboardCopy
+    variant="inline-compact"
+    style={{ display: 'inline-flex' }}
+    hoverTip="Copy"
+    clickTip="Copied"
+    onCopy={() => {
+      navigator.clipboard.writeText(textToCopy);
+    }}
+    data-testid={testId}
+  >
+    <Truncate content={textToCopy} />
+  </ClipboardCopy>
 );
 
 export default InlineTruncatedClipboardCopy;
