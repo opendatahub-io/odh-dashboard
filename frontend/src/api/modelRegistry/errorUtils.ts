84e7950f0586fe07991a3ff777d70b532ef1c57c
Mike Turley <mike.turley@alum.cs.umass.edu>
2024-09-26 13:46:05 -0400

    Remove stale TODO - we do need the code in question
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- frontend/src/api/modelRegistry/errorUtils.ts
+++ frontend/src/api/modelRegistry/errorUtils.ts
@@ -18,7 +18,6 @@ export const handleModelRegistryFailures = <T>(promise: Promise<T>): Promise<T> 
       }
       if (isCommonStateError(e)) {
         // Common state errors are handled by useFetchState at storage level, let them deal with it
-        // TODO: check whether we need this or not
         throw e;
       }
       // eslint-disable-next-line no-console
