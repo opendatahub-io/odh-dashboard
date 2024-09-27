d121dd043925e451b056255a34617584d0e3618b
Jeffrey Phillips <jephilli@redhat.com>
2024-09-27 09:19:23 -0400

    Project list view: Fetch notebooks for projects only on row render

---
+++ frontend/src/utilities/useWatchNotebooks.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import useFetchState, { FetchState } from '~/utilities/useFetchState';
+import { NotebookKind } from '~/k8sTypes';
+import { getNotebooks } from '~/api';
+import { POLL_INTERVAL } from '~/utilities/const';
+
+export const useWatchNotebooks = (
+  namespace: string,
+  refreshRate = POLL_INTERVAL,
+): FetchState<NotebookKind[]> =>
+  useFetchState<NotebookKind[]>(
+    React.useCallback(() => getNotebooks(namespace), [namespace]),
+    [],
+    { refreshRate },
+  );
