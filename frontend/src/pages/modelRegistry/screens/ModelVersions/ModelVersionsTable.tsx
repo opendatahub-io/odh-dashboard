1cb71d670757d79daec0d9036c9fe664d9d14a86
gitdallas <5322142+gitdallas@users.noreply.github.com>
2024-09-30 15:17:46 -0500

    fix(MR): disable archive for mr/mv if has deploys
    Signed-off-by: gitdallas <5322142+gitdallas@users.noreply.github.com>

--- frontend/src/pages/modelRegistry/screens/ModelVersions/ModelVersionsTable.tsx
+++ frontend/src/pages/modelRegistry/screens/ModelVersions/ModelVersionsTable.tsx
@@ -2,8 +2,11 @@ import * as React from 'react';
 import { Table } from '~/components/table';
 import { ModelVersion } from '~/concepts/modelRegistry/types';
 import DashboardEmptyTableView from '~/concepts/dashboard/DashboardEmptyTableView';
-import ModelVersionsTableRow from './ModelVersionsTableRow';
+import useInferenceServices from '~/pages/modelServing/useInferenceServices';
+import { useMakeFetchObject } from '~/utilities/useMakeFetchObject';
+import { KnownLabels } from '~/k8sTypes';
 import { mvColumns } from './ModelVersionsTableColumns';
+import ModelVersionsTableRow from './ModelVersionsTableRow';
 
 type ModelVersionsTableProps = {
   clearFilters: () => void;
@@ -18,25 +21,37 @@ const ModelVersionsTable: React.FC<ModelVersionsTableProps> = ({
   toolbarContent,
   isArchiveModel,
   refresh,
-}) => (
-  <Table
-    data-testid="model-versions-table"
-    data={modelVersions}
-    columns={mvColumns}
-    toolbarContent={toolbarContent}
-    defaultSortColumn={3}
-    enablePagination
-    onClearFilters={clearFilters}
-    emptyTableView={<DashboardEmptyTableView onClearFilters={clearFilters} />}
-    rowRenderer={(mv) => (
-      <ModelVersionsTableRow
-        key={mv.name}
-        modelVersion={mv}
-        isArchiveModel={isArchiveModel}
-        refresh={refresh}
-      />
-    )}
-  />
-);
+}) => {
+  const inferenceServices = useMakeFetchObject(
+    useInferenceServices(undefined, modelVersions[0].registeredModelId),
+  );
+  return (
+    <Table
+      data-testid="model-versions-table"
+      data={modelVersions}
+      columns={mvColumns}
+      toolbarContent={toolbarContent}
+      defaultSortColumn={3}
+      enablePagination
+      onClearFilters={clearFilters}
+      emptyTableView={<DashboardEmptyTableView onClearFilters={clearFilters} />}
+      rowRenderer={(mv) => {
+        const hasDeployment = !!inferenceServices.data.some(
+          (s) => s.metadata.labels?.[KnownLabels.MODEL_VERSION_ID] === mv.id,
+        );
+        return (
+          <ModelVersionsTableRow
+            hasDeployment={hasDeployment}
+            key={mv.name}
+            modelVersion={mv}
+            isArchiveModel={isArchiveModel}
+            refresh={refresh}
+          />
+        );
+      }}
+    />
+  );
+};
+// >>>>>>> 980ca3be (fix(MR): disable archive for mr/mv if has deploys)
 
 export default ModelVersionsTable;
