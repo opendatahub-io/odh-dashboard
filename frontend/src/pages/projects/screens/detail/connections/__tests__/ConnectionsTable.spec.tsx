9a3179f59a11bd8b0e9b0671ba5866dd9fd0e1f9
Jeffrey Phillips <jephilli@redhat.com>
2024-09-30 16:14:40 -0400

    List connected deployed models for each connection table row

--- frontend/src/pages/projects/screens/detail/connections/__tests__/ConnectionsTable.spec.tsx
+++ frontend/src/pages/projects/screens/detail/connections/__tests__/ConnectionsTable.spec.tsx
@@ -5,17 +5,30 @@ import { mockConnectionTypeConfigMapObj } from '~/__mocks__/mockConnectionType';
 import { mockConnection } from '~/__mocks__/mockConnection';
 import { mockNotebookK8sResource } from '~/__mocks__/mockNotebookK8sResource';
 import { useRelatedNotebooks } from '~/pages/projects/notebook/useRelatedNotebooks';
+import { useInferenceServicesForConnection } from '~/pages/projects/useInferenceServicesForConnection';
+import { mockInferenceServiceK8sResource } from '~/__mocks__';
 
 jest.mock('~/pages/projects/notebook/useRelatedNotebooks', () => ({
   ...jest.requireActual('~/pages/projects/notebook/useRelatedNotebooks'),
   useRelatedNotebooks: jest.fn(),
 }));
 
+jest.mock('~/pages/projects/useInferenceServicesForConnection', () => ({
+  useInferenceServicesForConnection: jest.fn(),
+}));
+
 const useRelatedNotebooksMock = useRelatedNotebooks as jest.Mock;
+const useInferenceServicesForConnectionMock = useInferenceServicesForConnection as jest.Mock;
+
+const mockInferenceServices = [
+  mockInferenceServiceK8sResource({ name: 'deployed-model-1', displayName: 'Deployed model 1' }),
+  mockInferenceServiceK8sResource({ name: 'deployed-model-2', displayName: 'Deployed model 2' }),
+];
 
 describe('ConnectionsTable', () => {
   beforeEach(() => {
     useRelatedNotebooksMock.mockReturnValue({ notebooks: [], loaded: true });
+    useInferenceServicesForConnectionMock.mockReturnValue([]);
   });
 
   it('should render table', () => {
@@ -61,6 +74,7 @@ describe('ConnectionsTable', () => {
       notebooks: [mockNotebookK8sResource({ displayName: 'Connected notebook' })],
       loaded: true,
     });
+    useInferenceServicesForConnectionMock.mockReturnValue(mockInferenceServices);
 
     const connection = mockConnection({ displayName: 'connection1', description: 'desc1' });
     render(
@@ -81,5 +95,7 @@ describe('ConnectionsTable', () => {
     expect(screen.queryByText('s3')).toBeFalsy();
     expect(screen.getByText('S3 Buckets')).toBeTruthy();
     expect(screen.getByText('Connected notebook')).toBeTruthy();
+    expect(screen.getByText('Deployed model 1')).toBeTruthy();
+    expect(screen.getByText('Deployed model 2')).toBeTruthy();
   });
 });
