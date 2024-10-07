ea2516cc0f382146f6e326d5137e80c4e54f11c8
Gage Krumbach <gkrumbach@gmail.com>
2024-10-07 14:20:14 -0500

    update test IDS

--- frontend/src/concepts/pipelines/content/import/PipelineVersionImportModal.tsx
+++ frontend/src/concepts/pipelines/content/import/PipelineVersionImportModal.tsx
@@ -4,7 +4,6 @@ import { FormGroup, StackItem } from '@patternfly/react-core';
 import { usePipelinesAPI } from '~/concepts/pipelines/context';
 import { PipelineKFv2, PipelineVersionKFv2 } from '~/concepts/pipelines/kfTypes';
 import PipelineSelector from '~/concepts/pipelines/content/pipelineSelector/PipelineSelector';
-import useDebounceCallback from '~/utilities/useDebounceCallback';
 import { getNameEqualsFilter } from '~/concepts/pipelines/utils';
 import { generatePipelineVersionName, PipelineUploadOption } from './utils';
 import { usePipelineVersionImportModalData } from './useImportModalData';
@@ -22,27 +21,23 @@ const PipelineVersionImportModal: React.FC<PipelineVersionImportModalProps> = ({
   const { api } = usePipelinesAPI();
   const [modalData, setData, resetData] = usePipelineVersionImportModalData(existingPipeline);
 
-  const checkForDuplicateName = useDebounceCallback(
-    React.useCallback(
-      async (value: string) => {
-        if (modalData.pipeline?.pipeline_id && value) {
-          const { pipeline_versions: duplicateVersions } = await api.listPipelineVersions(
-            {},
-            modalData.pipeline.pipeline_id,
-            getNameEqualsFilter(value),
-          );
+  const checkForDuplicateName = React.useCallback(
+    async (value: string) => {
+      if (modalData.pipeline?.pipeline_id && value) {
+        const { pipeline_versions: duplicateVersions } = await api.listPipelineVersions(
+          {},
+          modalData.pipeline.pipeline_id,
+          getNameEqualsFilter(value),
+        );
 
-          if (duplicateVersions?.length) {
-            return true;
-          }
+        if (duplicateVersions?.length) {
+          return true;
         }
-        return false;
-      },
-      [api, modalData.pipeline?.pipeline_id],
-    ),
-    500,
+      }
+      return false;
+    },
+    [api, modalData.pipeline?.pipeline_id],
   );
-
   const submitAction = React.useCallback(() => {
     const { name, description, fileContents, pipelineUrl, uploadOption, pipeline } = modalData;
     const pipelineId = pipeline?.pipeline_id || '';
