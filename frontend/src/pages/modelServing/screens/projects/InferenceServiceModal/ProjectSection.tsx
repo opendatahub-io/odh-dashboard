2cc9597581a0b61b3a675a8a6a294371dee16166
Olga Lavtar <olavtar@redhat.com>
2024-10-08 13:57:18 -0700

    fix: added validation for model names and disabled the NIM models metrics hyperlink on the Models tab
    Signed-off-by: Olga Lavtar <olavtar@redhat.com>

--- frontend/src/pages/modelServing/screens/projects/InferenceServiceModal/ProjectSection.tsx
+++ frontend/src/pages/modelServing/screens/projects/InferenceServiceModal/ProjectSection.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import { FormGroup, Text } from '@patternfly/react-core';
+import { translateDisplayNameForK8sAndReport } from '~/concepts/k8s/utils';
 
 type ProjectSectionType = {
   projectName: string;
 };
 
-const ProjectSection: React.FC<ProjectSectionType> = ({ projectName }) => (
-  <FormGroup label="Project">
-    <Text>{projectName}</Text>
-  </FormGroup>
-);
+const ProjectSection: React.FC<ProjectSectionType> = ({ projectName }) => {
+  const [translatedName] = translateDisplayNameForK8sAndReport(projectName);
+
+  return (
+    <FormGroup label="Project">
+      <Text>{translatedName}</Text>
+    </FormGroup>
+  );
+};
 
 export default ProjectSection;
