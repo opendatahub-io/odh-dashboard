e1e8c6f77b99f1a6e80f0bd79c3af3f39fbd61d6
Olga Lavtar <olavtar@redhat.com>
2024-10-11 14:53:08 -0700

    fix: RHOAIENG-13881 - model deployment name allowed illegal chars
    Signed-off-by: Olga Lavtar <olavtar@redhat.com>

--- frontend/src/pages/modelServing/screens/projects/InferenceServiceModal/ProjectSection.tsx
+++ frontend/src/pages/modelServing/screens/projects/InferenceServiceModal/ProjectSection.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
 import { FormGroup, Text } from '@patternfly/react-core';
-import { translateDisplayNameForK8sAndReport } from '~/concepts/k8s/utils';
 
 type ProjectSectionType = {
   projectName: string;
 };
 
-const ProjectSection: React.FC<ProjectSectionType> = ({ projectName }) => {
-  const [translatedName] = translateDisplayNameForK8sAndReport(projectName);
-
-  return (
-    <FormGroup label="Project">
-      <Text>{translatedName}</Text>
-    </FormGroup>
-  );
-};
+const ProjectSection: React.FC<ProjectSectionType> = ({ projectName }) => (
+  <FormGroup label="Project">
+    <Text>{projectName}</Text>
+  </FormGroup>
+);
 
 export default ProjectSection;
