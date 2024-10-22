53154fe92429c790a4e8f93332bba15a783df452
Olga Lavtar <olavtar@redhat.com>
2024-10-22 15:02:07 -0700

    fix: disabling NIM models metrics hyperlink on the Models tab
    Signed-off-by: Olga Lavtar <olavtar@redhat.com>

--- frontend/src/pages/modelServing/screens/global/InferenceServiceTableRow.tsx
+++ frontend/src/pages/modelServing/screens/global/InferenceServiceTableRow.tsx
@@ -35,25 +35,24 @@ const InferenceServiceTableRow: React.FC<InferenceServiceTableRowProps> = ({
   isGlobal,
   columnNames,
 }) => {
+  const { projects } = React.useContext(ProjectsContext);
+  const project = projects.find(byName(inferenceService.metadata.namespace)) ?? null;
+  const isKServeNIMEnabled = project ? isProjectNIMSupported(project) : false;
+
   const [modelMetricsEnabled] = useModelMetricsEnabled();
   const kserveMetricsEnabled = useIsAreaAvailable(SupportedArea.K_SERVE_METRICS).status;
 
   const modelMesh = isModelMesh(inferenceService);
   const modelMeshMetricsSupported = modelMetricsEnabled && modelMesh;
-  const kserveMetricsSupported = modelMetricsEnabled && kserveMetricsEnabled && !modelMesh;
+  const kserveMetricsSupported =
+    modelMetricsEnabled && kserveMetricsEnabled && !modelMesh && !isKServeNIMEnabled;
   const displayName = getDisplayNameFromK8sResource(inferenceService);
-
-  const { projects } = React.useContext(ProjectsContext);
-  const project = projects.find(byName(inferenceService.metadata.namespace)) ?? null;
-  const isKServeNIMEnabled = project ? isProjectNIMSupported(project) : false;
 
   return (
     <>
       <Td dataLabel="Name">
         <ResourceNameTooltip resource={inferenceService}>
-          {isKServeNIMEnabled ? (
-            displayName
-          ) : modelMeshMetricsSupported || kserveMetricsSupported ? (
+          {modelMeshMetricsSupported || kserveMetricsSupported ? (
             <Link
               data-testid={`metrics-link-${displayName}`}
               to={
