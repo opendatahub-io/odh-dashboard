636d2b91f1f78082e4272dba9f676435a7b6309a
Mike Turley <mike.turley@alum.cs.umass.edu>
2024-10-28 13:52:37 -0400

    Partial revert - put back deploy logic in empty state cards
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- frontend/src/pages/modelServing/screens/projects/EmptyMultiModelServingCard.tsx
+++ frontend/src/pages/modelServing/screens/projects/EmptyMultiModelServingCard.tsx
@@ -10,47 +10,87 @@ import {
   TextVariants,
 } from '@patternfly/react-core';
 import { ProjectDetailsContext } from '~/pages/projects/ProjectDetailsContext';
-import { NamespaceApplicationCase } from '~/pages/projects/types';
-import ModelServingPlatformSelectButton from '~/pages/modelServing/screens/projects/ModelServingPlatformSelectButton';
+import { ServingRuntimePlatform } from '~/types';
+import {
+  getSortedTemplates,
+  getTemplateEnabled,
+  getTemplateEnabledForPlatform,
+} from '~/pages/modelServing/customServingRuntimes/utils';
+import ModelServingPlatformButtonAction from '~/pages/modelServing/screens/projects/ModelServingPlatformButtonAction';
+import ManageServingRuntimeModal from './ServingRuntimeModal/ManageServingRuntimeModal';
 
-type EmptyMultiModelServingCardProps = {
-  setErrorSelectingPlatform: (e?: Error) => void;
-};
+const EmptyMultiModelServingCard: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
 
-const EmptyMultiModelServingCard: React.FC<EmptyMultiModelServingCardProps> = ({
-  setErrorSelectingPlatform,
-}) => {
-  const { currentProject } = React.useContext(ProjectDetailsContext);
+  const {
+    servingRuntimes: { refresh: refreshServingRuntime },
+    servingRuntimeTemplates: [templates],
+    servingRuntimeTemplateOrder: { data: templateOrder },
+    servingRuntimeTemplateDisablement: { data: templateDisablement },
+    serverSecrets: { refresh: refreshTokens },
+    inferenceServices: { refresh: refreshInferenceServices },
+    currentProject,
+  } = React.useContext(ProjectDetailsContext);
+
+  const templatesSorted = getSortedTemplates(templates, templateOrder);
+  const templatesEnabled = templatesSorted.filter((template) =>
+    getTemplateEnabled(template, templateDisablement),
+  );
+  const emptyTemplates = templatesEnabled.length === 0;
+
+  const onSubmit = (submit: boolean) => {
+    if (submit) {
+      refreshServingRuntime();
+      refreshInferenceServices();
+      setTimeout(refreshTokens, 500); // need a timeout to wait for tokens creation
+    }
+  };
+
   return (
-    <Card
-      style={{
-        height: '100%',
-        border: '1px solid var(--pf-v5-global--BorderColor--100)',
-        borderRadius: 16,
-      }}
-      data-testid="multi-serving-platform-card"
-    >
-      <CardTitle>
-        <TextContent>
-          <Text component={TextVariants.h2}>Multi-model serving platform</Text>
-        </TextContent>
-      </CardTitle>
-      <CardBody>
-        Multiple models can be deployed on one shared model server. Choose this option when you want
-        to deploy a number of small or medium-sized models that can share the server resources.
-      </CardBody>
-      <CardFooter>
-        <Bullseye>
-          <ModelServingPlatformSelectButton
-            namespace={currentProject.metadata.name}
-            servingPlatform={NamespaceApplicationCase.MODEL_MESH_PROMOTION}
-            setError={setErrorSelectingPlatform}
-            variant="secondary"
-            data-testid="multi-serving-select-button" // TODO this changed from multi-serving-add-server-button, inform QE and look for other cases
-          />
-        </Bullseye>
-      </CardFooter>
-    </Card>
+    <>
+      <Card
+        style={{
+          height: '100%',
+          border: '1px solid var(--pf-v5-global--BorderColor--100)',
+          borderRadius: 16,
+        }}
+        data-testid="multi-serving-platform-card"
+      >
+        <CardTitle>
+          <TextContent>
+            <Text component={TextVariants.h2}>Multi-model serving platform</Text>
+          </TextContent>
+        </CardTitle>
+        <CardBody>
+          Multiple models can be deployed on one shared model server. Choose this option when you
+          want to deploy a number of small or medium-sized models that can share the server
+          resources.
+        </CardBody>
+        <CardFooter>
+          <Bullseye>
+            <ModelServingPlatformButtonAction
+              isProjectModelMesh
+              emptyTemplates={emptyTemplates}
+              onClick={() => setOpen(true)}
+              variant="secondary"
+              testId="multi-serving-add-server-button"
+            />
+          </Bullseye>
+        </CardFooter>
+      </Card>
+      {open ? (
+        <ManageServingRuntimeModal
+          currentProject={currentProject}
+          servingRuntimeTemplates={templatesEnabled.filter((template) =>
+            getTemplateEnabledForPlatform(template, ServingRuntimePlatform.MULTI),
+          )}
+          onClose={(submit: boolean) => {
+            setOpen(false);
+            onSubmit(submit);
+          }}
+        />
+      ) : null}
+    </>
   );
 };
 
--- frontend/src/pages/projects/screens/detail/overview/serverModels/SelectMultiModelCard.tsx
+++ frontend/src/pages/projects/screens/detail/overview/serverModels/SelectMultiModelCard.tsx
@@ -1,47 +1,28 @@
 import * as React from 'react';
-import { CardBody, CardFooter, Text, TextContent } from '@patternfly/react-core';
+import { CardBody, Text, TextContent } from '@patternfly/react-core';
 import { ProjectObjectType, SectionType } from '~/concepts/design/utils';
 import OverviewCard from '~/pages/projects/screens/detail/overview/components/OverviewCard';
-import { ProjectDetailsContext } from '~/pages/projects/ProjectDetailsContext';
-import { NamespaceApplicationCase } from '~/pages/projects/types';
-import ModelServingPlatformSelectButton from '~/pages/modelServing/screens/projects/ModelServingPlatformSelectButton';
+import { ServingRuntimePlatform } from '~/types';
+import AddModelFooter from './AddModelFooter';
 
-type SelectMultiModelCardProps = {
-  setErrorSelectingPlatform: (e?: Error) => void;
-};
-
-const SelectMultiModelCard: React.FC<SelectMultiModelCardProps> = ({
-  setErrorSelectingPlatform,
-}) => {
-  const { currentProject } = React.useContext(ProjectDetailsContext);
-  return (
-    <OverviewCard
-      objectType={ProjectObjectType.modelServer}
-      sectionType={SectionType.serving}
-      title="Multi-model serving platform"
-      data-testid="multi-serving-platform-card"
-    >
-      <CardBody>
-        <TextContent>
-          <Text component="small">
-            Multiple models can be deployed on one shared model server. Choose this option when you
-            want to deploy a number of small or medium-sized models that can share the server
-            resources.
-          </Text>
-        </TextContent>
-      </CardBody>
-      <CardFooter>
-        <ModelServingPlatformSelectButton
-          namespace={currentProject.metadata.name}
-          servingPlatform={NamespaceApplicationCase.MODEL_MESH_PROMOTION}
-          setError={setErrorSelectingPlatform}
-          variant="link"
-          isInline
-          data-testid="multi-serving-select-button" // TODO this changed from model-serving-platform-button (which was duplicated), inform QE and look for other cases
-        />
-      </CardFooter>
-    </OverviewCard>
-  );
-};
+const SelectMultiModelCard: React.FC = () => (
+  <OverviewCard
+    objectType={ProjectObjectType.modelServer}
+    sectionType={SectionType.serving}
+    title="Multi-model serving platform"
+    data-testid="multi-serving-platform-card"
+  >
+    <CardBody>
+      <TextContent>
+        <Text component="small">
+          Multiple models can be deployed on one shared model server. Choose this option when you
+          want to deploy a number of small or medium-sized models that can share the server
+          resources.
+        </Text>
+      </TextContent>
+    </CardBody>
+    <AddModelFooter selectedPlatform={ServingRuntimePlatform.MULTI} />
+  </OverviewCard>
+);
 
 export default SelectMultiModelCard;
