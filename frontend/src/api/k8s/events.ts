30440ecf4ae609b907cdaed43c8a7dfea49e9f7c
Gage Krumbach <gkrumbach@gmail.com>
2024-11-06 07:25:06 -0600

    websocket for notebooks scheduling (#3412)
    * use websockets for events
    
    * remove both useWatchNotebookEvents

--- frontend/src/api/k8s/events.ts
+++ frontend/src/api/k8s/events.ts
@@ -1,6 +1,9 @@
 import { k8sListResourceItems } from '@openshift/dynamic-plugin-sdk-utils';
 import { EventKind } from '~/k8sTypes';
 import { EventModel } from '~/api/models';
+import useK8sWatchResourceList from '~/utilities/useK8sWatchResourceList';
+import { CustomWatchK8sResult } from '~/types';
+import { groupVersionKind } from '..';
 
 export const getNotebookEvents = async (
   namespace: string,
@@ -18,3 +21,20 @@ export const getNotebookEvents = async (
       },
     },
   });
+
+export const useWatchNotebookEvents = (
+  namespace: string,
+  name: string,
+  podUid?: string,
+): CustomWatchK8sResult<EventKind[]> =>
+  useK8sWatchResourceList(
+    {
+      isList: true,
+      groupVersionKind: groupVersionKind(EventModel),
+      namespace,
+      fieldSelector: podUid
+        ? `involvedObject.kind=Pod,involvedObject.uid=${podUid}`
+        : `involvedObject.kind=StatefulSet,involvedObject.name=${name}`,
+    },
+    EventModel,
+  );
