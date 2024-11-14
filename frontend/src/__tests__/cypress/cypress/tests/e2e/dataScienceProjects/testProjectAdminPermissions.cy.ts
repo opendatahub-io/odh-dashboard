e643c893b6594ed7703f57df852a2a3dbb3f514c
Fede Alonso <fealonso@redhat.com>
2024-11-14 08:04:49 +0100

    modify new tests

--- frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/testProjectAdminPermissions.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/testProjectAdminPermissions.cy.ts
@@ -1,7 +1,10 @@
 import type { DataScienceProjectData } from '~/__tests__/cypress/cypress/types';
 import { projectDetails, projectListPage } from '~/__tests__/cypress/cypress/pages/projects';
 import { permissions } from '~/__tests__/cypress/cypress/pages/permissions';
-import { ADMIN_USER, TEST_USER } from '~/__tests__/cypress/cypress/utils/e2eUsers';
+import {
+  HTPASSWD_CLUSTER_ADMIN_USER,
+  LDAP_CONTRIBUTOR_USER,
+} from '~/__tests__/cypress/cypress/utils/e2eUsers';
 import { loadFixture } from '~/__tests__/cypress/cypress/utils/dataLoader';
 import { createCleanProject } from '~/__tests__/cypress/cypress/utils/projectChecker';
 import { deleteOpenShiftProject } from '~/__tests__/cypress/cypress/utils/oc_commands/project';
@@ -37,7 +40,7 @@ describe('Verify that users can provide admin project permissions to non-admin u
   it('Verify that user can be added as an Admin for a Project', () => {
     // Authentication and navigation
     cy.step('Log into the application');
-    cy.visitWithLogin('/', ADMIN_USER);
+    cy.visitWithLogin('/', HTPASSWD_CLUSTER_ADMIN_USER);
 
     // Project navigation, add user and provide admin permissions
     cy.step(
@@ -50,21 +53,24 @@ describe('Verify that users can provide admin project permissions to non-admin u
 
     cy.step('Assign admin user Project Permissions');
     permissions.findAddUserButton().click();
-    permissions.getUserTable().findAddInput().type(TEST_USER.USERNAME);
+    permissions.getUserTable().findAddInput().type(LDAP_CONTRIBUTOR_USER.USERNAME);
     permissions
       .getUserTable()
-      .selectPermission(TEST_USER.USERNAME, 'Admin Edit the project and manage user access');
+      .selectPermission(
+        LDAP_CONTRIBUTOR_USER.USERNAME,
+        'Admin Edit the project and manage user access',
+      );
 
     cy.step(
-      `Save the user and validate that ${TEST_USER.USERNAME} has been saved with admin permissions`,
+      `Save the user and validate that ${LDAP_CONTRIBUTOR_USER.USERNAME} has been saved with admin permissions`,
     );
     permissions.getUserTable().findSaveNewButton().should('exist').and('be.visible').click();
-    cy.contains(TEST_USER.USERNAME).should('exist');
+    cy.contains(LDAP_CONTRIBUTOR_USER.USERNAME).should('exist');
   });
   it('Verify that user can access the created project with Admin rights', () => {
     // Authentication and navigation
-    cy.step(`Log into the application with ${TEST_USER.USERNAME}`);
-    cy.visitWithLogin('/', TEST_USER);
+    cy.step(`Log into the application with ${LDAP_CONTRIBUTOR_USER.USERNAME}`);
+    cy.visitWithLogin('/', LDAP_CONTRIBUTOR_USER);
 
     // Project navigation and validate permissions tab is accessible
     cy.step('Verify that the user has access to the created project and can access Permissions');
