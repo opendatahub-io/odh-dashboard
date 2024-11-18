5fccd1426ec1590ff726703ba4f124a6cfdd84ce
Anthony Coughlin <33689325+antowaddle@users.noreply.github.com>
2024-11-18 18:27:40 +0000

    Cypress e2e Model Serving Tests ODS-2276 and ODS-2279 (#3480)
    * Created new tests for ODS-2276 and ODS-2279
    
    * Amended admin user required in latest merge to main
    
    * Minor changes requested in the PR
    
    * Additional changes to page object names

---
+++ frontend/src/__tests__/cypress/cypress/utils/fileParserUtil.ts
@@ -0,0 +1,73 @@
+import * as yaml from 'js-yaml';
+import {
+  getMultiModelPath,
+  getSingleModelPath,
+} from '~/__tests__/cypress/cypress/utils/fileImportUtils';
+
+// Define interfaces for the parsed YAML structure
+interface Metadata {
+  name: string; // Expecting a string for name
+  annotations?: {
+    [key: string]: string; // Allow any string key for annotations
+  };
+}
+
+interface MultiModelParsedYaml {
+  metadata: Metadata;
+}
+
+interface SingleModelParsedYaml {
+  metadata: Metadata;
+}
+
+// Function to get multi-model serving runtime info
+export function getMultiModelServingRuntimeInfo(): Cypress.Chainable<{
+  multiModelServingName: string; // Ensure this is always a string
+  displayName: string; // Ensure this is always a string
+}> {
+  const filePath = getMultiModelPath();
+
+  return cy.readFile(filePath).then((content) => {
+    const parsedYaml = yaml.load(content) as MultiModelParsedYaml;
+    const multiModelServingName = parsedYaml.metadata.name || '';
+    const displayName = parsedYaml.metadata.annotations?.['openshift.io/display-name'] || '';
+
+    // Check if values are strings
+    if (typeof multiModelServingName !== 'string' || typeof displayName !== 'string') {
+      throw new Error(
+        `Expected multiModelServingName and displayName to be strings, but got: ${typeof multiModelServingName}, ${typeof displayName}`,
+      );
+    }
+
+    return {
+      multiModelServingName,
+      displayName,
+    };
+  });
+}
+
+// Function to get single-model serving runtime info
+export function getSingleModelServingRuntimeInfo(): Cypress.Chainable<{
+  singleModelServingName: string;
+  displayName: string;
+}> {
+  const filePath = getSingleModelPath();
+
+  return cy.readFile(filePath).then((content) => {
+    const parsedYaml = yaml.load(content) as SingleModelParsedYaml;
+    const singleModelServingName = parsedYaml.metadata.name || '';
+    const displayName = parsedYaml.metadata.annotations?.['openshift.io/display-name'] || '';
+
+    // Check if values are strings
+    if (typeof singleModelServingName !== 'string' || typeof displayName !== 'string') {
+      throw new Error(
+        `Expected singleModelServingName to be a string, but got: ${typeof singleModelServingName}`,
+      );
+    }
+
+    return {
+      singleModelServingName,
+      displayName,
+    };
+  });
+}
