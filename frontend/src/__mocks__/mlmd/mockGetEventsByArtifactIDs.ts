537caadaa4aa099cd858d0ba953ce7e62665c28a
Purva Naik <pnaik@redhat.com>
2024-11-22 05:19:26 +0530

    Determine on the artifact view what execution produced the artifacts (#3470)

---
+++ frontend/src/__mocks__/mlmd/mockGetEventsByArtifactIDs.ts
@@ -0,0 +1,25 @@
+import { GetEventsByArtifactIDsResponse } from '~/__mocks__/third_party/mlmd';
+import createGrpcResponse, { GrpcResponse } from './utils';
+
+const mockedEventsByArtifactIdsResponse: GetEventsByArtifactIDsResponse = {
+  events: [
+    {
+      artifactId: 1,
+      executionId: 27,
+      path: {
+        steps: [
+          {
+            key: 'metrics',
+          },
+        ],
+      },
+      type: 4,
+      millisecondsSinceEpoch: 1712899529364,
+    },
+  ],
+};
+
+export const mockGetEventsByArtifactIDs = (): GrpcResponse => {
+  const binary = GetEventsByArtifactIDsResponse.encode(mockedEventsByArtifactIdsResponse).finish();
+  return createGrpcResponse(binary);
+};
