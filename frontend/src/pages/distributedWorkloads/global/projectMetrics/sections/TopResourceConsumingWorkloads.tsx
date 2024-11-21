f751bb8fbea6b568729718402ff32d069795bcae
Ashley McEntee <123661468+ashley-o0o@users.noreply.github.com>
2024-11-21 15:28:43 -0500

    Fix tooltip clipping on distributed workloads (#3494)

--- frontend/src/pages/distributedWorkloads/global/projectMetrics/sections/TopResourceConsumingWorkloads.tsx
+++ frontend/src/pages/distributedWorkloads/global/projectMetrics/sections/TopResourceConsumingWorkloads.tsx
@@ -61,7 +61,13 @@ const TopResourceConsumingWorkloadsChart: React.FC<TopResourceConsumingWorkloads
       <ChartDonut
         constrainToVisibleArea
         labelRadius={50}
-        labelComponent={<ChartTooltip center={{ x: 150, y: 0 }} />}
+        labelComponent={
+          <ChartTooltip
+            center={{ x: 225, y: 0 }}
+            constrainToVisibleArea
+            pointerOrientation="left"
+          />
+        }
         height={chartHeight}
         ariaTitle={`${metricLabel} chart`}
         data={
