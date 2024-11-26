c7dcbd74f1cc805095a1ddf8d42dae30bdecfe3c
manaswinidas <dasmanaswini10@gmail.com>
2024-11-26 22:07:35 +0530

    Add autofill modal and button for URI connections

--- frontend/src/pages/modelRegistry/screens/RegisterModel/ConnectionModal.tsx
+++ frontend/src/pages/modelRegistry/screens/RegisterModel/ConnectionModal.tsx
@@ -3,14 +3,18 @@ import { Button, FormGroup, HelperText, Form, FormHelperText } from '@patternfly
 import { Modal } from '@patternfly/react-core/deprecated';
 import ProjectSelector from '~/concepts/projects/ProjectSelector';
 import { DataConnection } from '~/pages/projects/types';
+import { Connection } from '~/concepts/connectionTypes/types';
 import { ConnectionDropdown } from './ConnectionDropdown';
+import { ModelLocationType } from './useRegisterModelData';
 
 export const ConnectionModal: React.FC<{
+  type: ModelLocationType;
   onClose: () => void;
-  onSubmit: (connection: DataConnection) => void;
-}> = ({ onClose, onSubmit }) => {
+  onSubmit: (connection: DataConnection | Connection) => void;
+}> = ({ type, onClose, onSubmit }) => {
   const [project, setProject] = React.useState<string | undefined>(undefined);
   const [connection, setConnection] = React.useState<DataConnection | undefined>(undefined);
+  const modelLocationType = type === ModelLocationType.ObjectStorage ? 'object storage' : 'URI';
 
   return (
     <Modal
@@ -18,7 +22,7 @@ export const ConnectionModal: React.FC<{
       data-testid="connection-autofill-modal"
       variant="medium"
       title="Autofill from data connection"
-      description="Select a project to list its object storage data connections. Select a data connection to autofill the model location."
+      description={`Select a project to list its ${modelLocationType} data connections. Select a data connection to autofill the model location.`}
       onClose={() => {
         setProject(undefined);
         setConnection(undefined);
@@ -62,7 +66,7 @@ export const ConnectionModal: React.FC<{
           />
           <FormHelperText>
             <HelperText>
-              Data connection list includes only object storage types that contain a bucket.
+              {`Data connection list includes only ${modelLocationType} types that contain a bucket.`}
             </HelperText>
           </FormHelperText>
         </FormGroup>
