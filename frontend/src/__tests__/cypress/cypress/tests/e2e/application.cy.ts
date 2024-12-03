9a1c6762723ce4564cdf81746b2589a35dfe43af
Fede Alonso <fealonso@redhat.com>
2024-12-03 10:48:59 +0100

    force failure on application.cy.ts

--- frontend/src/__tests__/cypress/cypress/tests/e2e/application.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/application.cy.ts
@@ -3,8 +3,6 @@ import { HTPASSWD_CLUSTER_ADMIN_USER } from '~/__tests__/cypress/cypress/utils/e
 describe('application', { testIsolation: false }, () => {
   it('should login and load page', () => {
     cy.visitWithLogin('/');
-    cy.findByRole('banner', { name: 'page masthead' }).contains(
-      HTPASSWD_CLUSTER_ADMIN_USER.USERNAME,
-    );
+    cy.findByRole('banner', { name: 'page masthead' }).contains('Force failure');
   });
 });
