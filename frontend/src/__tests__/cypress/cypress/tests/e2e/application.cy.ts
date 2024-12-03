8e55ff23222efa539c2d43f8df9b2eed57b091d6
Fede Alonso <fealonso@redhat.com>
2024-12-03 11:57:02 +0100

    fix application.cy.ts test

--- frontend/src/__tests__/cypress/cypress/tests/e2e/application.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/application.cy.ts
@@ -3,6 +3,8 @@ import { HTPASSWD_CLUSTER_ADMIN_USER } from '~/__tests__/cypress/cypress/utils/e
 describe('application', { testIsolation: false }, () => {
   it('should login and load page', () => {
     cy.visitWithLogin('/');
-    cy.findByRole('banner', { name: 'page masthead' }).contains('Force failure');
+    cy.findByRole('banner', { name: 'page masthead' }).contains(
+      HTPASSWD_CLUSTER_ADMIN_USER.USERNAME,
+    );
   });
 });
