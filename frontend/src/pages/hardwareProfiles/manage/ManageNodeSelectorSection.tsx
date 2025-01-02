9f0cc9dcdd86c04b58b32eebb35d19d012b6ed42
Juntao Wang <juntwang@redhat.com>
2025-01-02 16:35:39 -0500

    address UX feedbacks

--- frontend/src/pages/hardwareProfiles/manage/ManageNodeSelectorSection.tsx
+++ frontend/src/pages/hardwareProfiles/manage/ManageNodeSelectorSection.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { FormSection, Button } from '@patternfly/react-core';
+import { FormSection, Button, Flex, FlexItem } from '@patternfly/react-core';
 import { AddCircleOIcon } from '@patternfly/react-icons';
 import { NodeSelector } from '~/types';
 import ManageNodeSelectorModal from '~/pages/hardwareProfiles/nodeSelector/ManageNodeSelectorModal';
 import NodeSelectorTable from '~/pages/hardwareProfiles/nodeSelector/NodeSelectorTable';
+import { ManageHardwareProfileSectionTitles } from '~/pages/hardwareProfiles/const';
+import { ManageHardwareProfileSectionID } from '~/pages/hardwareProfiles/manage/types';
 
 type ManageNodeSelectorSectionProps = {
   nodeSelectors: NodeSelector[];
@@ -15,9 +17,29 @@ const ManageNodeSelectorSection: React.FC<ManageNodeSelectorSectionProps> = ({
   setNodeSelectors,
 }) => {
   const [isNodeSelectorModalOpen, setIsNodeSelectorModalOpen] = React.useState<boolean>(false);
+  const isEmpty = nodeSelectors.length === 0;
   return (
     <>
-      <FormSection title="Node resources">
+      <FormSection
+        title={
+          <Flex>
+            <FlexItem>
+              {ManageHardwareProfileSectionTitles[ManageHardwareProfileSectionID.NODE_SELECTORS]}
+            </FlexItem>
+            {!isEmpty && (
+              <FlexItem>
+                <Button
+                  variant="secondary"
+                  onClick={() => setIsNodeSelectorModalOpen(true)}
+                  data-testid="add-node-selector-button"
+                >
+                  Add node selector
+                </Button>
+              </FlexItem>
+            )}
+          </Flex>
+        }
+      >
         Node selectors are added to a pod spec to allow the pod to be scheduled on nodes with
         matching labels.
         {nodeSelectors.length !== 0 && (
@@ -33,15 +55,17 @@ const ManageNodeSelectorSection: React.FC<ManageNodeSelectorSectionProps> = ({
           onSave={(nodeSelector) => setNodeSelectors([...nodeSelectors, nodeSelector])}
         />
       )}
-      <Button
-        isInline
-        icon={<AddCircleOIcon />}
-        variant="link"
-        onClick={() => setIsNodeSelectorModalOpen(true)}
-        data-testid="add-node-selector-button"
-      >
-        Add node selector
-      </Button>
+      {isEmpty && (
+        <Button
+          isInline
+          icon={<AddCircleOIcon />}
+          variant="link"
+          onClick={() => setIsNodeSelectorModalOpen(true)}
+          data-testid="add-node-selector-button"
+        >
+          Add node selector
+        </Button>
+      )}
     </>
   );
 };
