0f2b4f86d856392d09d85a7db6526c7301db3242
Kevin Postlethwait <k.postlethwait24@gmail.com>
2025-01-02 17:28:59 -0500

    use all clusterqueues (#3540)
    * use all clusterqueues and filter by availability in ns
    
    Signed-off-by: Kevin <kpostlet@redhat.com>
    
    * do not filter
    
    Signed-off-by: Kevin <kpostlet@redhat.com>
    
    * fix styling and existing check for no CQ
    
    Signed-off-by: Kevin <kpostlet@redhat.com>
    
    * remove comment
    
    Signed-off-by: Kevin <kpostlet@redhat.com>
    
    * filter available CQs
    
    Signed-off-by: Kevin <kpostlet@redhat.com>
    
    * use suggested filter strategy
    
    Signed-off-by: Kevin <kpostlet@redhat.com>
    
    * use suggested flatmap
    
    Signed-off-by: Kevin <kpostlet@redhat.com>
    
    * use correct name for cq name
    
    Signed-off-by: Kevin <kpostlet@redhat.com>
    
    * use cqExists rather than filtered list
    
    Signed-off-by: Kevin <kpostlet@redhat.com>
    
    * fix conditional
    
    Signed-off-by: Kevin <kpostlet@redhat.com>
    
    ---------
    
    Signed-off-by: Kevin <kpostlet@redhat.com>

--- frontend/src/pages/distributedWorkloads/global/projectMetrics/sections/RequestedResources.tsx
+++ frontend/src/pages/distributedWorkloads/global/projectMetrics/sections/RequestedResources.tsx
@@ -11,8 +11,8 @@ import { LoadingState } from '~/pages/distributedWorkloads/components/LoadingSta
 import { RequestedResourcesBulletChart } from './RequestedResourcesBulletChart';
 
 export const RequestedResources: React.FC = () => {
-  const { localQueues, clusterQueue } = React.useContext(DistributedWorkloadsContext);
-  const requiredFetches = [localQueues, clusterQueue];
+  const { localQueues, clusterQueues } = React.useContext(DistributedWorkloadsContext);
+  const requiredFetches = [localQueues, clusterQueues];
   const error = requiredFetches.find((f) => !!f.error)?.error;
   const loaded = requiredFetches.every((f) => f.loaded);
 
@@ -25,8 +25,8 @@ export const RequestedResources: React.FC = () => {
   }
 
   const requestedByThisProject = getQueueRequestedResources(localQueues.data);
-  const requestedByAllProjects = getQueueRequestedResources([clusterQueue.data]);
-  const totalSharedQuota = getTotalSharedQuota(clusterQueue.data);
+  const requestedByAllProjects = getQueueRequestedResources(clusterQueues.data);
+  const totalSharedQuota = getTotalSharedQuota(clusterQueues.data);
 
   return (
     <CardBody>
