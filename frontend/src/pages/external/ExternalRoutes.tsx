113e8c256138f69e1860c5d138f4a5bff8aeda73
Gage Krumbach <gkrumbach@gmail.com>
2025-01-06 12:55:24 -0600

    quick comment

--- frontend/src/pages/external/ExternalRoutes.tsx
+++ frontend/src/pages/external/ExternalRoutes.tsx
@@ -3,6 +3,9 @@ import { Route, Routes } from 'react-router-dom';
 import PipelinesSdkRedirect from './redirectComponents/PipelinesSdkRedirects';
 import ExternalRedirectNotFound from './redirectComponents/ExternalRedirectNotFound';
 
+/**
+ * Be sure to keep this file in sync with the documentation in `docs/external-redirects.md`.
+ */
 const ExternalRoutes: React.FC = () => (
   <Routes>
     <Route path="/pipelinesSdk/:namespace/*" element={<PipelinesSdkRedirect />} />
