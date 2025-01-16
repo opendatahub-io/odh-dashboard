5a3d408a8470235b5a0dd8581ad38aa4a60ecb12
emilys314 <esamoylo@redhat.com>
2025-01-16 14:22:28 -0500

    Read and show default deployment mode in deploy modal

--- frontend/src/pages/modelServing/screens/projects/kServeModal/KServeDeploymentModeDropdown.tsx
+++ frontend/src/pages/modelServing/screens/projects/kServeModal/KServeDeploymentModeDropdown.tsx
@@ -2,22 +2,8 @@ import { FormGroup, Icon, Popover } from '@patternfly/react-core';
 import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
 import React from 'react';
 import SimpleSelect, { SimpleSelectOption } from '~/components/SimpleSelect';
-
-enum DeploymentMode {
-  Advanced = 'advanced',
-  Standard = 'standard',
-}
-
-const options: SimpleSelectOption[] = [
-  {
-    label: 'Standard',
-    key: DeploymentMode.Standard,
-  },
-  {
-    label: 'Advanced (default)',
-    key: DeploymentMode.Advanced,
-  },
-];
+import { DeploymentMode } from '~/k8sTypes';
+import { useDefaultDeploymentMode } from '~/pages/modelServing/useDefaultDeploymentMode';
 
 type Props = {
   isRaw: boolean;
@@ -25,28 +11,48 @@ type Props = {
   isDisabled?: boolean;
 };
 
-export const KServeDeploymentModeDropdown: React.FC<Props> = ({ isRaw, setIsRaw, isDisabled }) => (
-  <FormGroup
-    label="Deployment mode"
-    fieldId="deployment-mode"
-    isRequired
-    labelHelp={
-      <Popover bodyContent="Deployment modes define which technology stack will be used to deploy a model, offering different levels of management and scalability.">
-        <Icon aria-label="Model server replicas info" role="button">
-          <OutlinedQuestionCircleIcon />
-        </Icon>
-      </Popover>
-    }
-  >
-    <SimpleSelect
-      dataTestId="deployment-mode-select"
-      isDisabled={isDisabled}
-      value={isRaw ? DeploymentMode.Standard : DeploymentMode.Advanced}
-      options={options}
-      onChange={(key) => {
-        setIsRaw(key === DeploymentMode.Standard);
-      }}
-      isFullWidth
-    />
-  </FormGroup>
-);
+export const KServeDeploymentModeDropdown: React.FC<Props> = ({ isRaw, setIsRaw, isDisabled }) => {
+  const defaultDeploymentMode = useDefaultDeploymentMode();
+
+  const options: SimpleSelectOption[] = React.useMemo(
+    () => [
+      {
+        label: `Standard${
+          defaultDeploymentMode === DeploymentMode.RawDeployment ? ' (default)' : ''
+        }`,
+        key: DeploymentMode.RawDeployment,
+      },
+      {
+        label: `Advanced${defaultDeploymentMode === DeploymentMode.Serverless ? ' (default)' : ''}`,
+        key: DeploymentMode.Serverless,
+      },
+    ],
+    [defaultDeploymentMode],
+  );
+
+  return (
+    <FormGroup
+      label="Deployment mode"
+      fieldId="deployment-mode"
+      isRequired
+      labelHelp={
+        <Popover bodyContent="Deployment modes define which technology stack will be used to deploy a model, offering different levels of management and scalability.">
+          <Icon aria-label="Model server replicas info" role="button">
+            <OutlinedQuestionCircleIcon />
+          </Icon>
+        </Popover>
+      }
+    >
+      <SimpleSelect
+        dataTestId="deployment-mode-select"
+        isDisabled={isDisabled}
+        value={isRaw ? DeploymentMode.RawDeployment : DeploymentMode.Serverless}
+        options={options}
+        onChange={(key) => {
+          setIsRaw(key === DeploymentMode.RawDeployment);
+        }}
+        isFullWidth
+      />
+    </FormGroup>
+  );
+};
