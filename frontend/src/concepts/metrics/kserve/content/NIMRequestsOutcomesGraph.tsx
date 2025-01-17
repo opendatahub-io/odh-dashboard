b263b5714761f072ad82bf12063bcfef8bfc3660
Linoy Bitan <lbitan@redhat.com>
2025-01-17 16:44:25 +0200

    Implemented visualizations for the NIM metrics (#3612)
    * added metrics graphs for the new NIM integration
    
    Signed-off-by: LinoyBitan1 <lbitan@redhat.com>
    
    * added tests for the NIM integration tests
    
    Signed-off-by: LinoyBitan1 <lbitan@redhat.com>
    
    * added default value to nim flag
    
    * apply lint fixes and additional tests
    
    * fix before nim tab issue
    
    * remove unuse line
    
    * fix the nim tab broblem
    
    * all but tests
    
    * Update mockProjectK8sResource.ts Tomer Olga PR
    
    * Update modelMetrics.ts Tomer Olga PR
    
    * Update modelMetrics.cy.ts Tomer Olga PR
    
    * Update NimTab.tsx Tomer Olga PR
    
    * tests updates
    
    * updated mocks for tests
    
    * deleted comments
    
    * added changes for thr PR
    
    * change nim flag
    
    * fixes for lint tests
    
    * fix: remove comments and setting disableNIMModelServing to true in the initIntercepts
    
    Signed-off-by: Olga Lavtar <olavtar@redhat.com>
    
    ---------
    
    Signed-off-by: LinoyBitan1 <lbitan@redhat.com>
    Signed-off-by: Olga Lavtar <olavtar@redhat.com>
    Co-authored-by: mtalvi <mtalvi@redhat.com>
    Co-authored-by: Olga Lavtar <olavtar@redhat.com>

---
+++ frontend/src/concepts/metrics/kserve/content/NIMRequestsOutcomesGraph.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { NimMetricGraphDefinition } from '~/concepts/metrics/kserve/types';
+import { useFetchNimRequestsOutcomesData } from '~/api';
+import MetricsChart from '~/pages/modelServing/screens/metrics/MetricsChart';
+import { TimeframeTitle } from '~/concepts/metrics/types';
+import { MetricsChartTypes } from '~/pages/modelServing/screens/metrics/types';
+
+type NimRequestsOutcomesGraphProps = {
+  graphDefinition: NimMetricGraphDefinition;
+  timeframe: TimeframeTitle;
+  end: number;
+  namespace: string;
+};
+
+const NimRequestsOutcomesGraph: React.FC<NimRequestsOutcomesGraphProps> = ({
+  graphDefinition,
+  timeframe,
+  end,
+  namespace,
+}) => {
+  const {
+    data: { successCount, failedCount },
+  } = useFetchNimRequestsOutcomesData(graphDefinition, timeframe, end, namespace);
+
+  return (
+    <MetricsChart
+      metrics={[
+        ...(graphDefinition.queries[0]
+          ? [
+              {
+                name: `Successful`,
+                metric: successCount,
+              },
+            ]
+          : []),
+        ...(graphDefinition.queries[1]
+          ? [
+              {
+                name: `Failed`,
+                metric: failedCount,
+              },
+            ]
+          : []),
+      ]}
+      color="blue"
+      title={graphDefinition.title}
+      isStack
+      type={MetricsChartTypes.DONUT}
+    />
+  );
+};
+
+export default NimRequestsOutcomesGraph;
---
+++ frontend/src/concepts/metrics/kserve/content/NIMTokensCountGraph.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { NimMetricGraphDefinition } from '~/concepts/metrics/kserve/types';
+import { TimeframeTitle } from '~/concepts/metrics/types';
+import { useFetchNimTokensCountData } from '~/api';
+import MetricsChart from '~/pages/modelServing/screens/metrics/MetricsChart';
+import { MetricsChartTypes } from '~/pages/modelServing/screens/metrics/types';
+import { convertPrometheusNaNToZero } from '~/pages/modelServing/screens/metrics/utils';
+
+// Graph #3 - Total Prompt Token Count and Total Generation Token Count
+type NimTokensCountGraphProps = {
+  graphDefinition: NimMetricGraphDefinition;
+  timeframe: TimeframeTitle;
+  end: number;
+  namespace: string;
+};
+
+const NimTokensCountGraph: React.FC<NimTokensCountGraphProps> = ({
+  graphDefinition,
+  timeframe,
+  end,
+  namespace,
+}) => {
+  const {
+    data: { totalPromptTokenCount, totalGenerationTokenCount },
+  } = useFetchNimTokensCountData(graphDefinition, timeframe, end, namespace);
+
+  return (
+    <MetricsChart
+      title={graphDefinition.title}
+      metrics={[
+        ...(graphDefinition.queries[0]
+          ? [
+              {
+                name: graphDefinition.queries[0].title, // "Total Prompt Token Count"
+                metric: {
+                  ...totalPromptTokenCount,
+                  data: convertPrometheusNaNToZero(totalPromptTokenCount.data),
+                },
+              },
+            ]
+          : []),
+        ...(graphDefinition.queries[1]
+          ? [
+              {
+                name: graphDefinition.queries[1].title, // "Total Generation Token Count"
+                metric: {
+                  ...totalGenerationTokenCount,
+                  data: convertPrometheusNaNToZero(totalGenerationTokenCount.data),
+                },
+              },
+            ]
+          : []),
+      ]}
+      type={MetricsChartTypes.LINE}
+    />
+  );
+};
+
+export default NimTokensCountGraph;
