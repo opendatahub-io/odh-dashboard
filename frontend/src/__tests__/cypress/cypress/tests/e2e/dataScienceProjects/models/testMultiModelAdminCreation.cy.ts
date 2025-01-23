a30c1b8794a0eb4fdda6115ab017db1669d44cf1
Anthony Coughlin <acoughli@redhat.com>
2025-01-23 10:42:06 +0000

    Fixed linting

--- frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/models/testMultiModelAdminCreation.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/models/testMultiModelAdminCreation.cy.ts
@@ -57,7 +57,17 @@ describe('[Known Bugs: RHOAIENG-18579,RHOAIENG-18425] Verify Admin Multi Model C
 
   it(
     'Verify that an Admin can Serve, Query a Multi Model using both the UI and External links',
-    { tags: ['@Smoke', '@SmokeSet3', '@ODS-2053', '@ODS-2054', '@Dashboard', '@Modelserving', '@Bug'] },
+    {
+      tags: [
+        '@Smoke',
+        '@SmokeSet3',
+        '@ODS-2053',
+        '@ODS-2054',
+        '@Dashboard',
+        '@Modelserving',
+        '@Bug',
+      ],
+    },
     () => {
       cy.log('Model Name:', modelName);
       // Authentication and navigation
