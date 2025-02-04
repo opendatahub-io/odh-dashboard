cbf7eb820f3ce101226fefd226a0f541b39e9b74
gitdallas <5322142+gitdallas@users.noreply.github.com>
2025-02-04 10:12:57 -0600

    change cypress test tags
    Signed-off-by: gitdallas <5322142+gitdallas@users.noreply.github.com>

--- frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/testProjectAdminPermissions.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/testProjectAdminPermissions.cy.ts
@@ -41,17 +41,7 @@ describe('Verify that users can provide admin project permissions to non-admin u
   it(
     'Verify that user can be added as an Admin for a Project',
     {
-      tags: [
-        '@Smoke',
-        '@SmokeSet1',
-        '@ODS-2194',
-        '@ODS-2201',
-        '@Tier1',
-        '@Sanity',
-        '@SanitySet1',
-        '@ODS-2208',
-        '@Dashboard',
-      ],
+      tags: ['@Smoke', '@SmokeSet1', '@ODS-2194', '@ODS-2201', '@ODS-2208', '@Dashboard'],
     },
     () => {
       // Authentication and navigation
@@ -87,17 +77,7 @@ describe('Verify that users can provide admin project permissions to non-admin u
   it(
     'Verify user can assign access permissions to user group',
     {
-      tags: [
-        '@Smoke',
-        '@SmokeSet1',
-        '@ODS-2194',
-        '@ODS-2201',
-        '@Tier1',
-        '@Sanity',
-        '@SanitySet1',
-        '@ODS-2208',
-        '@Dashboard',
-      ],
+      tags: ['@Smoke', '@SmokeSet1', '@ODS-2194', '@ODS-2201', '@ODS-2208', '@Dashboard'],
     },
     () => {
       // Authentication and navigation
@@ -127,17 +107,7 @@ describe('Verify that users can provide admin project permissions to non-admin u
   it(
     'Verify that user can access the created project with Admin rights',
     {
-      tags: [
-        '@Smoke',
-        '@SmokeSet1',
-        '@ODS-2194',
-        '@ODS-2201',
-        '@Tier1',
-        '@Sanity',
-        '@SanitySet1',
-        '@ODS-2208',
-        '@Dashboard',
-      ],
+      tags: ['@Smoke', '@SmokeSet1', '@ODS-2194', '@ODS-2201', '@ODS-2208', '@Dashboard'],
     },
     () => {
       // Authentication and navigation
