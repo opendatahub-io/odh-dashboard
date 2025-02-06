8396094d36f028749cefe85097a099de9764455d
ppadti <ppadti@redhat.com>
2025-02-06 13:30:45 +0530

    Update ModelCatalog route

--- frontend/src/pages/modelCatalog/ModelCatalogRoutes.tsx
+++ frontend/src/pages/modelCatalog/ModelCatalogRoutes.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import ModelCatalogCoreLoader from './ModelCatalogCoreLoader';
 import ModelDetailsPage from './ModelDetailsPage';
+import ModelCatalog from './screens/ModelCatalog';
 
 const ModelCatalogRoutes: React.FC = () => (
   <Routes>
-    <Route path={'/:modelCatalog?/*'} element={<ModelCatalogCoreLoader />} />
-    <Route path="tempDetails" element={<ModelDetailsPage />} />
-    <Route path="*" element={<Navigate to="." />} />
+    <Route path={'/:modelCatalogSource?/*'} element={<ModelCatalogCoreLoader />}>
+      <Route index element={<ModelCatalog />} />
+      <Route path="tempDetails" element={<ModelDetailsPage />} />
+      <Route path="*" element={<Navigate to="." />} />
+    </Route>
   </Routes>
 );
 
