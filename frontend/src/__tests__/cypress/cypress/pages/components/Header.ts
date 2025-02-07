dfffa17ba2e34e4e92323b353282c948d6522143
Anthony Coughlin <acoughli@redhat.com>
2025-02-07 10:33:30 +0000

    Comitting working test

--- frontend/src/__tests__/cypress/cypress/pages/components/Header.ts
+++ frontend/src/__tests__/cypress/cypress/pages/components/Header.ts
@@ -2,6 +2,10 @@ export class Header {
   findApplicationLauncher(): Cypress.Chainable<JQuery<HTMLElement>> {
     return cy.get('[data-testid="application-launcher"]');
   }
+
+  findMenuDropdown(): Cypress.Chainable<JQuery<HTMLElement>> {
+    return cy.get('#user-menu-toggle');
+  }
 }
 
 export const header = new Header();
