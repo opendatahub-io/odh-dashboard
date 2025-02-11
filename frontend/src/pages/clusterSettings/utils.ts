cf893695b4a9ffda97f90ed2c48c17597f1661dd
Gage Krumbach <gkrumbach@gmail.com>
2025-02-11 08:07:55 -0600

    Add TODO for removing safeExecute in InstructLab feature check

--- frontend/src/pages/clusterSettings/utils.ts
+++ frontend/src/pages/clusterSettings/utils.ts
@@ -1,6 +1,7 @@
 import { DataScienceClusterKind } from '~/k8sTypes';
 import { safeExecute } from '~/utilities/utils';
 
+// TODO: Remove safeExecute usage in https://issues.redhat.com/browse/RHOAIENG-19825
 export const isInstructLabEnabled = (dsc: DataScienceClusterKind | null): boolean =>
   safeExecute(
     () =>
