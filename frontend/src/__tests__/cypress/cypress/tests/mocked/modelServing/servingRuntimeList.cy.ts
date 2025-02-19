737781f0e0a20abdd022a1a2ac0e582f50b9b281
Emily Samoylov <93456589+emilys314@users.noreply.github.com>
2025-02-19 14:53:38 -0500

    Check if deployment is raw for showing token (#3771)

--- frontend/src/__tests__/cypress/cypress/tests/mocked/modelServing/servingRuntimeList.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/mocked/modelServing/servingRuntimeList.cy.ts
@@ -2368,6 +2368,30 @@ describe('Serving Runtime List', () => {
       kserveRow.findToggleButton().click();
       kserveRow.findDescriptionListItem('Token authentication').should('not.exist');
     });
+
+    it('Check token section is always available for kserve raw', () => {
+      initIntercepts({
+        projectEnableModelMesh: false,
+        disableKServeConfig: false,
+        disableModelMeshConfig: true,
+        disableAccelerator: true,
+        disableKServeRaw: false,
+        inferenceServices: [
+          mockInferenceServiceK8sResource({
+            name: 'llama-caikit',
+            displayName: 'Llama Caikit',
+            url: 'http://llama-caikit.test-project.svc.cluster.local',
+            activeModelState: 'Loaded',
+            isKserveRaw: true,
+          }),
+        ],
+      });
+      projectDetails.visitSection('test-project', 'model-server');
+      const kserveRow = modelServingSection.getKServeRow('Llama Caikit');
+      kserveRow.findExpansion().should(be.collapsed);
+      kserveRow.findToggleButton().click();
+      kserveRow.findDescriptionListItem('Token authentication').should('exist');
+    });
   });
 
   describe('Dry run check', () => {
