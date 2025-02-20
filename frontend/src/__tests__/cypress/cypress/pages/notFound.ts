266f71d3e9822b8c8b74f413377489c883ddde51
Yulia Krimerman <juliapiterova@hotmail.com>
2025-02-20 12:33:13 -0500

    saving

---
+++ frontend/src/__tests__/cypress/cypress/pages/notFound.ts
@@ -0,0 +1,5 @@
+export const notFoundPage = {
+  getNotFoundPage: () => cy.get('[data-testid="not-found-page"]'),
+  getDescription: () => cy.get('[data-testid="not-found-page-description"]'),
+  getHomeButton: () => cy.get('[data-testid="home-page-button"]'),
+}; 
