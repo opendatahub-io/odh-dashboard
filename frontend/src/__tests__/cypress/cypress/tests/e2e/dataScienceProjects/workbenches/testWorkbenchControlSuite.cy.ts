c42453d99e3b02074931fdce22a4f7eec8450b1f
Anthony Coughlin <acoughli@redhat.com>
2025-02-24 12:52:43 +0000

    Linting fix

--- frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/workbenches/testWorkbenchControlSuite.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/workbenches/testWorkbenchControlSuite.cy.ts
@@ -53,7 +53,15 @@ describe('Start, Stop, Launch and Delete a Workbench in RHOAI', () => {
   it(
     'Starting, Stopping, Launching and Deleting a Workbench',
     {
-      tags: ['@Sanity', '@SanitySet2', '@ODS-1818', '@ODS-1823', '@ODS-1975', '@Dashboard', '@Workbenches'],
+      tags: [
+        '@Sanity',
+        '@SanitySet2',
+        '@ODS-1818',
+        '@ODS-1823',
+        '@ODS-1975',
+        '@Dashboard',
+        '@Workbenches',
+      ],
     },
     () => {
       const workbenchName = controlSuiteTestNamespace.replace('dsp-', '');
@@ -109,7 +117,15 @@ describe('Start, Stop, Launch and Delete a Workbench in RHOAI', () => {
   it(
     'Verify that a Workbench can be started and stopped using the Event log controls',
     {
-      tags: ['@Sanity', '@SanitySet2', '@ODS-1818', '@ODS-1823', '@ODS-1975', '@Dashboard', '@Workbenches'],
+      tags: [
+        '@Sanity',
+        '@SanitySet2',
+        '@ODS-1818',
+        '@ODS-1823',
+        '@ODS-1975',
+        '@Dashboard',
+        '@Workbenches',
+      ],
     },
     () => {
       const workbenchName = controlSuiteTestNamespace.replace('dsp-', 'secondwb-');
