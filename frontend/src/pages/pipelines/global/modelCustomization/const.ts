38b07515fb6204552e8c746494a58206ea6a2746
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2025-02-24 10:13:32 -0300

    Add base model section to ilab form (#3784)
    * Add base model section to ilab form
    
    * Code review changes

--- frontend/src/pages/pipelines/global/modelCustomization/const.ts
+++ frontend/src/pages/pipelines/global/modelCustomization/const.ts
@@ -1,9 +1,13 @@
 export enum FineTunePageSections {
   PROJECT_DETAILS = 'fine-tune-section-project-details',
+  BASE_MODEL = 'fine-tune-section-base-model',
 }
 
 export const fineTunePageSectionTitles: Record<FineTunePageSections, string> = {
   [FineTunePageSections.PROJECT_DETAILS]: 'Project details',
+  [FineTunePageSections.BASE_MODEL]: 'Base model',
 };
 
 export const ILAB_PIPELINE_NAME = 'instructlab';
+
+export const BASE_MODEL_INPUT_STORAGE_LOCATION_URI_KEY = 'baseModelInputStorageLocationUri';
