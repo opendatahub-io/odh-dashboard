762cb4f9bf852a831966e717bc9d27119ba73194
condawrangler <condawrangler@protonmail.com>
2025-02-26 15:11:58 -0500

    NIM: remove nim card

--- frontend/src/__tests__/cypress/cypress/utils/oc_commands/baseCommands.ts
+++ frontend/src/__tests__/cypress/cypress/utils/oc_commands/baseCommands.ts
@@ -53,6 +53,42 @@ export const patchOpenShiftResource = (
 };
 
 /**
+ * Patches an OpenShift resource using the `oc patch` command.
+ *
+ * @param resourceType The type of resource to patch (e.g., 'storageclass')
+ * @param resourceName The name of the resource to patch
+ * @param patchContent The patch content as a JSON string
+ * @param namespace The namespace of the resource to patch, default is 'default'
+ * @param type The type of the resource to patch, default is json
+ * @returns Cypress Chainable
+ */
+export const patchOpenShiftNSResource = (
+  resourceType: string,
+  resourceName: string,
+  patchContent: string,
+  namespace: string = "default",
+  type: string = "json",
+): Cypress.Chainable<CommandLineResult> => {
+  // Escape single quotes in the patch content
+  const escapedPatchContent = patchContent.replace(/'/g, "'\\''");
+
+  const ocCommand = `oc -n ${namespace} patch ${resourceType} ${resourceName} --type=${type} -p '${escapedPatchContent}'`;
+
+  cy.log(ocCommand);
+
+  return cy.exec(ocCommand, { failOnNonZeroExit: false }).then((result: CommandLineResult) => {
+    if (result.code !== 0) {
+      // If there is an error, log the error and fail the test
+      cy.log(`ERROR patching ${resourceType} ${resourceName}
+              stdout: ${result.stdout}
+              stderr: ${result.stderr}`);
+      throw new Error(`Command failed with code ${result.code}`);
+    }
+    return result;
+  });
+};
+
+/**
  * Wait for a specific pod to become ready across all namespaces.
  *
  * @param podNameContains A substring to partially match against the pod name (e.g., "jupyter-nb").
