e3b27a0750b7ca29ca9b4babf3b5bbb4b33484d7
Juntao Wang <37624318+DaoDaoNoCode@users.noreply.github.com>
2025-02-27 10:50:53 -0500

    Add teacher/judge section to ilab form (#3777)
    * Add teacher/judge section to ilab form
    
    * Resolve conflicts, address feedback
    
    * update zod validation for URL
    
    * Address comments
    
    * address feedback
    
    * change run name

--- frontend/src/pages/pipelines/global/modelCustomization/const.ts
+++ frontend/src/pages/pipelines/global/modelCustomization/const.ts
@@ -1,11 +1,15 @@
 export enum FineTunePageSections {
   PROJECT_DETAILS = 'fine-tune-section-project-details',
   BASE_MODEL = 'fine-tune-section-base-model',
+  TEACHER_MODEL = 'fine-tune-section-teacher-model',
+  JUDGE_MODEL = 'fine-tune-section-judge-model',
 }
 
 export const fineTunePageSectionTitles: Record<FineTunePageSections, string> = {
   [FineTunePageSections.PROJECT_DETAILS]: 'Project details',
   [FineTunePageSections.BASE_MODEL]: 'Base model',
+  [FineTunePageSections.TEACHER_MODEL]: 'Teacher model',
+  [FineTunePageSections.JUDGE_MODEL]: 'Judge model',
 };
 
 export const ILAB_PIPELINE_NAME = 'instructlab';
---
+++ frontend/src/pages/pipelines/global/modelCustomization/teacherJudgeSection/TeacherJudgeInputComponents.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { FormGroup, TextInput } from '@patternfly/react-core';
+import PasswordInput from '~/components/PasswordInput';
+
+type TeacherJudgeInputBaseProps = TeacherJudgeInputProps & {
+  label: string;
+  fieldId: string;
+  isPasswordType?: boolean;
+};
+
+type TeacherJudgeInputProps = {
+  value: string;
+  setValue: (value: string) => void;
+};
+
+export const TeacherEndpointInput: React.FC<TeacherJudgeInputProps> = ({ value, setValue }) => (
+  <TeacherJudgeInputBase
+    label="Teacher endpoint"
+    fieldId="teacher-endpoint-input"
+    value={value}
+    setValue={setValue}
+  />
+);
+
+export const TeacherModelNameInput: React.FC<TeacherJudgeInputProps> = ({ value, setValue }) => (
+  <TeacherJudgeInputBase
+    label="Model name"
+    fieldId="teacher-model-name-input"
+    value={value}
+    setValue={setValue}
+  />
+);
+
+export const TeacherTokenInput: React.FC<TeacherJudgeInputProps> = ({ value, setValue }) => (
+  <TeacherJudgeInputBase
+    label="Token"
+    fieldId="teacher-token-input"
+    value={value}
+    setValue={setValue}
+    isPasswordType
+  />
+);
+
+export const JudgeEndpointInput: React.FC<TeacherJudgeInputProps> = ({ value, setValue }) => (
+  <TeacherJudgeInputBase
+    label="Judge endpoint"
+    fieldId="judge-endpoint-input"
+    value={value}
+    setValue={setValue}
+  />
+);
+
+export const JudgeModelNameInput: React.FC<TeacherJudgeInputProps> = ({ value, setValue }) => (
+  <TeacherJudgeInputBase
+    label="Model name"
+    fieldId="judge-model-name-input"
+    value={value}
+    setValue={setValue}
+  />
+);
+
+export const JudgeTokenInput: React.FC<TeacherJudgeInputProps> = ({ value, setValue }) => (
+  <TeacherJudgeInputBase
+    label="Token"
+    fieldId="judge-token-input"
+    value={value}
+    setValue={setValue}
+    isPasswordType
+  />
+);
+
+const TeacherJudgeInputBase: React.FC<TeacherJudgeInputBaseProps> = ({
+  label,
+  fieldId,
+  value,
+  setValue,
+  isPasswordType,
+}) => (
+  <>
+    <FormGroup isRequired label={label} fieldId={fieldId}>
+      {isPasswordType ? (
+        <PasswordInput
+          isRequired
+          type="password"
+          id={fieldId}
+          name={fieldId}
+          value={value}
+          data-testid={fieldId}
+          onChange={(_, newValue) => setValue(newValue)}
+        />
+      ) : (
+        <TextInput
+          value={value}
+          isRequired
+          id={fieldId}
+          name={fieldId}
+          data-testid={fieldId}
+          onChange={(_, newValue) => setValue(newValue)}
+        />
+      )}
+    </FormGroup>
+  </>
+);
