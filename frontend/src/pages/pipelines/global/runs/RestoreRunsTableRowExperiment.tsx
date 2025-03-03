opendatahub-io/odh-landing-page

allow to restore archived runs whose associate experiments are archived under all runs nav

frontend/src/pages/pipelines/global/runs/RestoreRunsTableRowExperiment.tsx
@@ -1,0 +1,53 @@
+import { Flex, FlexItem, Label } from '@patternfly/react-core';
+import React from 'react';
+import { TableText } from '@patternfly/react-table';
+import { ExperimentKF } from '~/concepts/pipelines/kfTypes';
+import { ExperimentStatus } from './types';
+import { RestoreRunsStatusIcon } from './RestoreRunsStatusIcon';
+
+type RestoreRunsTableRowExperimentProps = {
+  experimentStatus: ExperimentStatus[];
+  archivedExperiments: ExperimentKF[];
+  currentExperiment: ExperimentKF;
+  isSubmitting: boolean;
+};
+
+const RestoreRunsTableRowExperiment: React.FC<RestoreRunsTableRowExperimentProps> = ({
+  experimentStatus,
+  archivedExperiments,
+  currentExperiment,
+  isSubmitting,
+}) => {
+  const matchingExperiment = experimentStatus.find(
+    (experiment) => experiment.experiment.experiment_id === currentExperiment.experiment_id,
+  );
+
+  const isExperimentArchived = !!archivedExperiments.find(
+    (exp) => exp.experiment_id === currentExperiment.experiment_id,
+  );
+  return (
+    <Flex gap={{ default: 'gapSm' }}>
+      <FlexItem>
+        <TableText>
+          {isExperimentArchived && (
+            <RestoreRunsStatusIcon
+              status={matchingExperiment?.status}
+              isSubmitting={isSubmitting}
+            />
+          )}{' '}
+          {currentExperiment.display_name}
+        </TableText>
+      </FlexItem>
+
+      {archivedExperiments.includes(currentExperiment) && (
+        <FlexItem>
+          <Label isCompact variant="outline" color="blue">
+            Will be restored
+          </Label>
+        </FlexItem>
+      )}
+    </Flex>
+  );
+};
+
+export default RestoreRunsTableRowExperiment;

frontend/src/pages/pipelines/global/runs/RunsWithArchivedExperimentTable.tsx
@@ -1,0 +1,81 @@
+import * as React from 'react';
+import { Table, TableText, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
+import { Content, Stack, StackItem } from '@patternfly/react-core';
+import { PipelineRunExperimentsContext } from '~/pages/pipelines/global/runs/PipelineRunExperimentsContext';
+import { ExperimentKF, PipelineRunKF } from '~/concepts/pipelines/kfTypes';
+import { getGroupRunsByExperiment } from './utils';
+import { ExperimentStatus, PipelineResourceRestoreResult } from './types';
+import RestoreRunsTableRowExperiment from './RestoreRunsTableRowExperiment';
+import { RestoreRunsStatusIcon } from './RestoreRunsStatusIcon';
+
+type RunsWithArchivedExperimentProps = {
+  runs: PipelineRunKF[];
+  archivedExperiments: ExperimentKF[];
+  isSubmitting: boolean;
+  runStatus: { run: PipelineRunKF; status: PipelineResourceRestoreResult }[];
+  experimentStatus: ExperimentStatus[];
+};
+
+const RunsWithArchivedExperimentTable: React.FC<RunsWithArchivedExperimentProps> = ({
+  runs,
+  archivedExperiments,
+  runStatus,
+  isSubmitting,
+  experimentStatus,
+}) => {
+  const { experiments: allExperiments } = React.useContext(PipelineRunExperimentsContext);
+  const groupedRuns = getGroupRunsByExperiment(runs, allExperiments);
+
+  return (
+    <Table variant="compact">
+      <Thead noWrap>
+        <Tr>
+          <Th>Run</Th>
+          <Th>Experiment</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {groupedRuns.map(({ experiment, run }, i) => (
+          <Tr key={i}>
+            <Td dataLabel="Runs" modifier="truncate">
+              <Stack hasGutter>
+                {run.map((singleRun, index) => {
+                  const matchingRun = runStatus.find(
+                    (currentRun) => currentRun.run.run_id === singleRun.run_id,
+                  );
+
+                  return (
+                    <StackItem key={index}>
+                      <TableText>
+                        <RestoreRunsStatusIcon
+                          status={matchingRun?.status}
+                          isSubmitting={isSubmitting}
+                        />{' '}
+                        {singleRun.display_name}
+                      </TableText>
+                      <Content component="small">{singleRun.description}</Content>
+                    </StackItem>
+                  );
+                })}
+              </Stack>
+            </Td>
+            <Td
+              dataLabel="Experiment"
+              modifier="truncate"
+              style={{ paddingInlineStart: 'var(--pf-t--global--spacer--md' }}
+            >
+              <RestoreRunsTableRowExperiment
+                experimentStatus={experimentStatus}
+                archivedExperiments={archivedExperiments}
+                currentExperiment={experiment}
+                isSubmitting={isSubmitting}
+              />
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
+};
+
+export default RunsWithArchivedExperimentTable;

opendatahub-io/odh-dashboard

frontend/src/pages/pipelines/global/runs/RestoreRunsTableRowExperiment.tsx
@@ -1,0 +1,54 @@
+import { Flex, FlexItem, Label } from '@patternfly/react-core';
+import React from 'react';
+import { TableText } from '@patternfly/react-table';
+import { ExperimentKF } from '~/concepts/pipelines/kfTypes';
+
+import { RestoreRunsStatusIcon } from './RestoreRunsStatusIcon';
+import { StatusEntry } from './types';
+
+type RestoreRunsTableRowExperimentProps = {
+  experimentStatus: StatusEntry<ExperimentKF>[];
+  archivedExperiments: ExperimentKF[];
+  currentExperiment: ExperimentKF;
+  isSubmitting: boolean;
+};
+
+const RestoreRunsTableRowExperiment: React.FC<RestoreRunsTableRowExperimentProps> = ({
+  experimentStatus,
+  archivedExperiments,
+  currentExperiment,
+  isSubmitting,
+}) => {
+  const matchingExperiment = experimentStatus.find(
+    (experiment) => experiment.item.experiment_id === currentExperiment.experiment_id,
+  );
+
+  const isExperimentArchived = !!archivedExperiments.find(
+    (exp) => exp.experiment_id === currentExperiment.experiment_id,
+  );
+  return (
+    <Flex gap={{ default: 'gapSm' }}>
+      <FlexItem>
+        <TableText>
+          {isExperimentArchived && (
+            <RestoreRunsStatusIcon
+              status={matchingExperiment?.status}
+              isSubmitting={isSubmitting}
+            />
+          )}{' '}
+          {currentExperiment.display_name}
+        </TableText>
+      </FlexItem>
+
+      {archivedExperiments.includes(currentExperiment) && (
+        <FlexItem>
+          <Label isCompact variant="outline" color="blue">
+            Will be restored
+          </Label>
+        </FlexItem>
+      )}
+    </Flex>
+  );
+};
+
+export default RestoreRunsTableRowExperiment;

frontend/src/pages/pipelines/global/runs/RunsWithArchivedExperimentTable.tsx
@@ -1,0 +1,81 @@
+import * as React from 'react';
+import { Table, TableText, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
+import { Content, Stack, StackItem } from '@patternfly/react-core';
+import { PipelineRunExperimentsContext } from '~/pages/pipelines/global/runs/PipelineRunExperimentsContext';
+import { ExperimentKF, PipelineRunKF } from '~/concepts/pipelines/kfTypes';
+import { getGroupRunsByExperiment } from './utils';
+import RestoreRunsTableRowExperiment from './RestoreRunsTableRowExperiment';
+import { RestoreRunsStatusIcon } from './RestoreRunsStatusIcon';
+import { StatusEntry } from './types';
+
+type RunsWithArchivedExperimentProps = {
+  runs: PipelineRunKF[];
+  archivedExperiments: ExperimentKF[];
+  isSubmitting: boolean;
+  runStatus: StatusEntry<PipelineRunKF>[];
+  experimentStatus: StatusEntry<ExperimentKF>[];
+};
+
+const RunsWithArchivedExperimentTable: React.FC<RunsWithArchivedExperimentProps> = ({
+  runs,
+  archivedExperiments,
+  runStatus,
+  isSubmitting,
+  experimentStatus,
+}) => {
+  const { experiments: allExperiments } = React.useContext(PipelineRunExperimentsContext);
+  const groupedRuns = getGroupRunsByExperiment(runs, allExperiments);
+
+  return (
+    <Table variant="compact">
+      <Thead noWrap>
+        <Tr>
+          <Th>Run</Th>
+          <Th>Experiment</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {groupedRuns.map(({ experiment, run }, i) => (
+          <Tr key={i}>
+            <Td dataLabel="Runs" modifier="truncate">
+              <Stack hasGutter>
+                {run.map((singleRun, index) => {
+                  const matchingRun = runStatus.find(
+                    (currentRun) => currentRun.item.run_id === singleRun.run_id,
+                  );
+
+                  return (
+                    <StackItem key={index}>
+                      <TableText>
+                        <RestoreRunsStatusIcon
+                          status={matchingRun?.status}
+                          isSubmitting={isSubmitting}
+                        />{' '}
+                        {singleRun.display_name}
+                      </TableText>
+                      <Content component="small">{singleRun.description}</Content>
+                    </StackItem>
+                  );
+                })}
+              </Stack>
+            </Td>
+            <Td
+              dataLabel="Experiment"
+              modifier="truncate"
+              style={{ paddingInlineStart: 'var(--pf-t--global--spacer--md' }}
+            >
+              <RestoreRunsTableRowExperiment
+                experimentStatus={experimentStatus}
+                archivedExperiments={archivedExperiments}
+                currentExperiment={experiment}
+                isSubmitting={isSubmitting}
+              />
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
+};
+
+export default RunsWithArchivedExperimentTable;