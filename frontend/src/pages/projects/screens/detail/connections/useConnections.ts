fe3ba780155926819dba74b7a236aa176c85477b
emilys314 <esamoylo@redhat.com>
2025-03-05 15:09:04 -0500

    remove disableKServeOCIModels

--- frontend/src/pages/projects/screens/detail/connections/useConnections.ts
+++ frontend/src/pages/projects/screens/detail/connections/useConnections.ts
@@ -8,14 +8,11 @@ import useFetchState, {
 import { Connection } from '~/concepts/connectionTypes/types';
 import { LABEL_SELECTOR_DASHBOARD_RESOURCE } from '~/const';
 import { isConnection, isModelServingCompatible } from '~/concepts/connectionTypes/utils';
-import { SupportedArea, useIsAreaAvailable } from '~/concepts/areas';
 
 const useConnections = (
   namespace?: string,
   modelServingCompatible?: boolean,
 ): FetchState<Connection[]> => {
-  const isOciEnabled = useIsAreaAvailable(SupportedArea.K_SERVE_OCI).status;
-
   const callback = React.useCallback<FetchStateCallbackPromise<Connection[]>>(
     async (opts) => {
       if (!namespace) {
@@ -32,15 +29,10 @@ const useConnections = (
       if (modelServingCompatible) {
         connections = connections.filter((c) => isModelServingCompatible(c));
       }
-      if (!isOciEnabled) {
-        connections = connections.filter(
-          (c) => c.metadata.annotations['opendatahub.io/connection-type-ref'] !== 'oci-v1',
-        );
-      }
 
       return connections;
     },
-    [namespace, modelServingCompatible, isOciEnabled],
+    [namespace, modelServingCompatible],
   );
 
   return useFetchState(callback, []);
