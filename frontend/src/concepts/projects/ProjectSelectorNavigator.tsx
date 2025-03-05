a1f5f1578ef0126b321f5f5f7cea598c1689b017
emilys314 <esamoylo@redhat.com>
2025-03-05 14:41:09 -0500

    always update preferred projet on select

--- frontend/src/concepts/projects/ProjectSelectorNavigator.tsx
+++ frontend/src/concepts/projects/ProjectSelectorNavigator.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { ProjectsContext } from '~/concepts/projects/ProjectsContext';
+import { byName, ProjectsContext } from '~/concepts/projects/ProjectsContext';
 import ProjectSelector from './ProjectSelector';
 
 type ProjectSelectorProps = {
@@ -13,15 +13,13 @@ const ProjectSelectorNavigator: React.FC<ProjectSelectorProps> = ({
 }) => {
   const navigate = useNavigate();
   const { namespace } = useParams();
-  const { updatePreferredProject } = React.useContext(ProjectsContext);
+  const { projects, updatePreferredProject } = React.useContext(ProjectsContext);
 
   return (
     <ProjectSelector
       {...projectSelectorProps}
       onSelection={(projectName) => {
-        if (!projectName) {
-          updatePreferredProject(null);
-        }
+        updatePreferredProject(projects.find(byName(projectName)) || null);
         navigate(getRedirectPath(projectName));
       }}
       namespace={namespace ?? ''}
