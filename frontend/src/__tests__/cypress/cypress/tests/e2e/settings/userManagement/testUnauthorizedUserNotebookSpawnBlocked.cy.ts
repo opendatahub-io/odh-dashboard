635c1d62c57e41465117dcd80383fa6e437e65e6
Anthony Coughlin <acoughli@redhat.com>
2025-03-10 13:26:52 +0000

    Comitting working test

---
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/settings/userManagement/testUnauthorizedUserNotebookSpawnBlocked.cy.ts
@@ -0,0 +1,90 @@
+import {
+  HTPASSWD_CLUSTER_ADMIN_USER,
+  LDAP_CONTRIBUTOR_USER,
+} from '~/__tests__/cypress/cypress/utils/e2eUsers';
+import { userManagement } from '~/__tests__/cypress/cypress/pages/userManagement';
+import { header } from '~/__tests__/cypress/cypress/pages/components/Header';
+
+after(() => {
+  // Clear cookies and local storage
+  cy.clearCookies();
+  cy.clearLocalStorage();
+
+  // Reload the page forcefully
+  cy.reload(true);
+
+  // Authentication and navigation
+  cy.step('Login as an Admnin and restore settings');
+  cy.visitWithLogin('/', HTPASSWD_CLUSTER_ADMIN_USER);
+
+  cy.step('Navigate to User Management');
+  userManagement.visit();
+
+  cy.step('Clear the current Data Science User Groups');
+  userManagement.getUserGroupSection().clearMultiChipItem();
+  const userGroupSection = userManagement.getUserGroupSection();
+
+  cy.step('Select rhods-admin and save it');
+  // Click the text field to open the dropdown
+  userGroupSection.findMultiGroupSelectButton().click();
+  // Click the 'rhods-admin' option from the dropdown
+  userGroupSection.findMultiGroupOptions('system:authenticated').click();
+  // Click outside the dropdown to close it (e.g., clicking on the page title)
+  cy.findByTestId('app-page-title').click();
+  // Submit the form
+  userManagement.findSubmitButton().click();
+  // Validate that changes were saved successfully
+  userManagement.shouldHaveSuccessAlertMessage();
+});
+
+describe('Verify Unauthorized User Is Not Able To Spawn Jupyter Notebook', () => {
+  it(
+    'Remove Admin privlideges and apply access to the Dashboard only to Admin',
+    { tags: ['@Sanity', '@SanitySet2', '@ODS-1680', '@Dashboard'] },
+    () => {
+      // Authentication and navigation
+      cy.step('Log into the application');
+      cy.visitWithLogin('/', HTPASSWD_CLUSTER_ADMIN_USER);
+
+      // Navigate to User Management
+      cy.step('Navigate to User Management');
+      userManagement.visit();
+
+      // Remove system:authenticated from the Data Science User Groups and apply rhods-admins only permissions
+      cy.step('Clear the current Data Science User Groups');
+      userManagement.getUserGroupSection().clearMultiChipItem();
+      const userGroupSection = userManagement.getUserGroupSection();
+
+      cy.step('Select rhods-admin and save it');
+      // Click the text field to open the dropdown
+      userGroupSection.findMultiGroupSelectButton().click();
+      // Click the 'rhods-admin' option from the dropdown
+      userGroupSection.findMultiGroupOptions('rhods-admins').click();
+      // Click outside the dropdown to close it (e.g., clicking on the page title)
+      cy.findByTestId('app-page-title').click();
+      // Submit the form
+      userManagement.findSubmitButton().click();
+      // Validate that changes were saved successfully
+      userManagement.shouldHaveSuccessAlertMessage();
+    },
+  );
+  it(
+    'Login as the Admin and verify that the user does not have acceess to any tabs/applications',
+    { tags: ['@Sanity', '@SanitySet2', '@ODS-1680', '@Dashboard'] },
+    () => {
+      // Authentication and navigation
+      cy.step('Log into the application as a Non-admin');
+      cy.visitWithLogin('/', LDAP_CONTRIBUTOR_USER);
+
+      // Validate that the Dashboard and Jupyter Notebooks are not accessible
+      cy.step('Verify that the Dashboard is not accessible');
+      header.findUnauthorizedErrorSection().should('be.visible');
+      header.findUnauthorizedErrorSection().contains('Access permissions needed').should('exist');
+
+      cy.step('Verify that Jupyter Notebooks are also not accessible');
+      cy.visit(`/notebookController/spawner`);
+      header.findUnauthorizedErrorSection().should('be.visible');
+      header.findUnauthorizedErrorSection().contains('Access permissions needed').should('exist');
+    },
+  );
+});
