52a5f96c6c72422918a2cd6a6d5e1a3736a99362
Robert Sun <robertssun1234@gmail.com>
2025-03-10 14:41:51 -0400

    added ilab basic params (#3817)

--- frontend/src/__tests__/cypress/cypress/types.ts
+++ frontend/src/__tests__/cypress/cypress/types.ts
@@ -271,3 +271,23 @@ export type WorkloadMetricsTestData = {
   memoryQuota: number;
   refreshIntervals: number[];
 };
+
+export enum HyperparameterFields {
+  SDG_SAMPLE_SIZE = 'sdg_sample_size',
+  SDG_SCALE_FACTOR = 'sdg_scale_factor',
+  MAXIMUM_TOKENS_PER_ACCELERATOR = 'train_max_batch_len',
+  TRAINING_WORKERS = 'train_num_workers',
+  TRAIN_NUM_EPOCHS_PHASE_1 = 'train_num_epochs_phase_1',
+  TRAIN_NUM_EPOCHS_PHASE_2 = 'train_num_epochs_phase_2',
+  BATCH_SIZE_PHASE_1 = 'train_effective_batch_size_phase_1',
+  BATCH_SIZE_PHASE_2 = 'train_effective_batch_size_phase_2',
+  LEARNING_RATE_PHASE_1 = 'train_learning_rate_phase_1',
+  LEARNING_RATE_PHASE_2 = 'train_learning_rate_phase_2',
+  WARMUP_STEPS_PHASE_1 = 'train_num_warmup_steps_phase_1',
+  WARMUP_STEPS_PHASE_2 = 'train_num_warmup_steps_phase_2',
+  MAXIMUM_BATCH_LENGTH = 'sdg_max_batch_len',
+  TRAINING_SEED = 'train_seed',
+  QUESTION_ANSWER_PAIRS = 'final_eval_few_shots',
+  EVALUATION_WORKERS = 'final_eval_max_workers',
+  EVALUATION_BATCH_SIZE = 'final_eval_batch_size',
+}
