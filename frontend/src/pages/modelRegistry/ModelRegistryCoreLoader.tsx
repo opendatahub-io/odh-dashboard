ccbe113d0dce5e90cc8165e808395cd543b4d25b
Yulia Krimerman <juliapiterova@hotmail.com>
2025-03-25 15:32:50 -0400

    fix test

--- frontend/src/pages/modelRegistry/ModelRegistryCoreLoader.tsx
+++ frontend/src/pages/modelRegistry/ModelRegistryCoreLoader.tsx
@@ -39,7 +39,24 @@ const ModelRegistryCoreLoader: React.FC<ModelRegistryCoreLoaderProps> =
       modelRegistryServicesLoadError,
       modelRegistryServices,
       preferredModelRegistry,
+      updatePreferredModelRegistry,
     } = React.useContext(ModelRegistrySelectorContext);
+    const modelRegistryFromRoute = modelRegistryServices.find(
+      (mr) => mr.metadata.name === modelRegistry,
+    );
+
+    React.useEffect(() => {
+      if (
+        modelRegistryFromRoute &&
+        preferredModelRegistry?.metadata.name !== modelRegistryFromRoute.metadata.name
+      ) {
+        updatePreferredModelRegistry(modelRegistryFromRoute);
+      }
+    }, [
+      modelRegistryFromRoute,
+      updatePreferredModelRegistry,
+      preferredModelRegistry?.metadata.name,
+    ]);
 
     if (!modelRegistryNamespace) {
       return (
