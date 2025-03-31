8ff0f0c6532eb4c51f810a7836fb25366b16068b
Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>
2025-03-31 13:19:23 -0400

    fix(various drawers): updated wrappers to render content (#24)

--- frontend/src/concepts/pipelines/content/pipelinesDetails/pipeline/SelectedTaskDrawerContent.tsx
+++ frontend/src/concepts/pipelines/content/pipelinesDetails/pipeline/SelectedTaskDrawerContent.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import {
-  DrawerContext,
   DrawerActions,
   DrawerCloseButton,
   DrawerHead,
   DrawerPanelBody,
-  DrawerPanelContent,
   Title,
 } from '@patternfly/react-core';
 import { PipelineTask } from '~/concepts/pipelines/topology';
@@ -24,25 +22,24 @@ const SelectedTaskDrawerContent: React.FC<SelectedTaskDrawerContentProps> = ({ t
   return (
     // TODO: Revisit below approach, either to further to look into what caused the content to not render or
     // to restructure the code
-    <DrawerContext.Provider value={{ isExpanded: true }}>
-      <DrawerPanelContent
-        data-testid="task-drawer"
-        // TODO: look into removing this inline style; PF Drawers should handle height/scrolling by default
-        style={{ height: '100%', overflowY: 'auto' }}
-      >
-        <DrawerHead>
-          <Title headingLevel="h2" size="xl" data-testid="pipeline-task-name">
-            {task.name} {task.type === 'artifact' ? 'Artifact details' : ''}
-          </Title>
-          <DrawerActions>
-            <DrawerCloseButton onClick={onClose} />
-          </DrawerActions>
-        </DrawerHead>
-        <DrawerPanelBody>
-          <PipelineTaskDetails task={task} />
-        </DrawerPanelBody>
-      </DrawerPanelContent>
-    </DrawerContext.Provider>
+    <div
+      className="pf-v6-c-drawer__panel"
+      data-testid="task-drawer"
+      // TODO: look into removing this inline style; PF Drawers should handle height/scrolling by default
+      style={{ height: '100%', overflowY: 'auto' }}
+    >
+      <DrawerHead>
+        <Title headingLevel="h2" size="xl" data-testid="pipeline-task-name">
+          {task.name} {task.type === 'artifact' ? 'Artifact details' : ''}
+        </Title>
+        <DrawerActions>
+          <DrawerCloseButton onClick={onClose} />
+        </DrawerActions>
+      </DrawerHead>
+      <DrawerPanelBody>
+        <PipelineTaskDetails task={task} />
+      </DrawerPanelBody>
+    </div>
   );
 };
 
