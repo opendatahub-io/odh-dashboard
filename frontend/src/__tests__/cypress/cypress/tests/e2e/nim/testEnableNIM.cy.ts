c2887afdbace2097a94fbbaa4f8bcbb315dc70bc
condawrangler <condawrangler@protonmail.com>
2025-04-01 13:23:32 -0400

    RHOAI-21549: Add Account status checks

--- frontend/src/__tests__/cypress/cypress/tests/e2e/nim/testEnableNIM.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/nim/testEnableNIM.cy.ts
@@ -2,7 +2,10 @@ import { HTPASSWD_CLUSTER_ADMIN_USER } from '~/__tests__/cypress/cypress/utils/e
 import { explorePage } from '~/__tests__/cypress/cypress/pages/explore';
 import { enabledPage } from '~/__tests__/cypress/cypress/pages/enabled';
 import { nimCard } from '~/__tests__/cypress/cypress/pages/components/NIMCard';
-import { deleteNIMAccount } from '~/__tests__/cypress/cypress/utils/oc_commands/baseCommands';
+import {
+  deleteNIMAccount,
+  validateNIMAccountStatus,
+} from '~/__tests__/cypress/cypress/utils/oc_commands/baseCommands';
 
 describe('Verify NIM enable flow', () => {
   before(() => {
@@ -40,9 +43,15 @@ describe('Verify NIM enable flow', () => {
       .contains(
         'NVIDIA NIM is a set of easy-to-use microservices designed for secure, reliable deployment of high-performance AI model inferencing.',
       );
-    cy.step('Validate that the NIM card does not contain a Disabled button');
-    nimCard.getNIMCard().within(() => {
-      cy.contains('button', 'Disabled', { timeout: 60000 }).should('not.exist');
-    });
+    cy.step('Validate odh-nim-account AccountStatus is True');
+    validateNIMAccountStatus('AccountStatus', 'True');
+    cy.step('Validate odh-nim-account APIKeyValidation is True');
+    validateNIMAccountStatus('APIKeyValidation', 'True');
+    cy.step('Validate odh-nim-account ConfigMapUpdate is True');
+    validateNIMAccountStatus('ConfigMapUpdate', 'True');
+    cy.step('Validate odh-nim-account TemplateUpdate is True');
+    validateNIMAccountStatus('TemplateUpdate', 'True');
+    cy.step('Validate odh-nim-account SecretUpdate is True');
+    validateNIMAccountStatus('SecretUpdate', 'True');
   });
 });
