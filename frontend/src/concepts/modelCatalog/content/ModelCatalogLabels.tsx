a5f5ae3507a400bee9f28a6a5111fe7ef8982b42
Nana Nosirova <10577112+nananosirova@users.noreply.github.com>
2025-04-16 17:36:04 -0400

    Change loop var to a more descriptive name, use Truncate component, use LabelGroup prop numLabels

--- frontend/src/concepts/modelCatalog/content/ModelCatalogLabels.tsx
+++ frontend/src/concepts/modelCatalog/content/ModelCatalogLabels.tsx
@@ -17,59 +17,49 @@ export const ModelCatalogLabels: React.FC<ModelCatalogLabelsProps> = ({
   tasks = [],
   showNonILabLabels = false,
   truncate = false,
-}) => {
-  const iLabLabels = truncate
-    ? getILabLabels(labels).slice(0, TRUNCATED_MAX_LABEL_NUM)
-    : getILabLabels(labels);
-  const taskLabels = truncate ? tasks.slice(0, TRUNCATED_MAX_LABEL_NUM - iLabLabels.length) : tasks;
-  const nonILabLabels = truncate
-    ? removeILabLabels(labels).slice(
-        0,
-        TRUNCATED_MAX_LABEL_NUM - iLabLabels.length - taskLabels.length,
-      )
-    : removeILabLabels(labels);
-
-  return (
-    <LabelGroup data-testid="model-catalog-label-group">
-      {iLabLabels.map((l) => {
-        switch (l) {
-          case ReservedILabLabel.LabBase:
-            return (
-              <Label data-testid="model-catalog-label" color="yellow" variant="filled">
-                LAB starter
-              </Label>
-            );
-          case ReservedILabLabel.LabTeacher:
-            return (
-              <Label data-testid="model-catalog-label" color="purple" variant="filled">
-                LAB teacher
-              </Label>
-            );
-          case ReservedILabLabel.LabJudge:
-            return (
-              <Label data-testid="model-catalog-label" color="orange" variant="filled">
-                LAB judge
-              </Label>
-            );
-          default:
-            return (
-              <Label data-testid="model-catalog-label" key={l} variant="outline">
-                {l}
-              </Label>
-            );
-        }
-      })}
-      {taskLabels.map((task) => (
-        <Label data-testid="model-catalog-label" key={task} variant="outline">
-          {task}
+}) => (
+  <LabelGroup
+    numLabels={truncate ? TRUNCATED_MAX_LABEL_NUM : undefined}
+    data-testid="model-catalog-label-group"
+  >
+    {getILabLabels(labels).map((label) => {
+      switch (label) {
+        case ReservedILabLabel.LabBase:
+          return (
+            <Label data-testid="model-catalog-label" color="yellow" variant="filled">
+              LAB starter
+            </Label>
+          );
+        case ReservedILabLabel.LabTeacher:
+          return (
+            <Label data-testid="model-catalog-label" color="purple" variant="filled">
+              LAB teacher
+            </Label>
+          );
+        case ReservedILabLabel.LabJudge:
+          return (
+            <Label data-testid="model-catalog-label" color="orange" variant="filled">
+              LAB judge
+            </Label>
+          );
+        default:
+          return (
+            <Label data-testid="model-catalog-label" key={label} variant="outline">
+              {label}
+            </Label>
+          );
+      }
+    })}
+    {tasks.map((task) => (
+      <Label data-testid="model-catalog-label" key={task} variant="outline">
+        {task}
+      </Label>
+    ))}
+    {showNonILabLabels &&
+      removeILabLabels(labels).map((label) => (
+        <Label data-testid="model-catalog-label" key={label} variant="outline">
+          {label}
         </Label>
       ))}
-      {showNonILabLabels &&
-        nonILabLabels.map((label) => (
-          <Label data-testid="model-catalog-label" key={label} variant="outline">
-            {label}
-          </Label>
-        ))}
-    </LabelGroup>
-  );
-};
+  </LabelGroup>
+);
