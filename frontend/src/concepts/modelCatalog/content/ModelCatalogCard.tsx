c8ca0c4bfc3b044bb30ca5d4205043681f0ee9ce
Nana Nosirova <10577112+nananosirova@users.noreply.github.com>
2025-04-16 17:36:04 -0400

    Change loop var to a more descriptive name, use Truncate component, use LabelGroup prop numLabels

--- frontend/src/concepts/modelCatalog/content/ModelCatalogCard.tsx
+++ frontend/src/concepts/modelCatalog/content/ModelCatalogCard.tsx
@@ -14,7 +14,7 @@ import {
   SplitItem,
   Stack,
   StackItem,
-  Tooltip,
+  Truncate,
 } from '@patternfly/react-core';
 import { Link } from 'react-router-dom';
 import { CatalogModel } from '~/concepts/modelCatalog/types';
@@ -24,89 +24,70 @@ import { RhUiTagIcon } from '~/images/icons';
 import { ModelCatalogLabels } from '~/concepts/modelCatalog/content/ModelCatalogLabels';
 import TruncatedText from '~/components/TruncatedText';
 
-type ModelCatalogCardProps = {
+export const ModelCatalogCard: React.FC<{
   model: CatalogModel;
   source: string;
   truncate?: boolean;
-};
-
-const TRUNCATED_LEFT_EDGE_LENGTH = 15;
-const TRUNCATED_RIGHT_EDGE_LENGTH = Math.floor(TRUNCATED_LEFT_EDGE_LENGTH / 2);
-const ELLIPSIS = '...';
-const MAX_LENGTH = TRUNCATED_LEFT_EDGE_LENGTH + ELLIPSIS.length + TRUNCATED_RIGHT_EDGE_LENGTH;
-
-export const ModelCatalogCard: React.FC<ModelCatalogCardProps> = ({
-  model,
-  source,
-  truncate = false,
-}) => {
-  const truncateNameEnabled = truncate && model.name.length > MAX_LENGTH;
-  return (
-    <Card isFullHeight data-testid="model-catalog-card">
-      <CardHeader>
-        <CardTitle>
-          <Flex alignItems={{ default: 'alignItemsCenter' }}>
-            {model.logo ? (
-              <img src={model.logo} alt="model logo" style={{ height: '36px', width: '36px' }} />
-            ) : (
-              <Skeleton
-                shape="square"
-                width="36px"
-                height="36px"
-                screenreaderText="Brand image loading"
-              />
-            )}
-            <FlexItem align={{ default: 'alignRight' }}>
-              <Label>{source}</Label>
-            </FlexItem>
-          </Flex>
-        </CardTitle>
-      </CardHeader>
-      <CardBody>
-        <Stack hasGutter>
-          <StackItem>
-            <Link
-              data-testid="model-catalog-detail-link"
-              to={getCatalogModelDetailsRouteFromModel(model, source) || '#'}
-              style={{
-                fontSize: 'var(--pf-t--global--font--size--body--default)',
-                fontWeight: 'var(--pf-t--global--font--weight--body--bold)',
-              }}
-            >
-              {truncateNameEnabled ? (
-                <Tooltip content={model.name} position="top">
-                  <span>{`${model.name.substring(
-                    0,
-                    TRUNCATED_LEFT_EDGE_LENGTH,
-                  )}${ELLIPSIS}${model.name.slice(-TRUNCATED_RIGHT_EDGE_LENGTH)}`}</span>
-                </Tooltip>
-              ) : (
-                <span>{model.name}</span>
-              )}
-            </Link>
-            <Split hasGutter>
-              <SplitItem>
-                <Icon isInline>
-                  <RhUiTagIcon />
-                </Icon>
-                <span style={{ marginLeft: 'var(--pf-t--global--spacer--sm)' }}>
-                  {getTagFromModel(model)}
-                </span>
-              </SplitItem>
-            </Split>
-          </StackItem>
-          <StackItem isFilled>
+}> = ({ model, source, truncate = false }) => (
+  <Card isFullHeight data-testid="model-catalog-card">
+    <CardHeader>
+      <CardTitle>
+        <Flex alignItems={{ default: 'alignItemsCenter' }}>
+          {model.logo ? (
+            <img src={model.logo} alt="model logo" style={{ height: '36px', width: '36px' }} />
+          ) : (
+            <Skeleton
+              shape="square"
+              width="36px"
+              height="36px"
+              screenreaderText="Brand image loading"
+            />
+          )}
+          <FlexItem align={{ default: 'alignRight' }}>
+            <Label>{source}</Label>
+          </FlexItem>
+        </Flex>
+      </CardTitle>
+    </CardHeader>
+    <CardBody>
+      <Stack hasGutter>
+        <StackItem>
+          <Link
+            data-testid="model-catalog-detail-link"
+            to={getCatalogModelDetailsRouteFromModel(model, source) || '#'}
+            style={{
+              fontSize: 'var(--pf-t--global--font--size--body--default)',
+              fontWeight: 'var(--pf-t--global--font--weight--body--bold)',
+            }}
+          >
             {truncate ? (
-              <TruncatedText maxLines={2} content={model.description} />
+              <Truncate content={model.name} position="middle" tooltipPosition="top" />
             ) : (
-              model.description
+              <span>{model.name}</span>
             )}
-          </StackItem>
-        </Stack>
-      </CardBody>
-      <CardFooter>
-        <ModelCatalogLabels labels={model.labels} tasks={model.tasks} truncate={truncate} />
-      </CardFooter>
-    </Card>
-  );
-};
+          </Link>
+          <Split hasGutter>
+            <SplitItem>
+              <Icon isInline>
+                <RhUiTagIcon />
+              </Icon>
+              <span style={{ marginLeft: 'var(--pf-t--global--spacer--sm)' }}>
+                {getTagFromModel(model)}
+              </span>
+            </SplitItem>
+          </Split>
+        </StackItem>
+        <StackItem isFilled>
+          {truncate ? (
+            <TruncatedText maxLines={2} content={model.description} />
+          ) : (
+            model.description
+          )}
+        </StackItem>
+      </Stack>
+    </CardBody>
+    <CardFooter>
+      <ModelCatalogLabels labels={model.labels} tasks={model.tasks} numLabels={2} />
+    </CardFooter>
+  </Card>
+);
--- frontend/src/concepts/modelCatalog/content/ModelCatalogLabels.tsx
+++ frontend/src/concepts/modelCatalog/content/ModelCatalogLabels.tsx
@@ -3,73 +3,51 @@ import { Label, LabelGroup } from '@patternfly/react-core';
 import { getILabLabels, removeILabLabels } from '~/pages/modelCatalog/utils';
 import { ReservedILabLabel } from '~/pages/modelCatalog/const';
 
-const TRUNCATED_MAX_LABEL_NUM = 2;
-
-type ModelCatalogLabelsProps = {
+export const ModelCatalogLabels: React.FC<{
   labels?: string[];
   tasks?: string[];
   showNonILabLabels?: boolean;
-  truncate?: boolean;
-};
-
-export const ModelCatalogLabels: React.FC<ModelCatalogLabelsProps> = ({
-  labels = [],
-  tasks = [],
-  showNonILabLabels = false,
-  truncate = false,
-}) => {
-  const iLabLabels = truncate
-    ? getILabLabels(labels).slice(0, TRUNCATED_MAX_LABEL_NUM)
-    : getILabLabels(labels);
-  const taskLabels = truncate ? tasks.slice(0, TRUNCATED_MAX_LABEL_NUM - iLabLabels.length) : tasks;
-  const nonILabLabels = truncate
-    ? removeILabLabels(labels).slice(
-        0,
-        TRUNCATED_MAX_LABEL_NUM - iLabLabels.length - taskLabels.length,
-      )
-    : removeILabLabels(labels);
-
-  return (
-    <LabelGroup data-testid="model-catalog-label-group">
-      {iLabLabels.map((l) => {
-        switch (l) {
-          case ReservedILabLabel.LabBase:
-            return (
-              <Label data-testid="model-catalog-label" color="yellow" variant="filled">
-                LAB starter
-              </Label>
-            );
-          case ReservedILabLabel.LabTeacher:
-            return (
-              <Label data-testid="model-catalog-label" color="purple" variant="filled">
-                LAB teacher
-              </Label>
-            );
-          case ReservedILabLabel.LabJudge:
-            return (
-              <Label data-testid="model-catalog-label" color="orange" variant="filled">
-                LAB judge
-              </Label>
-            );
-          default:
-            return (
-              <Label data-testid="model-catalog-label" key={l} variant="outline">
-                {l}
-              </Label>
-            );
-        }
-      })}
-      {taskLabels.map((task) => (
-        <Label data-testid="model-catalog-label" key={task} variant="outline">
-          {task}
+  numLabels?: number;
+}> = ({ labels = [], tasks = [], showNonILabLabels = false, numLabels }) => (
+  <LabelGroup numLabels={numLabels} data-testid="model-catalog-label-group">
+    {getILabLabels(labels).map((label) => {
+      switch (label) {
+        case ReservedILabLabel.LabBase:
+          return (
+            <Label data-testid="model-catalog-label" color="yellow" variant="filled">
+              LAB starter
+            </Label>
+          );
+        case ReservedILabLabel.LabTeacher:
+          return (
+            <Label data-testid="model-catalog-label" color="purple" variant="filled">
+              LAB teacher
+            </Label>
+          );
+        case ReservedILabLabel.LabJudge:
+          return (
+            <Label data-testid="model-catalog-label" color="orange" variant="filled">
+              LAB judge
+            </Label>
+          );
+        default:
+          return (
+            <Label data-testid="model-catalog-label" key={label} variant="outline">
+              {label}
+            </Label>
+          );
+      }
+    })}
+    {tasks.map((task) => (
+      <Label data-testid="model-catalog-label" key={task} variant="outline">
+        {task}
+      </Label>
+    ))}
+    {showNonILabLabels &&
+      removeILabLabels(labels).map((label) => (
+        <Label data-testid="model-catalog-label" key={label} variant="outline">
+          {label}
         </Label>
       ))}
-      {showNonILabLabels &&
-        nonILabLabels.map((label) => (
-          <Label data-testid="model-catalog-label" key={label} variant="outline">
-            {label}
-          </Label>
-        ))}
-    </LabelGroup>
-  );
-};
+  </LabelGroup>
+);
