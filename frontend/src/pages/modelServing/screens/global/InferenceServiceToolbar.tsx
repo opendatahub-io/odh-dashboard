a10409c7ce7e3841700fcde699cc08975aa22d28
Anish Surti <asurti@redhat.com>
2025-04-16 16:50:33 -0400

    added new file for inference toolbar

---
+++ frontend/src/pages/modelServing/screens/global/InferenceServiceToolbar.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ServeModelButton from './ServeModelButton';
+
+const InferenceServiceToolbar = () => {
+  return <ServeModelButton />;
+};
+
+export default InferenceServiceToolbar;
