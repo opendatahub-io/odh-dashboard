89607311dcd02d3f6331979369545533f06fcfa4
Claudia Alphonse <claudialphonse@gmail.com>
2025-04-24 12:02:05 +0530

    feat(workload-message):added finished instead of displaying no message when jobs succeed

--- frontend/src/pages/distributedWorkloads/global/workloadStatus/DWWorkloadsTableRow.tsx
+++ frontend/src/pages/distributedWorkloads/global/workloadStatus/DWWorkloadsTableRow.tsx
@@ -2,13 +2,18 @@ import * as React from 'react';
 import { Td, Tr } from '@patternfly/react-table';
 import { Timestamp } from '@patternfly/react-core';
 import { WorkloadKind } from '~/k8sTypes';
-import { WorkloadStatusInfo, getWorkloadName } from '~/concepts/distributedWorkloads/utils';
+import {
+  WorkloadStatusInfo,
+  getWorkloadStatusMessage,
+  getWorkloadName,
+} from '~/concepts/distributedWorkloads/utils';
 import { WorkloadStatusLabel } from '~/pages/distributedWorkloads/components/WorkloadStatusLabel';
 
 type DWWorkloadsTableRowProps = {
   workload: WorkloadKind;
   statusInfo: WorkloadStatusInfo;
 };
+
 const DWWorkloadsTableRow: React.FC<DWWorkloadsTableRowProps> = ({ workload, statusInfo }) => (
   <Tr key={workload.metadata?.uid}>
     <Td dataLabel="Name">{getWorkloadName(workload)}</Td>
@@ -23,7 +28,7 @@ const DWWorkloadsTableRow: React.FC<DWWorkloadsTableRowProps> = ({ workload, sta
         'Unknown'
       )}
     </Td>
-    <Td dataLabel="Latest Message">{statusInfo.message}</Td>
+    <Td dataLabel="Latest Message">{getWorkloadStatusMessage(statusInfo)}</Td>
   </Tr>
 );
 
