2bc9ac2b9e6c7d37c5891ea4fb436f6d2fe69f41
Pushpa Padti <99261071+ppadti@users.noreply.github.com>
2025-04-26 02:45:25 +0530

    Load data before rendering Deploy form, when deploying from model catalog or registry (#4077)
    * Load data before rendering Deploy form, when deploying from model catalog or registry
    
    * Remove the useEffects
    
    * update key name

--- frontend/src/__tests__/cypress/cypress/pages/modelServing.ts
+++ frontend/src/__tests__/cypress/cypress/pages/modelServing.ts
@@ -133,6 +133,10 @@ class InferenceServiceModal extends Modal {
     return this.k8sNameDescription.findDisplayNameInput();
   }
 
+  findSpinner() {
+    return this.find().findByTestId('spinner');
+  }
+
   findServingRuntimeSelect() {
     return this.find().findByTestId('inference-service-model-selection');
   }
