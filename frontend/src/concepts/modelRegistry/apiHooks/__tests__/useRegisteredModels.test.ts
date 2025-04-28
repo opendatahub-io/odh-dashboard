584a2f83b14342868fd570f81fd8888e3c7763f8
Emmanuel Ikeola <eikeola@redhat.com>
2025-04-28 13:07:58 +0100

    fixed linting

--- frontend/src/concepts/modelRegistry/apiHooks/__tests__/useRegisteredModels.test.ts
+++ frontend/src/concepts/modelRegistry/apiHooks/__tests__/useRegisteredModels.test.ts
@@ -1,7 +1,7 @@
 import { testHook, standardUseFetchState } from '~/__tests__/unit/testUtils/hooks';
 import { useModelRegistryAPI } from '~/concepts/modelRegistry/context/ModelRegistryContext';
 import { RegisteredModelList } from '~/concepts/modelRegistry/types';
-import useRegisteredModels from '../useRegisteredModels';
+import useRegisteredModels from '~/concepts/modelRegistry/apiHooks/useRegisteredModels';
 
 // Mock the useModelRegistryAPI hook
 jest.mock('~/concepts/modelRegistry/context/ModelRegistryContext', () => ({
