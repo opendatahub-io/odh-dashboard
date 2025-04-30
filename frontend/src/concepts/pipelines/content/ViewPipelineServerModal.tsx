169ccb17213b2f3e483d297e54089fb3404ef802
Anish Surti <asurti@redhat.com>
2025-04-30 15:05:32 -0400

    replace deprecated Model with PF6 Modal in ViewPipelineServerModal

--- frontend/src/concepts/pipelines/content/ViewPipelineServerModal.tsx
+++ frontend/src/concepts/pipelines/content/ViewPipelineServerModal.tsx
@@ -5,8 +5,10 @@ import {
   DescriptionListGroup,
   DescriptionListTerm,
   Title,
+  Modal,
+  ModalBody,
+  ModalHeader,
 } from '@patternfly/react-core';
-import { Modal } from '@patternfly/react-core/deprecated';
 import { usePipelinesAPI } from '~/concepts/pipelines/context';
 import PasswordHiddenText from '~/components/PasswordHiddenText';
 import { dataEntryToRecord } from '~/utilities/dataEntryToRecord';
@@ -33,91 +35,94 @@ const ViewPipelineServerModal: React.FC<ViewPipelineServerModalProps> = ({
   const databaseSecret = dataEntryToRecord(result?.values?.data ?? []);
 
   return (
-    <Modal title="View pipeline server" isOpen onClose={onClose} variant="small">
-      {pipelineNamespaceCR && (
-        <DescriptionList termWidth="20ch" isHorizontal>
-          {!!pipelineNamespaceCR.spec.objectStorage.externalStorage?.s3CredentialsSecret
-            .secretName && (
-            <>
-              <Title headingLevel="h2">Object storage connection</Title>
-              <DescriptionListGroup>
-                <DescriptionListTerm>Access key</DescriptionListTerm>
-                <DescriptionListDescription data-testid="access-key-field">
-                  {pipelineSecret[
-                    pipelineNamespaceCR.spec.objectStorage.externalStorage.s3CredentialsSecret
-                      .accessKey
-                  ] || ''}
-                </DescriptionListDescription>
-              </DescriptionListGroup>
-              <DescriptionListGroup>
-                <DescriptionListTerm>Secret key</DescriptionListTerm>
-                <DescriptionListDescription data-testid="secret-key-field">
-                  <PasswordHiddenText
-                    password={
-                      pipelineSecret[
-                        pipelineNamespaceCR.spec.objectStorage.externalStorage.s3CredentialsSecret
-                          .secretKey
-                      ] ?? ''
-                    }
-                  />
-                </DescriptionListDescription>
-              </DescriptionListGroup>
-              <DescriptionListGroup>
-                <DescriptionListTerm>Endpoint</DescriptionListTerm>
-                <DescriptionListDescription data-testid="endpoint-field">
-                  {pipelineNamespaceCR.spec.objectStorage.externalStorage.scheme &&
-                  pipelineNamespaceCR.spec.objectStorage.externalStorage.host
-                    ? `${pipelineNamespaceCR.spec.objectStorage.externalStorage.scheme}://${pipelineNamespaceCR.spec.objectStorage.externalStorage.host}`
-                    : ''}
-                </DescriptionListDescription>
-              </DescriptionListGroup>
-              <DescriptionListGroup>
-                <DescriptionListTerm>Bucket</DescriptionListTerm>
-                <DescriptionListDescription data-testid="bucket-field">
-                  {pipelineNamespaceCR.spec.objectStorage.externalStorage.bucket}
-                </DescriptionListDescription>
-              </DescriptionListGroup>
-            </>
-          )}
-          {!!pipelineNamespaceCR.spec.database &&
-            !!pipelineNamespaceCR.spec.database.externalDB &&
-            !!databaseSecret[ExternalDatabaseSecret.KEY] && (
+    <Modal isOpen onClose={onClose} variant="small">
+      <ModalHeader title="View pipeline server" />
+      <ModalBody>
+        {pipelineNamespaceCR && (
+          <DescriptionList termWidth="20ch" isHorizontal>
+            {!!pipelineNamespaceCR.spec.objectStorage.externalStorage?.s3CredentialsSecret
+              .secretName && (
               <>
-                <Title headingLevel="h2">Database</Title>
+                <Title headingLevel="h2">Object storage connection</Title>
                 <DescriptionListGroup>
-                  <DescriptionListTerm>Host</DescriptionListTerm>
-                  <DescriptionListDescription>
-                    {pipelineNamespaceCR.spec.database.externalDB.host}
+                  <DescriptionListTerm>Access key</DescriptionListTerm>
+                  <DescriptionListDescription data-testid="access-key-field">
+                    {pipelineSecret[
+                      pipelineNamespaceCR.spec.objectStorage.externalStorage.s3CredentialsSecret
+                        .accessKey
+                    ] || ''}
                   </DescriptionListDescription>
                 </DescriptionListGroup>
                 <DescriptionListGroup>
-                  <DescriptionListTerm>Port</DescriptionListTerm>
-                  <DescriptionListDescription>
-                    {pipelineNamespaceCR.spec.database.externalDB.port}
+                  <DescriptionListTerm>Secret key</DescriptionListTerm>
+                  <DescriptionListDescription data-testid="secret-key-field">
+                    <PasswordHiddenText
+                      password={
+                        pipelineSecret[
+                          pipelineNamespaceCR.spec.objectStorage.externalStorage.s3CredentialsSecret
+                            .secretKey
+                        ] ?? ''
+                      }
+                    />
                   </DescriptionListDescription>
                 </DescriptionListGroup>
                 <DescriptionListGroup>
-                  <DescriptionListTerm>Username</DescriptionListTerm>
-                  <DescriptionListDescription>
-                    {pipelineNamespaceCR.spec.database.externalDB.username}
+                  <DescriptionListTerm>Endpoint</DescriptionListTerm>
+                  <DescriptionListDescription data-testid="endpoint-field">
+                    {pipelineNamespaceCR.spec.objectStorage.externalStorage.scheme &&
+                    pipelineNamespaceCR.spec.objectStorage.externalStorage.host
+                      ? `${pipelineNamespaceCR.spec.objectStorage.externalStorage.scheme}://${pipelineNamespaceCR.spec.objectStorage.externalStorage.host}`
+                      : ''}
                   </DescriptionListDescription>
                 </DescriptionListGroup>
                 <DescriptionListGroup>
-                  <DescriptionListTerm>Password</DescriptionListTerm>
-                  <DescriptionListDescription>
-                    <PasswordHiddenText password={databaseSecret[ExternalDatabaseSecret.KEY]} />
-                  </DescriptionListDescription>
-                </DescriptionListGroup>
-                <DescriptionListGroup>
-                  <DescriptionListTerm>Database</DescriptionListTerm>
-                  <DescriptionListDescription>
-                    {pipelineNamespaceCR.spec.database.externalDB.pipelineDBName}
+                  <DescriptionListTerm>Bucket</DescriptionListTerm>
+                  <DescriptionListDescription data-testid="bucket-field">
+                    {pipelineNamespaceCR.spec.objectStorage.externalStorage.bucket}
                   </DescriptionListDescription>
                 </DescriptionListGroup>
               </>
             )}
-        </DescriptionList>
-      )}
+            {!!pipelineNamespaceCR.spec.database &&
+              !!pipelineNamespaceCR.spec.database.externalDB &&
+              !!databaseSecret[ExternalDatabaseSecret.KEY] && (
+                <>
+                  <Title headingLevel="h2">Database</Title>
+                  <DescriptionListGroup>
+                    <DescriptionListTerm>Host</DescriptionListTerm>
+                    <DescriptionListDescription>
+                      {pipelineNamespaceCR.spec.database.externalDB.host}
+                    </DescriptionListDescription>
+                  </DescriptionListGroup>
+                  <DescriptionListGroup>
+                    <DescriptionListTerm>Port</DescriptionListTerm>
+                    <DescriptionListDescription>
+                      {pipelineNamespaceCR.spec.database.externalDB.port}
+                    </DescriptionListDescription>
+                  </DescriptionListGroup>
+                  <DescriptionListGroup>
+                    <DescriptionListTerm>Username</DescriptionListTerm>
+                    <DescriptionListDescription>
+                      {pipelineNamespaceCR.spec.database.externalDB.username}
+                    </DescriptionListDescription>
+                  </DescriptionListGroup>
+                  <DescriptionListGroup>
+                    <DescriptionListTerm>Password</DescriptionListTerm>
+                    <DescriptionListDescription>
+                      <PasswordHiddenText password={databaseSecret[ExternalDatabaseSecret.KEY]} />
+                    </DescriptionListDescription>
+                  </DescriptionListGroup>
+                  <DescriptionListGroup>
+                    <DescriptionListTerm>Database</DescriptionListTerm>
+                    <DescriptionListDescription>
+                      {pipelineNamespaceCR.spec.database.externalDB.pipelineDBName}
+                    </DescriptionListDescription>
+                  </DescriptionListGroup>
+                </>
+              )}
+          </DescriptionList>
+        )}
+      </ModalBody>
     </Modal>
   );
 };
