6c2a00e34067b6461861536d5f602737202311cc
Claudia Alphonse <claudialphonse@gmail.com>
2025-05-06 00:51:31 +0530

    feat(model-version):added conditional for model versions page

--- frontend/src/pages/modelRegistry/screens/ModelVersionDetails/ModelVersionDetailsHeaderActions.tsx
+++ frontend/src/pages/modelRegistry/screens/ModelVersionDetails/ModelVersionDetailsHeaderActions.tsx
@@ -21,6 +21,9 @@ import { useIsAreaAvailable, SupportedArea } from '~/concepts/areas';
 import StartRunModal from '~/pages/pipelines/global/modelCustomization/startRunModal/StartRunModal';
 import { useModelVersionTuningData } from '~/concepts/modelRegistry/hooks/useModelVersionTuningData';
 import { getModelCustomizationPath } from '~/routes/pipelines/modelCustomization';
+import useDeployButtonState from '~/pages/modelServing/screens/projects/useDeployButtonState';
+import useModelArtifactsByVersionId from '~/concepts/modelRegistry/apiHooks/useModelArtifactsByVersionId';
+import { isOciModelUri } from '~/pages/modelServing/utils';
 
 interface ModelVersionsDetailsHeaderActionsProps {
   mv: ModelVersion;
@@ -51,6 +54,9 @@ const ModelVersionsDetailsHeaderActions: React.FC<ModelVersionsDetailsHeaderActi
     mv,
     registeredModel,
   );
+  const [modelArtifacts] = useModelArtifactsByVersionId(mv.id);
+  const isOciModel = isOciModelUri(modelArtifacts.items.map((artifact) => artifact.uri)[0]);
+  const deployButtonState = useDeployButtonState(isOciModel);
 
   if (!preferredModelRegistry) {
     return null;
@@ -59,17 +65,19 @@ const ModelVersionsDetailsHeaderActions: React.FC<ModelVersionsDetailsHeaderActi
   return (
     <ActionList className="pf-v5-u-display-flex">
       <ActionListGroup className="pf-v5-u-flex-1">
-        <ActionListItem>
-          <Button
-            id="deploy-button"
-            aria-label="Deploy version"
-            ref={tooltipRef}
-            variant={ButtonVariant.primary}
-            onClick={() => setIsDeployModalOpen(true)}
-          >
-            Deploy
-          </Button>
-        </ActionListItem>
+        {deployButtonState.visible && (
+          <ActionListItem>
+            <Button
+              id="deploy-button"
+              aria-label="Deploy version"
+              ref={tooltipRef}
+              variant={ButtonVariant.primary}
+              onClick={() => setIsDeployModalOpen(true)}
+            >
+              Deploy
+            </Button>
+          </ActionListItem>
+        )}
         {isFineTuningEnabled && (
           <ActionListItem
             className="pf-v5-u-w-100"
