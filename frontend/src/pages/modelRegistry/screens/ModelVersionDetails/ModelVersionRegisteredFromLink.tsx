1d9e156e869a7b85cb210a92675605075ed50dd2
Emmanuel Ikeola <eikeola@redhat.com>
2025-05-07 14:06:16 +0100

    Model catalog link enabled when model catalog featureflag is disabled (#4118)
    * Disabled model catalog link when model catalog featureflag is disabled
    
    * modified cypress test for modelVersionDetails
    
    * removed inline css
    
    * modified cypress test to use id

--- frontend/src/pages/modelRegistry/screens/ModelVersionDetails/ModelVersionRegisteredFromLink.tsx
+++ frontend/src/pages/modelRegistry/screens/ModelVersionDetails/ModelVersionRegisteredFromLink.tsx
@@ -17,14 +17,22 @@ import {
 
 type ModelVersionRegisteredFromLinkProps = {
   modelArtifact: ModelArtifact;
+  isModelCatalogAvailable: boolean;
 };
 
 const ModelVersionRegisteredFromLink: React.FC<ModelVersionRegisteredFromLinkProps> = ({
   modelArtifact,
+  isModelCatalogAvailable,
 }) => {
   const { projects } = React.useContext(ProjectsContext);
   const registeredFromCatalogDetails = modelSourcePropertiesToCatalogParams(modelArtifact);
   const registeredFromPipelineDetails = modelSourcePropertiesToPipelineRunRef(modelArtifact);
+
+  const registeredfromText = (
+    <span className="pf-v6-u-font-weight-bold">
+      {registeredFromCatalogDetails?.modelName} ({registeredFromCatalogDetails?.tag})
+    </span>
+  );
 
   if (!registeredFromCatalogDetails && !registeredFromPipelineDetails) {
     return null;
@@ -38,7 +46,7 @@ const ModelVersionRegisteredFromLink: React.FC<ModelVersionRegisteredFromLinkPro
 
       const renderRunLink = registeredPipelineProject ? (
         <Link
-          style={{ fontWeight: 'var(--pf-t--global--font--weight--body--bold)' }}
+          className="pf-v6-u-font-weight-bold"
           to={globalPipelineRunDetailsRoute(
             registeredFromPipelineDetails.project,
             registeredFromPipelineDetails.runId,
@@ -109,12 +117,16 @@ const ModelVersionRegisteredFromLink: React.FC<ModelVersionRegisteredFromLinkPro
     if (registeredFromCatalogDetails) {
       const catalogModelUrl = getCatalogModelDetailsRoute(registeredFromCatalogDetails);
       return (
-        <Link to={catalogModelUrl} data-testid="registered-from-catalog">
-          <span style={{ fontWeight: 'var(--pf-t--global--font--weight--body--bold)' }}>
-            {registeredFromCatalogDetails.modelName} ({registeredFromCatalogDetails.tag})
-          </span>{' '}
+        <>
+          {isModelCatalogAvailable ? (
+            <Link to={catalogModelUrl} data-testid="registered-from-catalog">
+              {registeredfromText}
+            </Link>
+          ) : (
+            registeredfromText
+          )}{' '}
           in Model catalog
-        </Link>
+        </>
       );
     }
 
