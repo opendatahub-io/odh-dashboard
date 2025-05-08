fd04bfaacf57364a5b4593da11163d4c583f8c69
katieperry4 <katelynnperry99@gmail.com>
2025-05-08 15:36:51 -0400

    wip modal

---
+++ frontend/src/concepts/roleBinding/RoleBindingPermissionsChangeModal.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { Button } from '@patternfly/react-core';
+import { Modal, ModalVariant } from '@patternfly/react-core/deprecated';
+
+type RoleBindingPermissionsChangeModalProps = {
+  onClose: () => void;
+  onEdit: () => void;
+  onDelete: () => void;
+  isDeleting: boolean;
+};
+
+const RoleBindingPermissionsChangeModal: React.FC<RoleBindingPermissionsChangeModalProps> = ({
+  onClose,
+  onEdit,
+  onDelete,
+  isDeleting,
+}) => {
+  const textToShow = isDeleting ? 'Delete' : 'Edit';
+  const modalActions = [
+    <Button key="confirm" variant="primary" onClick={isDeleting ? onDelete : onEdit}>
+      {isDeleting ? 'Delete' : 'Save'}
+    </Button>,
+    <Button data-id="cancel-button" key="cancel" variant="secondary" onClick={onClose}>
+      Cancel
+    </Button>,
+  ];
+  return (
+    <Modal
+      aria-label="Role binding permissions change modal"
+      appendTo={document.body}
+      variant={ModalVariant.small}
+      title={`Confirm ${textToShow.toLowerCase()}`}
+      isOpen
+      showClose
+      onClose={onClose}
+      actions={modalActions}
+    >
+      Are you sure you want to {isDeleting ? 'delete' : 'edit'} this role binding?{' '}
+      {isDeleting ? 'Deleting' : 'Editing'} your permissions could result in loss of access to the
+      project.
+    </Modal>
+  );
+};
+
+export default RoleBindingPermissionsChangeModal;
