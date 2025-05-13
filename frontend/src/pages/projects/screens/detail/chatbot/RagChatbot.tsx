65bb9500c7c5860d753cf9ab51070a45d2423c02
Agnieszka Gancarczyk <agagancarczyk@gmail.com>
2025-05-13 17:21:29 +0100

    Refactor
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>

--- frontend/src/pages/projects/screens/detail/chatbot/RagChatbot.tsx
+++ frontend/src/pages/projects/screens/detail/chatbot/RagChatbot.tsx
@@ -18,7 +18,8 @@ const RagChatbot: React.FC = () => {
     label: 'ChatBot uses AI. Check for mistakes.',
     popover: {
       title: 'Verify information',
-      description: `While ChatBot strives for accuracy, AI is experimental and can make mistakes. We cannot guarantee that all information provided by ChatBot is up to date or without error. You should always verify responses using reliable sources, especially for crucial information and decision making.`,
+      description:
+        'While ChatBot strives for accuracy, AI is experimental and can make mistakes. We cannot guarantee that all information provided by ChatBot is up to date or without error. You should always verify responses using reliable sources, especially for crucial information and decision making.',
       bannerImage: {
         src: 'https://cdn.dribbble.com/userupload/10651749/file/original-8a07b8e39d9e8bf002358c66fce1223e.gif',
         alt: 'Image for footnote popover',
