87e41802dcc99c2c5ca9ca89e12a86c9a635c6f5
Ashley McEntee <123661468+ashley-o0o@users.noreply.github.com>
2025-05-16 11:28:32 -0400

    Small update to remove documentation link (#4196)

--- frontend/src/pages/clusterSettings/TelemetrySettings.tsx
+++ frontend/src/pages/clusterSettings/TelemetrySettings.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Checkbox, Content, ContentVariants } from '@patternfly/react-core';
+import { Checkbox } from '@patternfly/react-core';
 import SettingSection from '~/components/SettingSection';
 
 type TelemetrySettingsProps = {
@@ -20,22 +20,7 @@ const TelemetrySettings: React.FC<TelemetrySettingsProps> = ({
   }, [initialValue, setEnabled]);
 
   return (
-    <SettingSection
-      title="Usage data collection"
-      footer={
-        <Content component={ContentVariants.small}>
-          For more information see the{' '}
-          <Content
-            component={ContentVariants.a}
-            href="https://docs.redhat.com/en/documentation/red_hat_openshift_ai_cloud_service/1/html/managing_resources/managing-collection-of-usage-data"
-            target="_blank"
-          >
-            documentation
-          </Content>
-          .
-        </Content>
-      }
-    >
+    <SettingSection title="Usage data collection">
       <Checkbox
         label="Allow collection of usage data"
         isChecked={enabled}
