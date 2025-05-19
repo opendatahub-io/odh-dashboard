6451d5e183a245930c14beca29887d986c58fdbc
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2025-05-19 21:50:54 +0200

    Revamp dockerfile and dependencies to improve konflux builds (#4232)

--- frontend/src/utilities/__tests__/markdown.spec.ts
+++ frontend/src/utilities/__tests__/markdown.spec.ts
@@ -2,9 +2,10 @@ import { markdownConverter } from '~/utilities/markdown';
 
 describe('markdownConverter', () => {
   it('should convert markdown to sanitized HTML and add hook to transform anchor tags', () => {
-    const mockMarkdown = '## Heading\n[Link](https://example.com)';
+    const mockMarkdown = '## Heading\n\n[Link](https://example.com)';
     const expectedHtml =
-      '<h2>Heading</h2>\n<p><a target="_blank" rel="noopener noreferrer" href="https://example.com">Link</a></p>';
+      '<h2>Heading</h2>\n' +
+      '<p><a href="https://example.com" rel="noopener noreferrer" target="_blank">Link</a></p>';
 
     const result = markdownConverter.makeHtml(mockMarkdown);
     expect(result).toEqual(expectedHtml);
