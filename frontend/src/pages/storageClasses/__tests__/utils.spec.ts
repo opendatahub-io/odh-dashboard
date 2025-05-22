43335b8c7e96b565d23352d7b42590baefa41b0c
Anish Surti <asurti@redhat.com>
2025-05-22 10:52:52 -0400

    fixed check and added unit tests

---
+++ frontend/src/pages/storageClasses/__tests__/utils.spec.ts
@@ -0,0 +1,42 @@
+import { AccessMode, StorageProvisioner, provisionerAccessModes } from '../storageEnums';
+import { getSupportedAccessModesForProvisioner, getDefaultAccessModeSettings } from '../utils';
+
+describe('getSupportedAccessModesForProvisioner', () => {
+  it('should return correct access modes for a known provisioner', () => {
+    const provisioner = StorageProvisioner.AWS_EBS;
+    const expectedModes = provisionerAccessModes[provisioner];
+    expect(getSupportedAccessModesForProvisioner(provisioner)).toEqual(expectedModes);
+  });
+});
+
+describe('getDefaultAccessModeSettings', () => {
+  it('should set ROX to false and other supported modes to true', () => {
+    const supportedModes = [AccessMode.RWO, AccessMode.RWX, AccessMode.ROX, AccessMode.RWOP];
+    const expectedSettings = {
+      [AccessMode.RWO]: true,
+      [AccessMode.RWX]: true,
+      [AccessMode.ROX]: false,
+      [AccessMode.RWOP]: true,
+    };
+    expect(getDefaultAccessModeSettings(supportedModes)).toEqual(expectedSettings);
+  });
+
+  it('should set all supported modes to true if ROX is not present', () => {
+    const supportedModes = [AccessMode.RWO, AccessMode.RWX, AccessMode.RWOP];
+    const expectedSettings = {
+      [AccessMode.RWO]: true,
+      [AccessMode.RWX]: true,
+      [AccessMode.RWOP]: true,
+    };
+    expect(getDefaultAccessModeSettings(supportedModes)).toEqual(expectedSettings);
+  });
+
+  it('should handle a mix of modes correctly', () => {
+    const supportedModes = [AccessMode.RWO, AccessMode.ROX];
+    const expectedSettings = {
+      [AccessMode.RWO]: true,
+      [AccessMode.ROX]: false,
+    };
+    expect(getDefaultAccessModeSettings(supportedModes)).toEqual(expectedSettings);
+  });
+});
