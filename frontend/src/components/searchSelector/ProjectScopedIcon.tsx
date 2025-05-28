24d8ed38bc6c05d4532d54f33e8a2b6edd329ae2
Claudia Alphonse <claudialphonse@gmail.com>
2025-05-29 00:29:59 +0530

    feat(RHOAIENG-23663):added scoped label,projectscoped icon and toggle content components and enum for scoped type

---
+++ frontend/src/components/searchSelector/ProjectScopedIcon.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import TypedObjectIcon from '~/concepts/design/TypedObjectIcon';
+import GlobalIcon from '~/images/icons/GlobalIcon';
+import { ProjectObjectType } from '~/concepts/design/utils';
+
+export type ProjectScopedIconProps = {
+  isProject: boolean;
+  style?: React.CSSProperties;
+  alt?: string;
+};
+
+const ProjectScopedIcon: React.FC<ProjectScopedIconProps> = ({ isProject, style, alt }) =>
+  isProject ? (
+    <TypedObjectIcon alt={alt ?? ''} resourceType={ProjectObjectType.project} style={style} />
+  ) : (
+    <GlobalIcon style={style} />
+  );
+
+export default ProjectScopedIcon;
