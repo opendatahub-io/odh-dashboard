d04867b2aac8a4d690a9474cdb753d1161ddfd83
Fede Alonso <fealonso@redhat.com>
2025-05-28 17:03:30 +0200

    cypress e2e fixes (#4267)
    * testManifestLinks: Validate multiple HTTP status code responses (#4222)
    
    * testManifestLinks: Accept additional valid HTTP status codes in manifest URLs
    
    Add support for common successful responses (201, 202, 204), redirects (301, 302, 307, 308),
    and rate limiting (429) in testManifestLinks.cy.ts.
    
    Signed-off-by: manosnoam <nmanos@redhat.com>
    
    * Add urlValidator.ts to check URLs in the manifest files via Node.js
    
    Signed-off-by: manosnoam <nmanos@redhat.com>
    
    ---------
    
    Signed-off-by: manosnoam <nmanos@redhat.com>
    Co-authored-by: Fede Alonso <fealonso@redhat.com>
    
    * Make urlValidator.ts Retry if network errors occur (#4229)
    
    Signed-off-by: manosnoam <nmanos@redhat.com>
    
    * test : Fix multiModelAdminCreation (#4241)
    
    * Reload the page when the status is Succeeded
    
    * Add debug logs
    
    * lint fix
    
    * Delete frontend/src/__tests__/cypress/config.json
    
    * fix: testProjectAdminPermissions test (#4250)
    
    * fix: testProjectAdminPermissions test
    
    * lint fixes
    
    * Fix Cypress URL regex and increase validation timeout and retries (#4246)
    
    Signed-off-by: manosnoam <nmanos@redhat.com>
    Co-authored-by: Fede Alonso <fealonso@redhat.com>
    
    * test: support not waiting for oc delete (#4244)
    
    * test: Support not waiting for oc delete
    
    * lint fixes
    
    * add wait=false in all the e2e tests teardown
    
    * wait project to be deleted completely before recreating it
    
    * Print cypress step(), exec() and log() to terminal (#4255)
    
    Signed-off-by: manosnoam <nmanos@redhat.com>
    
    * Cypress e2e Test - Verify a model registry can be created and deleted (#4257)
    
    * adding new test
    
    * implement existing selector page objects
    
    ---------
    
    Co-authored-by: Fede Alonso <fealonso@redhat.com>
    
    * Cypress parallel tests resources teardown (#4264)
    
    * Support ignoreNotFound when doing oc delete
    
    * modify the cleanup
    
    * add debug log
    
    * add wait too
    
    * delete the mocha after() override
    
    * lint fix
    
    ---------
    
    Signed-off-by: manosnoam <nmanos@redhat.com>
    Co-authored-by: Noam Manos <nmanos@redhat.com>
    Co-authored-by: Conor O'Malley <97108400+ConorOM1@users.noreply.github.com>

--- frontend/src/__tests__/cypress/cypress/utils/projectChecker.ts
+++ frontend/src/__tests__/cypress/cypress/utils/projectChecker.ts
@@ -20,7 +20,7 @@ export const createCleanProject = (projectName: string): void => {
   verifyOpenShiftProjectExists(projectName).then((exists) => {
     if (exists) {
       cy.log(`Project ${projectName} already exists. Deleting it.`);
-      deleteOpenShiftProject(projectName, { timeout: 300000 });
+      deleteOpenShiftProject(projectName, { wait: true });
     }
     cy.log(`Creating project ${projectName}`);
     createAndVerifyProject(projectName);
