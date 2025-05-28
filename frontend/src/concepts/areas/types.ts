6b87e36d278541b7b4f1cafe91edaf652ca771c4
katieperry4 <katelynnperry99@gmail.com>
2025-05-28 16:23:05 -0400

    quick fix

--- frontend/src/concepts/areas/types.ts
+++ frontend/src/concepts/areas/types.ts
@@ -74,7 +74,6 @@ export enum SupportedArea {
 
   /* Plugins */
   PLUGIN_MODEL_SERVING = 'model-serving-plugin',
-  //PLUGIN_MODEL_SERVING = 'plugin-model-serving',
 
   /* RAG & Agentic */
   LLAMA_STACK_CHAT_BOT = 'llama-stack-chat-bot',
