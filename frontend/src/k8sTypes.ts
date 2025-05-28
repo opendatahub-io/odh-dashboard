232ac832f0f8cc33107fcb5ffc5b8892ef08efdb
Anish Surti <asurti@redhat.com>
2025-05-28 11:18:36 -0400

    use record

--- frontend/src/k8sTypes.ts
+++ frontend/src/k8sTypes.ts
@@ -2,7 +2,6 @@ import { K8sResourceCommon, MatchExpression } from '@openshift/dynamic-plugin-sd
 import { EitherNotBoth } from '@openshift/dynamic-plugin-sdk';
 import { AwsKeys } from '~/pages/projects/dataConnections/const';
 import { DataScienceStackComponent, StackComponent } from '~/concepts/areas/types';
-import { AccessMode } from '~/pages/storageClasses/storageEnums';
 import {
   ContainerResourceAttributes,
   ContainerResources,
@@ -57,9 +56,7 @@ export type StorageClassConfig = {
   isDefault: boolean;
   lastModified: string;
   description?: string;
-  accessModeSettings: {
-    [key in AccessMode]?: boolean;
-  };
+  accessModeSettings: Record<string, boolean>;
 };
 
 export enum MetadataAnnotation {
