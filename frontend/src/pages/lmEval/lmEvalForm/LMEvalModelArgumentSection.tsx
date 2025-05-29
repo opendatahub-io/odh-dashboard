88944bc826c4345a9a83ea180b43c73bee9260e1
Purva Naik <pnaik@redhat.com>
2025-05-29 22:09:33 +0530

    static lmEval form (#4266)
    * static lmEval form
    
    * change the folder name

---
+++ frontend/src/pages/lmEval/lmEvalForm/LMEvalModelArgumentSection.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {
+  Button,
+  Content,
+  FormGroup,
+  FormSection,
+  Icon,
+  Popover,
+  Radio,
+  TextInput,
+} from '@patternfly/react-core';
+import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
+import { LmModelArgument } from '~/pages/lmEval/types';
+
+type LmEvalModelArgumentSectionProps = {
+  modelArgument: LmModelArgument;
+  setModelArgument: (modelArgument: LmModelArgument) => void;
+};
+
+const LmEvalModelArgumentSection: React.FC<LmEvalModelArgumentSectionProps> = ({
+  modelArgument,
+  setModelArgument,
+}) => (
+  <FormSection
+    title={
+      <>
+        <Content className="pf-v6-c-form__section-title">Model arguments</Content>
+        <Content component="p" className="odh-form-section__desc">
+          Details about the model you are evaluating.
+        </Content>
+      </>
+    }
+  >
+    <FormGroup label="Model">{modelArgument.name || '-'}</FormGroup>
+    <FormGroup label="Url">{modelArgument.url || '-'}</FormGroup>
+    <FormGroup
+      label="Tokenized requests"
+      labelHelp={
+        <Popover bodyContent={<></>}>
+          <Button
+            icon={
+              <Icon isInline>
+                <OutlinedQuestionCircleIcon />
+              </Icon>
+            }
+            variant="plain"
+            isInline
+          />
+        </Popover>
+      }
+    >
+      <Radio
+        name="model-tokenized-request-true-radio"
+        id="model-tokenized-request-true-radio"
+        label="True"
+        isChecked={modelArgument.tokenizedRequest}
+        onChange={() => setModelArgument({ ...modelArgument, tokenizedRequest: true })}
+      />
+      <Radio
+        name="model-tokenized-request-false-radio"
+        id="model-tokenized-request-false-radio"
+        label="False"
+        isChecked={!modelArgument.tokenizedRequest}
+        onChange={() => setModelArgument({ ...modelArgument, tokenizedRequest: false })}
+      />
+    </FormGroup>
+    <FormGroup
+      label="Tokenizer"
+      isRequired
+      labelHelp={
+        <Popover bodyContent={<></>}>
+          <Button
+            icon={
+              <Icon isInline>
+                <OutlinedQuestionCircleIcon />
+              </Icon>
+            }
+            variant="plain"
+            isInline
+          />
+        </Popover>
+      }
+    >
+      <TextInput
+        aria-label="Tokenizer text input"
+        value={modelArgument.tokenizer}
+        onChange={(_event, v) => setModelArgument({ ...modelArgument, tokenizer: v })}
+      />
+    </FormGroup>
+  </FormSection>
+);
+
+export default LmEvalModelArgumentSection;
