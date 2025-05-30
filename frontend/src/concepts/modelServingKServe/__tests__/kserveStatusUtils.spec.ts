d8f935f56aec59e80946fd56978436c76b7b8323
emilys314 <esamoylo@redhat.com>
2025-05-30 09:00:30 -0400

    Move jest test too for kserve status utils

--- frontend/src/concepts/modelServingKServe/__tests__/kserveStatusUtils.spec.ts
+++ frontend/src/concepts/modelServingKServe/__tests__/kserveStatusUtils.spec.ts
@@ -1,10 +1,10 @@
+import { mockPodK8sResource } from '#~/__mocks__/mockPodK8sResource';
+import { mockInferenceServiceK8sResource } from '#~/__mocks__';
 import {
   checkModelStatus,
   getInferenceServiceStatusMessage,
   getInferenceServiceLastFailureReason,
 } from '#~/concepts/modelServingKServe/kserveStatusUtils';
-import { mockPodK8sResource } from '#~/__mocks__/mockPodK8sResource';
-import { mockInferenceServiceK8sResource } from '#~/__mocks__';
 
 describe('checkModelStatus', () => {
   it('Should return true when pod fails to schedule due to insufficient resources.', () => {
