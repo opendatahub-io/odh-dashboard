8e3f112ef014acf2947fcbf8ad321919e1a0cce6
Jill Pelavin <jpelavin@redhat.com>
2025-06-03 15:49:12 -0400

    accessMode columns (#4301)
    * clean import; dev done on another branch
    
    * lint fixes
    
    * review feedback
    
    * data is now ts (not tsx) again
    
    * final review feedback
    
    * one more fix
    
    * removing 'as const'

---
+++ frontend/src/pages/projects/screens/detail/storage/AccessModeLabel.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { AccessMode } from '#~/pages/storageClasses/storageEnums';
+import PopoverListContent from '#~/components/PopoverListContent';
+
+// Safely convert string to AccessMode
+export const toAccessMode = (mode: string | undefined): AccessMode | undefined => {
+  if (mode) {
+    const validModes = Object.values(AccessMode);
+    return validModes.find((validMode) => validMode === mode);
+  }
+  return undefined;
+};
+
+const ACCESS_MODE_MAP: Record<AccessMode, string> = {
+  [AccessMode.RWO]: 'ReadWriteOnce (RWO)',
+  [AccessMode.RWOP]: 'ReadWriteOncePod (RWOP)',
+  [AccessMode.RWX]: 'ReadWriteMany (RWX)',
+  [AccessMode.ROX]: 'ReadOnlyMany (ROX)',
+};
+
+const ACCESS_MODE_DESCRIPTIONS: Record<AccessMode, string> = {
+  [AccessMode.RWO]: 'means that the storage can be attached to a single workbench at a given time.',
+  [AccessMode.RWOP]:
+    'means that the storage can be attached to a single pod on a single node as read-write',
+  [AccessMode.RWX]: 'means that the storage can be attached to many workbenches simultaneously.',
+  [AccessMode.ROX]: 'means that the storage can be attached to many workbenches as read-only.',
+};
+
+export const AccessModeColumnInfo = (
+  <PopoverListContent
+    leadText="Access mode is a Kubernetes concept that determines how nodes can interact with the volume."
+    listItems={[
+      <React.Fragment key="rwo">
+        <strong>ReadWriteOnce (RWO):</strong> {ACCESS_MODE_DESCRIPTIONS[AccessMode.RWO]}
+      </React.Fragment>,
+      <React.Fragment key="rwx">
+        <strong>ReadWriteMany (RWX):</strong> {ACCESS_MODE_DESCRIPTIONS[AccessMode.RWX]}
+      </React.Fragment>,
+      <React.Fragment key="rox">
+        <strong>ReadOnlyMany (ROX):</strong> {ACCESS_MODE_DESCRIPTIONS[AccessMode.ROX]}
+      </React.Fragment>,
+      <React.Fragment key="rwop">
+        <strong>ReadWriteOncePod (RWOP):</strong> {ACCESS_MODE_DESCRIPTIONS[AccessMode.RWOP]}
+      </React.Fragment>,
+    ]}
+  />
+);
+
+type Props = {
+  accessModeString: string | undefined;
+};
+
+const AccessModeLabel: React.FC<Props> = ({ accessModeString }) => {
+  const accessMode = toAccessMode(accessModeString);
+  const contents = accessMode ? ACCESS_MODE_MAP[accessMode] : '-';
+
+  return <>{contents}</>;
+};
+
+export default AccessModeLabel;
--- frontend/src/pages/projects/screens/spawner/storage/constants.ts
+++ frontend/src/pages/projects/screens/spawner/storage/constants.ts
@@ -1,4 +1,5 @@
 import { SortableData, kebabTableColumn } from '#~/components/table';
+import { AccessModeColumnInfo } from '#~/pages/projects/screens/detail/storage/AccessModeLabel.tsx';
 import { StorageData } from '#~/pages/projects/types';
 import { MOUNT_PATH_PREFIX } from './const';
 
@@ -13,6 +14,19 @@ export const clusterStorageTableColumns: SortableData<StorageData>[] = [
     label: 'Name',
     field: 'name',
     sortable: false,
+  },
+  {
+    label: 'Access mode',
+    field: 'accessMode',
+    sortable: false,
+    width: 30,
+    info: {
+      popover: AccessModeColumnInfo,
+      popoverProps: {
+        showClose: true,
+        maxWidth: '500px',
+      },
+    },
   },
   {
     label: 'Storage size',
@@ -32,4 +46,5 @@ export const defaultClusterStorage = {
   description: '',
   size: '20Gi',
   mountPath: MOUNT_PATH_PREFIX,
+  accessMode: 'ReadWriteOnce',
 };
