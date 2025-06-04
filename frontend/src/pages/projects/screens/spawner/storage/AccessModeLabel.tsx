b23b81dc4c36647696dbc5d4d80ed7df6a4e254e
rsun19 <robertssun1234@gmail.com>
2025-06-04 17:59:40 -0400

    added test id to label

--- frontend/src/pages/projects/screens/spawner/storage/AccessModeLabel.tsx
+++ frontend/src/pages/projects/screens/spawner/storage/AccessModeLabel.tsx
@@ -3,7 +3,13 @@ import * as React from 'react';
 import { AccessMode, AccessModeLabelMap } from '#~/pages/storageClasses/storageEnums';
 
 const AccessModeLabel: React.FC<{ accessMode: AccessMode }> = ({ accessMode }) => (
-  <Label key={accessMode} color="blue" isCompact variant="outline">
+  <Label
+    key={accessMode}
+    data-testid={`${accessMode}-label`}
+    color="blue"
+    isCompact
+    variant="outline"
+  >
     {AccessModeLabelMap[accessMode]}
   </Label>
 );
