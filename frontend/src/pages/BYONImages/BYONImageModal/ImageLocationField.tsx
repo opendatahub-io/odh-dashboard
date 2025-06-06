508c5dc432216776add31b953dc56a0357653dcf
Derek Xu <derekxu888@gmail.com>
2025-06-06 13:02:29 -0400

    Content issues related to "notebooks" -> "workbenches" (#4286)
    * adjust manifest
    
    * revert manifest change
    
    * change create jupyter notebook manifest
    
    * test manifest change
    
    * change test
    
    * revert test changes
    
    * change jupyter notebook to basic workbench
    
    * change progression step name
    
    * make by text appear conditionally
    
    * remove file

--- frontend/src/pages/BYONImages/BYONImageModal/ImageLocationField.tsx
+++ frontend/src/pages/BYONImages/BYONImageModal/ImageLocationField.tsx
@@ -1,12 +1,5 @@
 import * as React from 'react';
-import {
-  FormGroup,
-  FormHelperText,
-  HelperText,
-  HelperTextItem,
-  Popover,
-  TextInput,
-} from '@patternfly/react-core';
+import { FormGroup, Popover, Stack, StackItem, TextInput } from '@patternfly/react-core';
 import { HelpIcon } from '@patternfly/react-icons';
 
 type ImageLocationFieldProps = {
@@ -27,14 +20,19 @@ const ImageLocationField: React.FC<ImageLocationFieldProps> = ({
     labelHelp={
       <Popover
         aria-label="Image location help popover"
-        headerContent="Location examples"
         hasAutoWidth
         bodyContent={
-          <>
-            <p>quay.io/my-repo/my-image:tag</p>
-            <p>quay.io/my-repo/my-image@sha256:xxxxxxxxxxxxx</p>
-            <p>docker.io/my-repo/my-image:tag</p>
-          </>
+          <Stack hasGutter>
+            <StackItem>
+              <p>The image location is the URL where the workbench image is hosted or stored.</p>
+              <p>This should be a valid web address such as:</p>
+            </StackItem>
+            <StackItem>
+              <p>quay.io/my-repo/my-image:tag</p>
+              <p>quay.io/my-repo/my-image@sha256:xxxxxxxxxxxxx</p>
+              <p>docker.io/my-repo/my-image:tag</p>
+            </StackItem>
+          </Stack>
         }
       >
         <HelpIcon />
@@ -54,13 +52,6 @@ const ImageLocationField: React.FC<ImageLocationFieldProps> = ({
       }}
       isDisabled={isDisabled}
     />
-    <FormHelperText>
-      <HelperText>
-        <HelperTextItem>
-          The address where the notebook image is located. See the help icon for examples.
-        </HelperTextItem>
-      </HelperText>
-    </FormHelperText>
   </FormGroup>
 );
 
