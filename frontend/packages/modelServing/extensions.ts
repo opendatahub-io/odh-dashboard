5a7be4060292819cde94333635fad2730182d5f2
Ashley McEntee <123661468+ashley-o0o@users.noreply.github.com>
2025-06-11 16:29:31 -0400

    Models platform selection (Part 1) (#4331)
    * Models platform selection
    
    * Complete extension set up for serve models
    
    * Comment fixes
    
    * Name updates

--- frontend/packages/modelServing/extensions.ts
+++ frontend/packages/modelServing/extensions.ts
@@ -3,6 +3,7 @@ import type {
   ProjectDetailsTab,
   AreaExtension,
   RouteExtension,
+  OverviewSectionExtension,
 } from '@odh-dashboard/plugin-core/extension-points';
 // Allow this import as it consists of types and enums only.
 // eslint-disable-next-line no-restricted-syntax
@@ -10,7 +11,13 @@ import { SupportedArea } from '@odh-dashboard/internal/concepts/areas/types';
 
 const PLUGIN_MODEL_SERVING = 'plugin-model-serving';
 
-const extensions: (AreaExtension | ProjectDetailsTab | HrefNavItemExtension | RouteExtension)[] = [
+const extensions: (
+  | AreaExtension
+  | ProjectDetailsTab
+  | HrefNavItemExtension
+  | RouteExtension
+  | OverviewSectionExtension
+)[] = [
   {
     type: 'app.area',
     properties: {
@@ -25,6 +32,17 @@ const extensions: (AreaExtension | ProjectDetailsTab | HrefNavItemExtension | Ro
       id: 'model-server', // same value as ProjectSectionID.MODEL_SERVER
       title: 'Models',
       component: () => import('./src/ModelsProjectDetailsTab'),
+    },
+    flags: {
+      required: [PLUGIN_MODEL_SERVING],
+    },
+  },
+  {
+    type: 'app.project-details/overview-section',
+    properties: {
+      id: 'model-server',
+      title: 'Serve Models',
+      component: () => import('./src/ServeModelsSection'),
     },
     flags: {
       required: [PLUGIN_MODEL_SERVING],
