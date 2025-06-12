01b35226c7cc24398d129b91693fd8c467b6ed36
katieperry4 <katelynnperry99@gmail.com>
2025-06-12 14:41:00 -0400

    remove unnecessary label for test

--- frontend/src/__mocks__/mockInferenceServiceK8sResource.ts
+++ frontend/src/__mocks__/mockInferenceServiceK8sResource.ts
@@ -112,7 +112,6 @@ export const mockInferenceServiceK8sResource = ({
   metadata: {
     annotations: {
       'openshift.io/display-name': displayName,
-      'serving.kserve.io/stop': 'false',
       'serving.kserve.io/deploymentMode': isModelMesh
         ? DeploymentMode.ModelMesh
         : isKserveRaw
