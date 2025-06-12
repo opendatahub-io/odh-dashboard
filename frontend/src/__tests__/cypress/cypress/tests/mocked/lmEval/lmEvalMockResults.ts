b2acc09aac6e8b6f6d8ed7a5b519865a4ee5cb4d
Emmanuel Ikeola <eikeola@redhat.com>
2025-06-12 11:15:43 +0100

    added cypress tests

---
+++ frontend/src/__tests__/cypress/cypress/tests/mocked/lmEval/lmEvalMockResults.ts
@@ -0,0 +1,16 @@
+export const mockCompleteEvaluationResults = {
+  hellaswag: {
+    acc: { value: 0.85432, stderr: 0.02156 },
+    // eslint-disable-next-line camelcase
+    acc_norm: { value: 0.76543 },
+  },
+  // eslint-disable-next-line camelcase
+  arc_easy: {
+    acc: { value: 0.91234, stderr: 0.01234 },
+    // eslint-disable-next-line camelcase
+    acc_norm: { value: 0.89876, stderr: 0.00987 },
+  },
+  winogrande: {
+    acc: { value: 0.73456 },
+  },
+};
