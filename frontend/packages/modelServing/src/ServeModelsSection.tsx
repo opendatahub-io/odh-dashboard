675eeb5cb57a8e9b89cb0ae8c3ad0ad218e980b8
Ashley McEntee <123661468+ashley-o0o@users.noreply.github.com>
2025-06-13 16:10:27 -0400

    Models platform selection (Part 2)

--- frontend/packages/modelServing/src/ServeModelsSection.tsx
+++ frontend/packages/modelServing/src/ServeModelsSection.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import useServingPlatformStatuses from '@odh-dashboard/internal/pages/modelServing/useServingPlatformStatuses';
+import { getProjectModelServingPlatform } from '@odh-dashboard/internal/pages/modelServing/screens/projects/utils';
+import { ProjectDetailsContext } from '@odh-dashboard/internal/pages/projects/ProjectDetailsContext';
+import { ServingRuntimePlatform } from '@odh-dashboard/internal/types';
+import ServeModelsCard from './concepts/ModelPlatformSection';
 
-const serveModelsSection: React.FC = () => <>Serve Models</>;
+const ServeModelsSection: React.FC = () => {
+  const servingPlatformStatuses = useServingPlatformStatuses(true);
+  const {
+    modelMesh: { enabled: modelMeshEnabled },
+    platformEnabledCount,
+  } = servingPlatformStatuses;
 
-export default serveModelsSection;
+  const { currentProject } = React.useContext(ProjectDetailsContext);
+
+  const { platform: currentProjectServingPlatform } = getProjectModelServingPlatform(
+    currentProject,
+    servingPlatformStatuses,
+  );
+
+  if (platformEnabledCount > 1 && !currentProjectServingPlatform) {
+    return <ServeModelsCard isMultiPlatform={false} />;
+  }
+  return (
+    <ServeModelsCard
+      isMultiPlatform={
+        modelMeshEnabled && currentProjectServingPlatform === ServingRuntimePlatform.MULTI
+      }
+    />
+  );
+};
+
+export default ServeModelsSection;
