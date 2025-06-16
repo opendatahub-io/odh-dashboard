3d71dcc1e356da45c6b2934970f0bfde6e3231c0
emilys314 <esamoylo@redhat.com>
2025-06-16 12:13:29 -0400

    Add useProjectPlatform test

---
+++ frontend/packages/modelServing/src/__tests__/mockUtils.ts
@@ -0,0 +1,66 @@
+import { NamespaceApplicationCase } from '@odh-dashboard/internal/pages/projects/types';
+import type { ProjectKind } from '@odh-dashboard/internal/k8sTypes';
+import { ModelServingPlatform } from '../concepts/useProjectServingPlatform';
+
+export const mockModelServingPlatform = ({
+  id = 'kserve',
+  namespaceApplicationCase = NamespaceApplicationCase.KSERVE_PROMOTION,
+  enabledLabel = 'kserve',
+  enabledLabelValue = 'true',
+  title = 'KServe',
+  description = 'KServe',
+  selectText = 'Select',
+  enabledText = 'Enabled',
+  startHintTitle = 'KServe',
+  startHintDescription = 'KServe',
+  deployButtonText = 'Deploy',
+}: {
+  id?: string;
+  namespaceApplicationCase?: NamespaceApplicationCase;
+  enabledLabel?: string;
+  enabledLabelValue?: string;
+  title?: string;
+  description?: string;
+  selectText?: string;
+  enabledText?: string;
+  startHintTitle?: string;
+  startHintDescription?: string;
+  deployButtonText?: string;
+}): ModelServingPlatform => ({
+  type: 'model-serving.platform',
+  properties: {
+    id,
+    manage: {
+      namespaceApplicationCase,
+      enabledLabel,
+      enabledLabelValue,
+    },
+    enableCardText: {
+      title,
+      description,
+      selectText,
+      enabledText,
+    },
+    deployedModelsView: {
+      startHintTitle,
+      startHintDescription,
+      deployButtonText,
+    },
+  },
+});
+
+export const mockProjectWithPlatform = (
+  project: ProjectKind,
+  platform: ModelServingPlatform,
+): ProjectKind => {
+  const projectWithPlatform = {
+    ...project,
+    metadata: {
+      ...project.metadata,
+      labels: {
+        [platform.properties.manage.enabledLabel]: platform.properties.manage.enabledLabelValue,
+      },
+    },
+  };
+  return projectWithPlatform;
+};
