425efffe58a75667e828e91e029db298d229f09c
katieperry4 <katelynnperry99@gmail.com>
2025-06-17 15:48:56 -0400

    add global no projects page

--- frontend/packages/modelServing/src/components/global/GlobalDeploymentsView.tsx
+++ frontend/packages/modelServing/src/components/global/GlobalDeploymentsView.tsx
@@ -6,6 +6,7 @@ import type { ProjectKind } from '@odh-dashboard/internal/k8sTypes';
 import { GlobalNoModelsView } from './GlobalNoModelsView';
 import GlobalDeploymentsTable from './GlobalDeploymentsTable';
 import ModelServingProjectSelection from './ModelServingProjectSelection';
+import NoProjectsPage from './NoProjectsPage';
 import { ModelDeploymentsContext } from '../../concepts/ModelDeploymentsContext';
 
 type GlobalDeploymentsViewProps = {
@@ -13,21 +14,32 @@ type GlobalDeploymentsViewProps = {
 };
 
 const GlobalDeploymentsView: React.FC<GlobalDeploymentsViewProps> = ({ currentProject }) => {
-  const { deployments, loaded: deploymentsLoaded } = React.useContext(ModelDeploymentsContext);
+  const {
+    deployments,
+    loaded: deploymentsLoaded,
+    projects,
+  } = React.useContext(ModelDeploymentsContext);
   const hasDeployments = deployments && deployments.length > 0;
   const isLoading = !deploymentsLoaded;
+  const isEmpty = projects?.length === 0 || (!isLoading && !hasDeployments);
 
   return (
     <ApplicationsPage
       loaded={!isLoading}
-      empty={!hasDeployments}
-      emptyStatePage={<GlobalNoModelsView project={currentProject ?? undefined} />}
+      empty={isEmpty}
+      emptyStatePage={
+        projects?.length === 0 ? (
+          <NoProjectsPage />
+        ) : (
+          <GlobalNoModelsView project={currentProject ?? undefined} />
+        )
+      }
       description="Manage and view the health and performance of your deployed models."
       title={
         <TitleWithIcon title="Model deployments" objectType={ProjectObjectType.deployedModels} />
       }
       headerContent={
-        <ModelServingProjectSelection getRedirectPath={(ns: string) => `/modelServing/${ns}`} />
+        <ModelServingProjectSelection getRedirectPath={(ns: string) => `/model-serving/${ns}`} />
       }
     >
       <GlobalDeploymentsTable />
