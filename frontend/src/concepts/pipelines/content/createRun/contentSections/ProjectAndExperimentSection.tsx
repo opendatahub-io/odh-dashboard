29a3269d4dce0c5d8e84098ffbe7cc2f0fef8cbd
Arsheen Taj Syed <87820563+Taj010@users.noreply.github.com>
2025-06-24 10:46:58 -0400

    Moved the create new experiment button inside the experiment dropdown to resolve button cover dropdown (#4345)
    * base fix
    
    * moved the create experiment button to the drop down menu
    
    * fix file content
    
    * Update ProjectAndExperimentSection.tsx
    
    * Update ProjectAndExperimentSection and ExperimentSelector files
    
    * format fix
    
    * added test to check if the button exists
    
    * added button as sticky footer
    
    * fix pipelineCreateRuns.cy.ts
    
    * Update legacyHardwareProfiles.cy.ts
    
    * add divider to the footer button
    
    * add Cypress test for create new experiment button in experiment dropdown

--- frontend/src/concepts/pipelines/content/createRun/contentSections/ProjectAndExperimentSection.tsx
+++ frontend/src/concepts/pipelines/content/createRun/contentSections/ProjectAndExperimentSection.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import { FormGroup, FormSection, Stack, StackItem, Content } from '@patternfly/react-core';
-import { PlusCircleIcon } from '@patternfly/react-icons';
 import { ExperimentKF } from '#~/concepts/pipelines/kfTypes';
 import {
   CreateRunPageSections,
   runPageSectionTitles,
 } from '#~/concepts/pipelines/content/createRun/const';
 import { ActiveExperimentSelector } from '#~/concepts/pipelines/content/experiment/ExperimentSelector';
-import CreateExperimentButton from '#~/concepts/pipelines/content/experiment/CreateExperimentButton';
 
 type ProjectAndExperimentSectionProps = {
   projectName: string;
@@ -31,15 +29,6 @@ const ProjectAndExperimentSection: React.FC<ProjectAndExperimentSectionProps> = 
       <Stack hasGutter>
         <StackItem>
           <ActiveExperimentSelector selection={value?.display_name} onSelect={onChange} />
-        </StackItem>
-        <StackItem>
-          <CreateExperimentButton
-            variant="link"
-            icon={<PlusCircleIcon />}
-            onCreate={(experiment) => onChange(experiment)}
-          >
-            Create new experiment
-          </CreateExperimentButton>
         </StackItem>
       </Stack>
     </FormGroup>
