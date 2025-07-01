86e9ddfaf95487ca40fc952429a6a3c36b8bfdf7
Jill R. Pelavin <jpelavin@redhat.com>
2025-07-01 14:25:15 -0400

    better; need a break: next: compare tests (since files changed....)"

--- frontend/src/app/featureFlags/DevFeatureFlagsBanner.tsx
+++ frontend/src/app/featureFlags/DevFeatureFlagsBanner.tsx
@@ -1,25 +1,10 @@
-import {
-  Banner,
-  Button,
-  Checkbox,
-  Grid,
-  GridItem,
-  Split,
-  SplitItem,
-  Tooltip,
-  Modal,
-  ModalBody,
-  ModalFooter,
-  ModalHeader,
-  Content,
-} from '@patternfly/react-core';
+import { Banner, Button, Split, SplitItem, Tooltip } from '@patternfly/react-core';
 import { CloseIcon } from '@patternfly/react-icons';
 import * as React from 'react';
-import { definedFeatureFlags } from '#~/concepts/areas/const';
+
 import { FeatureFlag } from '#~/concepts/areas/types';
 import { DevFeatureFlags } from '#~/types';
-import { useDevFlags } from './useDevFeatureFlags';
-
+import FeatureFlagModal from '#~/app/featureFlags/FeatureFlagModal';
 // todo: change the props in the type! (and/or move to concepts/area/types ???? TODO)
 type Props = {
   dashboardConfig: Record<FeatureFlag | string, boolean | undefined>;
@@ -31,40 +16,16 @@ const DevFeatureFlagsBanner: React.FC<Props> = ({
   setDevFeatureFlag,
   resetDevFeatureFlags,
   setDevFeatureFlagQueryVisible,
+  isBannerVisible,
 }) => {
-  const devFlags = useDevFlags();
+
   const [isBannerHidden, setBannerHidden] = React.useState(false);
   const [isModalOpen, setModalOpen] = React.useState(false);
 
-  if (!devFeatureFlags || isBannerHidden) {
+  if (!isBannerVisible || !devFeatureFlags || isBannerHidden) {
     return null;
   }
 
-  const renderFlags = (flags: string[], fallbackFlags?: Record<string, boolean | undefined>) => (
-    <Grid hasGutter span={6} md={3}>
-      {flags.toSorted().map((key) => {
-        const value = devFeatureFlags[key] ?? fallbackFlags?.[key];
-        return (
-          <React.Fragment key={key}>
-            <GridItem>
-              <Checkbox
-                id={key}
-                data-testid={`${key}-checkbox`}
-                label={key}
-                isChecked={value ?? null}
-                onChange={(_, checked) => {
-                  setDevFeatureFlag(key, checked);
-                }}
-              />
-            </GridItem>
-            <GridItem data-testid={`${key}-value`}>{`${value ?? ''}${
-              key in devFeatureFlags ? ' (overridden)' : ''
-            }`}</GridItem>
-          </React.Fragment>
-        );
-      })}
-    </Grid>
-  );
   return (
     <>
       <Banner color="blue">
@@ -108,44 +69,16 @@ const DevFeatureFlagsBanner: React.FC<Props> = ({
         </Split>
       </Banner>
       {isModalOpen ? (
-        <Modal
-          data-testid="dev-feature-flags-modal"
-          variant="large"
-          isOpen
+        <FeatureFlagModal
           onClose={() => {
             setModalOpen(false);
             setDevFeatureFlagQueryVisible(false);
           }}
-        >
-          <ModalHeader title="Override Flags" />
-          <ModalBody>
-            <Content component="h2">Feature Flags</Content>
-            <Content component="p">
-              Feature flags default to the values defined in the dashboard config.
-            </Content>
-            {renderFlags(definedFeatureFlags, dashboardConfig)}
-
-            {devFlags.length > 0 ? (
-              <>
-                <Content component="h2">Dev Flags</Content>
-                <Content component="p">
-                  Dev flags default to inactive and can only be changed for the current session.
-                </Content>
-                {renderFlags(devFlags)}
-              </>
-            ) : null}
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              data-testid="reset-feature-flags-modal-button"
-              key="confirm"
-              variant="link"
-              onClick={() => resetDevFeatureFlags(false)}
-            >
-              Reset to defaults
-            </Button>
-          </ModalFooter>
-        </Modal>
+          dashboardConfig={dashboardConfig}
+          devFeatureFlags={devFeatureFlags}
+          setDevFeatureFlag={setDevFeatureFlag}
+          resetDevFeatureFlags={resetDevFeatureFlags}
+        />
       ) : null}
     </>
   );
