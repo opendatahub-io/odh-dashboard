937e2adce63f69ea027c7f21ff64f8000ee53910
emilys314 <esamoylo@redhat.com>
2025-07-01 15:49:58 -0400

    Remove unused kserve auth function

--- frontend/packages/kserve/src/useAuth.ts
+++ frontend/packages/kserve/src/useAuth.ts
@@ -3,9 +3,7 @@ import { SupportedArea } from '@odh-dashboard/internal/concepts/areas/types';
 import { DeploymentMode } from '@odh-dashboard/internal/k8sTypes';
 import type { KServeDeployment } from './deployments';
 
-export const useKServeDeploymentAuthEnabled = (deployment: KServeDeployment): boolean =>
-  deployment.model.metadata.annotations?.['security.opendatahub.io/enable-auth'] === 'true';
-
+// This isn't really a hook but I figure it should be used as one anyway
 export const useKServePlatformAuthEnabled = (deployment?: KServeDeployment): boolean => {
   const isAuthAvailable = useIsAreaAvailable(SupportedArea.K_SERVE_AUTH).status;
   const isKServeRaw =
