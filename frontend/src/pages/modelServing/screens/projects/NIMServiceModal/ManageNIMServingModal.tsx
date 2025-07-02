1ed0a14a7e90dd69c76ba65208571cce4edf78ac
mtalvi <mtalvi@redhat.com>
2025-07-02 17:44:51 +0300

    v1

--- frontend/src/pages/modelServing/screens/projects/NIMServiceModal/ManageNIMServingModal.tsx
+++ frontend/src/pages/modelServing/screens/projects/NIMServiceModal/ManageNIMServingModal.tsx
@@ -26,9 +26,10 @@ import {
   SecretKind,
   ServingRuntimeKind,
 } from '#~/k8sTypes';
+import { EMPTY_AWS_SECRET_DATA } from '#~/pages/projects/dataConnections/const';
 import useCustomServingRuntimesEnabled from '#~/pages/modelServing/customServingRuntimes/useCustomServingRuntimesEnabled';
 import DashboardModalFooter from '#~/concepts/dashboard/DashboardModalFooter';
-import { ModelServingSize, ServingRuntimeEditInfo } from '#~/pages/modelServing/screens/types';
+import { InferenceServiceStorageType, ModelServingSize, ServingRuntimeEditInfo } from '#~/pages/modelServing/screens/types';
 import ServingRuntimeSizeSection from '#~/pages/modelServing/screens/projects/ServingRuntimeModal/ServingRuntimeSizeSection';
 import NIMModelListSection from '#~/pages/modelServing/screens/projects/NIMServiceModal/NIMModelListSection';
 import NIMModelDeploymentNameSection from '#~/pages/modelServing/screens/projects/NIMServiceModal/NIMModelDeploymentNameSection';
@@ -71,6 +72,8 @@ const accessReviewResource: AccessReviewResourceAttributes = {
   resource: 'rolebindings',
   verb: 'create',
 };
+
+type PVCMode = 'create-new' | 'use-existing';
 
 type ManageNIMServingModalProps = {
   onClose: (submit: boolean) => void;
@@ -157,6 +160,9 @@ const ManageNIMServingModal: React.FC<ManageNIMServingModalProps> = ({
   const [storageClassName, setStorageClassName] = React.useState(
     deployedStorageClassName || defaultStorageClassName,
   );
+  const [pvcMode, setPvcMode] = React.useState<PVCMode>('create-new');
+  const [existingPvcName, setExistingPvcName] = React.useState<string>('');
+  const [modelPath, setModelPath] = React.useState<string>('/mnt/models/cache');
 
   React.useEffect(() => {
     if (pvc?.spec.storageClassName) {
@@ -204,13 +210,18 @@ const ManageNIMServingModal: React.FC<ManageNIMServingModalProps> = ({
 
   const baseInputValueValid = createDataServingRuntime.numReplicas >= 0 && isModelServerSizeValid;
 
+  const isExistingPvcValid = 
+    pvcMode === 'create-new' || 
+    (existingPvcName.trim() !== '' && modelPath.trim() !== '');
+
   const isDisabledInferenceService =
     actionInProgress ||
     createDataInferenceService.name.trim() === '' ||
     createDataInferenceService.project === '' ||
     !translatedName ||
     !baseInputValueValid ||
-    !podSpecOptionsState.hardwareProfile.isFormDataValid;
+    !podSpecOptionsState.hardwareProfile.isFormDataValid ||
+    !isExistingPvcValid; // new validation
 
   const { dashboardNamespace } = useDashboardNamespace();
   const templateName = useNIMTemplateName();
@@ -249,6 +260,9 @@ const ManageNIMServingModal: React.FC<ManageNIMServingModalProps> = ({
     podSpecOptionsState.hardwareProfile.resetFormData();
     podSpecOptionsState.modelSize.setSelectedSize(podSpecOptionsState.modelSize.sizes[0]);
     setAlertVisible(true);
+    setPvcMode('create-new');
+    setExistingPvcName('');
+    setModelPath('/mnt/models/cache');
   };
 
   const setErrorModal = (e: Error) => {
@@ -269,11 +283,12 @@ const ManageNIMServingModal: React.FC<ManageNIMServingModalProps> = ({
       editInfo?.inferenceServiceEditInfo?.spec.predictor.model?.runtime ||
       translateDisplayNameForK8s(createDataInferenceService.name, { safeK8sPrefix: 'nim-' });
 
-    const nimPVCName = getUniqueId('nim-pvc');
-    const finalServingRuntime =
-      !editInfo && servingRuntimeSelected
-        ? updateServingRuntimeTemplate(servingRuntimeSelected, nimPVCName)
-        : servingRuntimeSelected;
+  const nimPVCName = pvcMode === 'create-new' ? getUniqueId('nim-pvc') : existingPvcName;
+
+  const finalServingRuntime =
+    !editInfo && servingRuntimeSelected
+      ? updateServingRuntimeTemplate(servingRuntimeSelected, nimPVCName)
+      : servingRuntimeSelected;
 
     const submitServingRuntimeResources = getSubmitServingRuntimeResourcesFn(
       finalServingRuntime,
@@ -290,6 +305,16 @@ const ManageNIMServingModal: React.FC<ManageNIMServingModalProps> = ({
     );
 
     const inferenceServiceName = createDataInferenceService.k8sName;
+    if (pvcMode === 'use-existing') {
+      // For existing PVC, configure storage to use local path instead of remote URI
+      setCreateDataInferenceService('storage', {
+        type: InferenceServiceStorageType.EXISTING_URI,
+        path: modelPath,
+        dataConnection: '',
+        uri: modelPath,
+        awsData: EMPTY_AWS_SECRET_DATA,
+      });
+    }
     const submitInferenceServiceResource = getSubmitInferenceServiceResourceFn(
       createDataInferenceService,
       editInfo?.inferenceServiceEditInfo,
@@ -319,7 +344,9 @@ const ManageNIMServingModal: React.FC<ManageNIMServingModalProps> = ({
           if (await isSecretNeeded(namespace, NIM_NGC_SECRET_NAME)) {
             promises.push(createNIMSecret(namespace, 'nimPullSecret', true, false));
           }
-          promises.push(createNIMPVC(namespace, nimPVCName, pvcSize, false, storageClassName));
+          if (pvcMode === 'create-new') {
+            promises.push(createNIMPVC(namespace, nimPVCName, pvcSize, false, storageClassName));
+          }
         } else if (pvc && pvc.spec.resources.requests.storage !== pvcSize) {
           const updatePvcData = {
             size: pvcSize, // New size
@@ -402,7 +429,16 @@ const ManageNIMServingModal: React.FC<ManageNIMServingModalProps> = ({
               )}
             </StackItem>
             <StackItem>
-              <NIMPVCSizeSection pvcSize={pvcSize} setPvcSize={setPvcSize} />
+              <NIMPVCSizeSection 
+                pvcSize={pvcSize} 
+                setPvcSize={setPvcSize}
+                pvcMode={pvcMode}
+                setPvcMode={setPvcMode}
+                existingPvcName={existingPvcName}
+                setExistingPvcName={setExistingPvcName}
+                modelPath={modelPath}
+                setModelPath={setModelPath}
+              />
             </StackItem>
             {isRawAvailable && isServerlessAvailable && (
               <StackItem>
--- frontend/src/pages/modelServing/screens/projects/NIMServiceModal/NIMPVCSizeSection.tsx
+++ frontend/src/pages/modelServing/screens/projects/NIMServiceModal/NIMPVCSizeSection.tsx
@@ -1,31 +1,137 @@
 import React from 'react';
-import { HelperText, HelperTextItem, StackItem } from '@patternfly/react-core';
+import { 
+  HelperText, 
+  HelperTextItem, 
+  StackItem,
+  Radio,
+  Stack,
+  FormGroup,
+  TextInput  
+} from '@patternfly/react-core';
 import PVSizeField from '#~/pages/projects/components/PVSizeField';
 import { MEMORY_UNITS_FOR_SELECTION } from '#~/utilities/valueUnits';
+
+// Add new type for PVC mode
+type PVCMode = 'create-new' | 'use-existing';
 
 type NIMPVCSizeSectionProps = {
   pvcSize: string;
   setPvcSize: (value: string) => void;
+  pvcMode: PVCMode;
+  setPvcMode: (mode: PVCMode) => void;
+  // Add these new props
+  existingPvcName: string;
+  setExistingPvcName: (name: string) => void;
+  modelPath: string;
+  setModelPath: (path: string) => void;
 };
 
-const NIMPVCSizeSection: React.FC<NIMPVCSizeSectionProps> = ({ pvcSize, setPvcSize }) => (
+const NIMPVCSizeSection: React.FC<NIMPVCSizeSectionProps> = ({ 
+  pvcSize, 
+  setPvcSize,
+  pvcMode,
+  setPvcMode,
+  existingPvcName,
+  setExistingPvcName,
+  modelPath,
+  setModelPath
+}) => (
   <StackItem>
-    <PVSizeField
-      fieldID="pvc-size"
-      size={pvcSize}
-      setSize={(value: string) => setPvcSize(value)}
-      label="NVIDIA NIM storage size"
-      options={MEMORY_UNITS_FOR_SELECTION.filter((option) => option.unit !== 'Mi')} // Filtering out 'Mi'
-    />
-    <HelperText>
-      <HelperTextItem>
-        Specify the size of the cluster storage instance that will be created to store the
-        downloaded NVIDIA NIM.
-      </HelperTextItem>
-      <HelperTextItem>
-        Make sure your storage size is greater than the NIM size specified by NVIDIA.
-      </HelperTextItem>
-    </HelperText>
+    <Stack hasGutter>
+      {/* New: PVC Mode Selection */}
+      <StackItem>
+        <FormGroup label="Storage option" fieldId="pvc-mode">
+          <Radio
+            id="create-new-pvc"
+            name="pvc-mode"
+            label="Create new storage for model caching"
+            description="A new storage volume will be created and models will be downloaded at deployment time."
+            isChecked={pvcMode === 'create-new'}
+            onChange={() => setPvcMode('create-new')}
+          />
+          <Radio
+            id="use-existing-pvc"
+            name="pvc-mode"
+            label="Use existing storage with pre-cached models"
+            description="Use a storage volume that already contains downloaded models for faster deployment."
+            isChecked={pvcMode === 'use-existing'}
+            onChange={() => setPvcMode('use-existing')}
+          />
+        </FormGroup>
+      </StackItem>
+      {/* Existing: PVC Size Field - only show when creating new */}
+      {pvcMode === 'create-new' && (
+        <StackItem>
+          <PVSizeField
+            fieldID="pvc-size"
+            size={pvcSize}
+            setSize={(value: string) => setPvcSize(value)}
+            label="NVIDIA NIM storage size"
+            options={MEMORY_UNITS_FOR_SELECTION.filter((option) => option.unit !== 'Mi')}
+          />
+          <HelperText>
+            <HelperTextItem>
+              Specify the size of the cluster storage instance that will be created to store the
+              downloaded NVIDIA NIM.
+            </HelperTextItem>
+            <HelperTextItem>
+              Make sure your storage size is greater than the NIM size specified by NVIDIA.
+            </HelperTextItem>
+          </HelperText>
+        </StackItem>
+      )}
+      {pvcMode === 'use-existing' && (
+        <StackItem>
+          <Stack hasGutter>
+            <StackItem>
+              <FormGroup 
+                label="Existing storage name" 
+                fieldId="existing-pvc-name"
+                isRequired
+              >
+                <TextInput
+                  id="existing-pvc-name"
+                  value={existingPvcName}
+                  onChange={(_event, value) => setExistingPvcName(value)}
+                  placeholder="Enter PVC name (e.g., nim-pvc-f75f401f3966fvfnhxw6yvfc)"
+                />
+              </FormGroup>
+              <HelperText>
+                <HelperTextItem>
+                  Enter the exact name of the Persistent Volume Claim (PVC) that contains your pre-downloaded models.
+                </HelperTextItem>
+                <HelperTextItem>
+                  The PVC must exist in the same project/namespace and be accessible to the deployment.
+                </HelperTextItem>
+              </HelperText>
+            </StackItem>
+            
+            <StackItem>
+              <FormGroup 
+                label="Model path in storage" 
+                fieldId="model-path"
+                isRequired
+              >
+                <TextInput
+                  id="model-path"
+                  value={modelPath}
+                  onChange={(_event, value) => setModelPath(value)}
+                  placeholder="/mnt/models/cache"
+                />
+              </FormGroup>
+              <HelperText>
+                <HelperTextItem>
+                  Specify the directory path within the storage where your model files are located.
+                </HelperTextItem>
+                <HelperTextItem>
+                  This path will be mounted into the container and should contain the model files that NIM expects.
+                </HelperTextItem>
+              </HelperText>
+            </StackItem>
+          </Stack>
+        </StackItem>
+      )}
+    </Stack>
   </StackItem>
 );
 
