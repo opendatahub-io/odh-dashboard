8f64ba58cf31dd2c1549247e785450071c4e4596
Nana Nosirova <10577112+nananosirova@users.noreply.github.com>
2025-07-02 14:17:52 -0400

    Add filtered options

--- frontend/src/pages/experiments/screens/CompareRuns/CompareRuns.tsx
+++ frontend/src/pages/experiments/screens/CompareRuns/CompareRuns.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Plot from 'react-plotly.js';
 import { useSearchParams } from 'react-router-dom';
 import { find, findLast } from 'lodash-es';
 
+import Select from 'react-select';
+import { Button, Split, SplitItem, Stack, StackItem } from '@patternfly/react-core';
 import ApplicationsPage from '#~/pages/ApplicationsPage.tsx';
 import { CompareRunsSearchParam } from '#~/routes/experiments/registryBase.ts';
 import useExperimentRunsArtifactsMetrics from '#~/concepts/modelRegistry/apiHooks/useExperimentRunsArtifactsMetrics';
-import { transformDataToDimensions, getColorScaleConfigsForDimension } from './utils';
+import {
+  transformDataToDimensions,
+  getColorScaleConfigsForDimension,
+  getUniqueParameterNames,
+  Option,
+  filterMockDataByParameters,
+} from './utils';
 
 type CompareRunsProps = Omit<
   React.ComponentProps<typeof ApplicationsPage>,
@@ -23,10 +31,40 @@ const CompareRuns: React.FC<CompareRunsProps> = ({ ...pageProps }) => {
 
   const [runsData, loaded] = useExperimentRunsArtifactsMetrics(runIds);
 
-  const transformedData = transformDataToDimensions(runsData);
+  const paramsNames = React.useMemo(
+    () => getUniqueParameterNames(runsData, 'parameter'),
+    [runsData],
+  );
+  const metricsNames = React.useMemo(() => getUniqueParameterNames(runsData, 'metric'), [runsData]);
+
+  const paramOptions: Option[] = paramsNames.map((n) => ({ label: n, value: n }));
+  const metricOptions: Option[] = metricsNames.map((n) => ({ label: n, value: n }));
+
+  const [selectedParams, setSelectedParams] = React.useState<string[]>([]);
+  const [selectedMetrics, setSelectedMetrics] = React.useState<string[]>([]);
+
+  const [filteredData, setFilteredData] = React.useState<any[]>([]);
+
+  const isEmpty = selectedParams.length === 0 && selectedMetrics.length === 0;
+
+  useEffect(() => {
+    const includeNames = [...selectedParams, ...selectedMetrics];
+    if (isEmpty) {
+      setFilteredData([]);
+    } else {
+      const newData = filterMockDataByParameters(runsData, {
+        parameterNames: includeNames,
+        includeMetrics: true,
+        includeModelArtifacts: false,
+      });
+      setFilteredData(newData);
+    }
+  }, [selectedParams, selectedMetrics, runsData, isEmpty]);
+
+  const dimensions = React.useMemo(() => transformDataToDimensions(filteredData), [filteredData]);
 
   const lastMetricDimensionMetricKey = findLast(
-    runsData[0]?.items,
+    filteredData[0]?.items,
     (item) => item.artifactType === 'metric',
   )?.name;
 
@@ -38,21 +76,74 @@ const CompareRuns: React.FC<CompareRunsProps> = ({ ...pageProps }) => {
       loaded
       provideChildrenPadding
     >
-      <div>
+      <>
         <h3>Parallel Coordinates Plot Data</h3>
-        <Plot
-          data={[
-            {
-              type: 'parcoords',
-              line: getColorScaleConfigsForDimension(
-                find(transformedData, ['label', lastMetricDimensionMetricKey]),
-              ),
-              dimensions: transformedData,
-            },
-          ]}
-          layout={{ autosize: true, margin: { t: 50 } }}
-        />
-      </div>
+        <Split hasGutter>
+          <SplitItem isFilled>
+            <Stack hasGutter>
+              <StackItem>
+                <Select
+                  isMulti
+                  name="parameters"
+                  options={paramOptions}
+                  className="basic-multi-select"
+                  classNamePrefix="select"
+                  placeholder="Please select parameters"
+                  value={paramOptions.filter((o) => selectedParams.includes(o.value))}
+                  onChange={(options) =>
+                    setSelectedParams(Array.isArray(options) ? options.map((o) => o.value) : [])
+                  }
+                />
+              </StackItem>
+              <StackItem>
+                <Select
+                  isMulti
+                  name="metrics"
+                  options={metricOptions}
+                  className="basic-multi-select"
+                  classNamePrefix="select"
+                  placeholder="Please select metrics"
+                  value={metricOptions.filter((o) => selectedMetrics.includes(o.value))}
+                  onChange={(options) =>
+                    setSelectedMetrics(Array.isArray(options) ? options.map((o) => o.value) : [])
+                  }
+                />
+              </StackItem>
+              <StackItem>
+                <Button
+                  onClick={() => {
+                    setSelectedParams([]);
+                    setSelectedMetrics([]);
+                    setFilteredData([]);
+                  }}
+                >
+                  Clear parameters
+                </Button>
+              </StackItem>
+            </Stack>
+          </SplitItem>
+          <SplitItem isFilled>
+            {!isEmpty ? (
+              <Plot
+                data={[
+                  {
+                    type: 'parcoords',
+                    ...(lastMetricDimensionMetricKey && {
+                      line: getColorScaleConfigsForDimension(
+                        find(dimensions, ['label', lastMetricDimensionMetricKey]),
+                      ),
+                    }),
+                    dimensions,
+                  },
+                ]}
+                layout={{ autosize: true, margin: { t: 50 } }}
+              />
+            ) : (
+              <div>Nothing to see</div>
+            )}
+          </SplitItem>
+        </Split>
+      </>
     </ApplicationsPage>
   );
 };
