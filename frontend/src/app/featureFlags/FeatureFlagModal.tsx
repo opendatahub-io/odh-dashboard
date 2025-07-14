567e253c22398352e297b0b8d26eb8abe26edb75
Jill R. Pelavin <jpelavin@redhat.com>
2025-07-14 15:04:32 -0400

    starting with cursor...added tabs

--- frontend/src/app/featureFlags/FeatureFlagModal.tsx
+++ frontend/src/app/featureFlags/FeatureFlagModal.tsx
@@ -9,6 +9,9 @@ import {
   ModalBody,
   ModalFooter,
   ModalHeader,
+  Tab,
+  TabTitleText,
+  Tabs,
 } from '@patternfly/react-core';
 import { FeatureFlagLauncherProps } from '#~/app/featureFlags/FeatureFlagLauncher';
 import { useDevFlags } from '#~/app/featureFlags/useDevFeatureFlags';
@@ -24,6 +27,7 @@ const FeatureFlagModal: React.FC<Props> = ({
   onClose,
 }) => {
   const devFlags = useDevFlags();
+  const [activeTabKey, setActiveTabKey] = React.useState<string | number>(0);
 
   const safeDevFeatureFlags = devFeatureFlags || {};
   console.log('safe dev feature flags:', safeDevFeatureFlags);
@@ -61,21 +65,28 @@ const FeatureFlagModal: React.FC<Props> = ({
     <Modal data-testid="dev-feature-flags-modal" variant="large" isOpen onClose={onClose}>
       <ModalHeader title="Override Flags" />
       <ModalBody>
-        <Content component="h2">Feature Flags</Content>
-        <Content component="p">
-          Feature flags default to the values defined in the dashboard config.
-        </Content>
-        {renderFlags(definedFeatureFlags, dashboardConfig)}
-
-        {devFlags.length > 0 ? (
-          <>
-            <Content component="h2">Dev Flags</Content>
+        <Tabs activeKey={activeTabKey} onSelect={(_event, tabIndex) => setActiveTabKey(tabIndex)}>
+          <Tab eventKey={0} title={<TabTitleText>Active</TabTitleText>}>
+            <Content component="h2">Feature Flags</Content>
             <Content component="p">
-              Dev flags default to inactive and can only be changed for the current session.
+              Feature flags default to the values defined in the dashboard config.
             </Content>
-            {renderFlags(devFlags)}
-          </>
-        ) : null}
+            {renderFlags(definedFeatureFlags, dashboardConfig)}
+
+            {devFlags.length > 0 ? (
+              <>
+                <Content component="h2">Dev Flags</Content>
+                <Content component="p">
+                  Dev flags default to inactive and can only be changed for the current session.
+                </Content>
+                {renderFlags(devFlags)}
+              </>
+            ) : null}
+          </Tab>
+          <Tab eventKey={1} title={<TabTitleText>Archived</TabTitleText>}>
+            <Content component="p">Archived feature flags will be displayed here.</Content>
+          </Tab>
+        </Tabs>
       </ModalBody>
       <ModalFooter>
         <Button
