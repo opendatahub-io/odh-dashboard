8d5c8f13e53b20afd8a91cdcf49fc3c2f5da4f20
Ashley McEntee <123661468+ashley-o0o@users.noreply.github.com>
2025-07-16 11:33:44 -0400

    Change how model deployment links appear (#4442)
    * Change how model deployment links appear
    
    * Cypress fixes
    
    * Adjustments from feedback
    
    * Move instances of isModelStopped into a util
    
    * Move duplicated code into shared resource
    
    * Rebase fixes
    
    * Integrate stop start changes
    
    * Small fixes
    
    * Fix issue with metrics link
    
    * Quick fix for stopping
    
    ---------
    
    Co-authored-by: Griffin-Sullivan <gsulliva@redhat.com>

--- frontend/packages/modelServing/src/components/deployments/DeploymentsTableRow.tsx
+++ frontend/packages/modelServing/src/components/deployments/DeploymentsTableRow.tsx
@@ -7,8 +7,8 @@ import { InferenceServiceModelState } from '@odh-dashboard/internal/pages/modelS
 import { getDisplayNameFromK8sResource } from '@odh-dashboard/internal/concepts/k8s/utils';
 import { Link } from 'react-router-dom';
 import ResourceNameTooltip from '@odh-dashboard/internal/components/ResourceNameTooltip';
-import { DeploymentEndpointsPopupButton } from './DeploymentEndpointsPopupButton';
 import { DeploymentRowExpandedSection } from './DeploymentsTableRowExpandedSection';
+import DeploymentStatus from './DeploymentStatus';
 import {
   useDeploymentExtension,
   useResolvedDeploymentExtension,
@@ -67,12 +67,11 @@ export const DeploymentRow: React.FC<{
         )}
         <Td dataLabel="Name">
           <ResourceNameTooltip resource={deployment.model}>
-            {metricsExtension && deployment.model.metadata.namespace ? (
+            {metricsExtension &&
+            deployment.model.metadata.namespace &&
+            deployment.status?.state === InferenceServiceModelState.LOADED ? (
               <Link
-                data-testid={`metrics-link-${getDisplayNameFromK8sResource(deployment.model)}`}
-                to={`/projects/${encodeURIComponent(
-                  deployment.model.metadata.namespace,
-                )}/metrics/model/${encodeURIComponent(deployment.model.metadata.name)}`}
+                to={`/projects/${deployment.model.metadata.namespace}/metrics/model/${deployment.model.metadata.name}`}
               >
                 {getDisplayNameFromK8sResource(deployment.model)}
               </Link>
@@ -87,10 +86,7 @@ export const DeploymentRow: React.FC<{
           </Td>
         ))}
         <Td dataLabel="Inference endpoint">
-          <DeploymentEndpointsPopupButton
-            endpoints={deployment.endpoints}
-            loading={deployment.status?.state === InferenceServiceModelState.LOADING}
-          />
+          <DeploymentStatus deployment={deployment} />
         </Td>
         <Td dataLabel="API protocol">
           {getServerApiProtocol(deployment) ? (
--- frontend/src/pages/projects/screens/spawner/storage/types.ts
+++ frontend/src/pages/projects/screens/spawner/storage/types.ts
@@ -7,3 +7,7 @@ export enum PvcModelAnnotation {
   MODEL_NAME = 'dashboard.opendatahub.io/model-name',
   MODEL_PATH = 'dashboard.opendatahub.io/model-path',
 }
+
+export enum ModelAnnotation {
+  STOPPED_ANNOTATION = 'serving.kserve.io/stop',
+}
