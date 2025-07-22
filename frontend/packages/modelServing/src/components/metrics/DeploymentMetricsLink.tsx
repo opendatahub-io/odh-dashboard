f447d64a39627696093601a5ae45088138407af3
emilys314 <esamoylo@redhat.com>
2025-07-22 13:27:48 -0400

    better metric link logic

--- frontend/packages/modelServing/src/components/metrics/DeploymentMetricsLink.tsx
+++ frontend/packages/modelServing/src/components/metrics/DeploymentMetricsLink.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { getDisplayNameFromK8sResource } from '@odh-dashboard/internal/concepts/k8s/utils';
 import type { Deployment } from '../../../extension-points';
 
 const getMetricsUrl = (currentPath: string, deployment: Deployment) => {
-  const firstPath = currentPath.split('/')[1];
+  const firstPath = currentPath.split('/').filter(Boolean)[0];
   if (firstPath === 'modelServing') {
     return `/modelServing/${deployment.model.metadata.namespace}/metrics/${deployment.model.metadata.name}`;
   }
@@ -14,8 +14,9 @@ const getMetricsUrl = (currentPath: string, deployment: Deployment) => {
 export const DeploymentMetricsLink: React.FC<{
   deployment: Deployment;
 }> = ({ deployment, ...props }) => {
-  const currentPath = window.location.pathname;
+  const currentPath = useLocation().pathname;
   const metricsUrl = getMetricsUrl(currentPath, deployment);
+
   return (
     <Link to={metricsUrl} {...props}>
       {getDisplayNameFromK8sResource(deployment.model)}
