dd04152f5761050c4d4197597bd9147a0e23a8c3
Ashley McEntee <123661468+ashley-o0o@users.noreply.github.com>
2025-07-24 15:53:39 -0400

    Add version labels to refactored code (#4523)
    * Add version labels to refactored code
    
    * Add generic component
    
    * Modify servingruntime to servingdetails, create shared version component

--- frontend/packages/modelServing/extension-points/index.ts
+++ frontend/packages/modelServing/extension-points/index.ts
@@ -200,3 +200,16 @@ export type ModelServingMetricsExtension<D extends Deployment = Deployment> = Ex
 export const isModelServingMetricsExtension = <D extends Deployment = Deployment>(
   extension: Extension,
 ): extension is ModelServingMetricsExtension<D> => extension.type === 'model-serving.metrics';
+
+export type DeployedModelServingDetails<D extends Deployment = Deployment> = Extension<
+  'model-serving.deployed-model/serving-runtime',
+  {
+    platform: D['modelServingPlatformId'];
+    ServingDetailsComponent: ComponentCodeRef<{ deployment: D }>;
+  }
+>;
+
+export const isDeployedModelServingDetails = <D extends Deployment = Deployment>(
+  extension: Extension,
+): extension is DeployedModelServingDetails<D> =>
+  extension.type === 'model-serving.deployed-model/serving-runtime';
