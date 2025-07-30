b8417b5e01511dfd28f892ec2d2d4fa8edb97da4
Anish Surti <asurti@redhat.com>
2025-07-29 23:35:11 -0400

    Test backend lint errors

---
+++ backend/test-backend.ts
@@ -0,0 +1,4 @@
+// This file contains intentional lint errors for backend testing
+const unused_variable = 'this will cause a lint error';
+console.log('Missing semicolon');
+var oldStyleVar = 'should use const/let';
