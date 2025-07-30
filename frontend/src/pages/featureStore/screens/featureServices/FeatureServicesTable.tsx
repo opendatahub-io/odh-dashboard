e1d2c0846e2ce9bbb2ad50ee023cbb6f74a17332
Dipanshu Gupta <dipgupta@redhat.com>
2025-07-30 20:49:32 +0530

    Added project col

--- frontend/src/pages/featureStore/screens/featureServices/FeatureServicesTable.tsx
+++ frontend/src/pages/featureStore/screens/featureServices/FeatureServicesTable.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Table } from '#~/components/table';
 import DashboardEmptyTableView from '#~/concepts/dashboard/DashboardEmptyTableView';
 import { FeatureService } from '#~/pages/featureStore/types/featureServices';
-import { columns } from './const';
+import { AllProjectColumns, columns } from './const';
 import FeatureServiceTableRow from './FeatureServiceTableRow';
 
 type FeatureServicesTableProps = {
@@ -16,24 +16,27 @@ const FeatureServicesTable: React.FC<FeatureServicesTableProps> = ({
   onClearFilters,
   toolbarContent,
   fsProject,
-}) => (
-  <Table
-    data-testid="feature-services-table"
-    id="feature-services-table"
-    enablePagination
-    data={featureServices}
-    columns={columns}
-    onClearFilters={onClearFilters}
-    toolbarContent={toolbarContent}
-    emptyTableView={<DashboardEmptyTableView onClearFilters={onClearFilters} />}
-    rowRenderer={(fs, idx) => (
-      <FeatureServiceTableRow
-        key={`${fs.spec.name}-${idx}`}
-        featureService={fs}
-        fsProject={fsProject}
-      />
-    )}
-  />
-);
+}) => {
+  const tableColumns = fsProject ? columns : AllProjectColumns;
+  return (
+    <Table
+      data-testid="feature-services-table"
+      id="feature-services-table"
+      enablePagination
+      data={featureServices}
+      columns={tableColumns}
+      onClearFilters={onClearFilters}
+      toolbarContent={toolbarContent}
+      emptyTableView={<DashboardEmptyTableView onClearFilters={onClearFilters} />}
+      rowRenderer={(fs, idx) => (
+        <FeatureServiceTableRow
+          key={`${fs.spec.name}-${idx}`}
+          featureService={fs}
+          fsProject={fsProject}
+        />
+      )}
+    />
+  );
+};
 
 export default FeatureServicesTable;
