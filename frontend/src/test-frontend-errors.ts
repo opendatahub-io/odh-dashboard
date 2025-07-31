c4a48190046215a7606702d6f35f22be7b0375eb
Anish Surti <asurti@redhat.com>
2025-07-31 09:52:26 -0400

    Test 1b: Frontend errors with hook disabled

---
+++ frontend/src/test-frontend-errors.ts
@@ -0,0 +1,5 @@
+// Frontend test file with intentional lint errors
+const unused_variable = 'this will cause lint errors';
+console.log('missing semicolon');
+var oldStyleVar = 'should use const';
+let anotherUnused = 'more errors';
