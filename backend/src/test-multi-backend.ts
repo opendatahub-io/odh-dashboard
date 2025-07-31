f86f71bac7c3ac82f5821156d860585089bf7c4d
Anish Surti <asurti@redhat.com>
2025-07-31 09:54:52 -0400

    Test 3b: Both directories with hook disabled

---
+++ backend/src/test-multi-backend.ts
@@ -0,0 +1,3 @@
+// Multi-directory test - backend file
+const unused = 'not used anywhere';
+var oldStyle = 'should be const';
---
+++ frontend/src/test-multi-frontend.ts
@@ -0,0 +1,3 @@
+// Multi-directory test - frontend file
+const bad_name = 'snake_case is bad';
+var useConst = 'should be const';
