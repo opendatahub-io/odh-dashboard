91b057a80b014e4eb016453ba5c1a48fd2ed4d32
Anthony Coughlin <acoughli@redhat.com>
2025-08-01 11:58:16 +0100

    Quarantine model serving tests - RHOAIENG-31261
    - Quarantined 6 Cypress E2E tests for model serving functionality
    - testModelPvcDeployment.cy.ts
    - testModelStopStart.cy.ts
    - testModelTokenAuth.cy.ts
    - testMultiModelAdminCreation.cy.ts
    - testSingleModelAdminCreation.cy.ts
    - testSingleModelContributorCreation.cy.ts

--- frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/models/testModelPvcDeployment.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/models/testModelPvcDeployment.cy.ts
@@ -39,7 +39,7 @@ const awsBucketRegion = AWS_BUCKETS.BUCKET_1.REGION;
 const podName = 'pvc-loader-pod';
 const uuid = generateTestUUID();
 
-describe('Verify a model can be deployed from a PVC', () => {
+describe('[Product Bug: RHOAIENG-31261] Verify a model can be deployed from a PVC', () => {
   retryableBefore(() => {
     Cypress.on('uncaught:exception', (err) => {
       if (err.message.includes('Error: secrets "ds-pipeline-config" already exists')) {
@@ -69,7 +69,7 @@ describe('Verify a model can be deployed from a PVC', () => {
   });
   it(
     'should deploy a model from a PVC',
-    { tags: ['@Smoke', '@SmokeSet3', '@Dashboard', '@Modelserving'] },
+    { tags: ['@Smoke', '@SmokeSet3', '@Dashboard', '@Modelserving', '@Bug'] },
     () => {
       cy.step('log into application with ${HTPASSWD_CLUSTER_ADMIN_USER.USERNAME}');
       cy.visitWithLogin('/', HTPASSWD_CLUSTER_ADMIN_USER);
