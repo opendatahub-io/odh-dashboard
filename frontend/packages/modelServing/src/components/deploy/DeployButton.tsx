4e01d1f97f5b3ba0a8a4a445ffbae249f905393b
Ashley McEntee <123661468+ashley-o0o@users.noreply.github.com>
2025-08-04 13:40:52 -0400

    Link refactor deploy buttons to existing deploy modal (#4542)
    * Link refactor deploy buttons to existing deploy modal
    
    * Adjustments from feedback
    
    * Add toolbar for refactored global models page
    
    * Small bug fixes
    
    * Refine some areas
    
    * Remove references to refresh
    
    * Clean up code and fix deploy button in global no models

--- frontend/packages/modelServing/src/components/deploy/DeployButton.tsx
+++ frontend/packages/modelServing/src/components/deploy/DeployButton.tsx
@@ -1,30 +1,141 @@
 import React from 'react';
 import { Button, Tooltip } from '@patternfly/react-core';
-import { ModelServingPlatform } from '../../concepts/useProjectServingPlatform';
+import { ServingRuntimePlatform } from '@odh-dashboard/internal/types';
+import {
+  getSortedTemplates,
+  getTemplateEnabled,
+  getTemplateEnabledForPlatform,
+} from '@odh-dashboard/internal/pages/modelServing/customServingRuntimes/utils';
+import ManageServingRuntimeModal from '@odh-dashboard/internal/pages/modelServing/screens/projects/ServingRuntimeModal/ManageServingRuntimeModal';
+import ManageKServeModal from '@odh-dashboard/internal/pages/modelServing/screens/projects/kServeModal/ManageKServeModal';
+import ManageNIMServingModal from '@odh-dashboard/internal/pages/modelServing/screens/projects/NIMServiceModal/ManageNIMServingModal';
+import { byName, ProjectsContext } from '@odh-dashboard/internal/concepts/projects/ProjectsContext';
+import { useParams } from 'react-router-dom';
+import { useTemplates } from '@odh-dashboard/internal/api/k8s/templates';
+import { useDashboardNamespace } from '@odh-dashboard/internal/redux/selectors/index';
+import useTemplateOrder from '@odh-dashboard/internal/pages/modelServing/customServingRuntimes/useTemplateOrder';
+import useTemplateDisablement from '@odh-dashboard/internal/pages/modelServing/customServingRuntimes/useTemplateDisablement';
+import useConnections from '@odh-dashboard/internal/pages/projects/screens/detail/connections/useConnections';
+import { ProjectKind } from '@odh-dashboard/internal/k8sTypes';
+import { isProjectNIMSupported } from '@odh-dashboard/internal/pages/modelServing/screens/projects/nimUtils';
+import { ModelDeploymentsContext } from '../../concepts/ModelDeploymentsContext';
+import {
+  useProjectServingPlatform,
+  ModelServingPlatform,
+} from '../../concepts/useProjectServingPlatform';
+import { useAvailableClusterPlatforms } from '../../concepts/useAvailableClusterPlatforms';
+
+const DeployButtonModal: React.FC<{
+  platform: ServingRuntimePlatform;
+  currentProject: ProjectKind;
+  namespace?: string;
+  onClose: (submit: boolean) => void;
+}> = ({ platform, currentProject, namespace, onClose }) => {
+  const { dashboardNamespace } = useDashboardNamespace();
+  const [servingRuntimeTemplates] = useTemplates(dashboardNamespace);
+  const servingRuntimeTemplateOrder = useTemplateOrder(dashboardNamespace, undefined);
+  const servingRuntimeTemplateDisablement = useTemplateDisablement(dashboardNamespace, undefined);
+  const connections = useConnections(namespace || '');
+  const templatesSorted = getSortedTemplates(
+    servingRuntimeTemplates,
+    servingRuntimeTemplateOrder.data,
+  );
+  const templatesEnabled = templatesSorted.filter((template) =>
+    getTemplateEnabled(template, servingRuntimeTemplateDisablement.data),
+  );
+
+  if (platform === ServingRuntimePlatform.MULTI) {
+    return (
+      <ManageServingRuntimeModal
+        currentProject={currentProject}
+        servingRuntimeTemplates={templatesEnabled.filter((t) =>
+          getTemplateEnabledForPlatform(t, ServingRuntimePlatform.MULTI),
+        )}
+        onClose={onClose}
+      />
+    );
+  }
+
+  const isNIMSupported = isProjectNIMSupported(currentProject);
+  if (isNIMSupported) {
+    return <ManageNIMServingModal projectContext={{ currentProject }} onClose={onClose} />;
+  }
+  return (
+    <ManageKServeModal
+      projectContext={{ currentProject, connections: connections.data }}
+      servingRuntimeTemplates={templatesEnabled.filter((t) =>
+        getTemplateEnabledForPlatform(t, ServingRuntimePlatform.SINGLE),
+      )}
+      onClose={onClose}
+    />
+  );
+};
 
 export const DeployButton: React.FC<{
   platform?: ModelServingPlatform;
   variant?: 'primary' | 'secondary';
   isDisabled?: boolean;
 }> = ({ platform, variant = 'primary', isDisabled }) => {
+  const [modalShown, setModalShown] = React.useState<boolean>(false);
+  const [platformSelected, setPlatformSelected] = React.useState<ServingRuntimePlatform>();
+  const { namespace: modelNamespace } = useParams<{ namespace: string }>();
+  const { projects } = React.useContext(ProjectsContext);
+  const { clusterPlatforms } = useAvailableClusterPlatforms();
+  const { projects: modelProjects } = React.useContext(ModelDeploymentsContext);
+  const match = modelNamespace ? projects.find(byName(modelNamespace)) : undefined;
+  const currentProject = modelProjects?.find(byName(modelNamespace));
+  const { activePlatform, projectPlatform } = useProjectServingPlatform(match, clusterPlatforms);
+
+  const getServingRuntimePlatform = (platformId: string): ServingRuntimePlatform =>
+    platformId === 'modelmesh' ? ServingRuntimePlatform.MULTI : ServingRuntimePlatform.SINGLE;
+
+  const onSubmit = () => {
+    setModalShown(false);
+    setPlatformSelected(undefined);
+  };
+
+  const handleDeployClick = () => {
+    if (platform) {
+      setPlatformSelected(getServingRuntimePlatform(platform.properties.id));
+    } else {
+      const currentPlatform = activePlatform || projectPlatform;
+      if (currentProject && currentPlatform) {
+        setPlatformSelected(getServingRuntimePlatform(currentPlatform.properties.id));
+      }
+    }
+    setModalShown(true);
+  };
+
   const deployButton = (
     <Button
       variant={variant}
       data-testid="deploy-button"
-      // onClick={() => {
-      //   do something
-      // }}
-      isAriaDisabled={isDisabled}
+      onClick={handleDeployClick}
+      isAriaDisabled={!currentProject}
     >
       Deploy model
     </Button>
   );
-  if (!platform || isDisabled) {
+
+  if (!currentProject || isDisabled) {
     return (
       <Tooltip data-testid="deploy-model-tooltip" content="To deploy a model, select a project.">
         {deployButton}
       </Tooltip>
     );
   }
-  return <>{deployButton}</>;
+
+  return (
+    <>
+      {deployButton}
+      {modalShown && platformSelected ? (
+        <DeployButtonModal
+          platform={platformSelected}
+          currentProject={currentProject}
+          namespace={modelNamespace}
+          onClose={onSubmit}
+        />
+      ) : null}
+    </>
+  );
 };
