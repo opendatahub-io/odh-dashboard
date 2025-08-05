81defa65b2a7dc91d9684cba484d70aadac57369
Derek Xu <derekxu888@gmail.com>
2025-08-05 11:18:11 -0400

    change back to truncated text

--- frontend/src/components/TruncatedText.tsx
+++ frontend/src/components/TruncatedText.tsx
@@ -1,51 +1,27 @@
 import React from 'react';
-import { Flex, Panel, PanelMain, PanelMainBody, Popover, Tooltip } from '@patternfly/react-core';
+import { Tooltip } from '@patternfly/react-core';
 
 type TruncatedTextProps = {
   maxLines: number;
   content: React.ReactNode;
-  truncateTooltip?: boolean;
+  tooltipMaxLines?: number;
 } & Omit<React.HTMLProps<HTMLSpanElement>, 'content'>;
-
-const tooltipShowDelay = 300;
-const tooltipHideDelay = 150;
 
 const TruncatedText: React.FC<TruncatedTextProps> = ({
   maxLines,
   content,
-  truncateTooltip = false,
+  tooltipMaxLines,
   ...props
 }) => {
   const outerElementRef = React.useRef<HTMLElement>(null);
   const textElementRef = React.useRef<HTMLElement>(null);
-  const timeoutRef = React.useRef<NodeJS.Timeout>();
   const [isTruncated, setIsTruncated] = React.useState<boolean>(false);
-  const [showPopover, setShowPopover] = React.useState<boolean>(false);
 
   const updateTruncation = React.useCallback(() => {
     if (textElementRef.current && outerElementRef.current) {
       setIsTruncated(textElementRef.current.offsetHeight > outerElementRef.current.offsetHeight);
     }
   }, []);
-
-  const handlePopoverMouseEnter = () => {
-    // Popover has no delay, so need to set manually
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-    if (isTruncated && truncateTooltip) {
-      timeoutRef.current = setTimeout(() => setShowPopover(true), tooltipShowDelay);
-    }
-  };
-
-  const handlePopoverMouseLeave = () => {
-    if (truncateTooltip) {
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
-      }
-      timeoutRef.current = setTimeout(() => setShowPopover(false), tooltipHideDelay);
-    }
-  };
 
   const truncateBody = (
     <span
@@ -72,31 +48,22 @@ const TruncatedText: React.FC<TruncatedTextProps> = ({
     </span>
   );
 
-  return truncateTooltip ? (
-    <>
-      {isTruncated ? (
-        <Flex onMouseEnter={handlePopoverMouseEnter} onMouseLeave={handlePopoverMouseLeave}>
-          <Popover
-            hasNoPadding
-            triggerAction="hover"
-            isVisible={showPopover}
-            bodyContent={
-              <Panel isScrollable>
-                <PanelMain maxHeight="500px" tabIndex={0}>
-                  <PanelMainBody>{content}</PanelMainBody>
-                </PanelMain>
-              </Panel>
-            }
-          >
-            {truncateBody}
-          </Popover>
-        </Flex>
-      ) : (
-        <>{truncateBody}</>
-      )}
-    </>
-  ) : (
-    <Tooltip hidden={!isTruncated ? true : undefined} content={content}>
+  const truncateTooltipBody = (
+    <span
+      style={{
+        display: '-webkit-box',
+        WebkitBoxOrient: 'vertical',
+        overflowWrap: 'anywhere',
+        overflow: 'hidden',
+        WebkitLineClamp: tooltipMaxLines,
+      }}
+    >
+      {content}
+    </span>
+  );
+
+  return (
+    <Tooltip hidden={!isTruncated ? true : undefined} content={truncateTooltipBody}>
       {truncateBody}
     </Tooltip>
   );
