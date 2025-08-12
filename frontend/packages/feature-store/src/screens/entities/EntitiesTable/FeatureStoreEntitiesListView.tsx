5406d32e41a522a8c08d26cf02fbd1132fff9da8
Claudia Alphonse <claudialphonse@gmail.com>
2025-08-12 17:05:59 +0530

    feat:added tags filtering and dates filtering for entities page, updated cypress tests

--- frontend/packages/feature-store/src/screens/entities/EntitiesTable/FeatureStoreEntitiesListView.tsx
+++ frontend/packages/feature-store/src/screens/entities/EntitiesTable/FeatureStoreEntitiesListView.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import FeatureStoreEntitiesTable from './FeatureStoreEntitiesTable';
 import { EntityList } from '../../../types/entities';
 import { FeatureStoreToolbar } from '../../../components/FeatureStoreToolbar';
-import { useFeatureStoreProject } from '../../../FeatureStoreContext';
 import { applyEntityFilters } from '../utils';
 import { entityTableFilterOptions } from '../const';
+import { useFeatureStoreProject } from '../../../FeatureStoreContext';
+import { useTagFilterHandlers } from '../../../utils/useTagFilterHandlers';
 
 const FeatureStoreEntitiesListView = ({
   entities,
@@ -14,7 +15,12 @@ const FeatureStoreEntitiesListView = ({
   const [filterData, setFilterData] = React.useState<
     Record<string, string | { label: string; value: string } | undefined>
   >({});
+  const [currentFilterType, setCurrentFilterType] = React.useState<string>('entity');
+  const [tagFilters, setTagFilters] = React.useState<string[]>([]);
   const { currentProject } = useFeatureStoreProject();
+
+  const tagHandlers = useTagFilterHandlers(setTagFilters, setCurrentFilterType);
+
   const processedEntities = React.useMemo(() => {
     if (currentProject) {
       return entities.entities.map((entity) => ({
@@ -31,23 +37,43 @@ const FeatureStoreEntitiesListView = ({
     [setFilterData],
   );
 
-  const onClearFilters = React.useCallback(() => setFilterData({}), [setFilterData]);
+  const onClearFilters = React.useCallback(() => {
+    setFilterData({});
+    setTagFilters([]);
+  }, []);
 
-  const filteredEntities = React.useMemo(
-    () => applyEntityFilters(processedEntities, entities.relationships, filterData),
-    [processedEntities, entities.relationships, filterData],
-  );
+  const filteredEntities = React.useMemo(() => {
+    let filtered = processedEntities;
+    filtered = applyEntityFilters(filtered, entities.relationships, filterData);
+    if (tagFilters.length > 0) {
+      filtered = filtered.filter((entity) => {
+        const entityTags = entity.spec.tags || {};
+        return tagFilters.every((tagFilter) => {
+          const tagEntries = Object.entries(entityTags);
+          return tagEntries.some(([key, value]) => `${key}=${value}` === tagFilter);
+        });
+      });
+    }
+
+    return filtered;
+  }, [processedEntities, entities.relationships, filterData, tagFilters]);
 
   return (
     <FeatureStoreEntitiesTable
       entities={filteredEntities}
       relationships={entities.relationships}
       onClearFilters={onClearFilters}
+      onTagClick={tagHandlers.handleTagClick}
       toolbarContent={
         <FeatureStoreToolbar
           filterOptions={entityTableFilterOptions}
           filterData={filterData}
           onFilterUpdate={onFilterUpdate}
+          currentFilterType={currentFilterType}
+          onFilterTypeChange={tagHandlers.handleFilterTypeChange}
+          tagFilters={tagFilters}
+          onTagFilterRemove={tagHandlers.handleTagFilterRemove}
+          onTagFilterAdd={tagHandlers.handleTagFilterAdd}
         />
       }
     />
