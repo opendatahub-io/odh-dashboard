176bca621f91c73fa37baa66ea3567c2ce458e78
Dipanshu Gupta <dipgupta@redhat.com>
2025-08-13 19:59:41 +0530

    Tests for table

--- frontend/src/api/models/kubeflow.ts
+++ frontend/src/api/models/kubeflow.ts
@@ -6,3 +6,10 @@ export const NotebookModel: K8sModelCommon = {
   kind: 'Notebook',
   plural: 'notebooks',
 };
+
+export const PyTorchJobModel: K8sModelCommon = {
+  apiVersion: 'v1',
+  apiGroup: 'kubeflow.org',
+  kind: 'PyTorchJob',
+  plural: 'pytorchjobs',
+};
