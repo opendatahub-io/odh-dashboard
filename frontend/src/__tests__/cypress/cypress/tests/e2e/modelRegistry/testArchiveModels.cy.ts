95480ec03b2a52faf919a58d3b8411c3dff56ac2
Conor O'Malley <conormomalley@gmail.com>
2025-08-19 15:11:03 +0100

    update tags & remove bug

--- frontend/src/__tests__/cypress/cypress/tests/e2e/modelRegistry/testArchiveModels.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/modelRegistry/testArchiveModels.cy.ts
@@ -70,7 +70,7 @@ describe('Verify that models and versions can be archived and restored via model
 
   it(
     'Registers model, adds versions, archives version, restores version, archives whole model, restores whole model',
-    { tags: ['@Dashboard', '@ModelRegistry', '@NonConcurrent', '@Featureflagged'] },
+    { tags: ['@Dashboard', '@ModelRegistry', '@NonConcurrent'] },
     () => {
       cy.step('Login as an Admin');
       cy.visitWithLogin('/', HTPASSWD_CLUSTER_ADMIN_USER);
