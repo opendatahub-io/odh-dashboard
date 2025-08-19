3a88d016ee45115d148d099660ff56daa312fc5a
Claudia Alphonse <claudialphonse@gmail.com>
2025-08-19 13:03:33 +0530

    RHOAIENG 31795 (#4671)
    * feat:added support for tags filtering in the generic filter utils
    
    * feat:added a separate generic toolbar component to support multi tags and date picker filtering
    
    * feat:added on click functionality for tags
    
    * feat:added tags filtering and dates filtering for entities page, updated cypress tests
    
    * feat:added tags filtering and dates filtering for feature views page, updated jest tests
    
    * feat:added tags filtering and dates filtering for feature services page, updated jest tests
    
    * feat:added a reusable hook for the tagfiltering for the filter toolbar and test case fixes and linting fixes
    
    * refactor: simplified the toolbar logic, added tests for utils
    
    * review: addressed code rabbit review comments

---
+++ frontend/packages/feature-store/src/types/toolbarTypes.ts
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { ToolbarGroup } from '@patternfly/react-core';
+
+export type FeatureStoreFilterToolbarProps<T extends string> = React.ComponentProps<
+  typeof ToolbarGroup
+> & {
+  children?: React.ReactNode;
+  filterOptions: { [key in T]?: string };
+  filterOptionRenders: Record<T, (props: BaseFilterOptionRenders) => React.ReactNode>;
+  filterData: FilterData<T>;
+  onFilterUpdate: OnFilterUpdate<T>;
+  testId?: string;
+  currentFilterType?: T;
+  onFilterTypeChange?: (filterType: T) => void;
+  multipleLabels?: MultipleLabels<T>;
+};
+
+// Common types for toolbar functionality
+export type FilterLabel = {
+  key: string;
+  node: string;
+  props?: { 'data-testid': string };
+};
+
+export type FilterData<T extends string> = Record<
+  T,
+  string | { label: string; value: string } | undefined
+>;
+
+export type MultipleLabel = {
+  key: string;
+  label: string;
+  onRemove: () => void;
+  testId?: string;
+};
+
+export type MultipleLabels<T extends string> = Record<T, Array<MultipleLabel>>;
+
+export type BaseFilterOptionRenders = {
+  onChange: (value?: string, label?: string) => void;
+  value?: string;
+  label?: string;
+};
+
+export type OnFilterUpdate<T extends string> = (
+  filterType: T,
+  value?: string | { label: string; value: string },
+) => void;
+
+export type LabelToDelete = string | { key: string };
+
+export type MultipleLabelForType = Array<{ key: string; onRemove: () => void }>;
