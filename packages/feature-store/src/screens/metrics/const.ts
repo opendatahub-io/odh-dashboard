9620c07e7ef928b672634b3496c5db9453215d59
Claudia Alphonse <claudialphonse@gmail.com>
2025-08-26 13:48:03 +0530

    feat: added constants and utility function used and mocks for metrics

---
+++ packages/feature-store/src/screens/metrics/const.ts
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+import { SortableData } from '@odh-dashboard/internal/components/table/types';
+import { RecentlyVisitedResource } from 'src/types/metrics';
+
+export const recentlyVisitedResourcesColumns: SortableData<RecentlyVisitedResource>[] = [
+  {
+    field: 'object_name',
+    label: 'Resource name',
+    width: 25,
+    sortable: false,
+  },
+  {
+    field: 'object_type',
+    label: 'Resource type',
+    width: 25,
+    sortable: false,
+  },
+  {
+    field: 'timestamp',
+    label: 'Last viewed',
+    width: 25,
+    sortable: (a: RecentlyVisitedResource, b: RecentlyVisitedResource): number => {
+      const aTime = new Date(a.timestamp).getTime();
+      const bTime = new Date(b.timestamp).getTime();
+      return bTime - aTime;
+    },
+  },
+];
+
+export const resourceTypeMap: Record<string, string> = {
+  entities: 'entity',
+  feature_views: 'feature views',
+  saved_datasets: 'saved datasets',
+  feature_services: 'feature services',
+  features: 'features',
+};
+
+export const EMPTY_STATE_MESSAGES = {
+  RECENTLY_VISITED_RESOURCES: 'No recently visited resources.',
+  POPULAR_TAGS: 'No feature views yet',
+  METRICS: 'No metrics available',
+} as const;
