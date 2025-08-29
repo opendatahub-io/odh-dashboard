86693c1c3a9e3c3159bd9cb2885caf471b46aebc
sridarna <sdarna@redhat.com>
2025-08-29 14:52:42 -0400

    fix lint issues

--- frontend/src/__tests__/cypress/cypress/tests/e2e/dataSciencePipelines/pipelines.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/dataSciencePipelines/pipelines.cy.ts
@@ -18,56 +18,60 @@ const testPipelineName = 'test-pipelines-pipeline';
 const testRunName = 'test-pipelines-run';
 const awsBucket = 'BUCKET_3' as const;
 
-describe('[Product Bug: RHOAIENG-33241] An admin user can import and run a pipeline', { testIsolation: false }, () => {
-  retryableBefore(() => {
-    // Create a Project for pipelines
-    provisionProjectForPipelines(projectName, dspaSecretName, awsBucket);
-  });
+describe(
+  '[Product Bug: RHOAIENG-33241] An admin user can import and run a pipeline',
+  { testIsolation: false },
+  () => {
+    retryableBefore(() => {
+      // Create a Project for pipelines
+      provisionProjectForPipelines(projectName, dspaSecretName, awsBucket);
+    });
 
-  after(() => {
-    // Delete provisioned Project
-    deleteOpenShiftProject(projectName, { wait: false, ignoreNotFound: true });
-  });
+    after(() => {
+      // Delete provisioned Project
+      deleteOpenShiftProject(projectName, { wait: false, ignoreNotFound: true });
+    });
 
-  it(
-    'An admin User can Import and Run a Pipeline',
-    { tags: ['@Smoke', '@SmokeSet1', '@Dashboard', '@Pipelines', '@Bug'] },
-    () => {
-      cy.step('Navigate to DSP ${projectName}');
-      cy.visitWithLogin('/', HTPASSWD_CLUSTER_ADMIN_USER);
-      projectListPage.navigate();
-      projectListPage.filterProjectByName(projectName);
-      projectListPage.findProjectLink(projectName).click();
+    it(
+      'An admin User can Import and Run a Pipeline',
+      { tags: ['@Smoke', '@SmokeSet1', '@Dashboard', '@Pipelines', '@Bug'] },
+      () => {
+        cy.step('Navigate to DSP ${projectName}');
+        cy.visitWithLogin('/', HTPASSWD_CLUSTER_ADMIN_USER);
+        projectListPage.navigate();
+        projectListPage.filterProjectByName(projectName);
+        projectListPage.findProjectLink(projectName).click();
 
-      cy.step('Import a pipeline by URL');
-      // Increasing the timeout to ~3mins so the DSPA can be loaded
-      projectDetails.findImportPipelineButton(180000).click();
-      // Fill the Import Pipeline modal
-      pipelineImportModal.findPipelineNameInput().type(testPipelineName);
-      pipelineImportModal.findPipelineDescriptionInput().type('Pipeline Description');
-      pipelineImportModal.findImportPipelineRadio().click();
-      pipelineImportModal
-        .findPipelineUrlInput()
-        .type(
-          'https://raw.githubusercontent.com/opendatahub-io/odh-dashboard/refs/heads/main/frontend/src/__tests__/resources/pipelines_samples/dummy_pipeline_compiled.yaml',
-        );
-      pipelineImportModal.submit();
+        cy.step('Import a pipeline by URL');
+        // Increasing the timeout to ~3mins so the DSPA can be loaded
+        projectDetails.findImportPipelineButton(180000).click();
+        // Fill the Import Pipeline modal
+        pipelineImportModal.findPipelineNameInput().type(testPipelineName);
+        pipelineImportModal.findPipelineDescriptionInput().type('Pipeline Description');
+        pipelineImportModal.findImportPipelineRadio().click();
+        pipelineImportModal
+          .findPipelineUrlInput()
+          .type(
+            'https://raw.githubusercontent.com/opendatahub-io/odh-dashboard/refs/heads/main/frontend/src/__tests__/resources/pipelines_samples/dummy_pipeline_compiled.yaml',
+          );
+        pipelineImportModal.submit();
 
-      // Verify that we are at the details page of the pipeline by checking the title
-      // It can take a little longer to load
-      pipelineDetails.findPageTitle(60000).should('have.text', testPipelineName);
+        // Verify that we are at the details page of the pipeline by checking the title
+        // It can take a little longer to load
+        pipelineDetails.findPageTitle(60000).should('have.text', testPipelineName);
 
-      cy.step('Run the pipeline from the Actions button in the pipeline detail view');
-      pipelineDetails.selectActionDropdownItem('Create run');
-      createRunPage.experimentSelect.findToggleButton().click();
-      createRunPage.selectExperimentByName('Default');
-      createRunPage.fillName(testRunName);
-      createRunPage.fillDescription('Run Description');
-      createRunPage.findSubmitButton().click();
+        cy.step('Run the pipeline from the Actions button in the pipeline detail view');
+        pipelineDetails.selectActionDropdownItem('Create run');
+        createRunPage.experimentSelect.findToggleButton().click();
+        createRunPage.selectExperimentByName('Default');
+        createRunPage.fillName(testRunName);
+        createRunPage.fillDescription('Run Description');
+        createRunPage.findSubmitButton().click();
 
-      cy.step('Expect the run to Succeed');
-      //Redirected to the Graph view of the created run
-      pipelineRunDetails.expectStatusLabelToBe('Succeeded', 240000);
-    },
-  );
-});
+        cy.step('Expect the run to Succeed');
+        //Redirected to the Graph view of the created run
+        pipelineRunDetails.expectStatusLabelToBe('Succeeded', 240000);
+      },
+    );
+  },
+);
