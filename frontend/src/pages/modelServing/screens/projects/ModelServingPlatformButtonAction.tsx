4392a5a1fbf5258b958525564b3bf379740aee77
Anish Surti <21984673+srtanish1992@users.noreply.github.com>
2025-08-29 16:28:02 -0400

    RHOAIENG-30493: Error creating a new workbench from a kueue enabled project, assigning a hwp with node scheduling (#4634)
    * added alert
    
    * added check
    
    * added hook
    
    * disabled button for workbenches
    
    * disabled create button for model deployment
    
    * display alert
    
    * added info link
    
    * consolidated changes in one hook
    
    * update filtering logic
    
    * use new hook
    
    * delete old hook
    
    * new utils file
    
    * updated imports and used constants
    
    * deleted file
    
    * fix lint issue
    
    * updayed comment
    
    * use const
    
    * fix filtering for global model serving
    
    * resolved
    
    * added mock test
    
    * fix typos
    
    * dry
    
    * constants file, rem reduntant const
    
    * type check error
    
    * display no valid msg
    
    * updated prop
    
    * fixed default value bug
    
    * fixed fltering bug
    
    * move kueue utils
    
    * fix uni tests

--- frontend/src/pages/modelServing/screens/projects/ModelServingPlatformButtonAction.tsx
+++ frontend/src/pages/modelServing/screens/projects/ModelServingPlatformButtonAction.tsx
@@ -3,6 +3,8 @@ import { Button, ButtonProps, Content, Tooltip } from '@patternfly/react-core';
 import { ProjectDetailsContext } from '#~/pages/projects/ProjectDetailsContext';
 import { isProjectNIMSupported } from '#~/pages/modelServing/screens/projects/nimUtils';
 import useServingPlatformStatuses from '#~/pages/modelServing/useServingPlatformStatuses';
+import { useKueueConfiguration } from '#~/concepts/hardwareProfiles/kueueUtils';
+import { KUEUE_MODEL_DEPLOYMENT_DISABLED_MESSAGE } from '#~/concepts/hardwareProfiles/kueueConstants';
 
 type ModelServingPlatformButtonActionProps = ButtonProps & {
   isProjectModelMesh: boolean;
@@ -26,11 +28,18 @@ const ModelServingPlatformButtonAction: React.FC<ModelServingPlatformButtonActio
   const isKServeNIMEnabled = isProjectNIMSupported(currentProject);
   const isNimDisabled = !isNIMAvailable && isKServeNIMEnabled;
 
+  const { isKueueDisabled } = useKueueConfiguration(currentProject);
+
   const actionButton = (
     <Button
       {...buttonProps}
       isLoading={!templatesLoaded}
-      isAriaDisabled={!templatesLoaded || emptyTemplates || isNimDisabled}
+      isAriaDisabled={
+        !templatesLoaded ||
+        emptyTemplates ||
+        isNimDisabled ||
+        (!isProjectModelMesh && isKueueDisabled)
+      }
       data-testid={testId}
       variant={variant}
     >
@@ -38,7 +47,7 @@ const ModelServingPlatformButtonAction: React.FC<ModelServingPlatformButtonActio
     </Button>
   );
 
-  if (!emptyTemplates && !isNimDisabled) {
+  if (!emptyTemplates && !isNimDisabled && !(!isProjectModelMesh && isKueueDisabled)) {
     return actionButton;
   }
 
@@ -49,6 +58,8 @@ const ModelServingPlatformButtonAction: React.FC<ModelServingPlatformButtonActio
       content={
         isNimDisabled ? (
           'NIM is not available. Contact your administrator.'
+        ) : !isProjectModelMesh && isKueueDisabled ? (
+          KUEUE_MODEL_DEPLOYMENT_DISABLED_MESSAGE
         ) : (
           <Content component="p">{`At least one serving runtime must be enabled to ${
             isProjectModelMesh ? 'add a model server' : 'deploy a model'
