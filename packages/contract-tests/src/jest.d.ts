63c109220c0fe07cfb33b6436f66986f3bbf692d
Anthony Coughlin <acoughli@redhat.com>
2025-09-03 13:14:23 +0100

    fix: move jest.d.ts to contract-tests package and update type references

--- packages/contract-tests/src/jest.d.ts
+++ packages/contract-tests/src/jest.d.ts
@@ -1,4 +1,4 @@
-// Jest type extensions for contract tests
+// Jest type extensions for contract tests in model-registry
 declare namespace jest {
   interface Matchers<R> {
     toMatchContract: (schema: Record<string, unknown>, options?: Record<string, unknown>) => R;
--- packages/model-registry/contract-tests/jest.d.ts
+++
@@ -1,6 +0,0 @@
-// Jest type extensions for contract tests in model-registry
-declare namespace jest {
-  interface Matchers<R> {
-    toMatchContract: (schema: Record<string, unknown>, options?: Record<string, unknown>) => R;
-  }
-}
