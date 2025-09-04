1fdce7531cccb228074f440af24cb262a6077ffa
Claudia Alphonse <claudialphonse@gmail.com>
2025-09-04 20:01:54 +0530

    feat:added a CR context for feature store to fetch feature store accessible projects based on namespaces available to a user

--- packages/feature-store/src/apiHooks/useFeatureStoreCR.tsx
+++ packages/feature-store/src/apiHooks/useFeatureStoreCR.tsx
@@ -1,31 +1,22 @@
 import React from 'react';
-import { k8sListResource } from '@openshift/dynamic-plugin-sdk-utils';
-import { FeatureStoreModel } from '@odh-dashboard/internal/api/models/odh';
-import { FetchStateCallbackPromise } from '@odh-dashboard/internal/utilities/useFetchState';
-import useFetch, { FetchStateObject } from '@odh-dashboard/internal/utilities/useFetch';
 import { FeatureStoreKind } from '@odh-dashboard/internal/k8sTypes';
-import { FEATURE_STORE_UI_LABEL_KEY, FEATURE_STORE_UI_LABEL_VALUE } from '../const';
+import { FeatureStoreCRContext } from '../contexts/FeatureStoreContext';
 
-const listFeatureStoreCR = async (): Promise<FeatureStoreKind | null> => {
-  const labelSelector = `${FEATURE_STORE_UI_LABEL_KEY}=${FEATURE_STORE_UI_LABEL_VALUE}`;
-
-  const featureStoreCRs = await k8sListResource<FeatureStoreKind>({
-    model: FeatureStoreModel,
-    queryOptions: {
-      queryParams: { labelSelector },
-    },
-  });
-
-  return featureStoreCRs.items[0] || null;
+type UseFeatureStoreCRResult = {
+  data: FeatureStoreKind | null;
+  loaded: boolean;
+  error: Error | undefined;
 };
 
-export const useFeatureStoreCR: () => FetchStateObject<FeatureStoreKind | null> = () => {
-  const callback = React.useCallback<FetchStateCallbackPromise<FeatureStoreKind | null>>(
-    () => listFeatureStoreCR(),
-    [],
-  );
+export const useFeatureStoreCR = (): UseFeatureStoreCRResult => {
+  const { activeFeatureStore, loaded, loadError } = React.useContext(FeatureStoreCRContext);
 
-  return useFetch(callback, null, {
-    initialPromisePurity: true,
-  });
+  return React.useMemo(
+    () => ({
+      data: activeFeatureStore,
+      loaded,
+      error: loadError,
+    }),
+    [activeFeatureStore, loaded, loadError],
+  );
 };
