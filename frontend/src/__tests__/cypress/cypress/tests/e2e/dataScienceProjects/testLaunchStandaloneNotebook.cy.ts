01090a8769da25317df835b62ec266d8e917b44e
Sri Sai Sowjanya Darna <sdarna@redhat.com>
2025-09-04 17:39:35 -0400

    quarantine testLaunchStandaloneNotebook.cy test file (#4821)

--- frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/testLaunchStandaloneNotebook.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/testLaunchStandaloneNotebook.cy.ts
@@ -9,7 +9,7 @@ import {
 } from '#~/__tests__/cypress/cypress/utils/oc_commands/baseCommands';
 import { retryableBefore } from '#~/__tests__/cypress/cypress/utils/retryableHooks';
 
-describe('Verify a Jupyter Notebook can be launched directly from the Data Science Project List View', () => {
+describe('[Product Bug: RHOAIENG-33609] Verify a Jupyter Notebook can be launched directly from the Data Science Project List View', () => {
   let testData: NotebookImageData;
 
   retryableBefore(() =>
@@ -24,7 +24,7 @@ describe('Verify a Jupyter Notebook can be launched directly from the Data Scien
 
   it(
     'Verify User Can Access Jupyter Launcher From DS Project Page',
-    { tags: ['@Smoke', '@SmokeSet1', '@ODS-1877', '@Dashboard', '@NonConcurrent'] },
+    { tags: ['@Smoke', '@SmokeSet1', '@ODS-1877', '@Dashboard', '@NonConcurrent', '@Bug'] },
     () => {
       // Authentication and navigation
       cy.step('Log into the application');
