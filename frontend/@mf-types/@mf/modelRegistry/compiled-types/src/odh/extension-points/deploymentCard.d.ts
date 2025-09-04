edb2e180a40e51c50e971c825ff5876cc7f25c9a
ppadti <ppadti@redhat.com>
2025-09-04 16:12:33 +0530

    remove duplicate type definition

--- frontend/@mf-types/@mf/modelRegistry/compiled-types/src/odh/extension-points/deploymentCard.d.ts
+++
@@ -1,8 +0,0 @@
-import type { CodeRef, Extension } from '@openshift/dynamic-plugin-sdk';
-export type ModelDetailsDeploymentCardExtension = Extension<'model-registry.model-details/details-card', {
-    component: CodeRef<React.ComponentType<{
-        rmId?: string;
-        mrName?: string;
-    }>>;
-}>;
-export declare const isModelDetailsDeploymentCardExtension: (extension: Extension) => extension is ModelDetailsDeploymentCardExtension;
