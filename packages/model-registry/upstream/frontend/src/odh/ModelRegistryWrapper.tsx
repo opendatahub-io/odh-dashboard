522e92d99a5a082fae8caeeb4206605734bdc7ef
Yulia Krimerman <juliapiterova@hotmail.com>
2025-09-08 14:04:55 -0400

    reverted change in file

--- packages/model-registry/upstream/frontend/src/odh/ModelRegistryWrapper.tsx
+++ packages/model-registry/upstream/frontend/src/odh/ModelRegistryWrapper.tsx
@@ -16,7 +16,7 @@ const modularArchConfig: ModularArchConfig = {
   deploymentMode: DeploymentMode.Federated,
   URL_PREFIX,
   BFF_API_VERSION,
-  mandatoryNamespace: 'kubeflow',
+  mandatoryNamespace: 'odh-model-registries',
 };
 
 const ModelRegistryWrapper: React.FC = () => {
