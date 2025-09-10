e8dbb34d7c348f2f3627a9ea4607291f9818c47c
Jill R. Pelavin <jpelavin@redhat.com>
2025-09-10 13:11:57 -0400

    mega-commit
    * removed column that is no longer needed; plus filtering logic .  also added tests (via cursor)"
    * removed/not using loading for migrated hp
    * removed call to hook and the table itself
    * working on hardware profiles table....mass deletions starting
    * more deletions...but last modified column is now empty :(
    * last modified column put back1
    * removed the migrationmodal from the footer; removed some commented out code; added a 'delete me' label
    * deleting migrationmodal
    * deleting migration tooltip
    * deleting migration constants

--- frontend/src/pages/hardwareProfiles/migration/const.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationSourceType } from './types';
-
-export const MIGRATION_SOURCE_TYPE_LABELS = {
-  [MigrationSourceType.ACCELERATOR_PROFILE]: 'accelerator profile',
-  [MigrationSourceType.SERVING_CONTAINER_SIZE]: 'model serving container size',
-  [MigrationSourceType.NOTEBOOK_CONTAINER_SIZE]: 'workbench container size',
-};
