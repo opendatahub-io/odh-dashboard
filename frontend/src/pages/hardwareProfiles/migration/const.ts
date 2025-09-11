b0edcc9d45eccf1c32f64255a03e986bb72e584b
Jill R. Pelavin <jpelavin@redhat.com>
2025-09-11 11:46:30 -0400

    deleting migration constants

--- frontend/src/pages/hardwareProfiles/migration/const.ts
+++
@@ -1,8 +0,0 @@
-// DELETE ME TODO
-import { MigrationSourceType } from './types';
-
-export const MIGRATION_SOURCE_TYPE_LABELS = {
-  [MigrationSourceType.ACCELERATOR_PROFILE]: 'accelerator profile',
-  [MigrationSourceType.SERVING_CONTAINER_SIZE]: 'model serving container size',
-  [MigrationSourceType.NOTEBOOK_CONTAINER_SIZE]: 'workbench container size',
-};
