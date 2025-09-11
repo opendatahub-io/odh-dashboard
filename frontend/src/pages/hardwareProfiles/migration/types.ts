b5439d7a2688e7ece96afa21d4c7e1478003ceb9
Jill R. Pelavin <jpelavin@redhat.com>
2025-09-11 14:56:18 -0400

    removing defunct consts.....

--- frontend/src/pages/hardwareProfiles/migration/types.ts
+++ frontend/src/pages/hardwareProfiles/migration/types.ts
@@ -1,21 +1,6 @@
-import { K8sResourceCommon } from '@openshift/dynamic-plugin-sdk-utils';
-import { HardwareProfileKind, K8sAPIOptions } from '#~/k8sTypes';
-
 export type ContainerSizeLimits = {
   minCpu: string | number;
   maxCpu?: string | number;
   minMemory: string;
   maxMemory?: string;
-};
-
-export enum MigrationSourceType {
-  ACCELERATOR_PROFILE,
-  SERVING_CONTAINER_SIZE,
-  NOTEBOOK_CONTAINER_SIZE,
-}
-
-export type MigrationSource = {
-  type: MigrationSourceType;
-  label: string;
-  resource: K8sResourceCommon;
 };
