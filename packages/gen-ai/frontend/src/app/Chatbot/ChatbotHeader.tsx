f9cbbc172cea3513fdd8bd6060d3ad7d4fcc2b10
Juntao Wang <juntwang@redhat.com>
2025-09-11 15:56:28 -0400

    revert changes, use namespaces, create mock mode and non-mock mode

--- packages/gen-ai/frontend/src/app/Chatbot/ChatbotHeader.tsx
+++ packages/gen-ai/frontend/src/app/Chatbot/ChatbotHeader.tsx
@@ -1,59 +1,49 @@
 import * as React from 'react';
 import { Flex, FlexItem, Title } from '@patternfly/react-core';
 import ProjectIcon from '@odh-dashboard/internal/images/icons/ProjectIcon';
-import ProjectSelector from '@odh-dashboard/internal/concepts/projects/ProjectSelector';
-import { ProjectsContext } from '@odh-dashboard/internal/concepts/projects/ProjectsContext';
+import ProjectDropdown from './components/ProjectDropdown';
 
 interface ChatbotHeaderProps {
   selectedProject: string;
   onProjectChange: (projectName: string) => void;
-  // onProjectsLoaded: (projects: string[]) => void;
+  onProjectsLoaded: (projects: string[]) => void;
   isLoading?: boolean;
 }
 
 const ChatbotHeader: React.FunctionComponent<ChatbotHeaderProps> = ({
   selectedProject,
   onProjectChange,
-  // onProjectsLoaded,
+  onProjectsLoaded,
   isLoading = false,
-}) => {
-  const { projects } = React.useContext(ProjectsContext);
-  return (
-    <Flex component="span" alignItems={{ default: 'alignItemsCenter' }} gap={{ default: 'gapLg' }}>
-      <FlexItem>
-        <Title headingLevel="h1">AI playground</Title>
-      </FlexItem>
-      <FlexItem>
-        <Flex
-          spaceItems={{ default: 'spaceItemsXs' }}
-          alignItems={{ default: 'alignItemsCenter' }}
-          style={{ display: 'inline-flex' }}
-        >
-          <FlexItem>
-            <ProjectIcon
-              style={{ width: '20px', height: '20px', marginTop: '10px', marginLeft: '10px' }}
-            />
-          </FlexItem>
-          <FlexItem>
-            <span style={{ fontSize: '16px', marginRight: '10px' }}>Project</span>
-          </FlexItem>
-          <FlexItem>
-            <ProjectSelector
-              namespace={selectedProject}
-              onSelection={onProjectChange}
-              isLoading={isLoading}
-              projectsOverride={projects}
-            />
-            {/* <ProjectDropdown
+}) => (
+  <Flex component="span" alignItems={{ default: 'alignItemsCenter' }} gap={{ default: 'gapLg' }}>
+    <FlexItem>
+      <Title headingLevel="h1">AI playground</Title>
+    </FlexItem>
+    <FlexItem>
+      <Flex
+        spaceItems={{ default: 'spaceItemsXs' }}
+        alignItems={{ default: 'alignItemsCenter' }}
+        style={{ display: 'inline-flex' }}
+      >
+        <FlexItem>
+          <ProjectIcon
+            style={{ width: '20px', height: '20px', marginTop: '10px', marginLeft: '10px' }}
+          />
+        </FlexItem>
+        <FlexItem>
+          <span style={{ fontSize: '16px', marginRight: '10px' }}>Project</span>
+        </FlexItem>
+        <FlexItem>
+          <ProjectDropdown
             selectedProject={selectedProject}
             onProjectChange={onProjectChange}
             onProjectsLoaded={onProjectsLoaded}
             isDisabled={isLoading}
-          /> */}
-          </FlexItem>
-        </Flex>
-      </FlexItem>
-    </Flex>
-  );
-};
+          />
+        </FlexItem>
+      </Flex>
+    </FlexItem>
+  </Flex>
+);
 export default ChatbotHeader;
