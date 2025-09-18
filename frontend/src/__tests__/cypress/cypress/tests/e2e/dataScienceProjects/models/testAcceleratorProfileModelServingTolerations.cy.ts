3406950a3c308dc63abbdddfdb95b0b75e032044
Nana Nosirova <10577112+nananosirova@users.noreply.github.com>
2025-09-18 18:54:02 -0400

    Fix e2e tests

--- frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/models/testAcceleratorProfileModelServingTolerations.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/models/testAcceleratorProfileModelServingTolerations.cy.ts
@@ -1,6 +1,9 @@
-import type { DataScienceProjectData } from '#~/__tests__/cypress/cypress/types';
-import { deleteOpenShiftProject } from '#~/__tests__/cypress/cypress/utils/oc_commands/project';
-import { loadDSPFixture } from '#~/__tests__/cypress/cypress/utils/dataLoader';
+import type { AcceleratorProfilesModelTolerationsTestData } from '#~/__tests__/cypress/cypress/types';
+import {
+  addUserToProject,
+  deleteOpenShiftProject,
+} from '#~/__tests__/cypress/cypress/utils/oc_commands/project';
+import { loadAcceleratorProfileModelTolerationsFixture } from '#~/__tests__/cypress/cypress/utils/dataLoader';
 import { LDAP_CONTRIBUTOR_USER } from '#~/__tests__/cypress/cypress/utils/e2eUsers';
 import { projectDetails, projectListPage } from '#~/__tests__/cypress/cypress/pages/projects';
 import {
@@ -18,61 +21,67 @@ import {
   createAcceleratorProfile,
 } from '#~/__tests__/cypress/cypress/utils/oc_commands/acceleratorProfiles';
 import { generateTestUUID } from '#~/__tests__/cypress/cypress/utils/uuidGenerator';
+import { createCleanProject } from '../../../../utils/projectChecker.ts';
 
-let projectData: DataScienceProjectData;
+let testData: AcceleratorProfilesModelTolerationsTestData;
 let projectName: string;
+let contributor: string;
 let modelName: string;
 let modelFilePath: string;
-let acceleratorProfileName: string;
-const awsBucket = 'BUCKET_3';
-const uuid = generateTestUUID();
+let acceleratorProfileResourceName: string;
+const awsBucket = 'BUCKET_3' as const;
+const projectUuid = generateTestUUID();
 
 describe('[Bug: RHOAIENG-33131] Verify Model Serving Creation using Accelerator Profiles and applying Tolerations', () => {
   retryableBefore(() => {
-    return loadDSPFixture('e2e/dataScienceProjects/testSingleModelContributorCreation.yaml').then(
-      (fixtureData: DataScienceProjectData) => {
-        projectData = fixtureData;
-        projectName = `${projectData.projectSingleModelResourceName}-${uuid}`;
-        modelName = projectData.singleModelName;
-        modelFilePath = projectData.modelFilePath;
-        acceleratorProfileName = `cypress-accelerator-profile-model-${uuid}`;
+    return loadAcceleratorProfileModelTolerationsFixture(
+      'e2e/acceleratorProfiles/testAcceleratorProfileModelServingTolerations.yaml',
+    )
+      .then((fixtureData: AcceleratorProfilesModelTolerationsTestData) => {
+        testData = fixtureData;
+        projectName = `${testData.modelServingTolerationsTestNamespace}-${projectUuid}`;
+        contributor = LDAP_CONTRIBUTOR_USER.USERNAME;
+        modelName = testData.modelName;
+        modelFilePath = testData.modelFilePath;
+        acceleratorProfileResourceName = testData.acceleratorProfileName;
+
         if (!projectName) {
           throw new Error('Project name is undefined or empty in the loaded fixture');
         }
         cy.log(`Loaded project name: ${projectName}`);
+        return createCleanProject(projectName);
+      })
+      .then(() => {
+        cy.log(`Project ${projectName} confirmed to be created and verified successfully`);
+        cy.log(`Load Accelerator Profile: ${acceleratorProfileResourceName}`);
+        createAcceleratorProfile(testData.resourceYamlPath);
+
         provisionProjectForModelServing(
           projectName,
           awsBucket,
           'resources/yaml/data_connection_model_serving.yaml',
         );
-      },
-    );
+        addUserToProject(projectName, contributor, 'edit');
+      });
   });
 
   after(() => {
     // Clean up accelerator profile and project
-    cleanupAcceleratorProfiles(acceleratorProfileName);
-    deleteOpenShiftProject(projectName, { wait: true, ignoreNotFound: true });
+    cleanupAcceleratorProfiles(acceleratorProfileResourceName).then(() => {
+      if (projectName) {
+        cy.log(`Deleting Project ${projectName} after the test has finished.`);
+        deleteOpenShiftProject(projectName, { wait: false, ignoreNotFound: true });
+      }
+    });
   });
 
   it(
     'Verify that a Model can be deployed with Accelerator Profile tolerations',
     {
-      tags: [
-        '@Sanity',
-        '@SanitySet3',
-        '@ModelServing',
-        '@AcceleratorProfiles',
-        '@NonConcurrent',
-        '@Bug',
-      ],
+      tags: ['@Sanity', '@SanitySet2', '@Dashboard', '@ModelServing', '@AcceleratorProfiles'],
     },
     () => {
       cy.log('Model Name:', modelName);
-      // Create accelerator profile
-      cy.step('Create accelerator profile for testing');
-      cy.log(`Creating Accelerator Profile: ${acceleratorProfileName}`);
-      createAcceleratorProfile('resources/acceleratorProfile/acceleratorProfile.yaml');
       // Authentication and navigation
       cy.step(`Log into the application with ${LDAP_CONTRIBUTOR_USER.USERNAME}`);
       cy.visitWithLogin('/', LDAP_CONTRIBUTOR_USER);
@@ -91,15 +100,13 @@ describe('[Bug: RHOAIENG-33131] Verify Model Serving Creation using Accelerator 
       inferenceServiceModal.findGlobalScopedTemplateOption('OpenVINO Model Server').click();
       inferenceServiceModal.findModelFrameworkSelect().click();
       inferenceServiceModal.findOpenVinoIROpSet13().click();
-      inferenceServiceModal.findLocationPathInput().type(modelFilePath);
-      // Select the accelerator profile
       cy.step('Select the accelerator profile with tolerations');
-      // Select our test accelerator profile
-      inferenceServiceModal.selectAcceleratorProfileOption('cypress-accelerator-profile-model');
-      // Verify the accelerator profile is selected
+      inferenceServiceModal.selectAcceleratorProfileOption(acceleratorProfileResourceName);
       inferenceServiceModal
         .findAcceleratorProfileSelect()
-        .should('contain', 'cypress-accelerator-profile-model');
+        .should('contain', acceleratorProfileResourceName);
+      inferenceServiceModal.findLocationPathInput().type(modelFilePath);
+
       // Submit the form
       inferenceServiceModal.findSubmitButton().should('be.enabled').click();
       inferenceServiceModal.shouldBeOpen(false);
