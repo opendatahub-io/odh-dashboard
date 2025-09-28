ddd217267f2cc14748d11d1ed3a738dbff6e20c6
Andrew Ballantyne <aballant@redhat.com>
2025-09-28 16:36:24 -0400

    Fix NIM reliance on serverless

--- frontend/src/__mocks__/mockNimResource.ts
+++ frontend/src/__mocks__/mockNimResource.ts
@@ -64,7 +64,7 @@ export const mockNimInferenceService = ({
     modelName: 'test-name',
     displayName,
     namespace,
-    kserveInternalLabel: true,
+    hasExternalRoute: true,
     resources: {
       limits: { cpu: '16', memory: '64Gi' },
       requests: { cpu: '8', memory: '32Gi' },
