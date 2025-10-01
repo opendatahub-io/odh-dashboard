74c1798a059364448b0ca9c1cb3091abf40f1973
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-10-01 14:38:42 -0300

    Added odh dir in notebooks package and setup module federation
    Signed-off-by: paulovmr <832830+paulovmr@users.noreply.github.com>

--- packages/notebooks/upstream/config/moduleFederation.js
+++ packages/notebooks/upstream/config/moduleFederation.js
@@ -11,17 +11,27 @@ const moduleFederationConfig = {
     'react-dom': { singleton: true, eager: true, requiredVersion: deps['react-dom'] },
     'react-router': { singleton: true, eager: true, requiredVersion: deps['react-router'] },
     'react-router-dom': { singleton: true, eager: true, requiredVersion: deps['react-router-dom'] },
+    '@patternfly/react-core': { singleton: true, requiredVersion: deps['@patternfly/react-core'] },
+    '@openshift/dynamic-plugin-sdk': {
+      singleton: true,
+      requiredVersion: '*',
+    },
+    '@odh-dashboard/plugin-core': {
+      singleton: true,
+      requiredVersion: '0.0.0',
+    },
   },
   exposes: {
     // TODO expose api. eg:
     // './index': './src/plugin/index.tsx',
     // './plugin': './src/plugin/index.tsx',
+    './extensions': './src/odh/extensions',
   },
   // For module federation to work when optimization.runtimeChunk="single":
   // See https://github.com/webpack/webpack/issues/18810
   runtime: false,
   // TODO generate types when exposing api
-  dts: false,
+  dts: true,
 };
 
 module.exports = {
