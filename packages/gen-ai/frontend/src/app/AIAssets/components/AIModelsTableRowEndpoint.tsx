a339ea3aa1dab0eb76e55870f79b7949c9287ae2
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2025-10-01 17:45:27 +0530

    add external endpoint api token

--- packages/gen-ai/frontend/src/app/AIAssets/components/AIModelsTableRowEndpoint.tsx
+++ packages/gen-ai/frontend/src/app/AIAssets/components/AIModelsTableRowEndpoint.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
-import { Button, ButtonVariant, ClipboardCopy, Label, Popover } from '@patternfly/react-core';
+import {
+  Button,
+  ButtonVariant,
+  ClipboardCopy,
+  Content,
+  Flex,
+  FlexItem,
+  Label,
+  Popover,
+} from '@patternfly/react-core';
 import { InfoCircleIcon } from '@patternfly/react-icons';
 import { AIModel } from '~/app/types';
 
@@ -26,24 +35,51 @@ const AIModelsTableRowEndpoint: React.FC<AIModelsTableRowEndpointProps> = ({
       </Popover>
     );
   }
+
   return (
     <Popover
       position="right"
-      headerContent={
-        <div style={{ padding: '16px 16px 0 16px' }}>
-          {isExternal ? 'External' : 'Internal'} Endpoint URL
-        </div>
-      }
       bodyContent={
-        <div style={{ padding: '0 16px 16px 16px' }}>
-          <ClipboardCopy
-            hoverTip="Copy"
-            clickTip="Copied"
-            aria-label={`${isExternal ? 'external' : 'internal'} endpoint URL for ${model.model_name}`}
-          >
-            {endpoint}
-          </ClipboardCopy>
-        </div>
+        <Flex>
+          <FlexItem>
+            <Flex>
+              <FlexItem>
+                <Content style={{ fontWeight: 'var(--pf-t--global--font--weight--body--bold)' }}>
+                  {isExternal ? 'External' : 'Internal'} Endpoint URL
+                </Content>
+              </FlexItem>
+              <FlexItem>
+                <ClipboardCopy
+                  hoverTip="Copy"
+                  clickTip="Copied"
+                  aria-label={`${isExternal ? 'external' : 'internal'} endpoint URL for ${model.model_name}`}
+                >
+                  {endpoint}
+                </ClipboardCopy>
+              </FlexItem>
+            </Flex>
+          </FlexItem>
+          {isExternal && (
+            <FlexItem>
+              <Flex direction={{ default: 'column' }}>
+                <FlexItem>
+                  <Content style={{ fontWeight: 'var(--pf-t--global--font--weight--body--bold)' }}>
+                    API token
+                  </Content>
+                </FlexItem>
+                <FlexItem>
+                  <ClipboardCopy
+                    hoverTip="Copy"
+                    clickTip="Copied"
+                    aria-label={`External endpoint API token for ${model.model_name}`}
+                  >
+                    {model.sa_token.token}
+                  </ClipboardCopy>
+                </FlexItem>
+              </Flex>
+            </FlexItem>
+          )}
+        </Flex>
       }
     >
       <Button variant={ButtonVariant.link}>View</Button>
