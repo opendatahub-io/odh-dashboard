5b2c7e4948a0ee93362433bd1d3036b1dddcecd6
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-10-01 14:26:56 -0300

    Update @odh-dashboard/notebooks: feat(ws): Introduce drawer to workspace creation wizard (#310)
    Upstream commit: c81f412fd52a463d6286dc0410b8ccdbc41b63f7

--- packages/notebooks/upstream/src/app/pages/Workspaces/Creation/image/WorkspaceCreationImageSelection.tsx
+++ packages/notebooks/upstream/src/app/pages/Workspaces/Creation/image/WorkspaceCreationImageSelection.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
+import React, { useMemo, useState, useCallback } from 'react';
 import { Content, Divider, Split, SplitItem } from '@patternfly/react-core';
-import { useMemo, useState } from 'react';
 import { WorkspaceCreationImageDetails } from '~/app/pages/Workspaces/Creation/image/WorkspaceCreationImageDetails';
 import { WorkspaceCreationImageList } from '~/app/pages/Workspaces/Creation/image/WorkspaceCreationImageList';
 import { FilterByLabels } from '~/app/pages/Workspaces/Creation/labelFilter/FilterByLabels';
 import { WorkspaceImageConfigValue } from '~/shared/api/backendApiTypes';
+import { WorkspaceCreationDrawer } from '~/app/pages/Workspaces/Creation/WorkspaceCreationDrawer';
 
 interface WorkspaceCreationImageSelectionProps {
   images: WorkspaceImageConfigValue[];
@@ -16,6 +16,26 @@ const WorkspaceCreationImageSelection: React.FunctionComponent<
   WorkspaceCreationImageSelectionProps
 > = ({ images, selectedImage, onSelect }) => {
   const [selectedLabels, setSelectedLabels] = useState<Map<string, Set<string>>>(new Map());
+  const [isExpanded, setIsExpanded] = useState(false);
+  const drawerRef = React.useRef<HTMLSpanElement>(undefined);
+
+  const onExpand = useCallback(() => {
+    if (drawerRef.current) {
+      drawerRef.current.focus();
+    }
+  }, []);
+
+  const onClick = useCallback(
+    (image?: WorkspaceImageConfigValue) => {
+      setIsExpanded(true);
+      onSelect(image);
+    },
+    [onSelect],
+  );
+
+  const onCloseClick = useCallback(() => {
+    setIsExpanded(false);
+  }, []);
 
   const imageFilterContent = useMemo(
     () => (
@@ -37,18 +57,25 @@ const WorkspaceCreationImageSelection: React.FunctionComponent<
     <Content style={{ height: '100%' }}>
       <p>Select a workspace image and image version to use for the workspace.</p>
       <Divider />
-      <Split hasGutter>
-        <SplitItem style={{ minWidth: '200px' }}>{imageFilterContent}</SplitItem>
-        <SplitItem isFilled>
-          <WorkspaceCreationImageList
-            images={images}
-            selectedLabels={selectedLabels}
-            selectedImage={selectedImage}
-            onSelect={onSelect}
-          />
-        </SplitItem>
-        <SplitItem style={{ minWidth: '200px' }}>{imageDetailsContent}</SplitItem>
-      </Split>
+      <WorkspaceCreationDrawer
+        title="Image"
+        info={imageDetailsContent}
+        isExpanded={isExpanded}
+        onCloseClick={onCloseClick}
+        onExpand={onExpand}
+      >
+        <Split hasGutter>
+          <SplitItem style={{ minWidth: '200px' }}>{imageFilterContent}</SplitItem>
+          <SplitItem isFilled>
+            <WorkspaceCreationImageList
+              images={images}
+              selectedLabels={selectedLabels}
+              selectedImage={selectedImage}
+              onSelect={onClick}
+            />
+          </SplitItem>
+        </Split>
+      </WorkspaceCreationDrawer>
     </Content>
   );
 };
