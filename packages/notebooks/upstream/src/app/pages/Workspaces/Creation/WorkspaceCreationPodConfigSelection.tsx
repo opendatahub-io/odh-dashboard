f4f4ec6fb0592c0200f470a0ac0477058a92e065
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-10-01 14:26:54 -0300

    Update @odh-dashboard/notebooks: feat(ws): add initial workspace creation pod config step frontend (#236)
    Upstream commit: 8d3dc105546947a4e89907e94bb81c8950bad1bb

---
+++ packages/notebooks/upstream/src/app/pages/Workspaces/Creation/WorkspaceCreationPodConfigSelection.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Content } from '@patternfly/react-core';
+
+const WorkspaceCreationPodConfigSelection: React.FunctionComponent = () => (
+  <Content>
+    <p>Select a pod config to use for the workspace.</p>
+  </Content>
+);
+
+export { WorkspaceCreationPodConfigSelection };
