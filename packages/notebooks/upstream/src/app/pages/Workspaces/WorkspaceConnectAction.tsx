7b2e88062eb367cc23067b7a6e2ec10ae3dd3d18
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-10-01 14:26:55 -0300

    Update @odh-dashboard/notebooks: feat(ws): workspaces connect button sizing (#315)
    Upstream commit: afc3bf70da8843a5da99d5d8f4852beb52baafe4

--- packages/notebooks/upstream/src/app/pages/Workspaces/WorkspaceConnectAction.tsx
+++ packages/notebooks/upstream/src/app/pages/Workspaces/WorkspaceConnectAction.tsx
@@ -50,7 +50,6 @@ export const WorkspaceConnectAction: React.FunctionComponent<WorkspaceConnectAct
           ref={toggleRef}
           onClick={onToggleClick}
           isExpanded={open}
-          isFullWidth
           isDisabled={workspace.status.state !== WorkspaceState.Running}
           splitButtonItems={[
             <MenuToggleAction
