29714d51d114bf47c4d353bb1f9d59a999f9aeaa
Nana Nosirova <10577112+nananosirova@users.noreply.github.com>
2025-10-02 13:13:50 -0400

    Remove delete button for the OOTB hardware profile (#5065)

--- frontend/src/__mocks__/mockHardwareProfile.ts
+++ frontend/src/__mocks__/mockHardwareProfile.ts
@@ -20,7 +20,7 @@ type MockResourceConfigType = {
   description?: string;
   enabled?: boolean;
   nodeSelector?: NodeSelector;
-  schedulingType?: SchedulingType;
+  schedulingType?: SchedulingType | '';
   localQueueName?: string;
   priorityClass?: string;
   tolerations?: Toleration[];
@@ -88,21 +88,23 @@ export const mockHardwareProfile = ({
   },
   spec: {
     identifiers,
-    scheduling: {
-      type: schedulingType,
-      ...(schedulingType === SchedulingType.QUEUE && {
-        kueue: {
-          localQueueName,
-          priorityClass,
-        },
-      }),
-      ...(schedulingType === SchedulingType.NODE && {
-        node: {
-          ...(nodeSelector ? { nodeSelector } : {}),
-          tolerations,
-        },
-      }),
-    },
+    ...(schedulingType && {
+      scheduling: {
+        type: schedulingType,
+        ...(schedulingType === SchedulingType.QUEUE && {
+          kueue: {
+            localQueueName,
+            priorityClass,
+          },
+        }),
+        ...(schedulingType === SchedulingType.NODE && {
+          node: {
+            ...(nodeSelector ? { nodeSelector } : {}),
+            tolerations,
+          },
+        }),
+      },
+    }),
   },
 });
 
@@ -340,3 +342,42 @@ export const mockProjectScopedHardwareProfiles = [
     ],
   }),
 ];
+
+export const mockDefaultHardwareProfile = mockHardwareProfile({
+  name: 'default-profile',
+  namespace: 'opendatahub',
+  displayName: 'default-profile',
+  description:
+    'Provides a baseline hardware profile with 2 CPUs and 4Gi memory by default, adjustable up to 4 CPUs and 8Gi memory.',
+  identifiers: [
+    {
+      displayName: 'CPU',
+      identifier: 'cpu',
+      minCount: '1',
+      maxCount: '4',
+      defaultCount: '2',
+      resourceType: IdentifierResourceType.CPU,
+    },
+    {
+      displayName: 'Memory',
+      identifier: 'memory',
+      minCount: '2Gi',
+      maxCount: '8Gi',
+      defaultCount: '4Gi',
+      resourceType: IdentifierResourceType.MEMORY,
+    },
+  ],
+  annotations: {
+    'internal.config.kubernetes.io/previousKinds': 'HardwareProfile',
+    'internal.config.kubernetes.io/previousNames': 'default-profile',
+    'internal.config.kubernetes.io/previousNamespaces': 'opendatahub',
+    'opendatahub.io/dashboard-feature-visibility': '[]',
+    'opendatahub.io/disabled': 'false',
+    'opendatahub.io/managed': 'false',
+  },
+  labels: {
+    'app.kubernetes.io/part-of': 'hardwareprofile',
+    'app.opendatahub.io/hardwareprofile': 'true',
+  },
+  schedulingType: '',
+});
