89a2f727d7596d69c5b6a03c23a63b53d269578b
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2025-10-06 19:48:20 +0530

    fix gen-ai routes

--- packages/gen-ai/frontend/src/app/utilities/const.ts
+++ packages/gen-ai/frontend/src/app/utilities/const.ts
@@ -1,18 +1,10 @@
 import { asEnumMember, DeploymentMode } from 'mod-arch-core';
 
 const URL_PREFIX = process.env.URL_PREFIX || '/gen-ai';
-const PLAYGROUND_URL_PREFIX = `${URL_PREFIX}/playground`;
-const ASSETS_URL_PREFIX = `${URL_PREFIX}/assets`;
 
 const DEPLOYMENT_MODE =
   asEnumMember(process.env.DEPLOYMENT_MODE, DeploymentMode) || DeploymentMode.Federated;
 
 const MCP_SERVERS_SESSION_STORAGE_KEY = 'gen-ai-playground-servers';
 
-export {
-  URL_PREFIX,
-  DEPLOYMENT_MODE,
-  PLAYGROUND_URL_PREFIX,
-  ASSETS_URL_PREFIX,
-  MCP_SERVERS_SESSION_STORAGE_KEY,
-};
+export { URL_PREFIX, DEPLOYMENT_MODE, MCP_SERVERS_SESSION_STORAGE_KEY };
