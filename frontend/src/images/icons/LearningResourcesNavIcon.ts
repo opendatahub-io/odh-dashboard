212d2d8c8fc5b5d378ca5ba95360fe9f9cf5badb
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2025-10-07 11:04:59 -0300

    Update navigation icons
    Signed-off-by: Guilherme Caponetto <638737+caponetto@users.noreply.github.com>

---
+++ frontend/src/images/icons/LearningResourcesNavIcon.ts
@@ -0,0 +1,13 @@
+import { createIcon } from '@patternfly/react-icons/dist/esm/createIcon';
+
+const LearningResourcesNavIcon = createIcon({
+  name: 'LearningResourcesNavIcon',
+  width: 32,
+  height: 32,
+  svgPath:
+    'M21,16h4c.5527,0,1-.4473,1-1s-.4473-1-1-1h-4c-.5527,0-1,.4473-1,1s.4473,1,1,1Z M21,21h4c.5527,0,1-.4473,1-1s-.4473-1-1-1h-4c-.5527,0-1,.4473-1,1s.4473,1,1,1Z M23,24h-2c-.5527,0-1,.4473-1,1s.4473,1,1,1h2c.5527,0,1-.4473,1-1s-.4473-1-1-1Z M29.5,9h-13.0859L8.707,1.293c-.2852-.2861-.7148-.3721-1.0898-.2168-.374.1543-.6172.5195-.6172.9238v7H2.5c-.8271,0-1.5.6729-1.5,1.5v19c0,.8271.6729,1.5,1.5,1.5h27c.8271,0,1.5-.6729,1.5-1.5V10.5c0-.8271-.6729-1.5-1.5-1.5ZM9,4.4141l6,6v17.1719l-6-6V4.4141ZM3,11h4v11c0,.2656.1055.5195.293.707l6.293,6.293H3V11ZM29,29h-12V11h12v18Z',
+  xOffset: 0,
+  yOffset: 0,
+});
+
+export default LearningResourcesNavIcon;
