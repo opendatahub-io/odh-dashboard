71fed142b08f7930ad9fd5272a8ab2f2fc82c587
Jill R. Pelavin <jpelavin@redhat.com>
2025-10-10 13:58:40 -0400

    deleting/commenting out  references to the Acceleratorprofilesection

--- frontend/src/__tests__/cypress/cypress/tests/mocked/modelRegistry/modelVersion/modelVersionDeploy.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/mocked/modelRegistry/modelVersion/modelVersionDeploy.cy.ts
@@ -24,7 +24,6 @@ import { kserveModal } from '#~/__tests__/cypress/cypress/pages/modelServing';
 import { mockModelArtifact } from '#~/__mocks__/mockModelArtifact';
 import { initDeployPrefilledModelIntercepts } from '#~/__tests__/cypress/cypress/utils/modelServingUtils';
 import { hardwareProfileSection } from '#~/__tests__/cypress/cypress/pages/components/HardwareProfileSection';
-import { acceleratorProfileSection } from '#~/__tests__/cypress/cypress/pages/components/subComponents/AcceleratorProfileSection';
 
 const MODEL_REGISTRY_API_VERSION = 'v1alpha3';
 
@@ -361,7 +360,7 @@ describe.skip('Deploy model version', () => {
 
     // Check for project specific serving runtimes
     kserveModal.findProjectScopedTemplateOption('Caikit').click();
-    acceleratorProfileSection.findProjectScopedLabel().should('exist');
+    // acceleratorProfileSection.findProjectScopedLabel().should('exist');
     kserveModal.findModelFrameworkSelect().should('be.disabled');
     kserveModal.findModelFrameworkSelect().should('have.text', 'openvino_ir - opset1');
     cy.findByText(
@@ -373,7 +372,7 @@ describe.skip('Deploy model version', () => {
     // Check for global specific serving runtimes
     kserveModal.findServingRuntimeTemplateSearchSelector().click();
     kserveModal.findGlobalScopedTemplateOption('Multi Platform').click();
-    acceleratorProfileSection.findGlobalScopedLabel().should('exist');
+    // acceleratorProfileSection.findGlobalScopedLabel().should('exist');
     kserveModal.findModelFrameworkSelect().should('be.enabled');
     kserveModal.findModelFrameworkSelect().findSelectOption('onnx - 1').click();
     cy.findByText(
@@ -410,45 +409,6 @@ describe.skip('Deploy model version', () => {
     hardwareProfileSection.findSelect().should('exist');
     hardwareProfileSection.findSelect().click();
     // Read visible menu options from the open PF v6 menu and verify expected text
-  });
-
-  // note: when fixing this test; note that the accelerator profiles are removed as of 3.0
-  it('Display project specific accelerator profile while deploying', () => {
-    initIntercepts({ disableProjectScoped: false });
-    cy.visit(`/ai-hub/registry/modelregistry-sample/registered-models/1/versions`);
-    const modelVersionRow = modelRegistry.getModelVersionRow('test model version 4');
-    modelVersionRow.findKebabAction('Deploy').click();
-    modelVersionDeployModal.selectProjectByName('Test project');
-    kserveModal.findModelNameInput().should('exist');
-
-    // Verify accelerator profile section exists
-    acceleratorProfileSection.findAcceleratorProfileSearchSelector().should('exist');
-    acceleratorProfileSection.findAcceleratorProfileSearchSelector().click();
-
-    // verify available project-scoped accelerator profile
-    const projectScopedAcceleratorProfile =
-      acceleratorProfileSection.getProjectScopedAcceleratorProfile();
-    projectScopedAcceleratorProfile
-      .find()
-      .findByRole('menuitem', {
-        name: 'Small Profile nvidia.com/gpu',
-        hidden: true,
-      })
-      .click();
-    acceleratorProfileSection.findProjectScopedLabel().should('exist');
-
-    // verify available global-scoped accelerator profile
-    acceleratorProfileSection.findAcceleratorProfileSearchSelector().click();
-    const globalScopedAcceleratorProfile =
-      acceleratorProfileSection.getGlobalScopedAcceleratorProfile();
-    globalScopedAcceleratorProfile
-      .find()
-      .findByRole('menuitem', {
-        name: 'Small Profile Global nvidia.com/gpu',
-        hidden: true,
-      })
-      .click();
-    acceleratorProfileSection.findGlobalScopedLabel().should('exist');
   });
 
   it('Selects Create Connection in case of no matching connections', () => {
