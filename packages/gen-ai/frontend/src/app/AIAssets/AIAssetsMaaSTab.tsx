2ea9ac7ef629ae114c7b1045a898d6aa1d13ca60
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2025-10-16 12:28:23 +0530

    update empty state

--- packages/gen-ai/frontend/src/app/AIAssets/AIAssetsMaaSTab.tsx
+++ packages/gen-ai/frontend/src/app/AIAssets/AIAssetsMaaSTab.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Bullseye, Content, Spinner } from '@patternfly/react-core';
 import ModelsEmptyState from '~/app/EmptyStates/NoData';
 import useFetchMaaSModels from '~/app/hooks/useFetchMaaSModels';
 import MaaSModelsTable from '~/app/AIAssets/components/MaaSModelsTable';
+import { GenAiContext } from '~/app/context/GenAiContext';
 
 const AIAssetsMaaSTab: React.FC = () => {
+  const navigate = useNavigate();
+  const { namespace } = React.useContext(GenAiContext);
   const { data: models = [], loaded, error } = useFetchMaaSModels();
 
-  if (!loaded) {
+  if (!loaded && !error) {
     return (
       <Bullseye>
         <Spinner />
@@ -18,18 +22,25 @@ const AIAssetsMaaSTab: React.FC = () => {
   if (error || models.length === 0) {
     return (
       <ModelsEmptyState
-        title="No MaaS models available"
+        title="No models available as a service"
         description={
           <Content
             style={{
-              textAlign: 'left',
+              textAlign: 'center',
+              whiteSpace: 'nowrap',
             }}
           >
             <Content component="p">
-              No Model as a Service (MaaS) models are currently available.
+              This page displays only model deployments that are available through model as a
+              service. <br /> To make a deployment available as model as a service, edit it from the{' '}
+              <b>Model deployments</b> page.
             </Content>
           </Content>
         }
+        actionButtonText="Go to model deployments"
+        handleActionButtonClick={() => {
+          navigate(`/ai-hub/deployments/${namespace?.name}`);
+        }}
       />
     );
   }
