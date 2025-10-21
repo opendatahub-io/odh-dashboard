cd72c17c7c0ac32502db0ab4d306b9e8a7de0dcf
Anthony Coughlin <acoughli@redhat.com>
2025-10-21 16:54:04 +0100

    Fix lint errors in testLaunchStandaloneNotebook
    - Remove unused imports (yaml, NotebookImageData type)
    - Fix retryableBefore syntax and indentation
    - Apply proper Prettier formatting

--- frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/testLaunchStandaloneNotebook.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/dataScienceProjects/testLaunchStandaloneNotebook.cy.ts
@@ -1,8 +1,6 @@
-import yaml from 'js-yaml';
 import { HTPASSWD_CLUSTER_ADMIN_USER } from '#~/__tests__/cypress/cypress/utils/e2eUsers';
 import { projectListPage } from '#~/__tests__/cypress/cypress/pages/projects';
 import { notebookServer } from '#~/__tests__/cypress/cypress/pages/notebookServer';
-import type { NotebookImageData } from '#~/__tests__/cypress/cypress/types';
 import {
   waitForPodReady,
   deleteNotebook,
@@ -10,17 +8,9 @@ import {
 import { retryableBefore } from '#~/__tests__/cypress/cypress/utils/retryableHooks';
 
 describe('Verify a Jupyter Notebook can be launched directly from the Project List View', () => {
-  let testData: NotebookImageData;
-
-  retryableBefore(() =>
-    cy
-      .fixture('e2e/dataScienceProjects/testNotebookCreation.yaml', 'utf8')
-      .then((yamlContent: string) => {
-        testData = yaml.load(yamlContent) as NotebookImageData;
-        // Check if a notebook is running and delete if it is
-        deleteNotebook('jupyter-nb');
-      }),
-  );
+  retryableBefore(() => {
+    deleteNotebook('jupyter-nb');
+  });
 
   it(
     'Verify User Can Access Jupyter Launcher From Project Page',
