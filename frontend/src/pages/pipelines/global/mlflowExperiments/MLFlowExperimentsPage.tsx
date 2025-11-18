1bf40298952c1e98f8f041a2587e135ae07895a2
rsun19 <robertssun1234@gmail.com>
2025-11-18 13:27:37 -0500

    updated placeholder url

--- frontend/src/pages/pipelines/global/mlflowExperiments/MLFlowExperimentsPage.tsx
+++ frontend/src/pages/pipelines/global/mlflowExperiments/MLFlowExperimentsPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ApplicationsPage from '@odh-dashboard/internal/pages/ApplicationsPage';
+import ApplicationsPage from '#~/pages/ApplicationsPage';
 
 const GlobalMLflowExperimentsPage: React.FC = () => {
   return (
