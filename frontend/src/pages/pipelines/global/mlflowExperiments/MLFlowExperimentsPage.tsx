604b74207a9ca16d56fe25525549a4937f58e804
rsun19 <robertssun1234@gmail.com>
2025-11-18 12:04:10 -0500

    added mlflow link

--- frontend/src/pages/pipelines/global/mlflowExperiments/MLFlowExperimentsPage.tsx
+++ frontend/src/pages/pipelines/global/mlflowExperiments/MLFlowExperimentsPage.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import ApplicationsPage from '@odh-dashboard/internal/pages/ApplicationsPage';
 
 const GlobalMLflowExperimentsPage: React.FC = () => {
-  return <ApplicationsPage loaded empty={false} title="MLflow Experiments"></ApplicationsPage>;
+  return (
+    <ApplicationsPage loaded empty={false} title="MLflow Experiments">
+      <iframe
+        src="/mlflow/#/experiments"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+        }}
+      ></iframe>
+    </ApplicationsPage>
+  );
 };
 
 export default GlobalMLflowExperimentsPage;
