7fdafa3f43447ebafd02e9411e1f219a72f2baa8
Pushpa Padti <99261071+ppadti@users.noreply.github.com>
2025-11-19 21:34:56 +0530

    Fix model registry e2e test (#5489)

--- frontend/src/__tests__/cypress/cypress/tests/e2e/modelRegistry/testRegisterModel.cy.ts
+++ frontend/src/__tests__/cypress/cypress/tests/e2e/modelRegistry/testRegisterModel.cy.ts
@@ -199,6 +199,9 @@ describe('[Product Bug: RHOAIENG-35821] Verify models can be registered in a mod
       registerVersionPage
         .findFormField(VersionFormFieldSelector.SOURCE_MODEL_FORMAT_VERSION)
         .type(testData.formatVersion3_0);
+      registerVersionPage
+        .findFormField(VersionFormFieldSelector.LOCATION_PATH)
+        .type(testData.objectStoragePath);
 
       cy.step('Configure URI location for the new version');
       registerVersionPage.findFormField(VersionFormFieldSelector.LOCATION_TYPE_URI).click();
