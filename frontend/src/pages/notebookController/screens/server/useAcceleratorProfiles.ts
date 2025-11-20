0c919619f2b9bda4689a2b03bfcfa1dfb737927b
Jill R. Pelavin <jpelavin@redhat.com>
2025-11-20 14:22:30 -0500

    one more

--- frontend/src/pages/notebookController/screens/server/useAcceleratorProfiles.ts
+++ frontend/src/pages/notebookController/screens/server/useAcceleratorProfiles.ts
@@ -6,6 +6,7 @@ import { listAcceleratorProfiles } from '#~/api';
 /**
  *  @deprecated -- only in deprecation paths (modelmesh and finetuning (in both of them))
  * modelmesh: RHOAIENG-34917, finetuning: RHOAIENG-19185
+ * fine-tuning: RHOAIENG-36276, RHOAIENG-34285
  */
 const useAcceleratorProfiles = (namespace: string): FetchState<AcceleratorProfileKind[]> => {
   const getAcceleratorProfiles = React.useCallback(
