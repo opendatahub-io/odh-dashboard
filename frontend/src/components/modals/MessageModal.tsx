adda1728ae974f6eff654203b3c31ed9bf649397
Jill R. Pelavin <jpelavin@redhat.com>
2025-11-21 14:12:59 -0500

    starting to use the new modal in the startingstatus modal

--- frontend/src/components/modals/MessageModal.tsx
+++ frontend/src/components/modals/MessageModal.tsx
@@ -13,10 +13,12 @@ type ButtonAction = {
 type MessageModalProps = {
   onClose: () => void;
   contents: React.ReactNode;
-  title: string;
+  title: string | React.ReactNode;
   closeText?: string;
   buttonActions?: ButtonAction[];
   description?: React.ReactNode;
+  disableFocusTrap?: boolean;
+  dataTestId?: string;
 };
 
 const MessageModal: React.FC<MessageModalProps> = ({
@@ -25,20 +27,30 @@ const MessageModal: React.FC<MessageModalProps> = ({
   title,
   buttonActions,
   description,
+  disableFocusTrap,
+  dataTestId = 'pipeline-server-starting-modal',
 }) => {
   return (
     <Modal
-      data-testid="pipeline-server-starting-modal"
+      data-testid={dataTestId}
       isOpen
       variant="medium"
       onClose={onClose}
-      title={title}
+      title={typeof title === 'string' ? title : 'Modal'}
+      disableFocusTrap={disableFocusTrap}
     >
-      <ModalHeader title={title} description={description} />
+      <ModalHeader title={typeof title === 'string' ? title : undefined} description={description}>
+        {typeof title !== 'string' ? title : null}
+      </ModalHeader>
       <ModalBody className="odh-modal__content-height">{contents}</ModalBody>
       <ModalFooter>
-        {buttonActions?.map((action) => (
-          <Button variant={action.variant} onClick={action.onClick} data-testid={action.dataTestId}>
+        {buttonActions?.map((action, index) => (
+          <Button
+            key={`${action.label}-${index}`}
+            variant={action.variant}
+            onClick={action.onClick}
+            data-testid={action.dataTestId}
+          >
             {action.label}
           </Button>
         ))}
