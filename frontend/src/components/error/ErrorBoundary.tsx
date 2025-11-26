42cd7cd087805efb08d336e2c141ffeb8775a30d
Anthony Coughlin <33689325+antowaddle@users.noreply.github.com>
2025-11-26 09:57:13 +0000

    Error introduction

--- frontend/src/components/error/ErrorBoundary.tsx
+++ frontend/src/components/error/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from 'react';!!!!!!!
 import { Button, Split, SplitItem, Title } from '@patternfly/react-core';
 import { TimesIcon } from '@patternfly/react-icons';
 import ErrorDetails from './ErrorDetails';
