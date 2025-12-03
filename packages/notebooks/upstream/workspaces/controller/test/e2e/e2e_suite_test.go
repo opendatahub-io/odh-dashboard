b0a933f16f2297de05f918205dead44fd65dec19
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-12-03 12:14:49 -0300

    Update @odh-dashboard/notebooks: test(ws): enhance e2e test setup and cleanup (#39)
    Upstream commit: d1a8c3ccb7d397370d0797beaffc68c49475ea01

--- packages/notebooks/upstream/workspaces/controller/test/e2e/e2e_suite_test.go
+++ packages/notebooks/upstream/workspaces/controller/test/e2e/e2e_suite_test.go
@@ -18,15 +18,89 @@ package e2e
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"testing"
+
+	"github.com/kubeflow/notebooks/workspaces/controller/test/utils"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-// Run e2e tests using the Ginkgo runner.
+var (
+	// These variables are useful to avoid re-installation and conflicts:
+	//  - PROMETHEUS_INSTALL_SKIP=true: Skips Prometheus installation during test setup.
+	//  - CERT_MANAGER_INSTALL_SKIP=true: Skips CertManager installation during test setup.
+	skipCertManagerInstall = os.Getenv("CERT_MANAGER_INSTALL_SKIP") == "true"
+	// skipPrometheusInstall  = os.Getenv("PROMETHEUS_INSTALL_SKIP") == "true"
+
+	// isCertManagerAlreadyInstalled will be set true when CertManager CRDs be found on the cluster
+	isCertManagerAlreadyInstalled = false
+
+	// isPrometheusOperatorAlreadyInstalled will be set true when prometheus CRDs be found on the cluster
+	// isPrometheusOperatorAlreadyInstalled = false
+)
+
+// TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
+// temporary environment to validate project changes with the purposed to be used in CI jobs.
+// The default setup requires Kind, builds/loads the Manager Docker image locally, and installs
+// CertManager and Prometheus.
 func TestE2E(t *testing.T) {
 	RegisterFailHandler(Fail)
 	_, _ = fmt.Fprintf(GinkgoWriter, "Starting workspace-controller suite\n")
 	RunSpecs(t, "e2e suite")
 }
+
+var _ = BeforeSuite(func() {
+	By("building the controller image")
+	cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", controllerImage))
+	_, err := utils.Run(cmd)
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+
+	By("loading the controller image on Kind")
+	err = utils.LoadImageToKindClusterWithName(controllerImage)
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+
+	// TODO: enable Prometheus installation once we start using it
+	// if !skipPrometheusInstall {
+	// 	By("checking if prometheus is installed already")
+	// 	isPrometheusOperatorAlreadyInstalled = utils.IsPrometheusCRDsInstalled()
+	// 	if !isPrometheusOperatorAlreadyInstalled {
+	// 		_, _ = fmt.Fprintf(GinkgoWriter, "Installing Prometheus Operator...\n")
+	// 		Expect(utils.InstallPrometheusOperator()).To(Succeed(), "Failed to install Prometheus Operator")
+	// 	} else {
+	// 		_, _ = fmt.Fprintf(GinkgoWriter, "WARNING: Prometheus Operator is already installed. Skipping installation...\n")
+	// 	}
+	// }
+	// By("checking that prometheus is running")
+	// Expect(utils.WaitPrometheusOperatorRunning()).To(Succeed(), "Prometheus Operator is not running")
+
+	if !skipCertManagerInstall {
+		By("checking if cert manager is installed already")
+		isCertManagerAlreadyInstalled = utils.IsCertManagerCRDsInstalled()
+		if !isCertManagerAlreadyInstalled {
+			_, _ = fmt.Fprintf(GinkgoWriter, "Installing CertManager...\n")
+			Expect(utils.InstallCertManager()).To(Succeed(), "Failed to install CertManager")
+		} else {
+			_, _ = fmt.Fprintf(GinkgoWriter, "WARNING: CertManager is already installed. Skipping installation...\n")
+		}
+	}
+	By("checking that cert manager is running")
+	Expect(utils.WaitCertManagerRunning()).To(Succeed(), "CertManager is not running")
+})
+
+var _ = AfterSuite(func() {
+
+	// if !skipPrometheusInstall && !isPrometheusOperatorAlreadyInstalled {
+	// 	By("uninstalling Prometheus Operator")
+	// 	_, _ = fmt.Fprintf(GinkgoWriter, "Uninstalling Prometheus Operator...\n")
+	// 	utils.UninstallPrometheusOperator()
+	// }
+
+	if !skipCertManagerInstall && !isCertManagerAlreadyInstalled {
+		By("uninstalling CertManager")
+		_, _ = fmt.Fprintf(GinkgoWriter, "Uninstalling CertManager...\n")
+		utils.UninstallCertManager()
+	}
+})
