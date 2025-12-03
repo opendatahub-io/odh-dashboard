d69a820b0903406344da6c41a5a9b5e19e357576
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-12-03 12:18:08 -0300

    Update @odh-dashboard/notebooks: fix(ws): Improve workspace form drawer details and wizard flow (#467)
    Upstream commit: e76308aee3c30b8ed3158ac1338e4decb4face59

--- packages/notebooks/upstream/workspaces/frontend/src/app/pages/Workspaces/Form/image/WorkspaceFormImageDetails.tsx
+++ packages/notebooks/upstream/workspaces/frontend/src/app/pages/Workspaces/Form/image/WorkspaceFormImageDetails.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { List, ListItem, Title } from '@patternfly/react-core';
+import {
+  DescriptionList,
+  DescriptionListTerm,
+  DescriptionListGroup,
+  DescriptionListDescription,
+  Title,
+} from '@patternfly/react-core';
 import { WorkspacePodConfigValue } from '~/shared/api/backendApiTypes';
+import { formatLabelKey } from '~/shared/utilities/WorkspaceUtils';
 
 type WorkspaceFormImageDetailsProps = {
   workspaceImage?: WorkspacePodConfigValue;
@@ -14,13 +21,21 @@ export const WorkspaceFormImageDetails: React.FunctionComponent<WorkspaceFormIma
       <>
         <Title headingLevel="h3">{workspaceImage.displayName}</Title>
         <br />
-        <List isPlain>
-          {workspaceImage.labels.map((label) => (
-            <ListItem key={label.key}>
-              {label.key}={label.value}
-            </ListItem>
-          ))}
-        </List>
+        {workspaceImage.labels.map((label) => (
+          <DescriptionList
+            key={label.key}
+            isHorizontal
+            isCompact
+            horizontalTermWidthModifier={{
+              default: '17ch',
+            }}
+          >
+            <DescriptionListGroup>
+              <DescriptionListTerm>{formatLabelKey(label.key)}</DescriptionListTerm>
+              <DescriptionListDescription>{label.value}</DescriptionListDescription>
+            </DescriptionListGroup>
+          </DescriptionList>
+        ))}
       </>
     )}
   </div>
