ab54a6433a9261a69a60884f9ab26428dcd5023c
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-12-03 12:18:12 -0300

    Update @odh-dashboard/notebooks: feat: Render Loading if Namespaces are fetching (#604)
    Upstream commit: fa5fb5a7cc65d8829052e03f0be095238ff301c4

--- packages/notebooks/upstream/workspaces/frontend/src/app/hooks/useWorkspaces.ts
+++ packages/notebooks/upstream/workspaces/frontend/src/app/hooks/useWorkspaces.ts
@@ -2,11 +2,13 @@ import { FetchState, FetchStateCallbackPromise, useFetchState } from 'mod-arch-c
 import { useCallback } from 'react';
 import { useNotebookAPI } from '~/app/hooks/useNotebookAPI';
 import { ApiWorkspaceListEnvelope } from '~/generated/data-contracts';
+import { useNamespaceContext } from '~/app/context/NamespaceContextProvider';
 
 export const useWorkspacesByNamespace = (
   namespace: string,
 ): FetchState<ApiWorkspaceListEnvelope['data']> => {
   const { api, apiAvailable } = useNotebookAPI();
+  const { namespacesLoaded, selectedNamespace } = useNamespaceContext();
 
   const call = useCallback<
     FetchStateCallbackPromise<ApiWorkspaceListEnvelope['data']>
@@ -14,10 +16,12 @@ export const useWorkspacesByNamespace = (
     if (!apiAvailable) {
       return Promise.reject(new Error('API not yet available'));
     }
-
+    if (!namespacesLoaded || selectedNamespace === '') {
+      return Promise.reject(new Error('Namespaces not yet available'));
+    }
     const envelope = await api.workspaces.listWorkspacesByNamespace(namespace);
     return envelope.data;
-  }, [api, apiAvailable, namespace]);
+  }, [api.workspaces, apiAvailable, namespace, namespacesLoaded, selectedNamespace]);
 
   return useFetchState(call, []);
 };
