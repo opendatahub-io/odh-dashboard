09d4ed6b5d3dd45fb1660f4692389b7aa4f3859f
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-12-03 12:18:10 -0300

    Update @odh-dashboard/notebooks: feat: Conditionally render masthead on Production and Standalone modes (#516)
    Upstream commit: 03c14dd2d72d13492865ceea6b457be32d84d7ff

--- packages/notebooks/upstream/workspaces/frontend/config/webpack.prod.js
+++ packages/notebooks/upstream/workspaces/frontend/config/webpack.prod.js
@@ -2,10 +2,13 @@
 
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const { EnvironmentPlugin } = require('webpack');
 const { stylePaths } = require('./stylePaths');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
+
+const PRODUCTION = process.env.PRODUCTION || 'false';
 
 module.exports = merge(common('production'), {
   mode: 'production',
@@ -24,6 +27,9 @@ module.exports = merge(common('production'), {
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[name].bundle.css',
+    }),
+    new EnvironmentPlugin({
+      PRODUCTION,
     }),
   ],
   module: {
