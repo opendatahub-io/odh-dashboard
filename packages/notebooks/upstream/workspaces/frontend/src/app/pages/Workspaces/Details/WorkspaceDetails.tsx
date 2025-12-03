8b007ea234b82aae36079a75219089ed68ca108d
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-12-03 12:14:51 -0300

    Update @odh-dashboard/notebooks: feat(ws): Notebooks 2.0 // Frontend // Workspaces details // Pod template tab #173 (#197)
    Upstream commit: b981275b8139133e6c85b738ee7400862f2eea99

--- packages/notebooks/upstream/workspaces/frontend/src/app/pages/Workspaces/Details/WorkspaceDetails.tsx
+++ packages/notebooks/upstream/workspaces/frontend/src/app/pages/Workspaces/Details/WorkspaceDetails.tsx
@@ -16,6 +16,7 @@ import { Workspace } from '~/shared/types';
 import { WorkspaceDetailsOverview } from '~/app/pages/Workspaces/Details/WorkspaceDetailsOverview';
 import { WorkspaceDetailsActions } from '~/app/pages/Workspaces/Details/WorkspaceDetailsActions';
 import { WorkspaceDetailsActivity } from '~/app/pages/Workspaces/Details/WorkspaceDetailsActivity';
+import { WorkspaceDetailsPodTemplate } from '~/app/pages/Workspaces/Details/WorkspaceDetailsPodTemplate';
 
 type WorkspaceDetailsProps = {
   workspace: Workspace;
@@ -48,42 +49,84 @@ export const WorkspaceDetails: React.FunctionComponent<WorkspaceDetailsProps> = 
           <DrawerCloseButton onClick={onCloseClick} />
         </DrawerActions>
       </DrawerHead>
+
       <DrawerPanelBody>
         <Tabs activeKey={activeTabKey} onSelect={handleTabClick}>
-          <Tab eventKey={0} title={<TabTitleText>Overview</TabTitleText>} aria-label="Overview">
-            <TabContent id="overviewSectionBodyPadding">
-              <TabContentBody hasPadding>
-                <WorkspaceDetailsOverview workspace={workspace} />
-              </TabContentBody>
-            </TabContent>
-          </Tab>
+          <Tab
+            eventKey={0}
+            title={<TabTitleText>Overview</TabTitleText>}
+            tabContentId="overviewTabContent"
+            aria-label="Overview"
+          />
           <Tab
             eventKey={1}
             title={<TabTitleText>Activity</TabTitleText>}
+            tabContentId="activityTabContent"
             aria-label="Activity"
             data-testid="activityTab"
-          >
-            <TabContent id="activitySectionBodyPadding">
-              <TabContentBody hasPadding>
-                <WorkspaceDetailsActivity workspace={workspace} />
-              </TabContentBody>
-            </TabContent>
-          </Tab>
-          <Tab eventKey={2} title={<TabTitleText>Logs</TabTitleText>} aria-label="Logs">
-            <TabContent id="logsSectionBodyPadding">
-              <TabContentBody hasPadding>Logs</TabContentBody>
-            </TabContent>
-          </Tab>
+          />
+          <Tab
+            eventKey={2}
+            title={<TabTitleText>Logs</TabTitleText>}
+            tabContentId="logsTabContent"
+            aria-label="Logs"
+          />
           <Tab
             eventKey={3}
             title={<TabTitleText>Pod template</TabTitleText>}
+            tabContentId="podTemplateTabContent"
             aria-label="Pod template"
-          >
-            <TabContent id="podTemplateBodyPadding">
-              <TabContentBody hasPadding>Pod template</TabContentBody>
-            </TabContent>
-          </Tab>
+          />
         </Tabs>
+      </DrawerPanelBody>
+
+      <DrawerPanelBody>
+        <TabContent
+          key={0}
+          eventKey={0}
+          id="overviewTabContent"
+          activeKey={activeTabKey}
+          hidden={activeTabKey !== 0}
+        >
+          <TabContentBody hasPadding>
+            <WorkspaceDetailsOverview workspace={workspace} />
+          </TabContentBody>
+        </TabContent>
+
+        <TabContent
+          key={1}
+          eventKey={1}
+          id="activityTabContent"
+          activeKey={activeTabKey}
+          hidden={activeTabKey !== 1}
+        >
+          <TabContentBody hasPadding>
+            <WorkspaceDetailsActivity workspace={workspace} />
+          </TabContentBody>
+        </TabContent>
+
+        <TabContent
+          key={2}
+          eventKey={2}
+          id="logsTabContent"
+          activeKey={activeTabKey}
+          hidden={activeTabKey !== 2}
+        >
+          <TabContentBody hasPadding>Logs</TabContentBody>
+        </TabContent>
+
+        <TabContent
+          key={3}
+          style={{ height: '100%' }}
+          eventKey={3}
+          id="podTemplateTabContent"
+          activeKey={activeTabKey}
+          hidden={activeTabKey !== 3}
+        >
+          <TabContentBody style={{ height: '100%' }} hasPadding>
+            <WorkspaceDetailsPodTemplate />
+          </TabContentBody>
+        </TabContent>
       </DrawerPanelBody>
     </DrawerPanelContent>
   );
