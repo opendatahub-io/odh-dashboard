2c350b9dfbbaab06f922f0588de4259830c50949
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-12-03 12:18:15 -0300

    Update @odh-dashboard/notebooks: fix: remove split toggle from 'Connect' button (#760)
    Upstream commit: 054ef4e13e45fb29bb24ca286b10168c1d62350b

--- packages/notebooks/upstream/workspaces/frontend/src/app/pages/Workspaces/WorkspaceConnectAction.tsx
+++ packages/notebooks/upstream/workspaces/frontend/src/app/pages/Workspaces/WorkspaceConnectAction.tsx
@@ -7,7 +7,6 @@ import {
 import {
   MenuToggle,
   MenuToggleElement,
-  MenuToggleAction,
 } from '@patternfly/react-core/dist/esm/components/MenuToggle';
 import { WorkspacesWorkspace, WorkspacesWorkspaceState } from '~/generated/data-contracts';
 
@@ -34,62 +33,44 @@ export const WorkspaceConnectAction: React.FunctionComponent<WorkspaceConnectAct
     }
   };
 
-  const onClickConnect = () => {
-    if (workspace.services.length === 0 || !workspace.services[0].httpService) {
-      return;
-    }
-
-    openEndpoint(workspace.services[0].httpService.httpPath);
-  };
-
   const openEndpoint = (value: string) => {
     window.open(value, '_blank');
   };
 
   return (
-    <div className="workspace-connect-wrapper pf-v6-u-display-inline-block">
-      <Dropdown
-        isOpen={open}
-        onSelect={onSelect}
-        onOpenChange={(isOpen: boolean) => setIsOpen(isOpen)}
-        toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
-          <MenuToggle
-            ref={toggleRef}
-            variant="secondary"
-            onClick={onToggleClick}
-            isExpanded={open}
-            isDisabled={workspace.state !== WorkspacesWorkspaceState.WorkspaceStateRunning}
-            splitButtonItems={[
-              <MenuToggleAction
-                id="connect-endpoint-button"
-                key="connect-endpoint-button"
-                onClick={onClickConnect}
-                className="connect-button-no-wrap"
-              >
-                Connect
-              </MenuToggleAction>,
-            ]}
-          />
-        )}
-        ouiaId="BasicDropdown"
-        shouldFocusToggleOnSelect
-      >
-        <DropdownList>
-          {workspace.services.map((service) => {
-            if (!service.httpService) {
-              return null;
-            }
-            return (
-              <DropdownItem
-                value={service.httpService.httpPath}
-                key={`${workspace.name}-${service.httpService.displayName}`}
-              >
-                {service.httpService.displayName}
-              </DropdownItem>
-            );
-          })}
-        </DropdownList>
-      </Dropdown>
-    </div>
+    <Dropdown
+      isOpen={open}
+      onSelect={onSelect}
+      onOpenChange={(isOpen: boolean) => setIsOpen(isOpen)}
+      toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
+        <MenuToggle
+          ref={toggleRef}
+          variant="secondary"
+          onClick={onToggleClick}
+          isExpanded={open}
+          isDisabled={workspace.state !== WorkspacesWorkspaceState.WorkspaceStateRunning}
+        >
+          Connect
+        </MenuToggle>
+      )}
+      ouiaId="BasicDropdown"
+      shouldFocusToggleOnSelect
+    >
+      <DropdownList>
+        {workspace.services.map((service) => {
+          if (!service.httpService) {
+            return null;
+          }
+          return (
+            <DropdownItem
+              value={service.httpService.httpPath}
+              key={`${workspace.name}-${service.httpService.displayName}`}
+            >
+              {service.httpService.displayName}
+            </DropdownItem>
+          );
+        })}
+      </DropdownList>
+    </Dropdown>
   );
 };
