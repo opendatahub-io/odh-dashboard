178078429ef95d7a03c3c2393b0d1e62e32787e1
paulovmr <832830+paulovmr@users.noreply.github.com>
2025-12-03 12:14:51 -0300

    Update @odh-dashboard/notebooks: feat(ws): Notebooks 2.0 // Frontend // Workspaces table // Workspace redirect status column #147 (#191)
    Upstream commit: f22890d3078aeff71ffa47b473c7a1d6b9679cb2

--- packages/notebooks/upstream/workspaces/frontend/src/app/actions/WorkspaceKindsActions.tsx
+++ packages/notebooks/upstream/workspaces/frontend/src/app/actions/WorkspaceKindsActions.tsx
@@ -19,3 +19,34 @@ export function buildKindLogoDictionary(workspaceKinds: WorkspaceKind[] | []): K
   }
   return kindLogoDict;
 }
+
+type WorkspaceRedirectStatus = Record<
+  string,
+  { to: string; message: string; level: string } | null
+>;
+
+/**
+ * Builds a dictionary of workspace kinds to redirect statuses.
+ * @param {WorkspaceKind[]} workspaceKinds - The list of workspace kinds.
+ * @returns {WorkspaceRedirectStatus} A dictionary with kind names as keys and redirect status objects as values.
+ */
+export function buildWorkspaceRedirectStatus(
+  workspaceKinds: WorkspaceKind[] | [],
+): WorkspaceRedirectStatus {
+  const workspaceRedirectStatus: WorkspaceRedirectStatus = {};
+  for (const workspaceKind of workspaceKinds) {
+    // Loop through the `values` array inside `imageConfig`
+    const redirect = workspaceKind.podTemplate.options.imageConfig.values.find(
+      (value) => value.redirect,
+    )?.redirect;
+    // If redirect exists, extract the necessary properties
+    workspaceRedirectStatus[workspaceKind.name] = redirect
+      ? {
+          to: redirect.to,
+          message: redirect.message.text,
+          level: redirect.message.level,
+        }
+      : null;
+  }
+  return workspaceRedirectStatus;
+}
