175513799b16778af8fbde782922e160789190ff
Anish Surti <asurti@redhat.com>
2025-12-08 09:12:29 -0500

    removed the wb table header

--- frontend/src/pages/projects/screens/projects/tableData.tsx
+++ frontend/src/pages/projects/screens/projects/tableData.tsx
@@ -1,22 +1,7 @@
-import { OffIcon, PlayIcon } from '@patternfly/react-icons';
-import * as React from 'react';
 import { SortableData } from '#~/components/table';
 import { ProjectKind } from '#~/k8sTypes';
 import { getProjectCreationTime } from '#~/concepts/projects/utils';
 import { getDisplayNameFromK8sResource } from '#~/concepts/k8s/utils';
-
-const WorkBenchDescription = (
-  <div>
-    <div>
-      <PlayIcon className="pf-v6-u-mr-xs" />
-      The total number of running or starting workbenches in the project.
-    </div>
-    <div>
-      <OffIcon className="pf-v6-u-mr-xs" />
-      The total number of stopped workbenches in the project.
-    </div>
-  </div>
-);
 
 export const columns: SortableData<ProjectKind>[] = [
   {
@@ -31,16 +16,6 @@ export const columns: SortableData<ProjectKind>[] = [
     label: 'Created',
     sortable: (a, b) => getProjectCreationTime(a) - getProjectCreationTime(b),
     width: 30,
-  },
-  {
-    field: 'Workbenches',
-    label: 'Workbenches',
-    sortable: false,
-    width: 30,
-    info: {
-      popoverProps: { hasAutoWidth: true },
-      popover: WorkBenchDescription,
-    },
   },
   {
     field: 'kebab',
