83d210768a2c6f12de01b7fad7ea25e6db4c6b99
Anish Surti <asurti@redhat.com>
2025-12-08 09:29:48 -0500

    removed wb code

--- frontend/src/pages/projects/screens/projects/ProjectTableRow.tsx
+++ frontend/src/pages/projects/screens/projects/ProjectTableRow.tsx
@@ -1,33 +1,15 @@
 import * as React from 'react';
-import {
-  Spinner,
-  Content,
-  ContentVariants,
-  Timestamp,
-  Flex,
-  FlexItem,
-} from '@patternfly/react-core';
-import { ActionsColumn, Tbody, Td, Tr, ExpandableRowContent } from '@patternfly/react-table';
-import { OffIcon, PlayIcon } from '@patternfly/react-icons';
+import { Content, ContentVariants, Timestamp, Flex, FlexItem } from '@patternfly/react-core';
+import { ActionsColumn, Tbody, Td, Tr } from '@patternfly/react-table';
 import { ProjectKind } from '#~/k8sTypes';
 import useProjectTableRowItems from '#~/pages/projects/screens/projects/useProjectTableRowItems';
 import { getProjectOwner, isAiProject } from '#~/concepts/projects/utils';
-import ProjectTableRowNotebookTable from '#~/pages/projects/screens/projects/ProjectTableRowNotebookTable';
 import { TableRowTitleDescription } from '#~/components/table';
 import ResourceNameTooltip from '#~/components/ResourceNameTooltip';
 import { getDescriptionFromK8sResource } from '#~/concepts/k8s/utils';
-import useProjectNotebookStates from '#~/pages/projects/notebook/useProjectNotebookStates';
-import { FAST_POLL_INTERVAL, POLL_INTERVAL } from '#~/utilities/const';
-import useRefreshInterval from '#~/utilities/useRefreshInterval';
-import { SupportedArea, useIsAreaAvailable } from '#~/concepts/areas';
 import { allProjectFilterKey } from '#~/pages/projects/screens/projects/const';
 import ProjectLink from './ProjectLink';
 import { AILabel } from './AILabel';
-
-// Plans to add other expandable columns in the future
-export enum ExpandableColumns {
-  WORKBENCHES = 1,
-}
 
 type ProjectTableRowProps = {
   obj: ProjectKind;
@@ -44,44 +26,18 @@ const ProjectTableRow: React.FC<ProjectTableRowProps> = ({
   currentProjectFilterType,
 }) => {
   const owner = getProjectOwner(project);
-  const [expandColumn, setExpandColumn] = React.useState<ExpandableColumns | undefined>();
   const [item, runAccessCheck] = useProjectTableRowItems(
     project,
     isRefreshing,
     setEditData,
     setDeleteData,
   );
-  const { data: notebookStates, loaded, refresh } = useProjectNotebookStates(project.metadata.name);
-  const runningCount = notebookStates.filter(
-    (notebookState) => notebookState.isRunning || notebookState.isStarting,
-  ).length;
-  const stoppedCount = notebookStates.filter(
-    (notebookState) => notebookState.isStopping || notebookState.isStopped,
-  ).length;
-
-  const toggleExpandColumn = (colIndex: ExpandableColumns) => {
-    setExpandColumn(expandColumn === colIndex ? undefined : colIndex);
-  };
-
-  useRefreshInterval(FAST_POLL_INTERVAL, () =>
-    notebookStates
-      .filter((notebookState) => notebookState.isStarting || notebookState.isStopping)
-      .forEach((notebookState) => notebookState.refresh()),
-  );
-
-  useRefreshInterval(POLL_INTERVAL, () =>
-    notebookStates
-      .filter((notebookState) => !notebookState.isStarting && !notebookState.isStopping)
-      .forEach((notebookState) => notebookState.refresh()),
-  );
-
-  const workbenchEnabled = useIsAreaAvailable(SupportedArea.WORKBENCHES).status;
 
   const thisIsAiProject = isAiProject(project);
   const shouldShowAiLabel = currentProjectFilterType === allProjectFilterKey && thisIsAiProject;
 
   return (
-    <Tbody isExpanded={!!expandColumn}>
+    <Tbody>
       <Tr isControlRow>
         <Td dataLabel="Name">
           <TableRowTitleDescription
@@ -117,34 +73,6 @@ const ProjectTableRow: React.FC<ProjectTableRowProps> = ({
             'Unknown'
           )}
         </Td>
-        {workbenchEnabled && (
-          <Td
-            dataLabel="Workbenches"
-            compoundExpand={
-              notebookStates.length
-                ? {
-                    isExpanded: expandColumn === ExpandableColumns.WORKBENCHES,
-                    columnIndex: ExpandableColumns.WORKBENCHES,
-                    rowIndex: 1,
-                    expandId: `expand-table-row`,
-                    onToggle: (_, __, column) => toggleExpandColumn(column),
-                  }
-                : undefined
-            }
-            data-testid="notebook-column-expand"
-          >
-            {!loaded ? (
-              <Spinner size="sm" />
-            ) : (
-              <div data-testid="notebook-column-count">
-                <PlayIcon className="pf-v6-u-mr-xs" />
-                {runningCount}
-                <OffIcon className="pf-v6-u-ml-sm pf-v6-u-mr-xs" />
-                {stoppedCount}
-              </div>
-            )}
-          </Td>
-        )}
         <Td
           className="odh-project-table__action-column"
           isActionCell
@@ -154,26 +82,6 @@ const ProjectTableRow: React.FC<ProjectTableRowProps> = ({
           <ActionsColumn items={item} />
         </Td>
       </Tr>
-      {expandColumn ? (
-        <Tr isExpanded={!!expandColumn}>
-          <Td
-            colSpan={4}
-            style={{
-              borderTopWidth: 1,
-              borderTopStyle: 'solid',
-              borderTopColor: 'var(--pf-t--global--border--color--default)',
-            }}
-          >
-            <ExpandableRowContent>
-              <ProjectTableRowNotebookTable
-                obj={project}
-                notebookStates={notebookStates}
-                refresh={refresh}
-              />
-            </ExpandableRowContent>
-          </Td>
-        </Tr>
-      ) : null}
     </Tbody>
   );
 };
