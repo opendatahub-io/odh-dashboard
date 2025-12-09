d14866aabf2689e36066ad157c96ccf779b5ceba
Anish Surti <21984673+srtanish1992@users.noreply.github.com>
2025-12-09 16:11:43 -0500

    RHOAIENG-41318: Remove the workbenches column on the global project list (#5702)
    * removed the wb table header
    
    * removed wb code
    
    * deleted notebook files
    
    * removed leftover nb data file
    
    * updated tests
    
    * removed unnecessary prop and tbody
    
    * adjusted widths

--- frontend/src/pages/projects/screens/projects/ProjectTableRow.tsx
+++ frontend/src/pages/projects/screens/projects/ProjectTableRow.tsx
@@ -1,33 +1,15 @@
 import * as React from 'react';
-import {
-  Spinner,
-  Content,
-  ContentVariants,
-  Timestamp,
-  Flex,
-  FlexItem,
-} from '@patternfly/react-core';
-import { ActionsColumn, Tbody, Td, Tr, ExpandableRowContent } from '@patternfly/react-table';
-import { OffIcon, PlayIcon } from '@patternfly/react-icons';
+import { Content, ContentVariants, Timestamp, Flex, FlexItem } from '@patternfly/react-core';
+import { ActionsColumn, Td, Tr } from '@patternfly/react-table';
 import { ProjectKind } from '#~/k8sTypes';
 import useProjectTableRowItems from '#~/pages/projects/screens/projects/useProjectTableRowItems';
 import { getProjectOwner, isAiProject } from '#~/concepts/projects/utils';
-import ProjectTableRowNotebookTable from '#~/pages/projects/screens/projects/ProjectTableRowNotebookTable';
 import { TableRowTitleDescription } from '#~/components/table';
 import ResourceNameTooltip from '#~/components/ResourceNameTooltip';
 import { getDescriptionFromK8sResource } from '#~/concepts/k8s/utils';
-import useProjectNotebookStates from '#~/pages/projects/notebook/useProjectNotebookStates';
-import { FAST_POLL_INTERVAL, POLL_INTERVAL } from '#~/utilities/const';
-import useRefreshInterval from '#~/utilities/useRefreshInterval';
-import { SupportedArea, useIsAreaAvailable } from '#~/concepts/areas';
 import { allProjectFilterKey } from '#~/pages/projects/screens/projects/const';
 import ProjectLink from './ProjectLink';
 import { AILabel } from './AILabel';
-
-// Plans to add other expandable columns in the future
-export enum ExpandableColumns {
-  WORKBENCHES = 1,
-}
 
 type ProjectTableRowProps = {
   obj: ProjectKind;
@@ -44,137 +26,61 @@ const ProjectTableRow: React.FC<ProjectTableRowProps> = ({
   currentProjectFilterType,
 }) => {
   const owner = getProjectOwner(project);
-  const [expandColumn, setExpandColumn] = React.useState<ExpandableColumns | undefined>();
   const [item, runAccessCheck] = useProjectTableRowItems(
     project,
     isRefreshing,
     setEditData,
     setDeleteData,
   );
-  const { data: notebookStates, loaded, refresh } = useProjectNotebookStates(project.metadata.name);
-  const runningCount = notebookStates.filter(
-    (notebookState) => notebookState.isRunning || notebookState.isStarting,
-  ).length;
-  const stoppedCount = notebookStates.filter(
-    (notebookState) => notebookState.isStopping || notebookState.isStopped,
-  ).length;
-
-  const toggleExpandColumn = (colIndex: ExpandableColumns) => {
-    setExpandColumn(expandColumn === colIndex ? undefined : colIndex);
-  };
-
-  useRefreshInterval(FAST_POLL_INTERVAL, () =>
-    notebookStates
-      .filter((notebookState) => notebookState.isStarting || notebookState.isStopping)
-      .forEach((notebookState) => notebookState.refresh()),
-  );
-
-  useRefreshInterval(POLL_INTERVAL, () =>
-    notebookStates
-      .filter((notebookState) => !notebookState.isStarting && !notebookState.isStopping)
-      .forEach((notebookState) => notebookState.refresh()),
-  );
-
-  const workbenchEnabled = useIsAreaAvailable(SupportedArea.WORKBENCHES).status;
 
   const thisIsAiProject = isAiProject(project);
   const shouldShowAiLabel = currentProjectFilterType === allProjectFilterKey && thisIsAiProject;
 
   return (
-    <Tbody isExpanded={!!expandColumn}>
-      <Tr isControlRow>
-        <Td dataLabel="Name">
-          <TableRowTitleDescription
-            title={
-              <ResourceNameTooltip resource={project}>
-                <Flex spaceItems={{ default: 'spaceItemsSm' }}>
+    <Tr>
+      <Td dataLabel="Name">
+        <TableRowTitleDescription
+          title={
+            <ResourceNameTooltip resource={project}>
+              <Flex spaceItems={{ default: 'spaceItemsSm' }}>
+                <FlexItem>
+                  <ProjectLink project={project} />
+                </FlexItem>
+                {shouldShowAiLabel && (
                   <FlexItem>
-                    <ProjectLink project={project} />
+                    <AILabel />
                   </FlexItem>
-                  {shouldShowAiLabel && (
-                    <FlexItem>
-                      <AILabel />
-                    </FlexItem>
-                  )}
-                </Flex>
-              </ResourceNameTooltip>
-            }
-            description={getDescriptionFromK8sResource(project)}
-            truncateDescriptionLines={2}
-            subtitle={
-              owner ? (
-                <div>
-                  <Content component={ContentVariants.small}>{owner}</Content>
-                </div>
-              ) : undefined
-            }
-          />
-        </Td>
-        <Td dataLabel="Created">
-          {project.metadata.creationTimestamp ? (
-            <Timestamp date={new Date(project.metadata.creationTimestamp)} />
-          ) : (
-            'Unknown'
-          )}
-        </Td>
-        {workbenchEnabled && (
-          <Td
-            dataLabel="Workbenches"
-            compoundExpand={
-              notebookStates.length
-                ? {
-                    isExpanded: expandColumn === ExpandableColumns.WORKBENCHES,
-                    columnIndex: ExpandableColumns.WORKBENCHES,
-                    rowIndex: 1,
-                    expandId: `expand-table-row`,
-                    onToggle: (_, __, column) => toggleExpandColumn(column),
-                  }
-                : undefined
-            }
-            data-testid="notebook-column-expand"
-          >
-            {!loaded ? (
-              <Spinner size="sm" />
-            ) : (
-              <div data-testid="notebook-column-count">
-                <PlayIcon className="pf-v6-u-mr-xs" />
-                {runningCount}
-                <OffIcon className="pf-v6-u-ml-sm pf-v6-u-mr-xs" />
-                {stoppedCount}
+                )}
+              </Flex>
+            </ResourceNameTooltip>
+          }
+          description={getDescriptionFromK8sResource(project)}
+          truncateDescriptionLines={2}
+          subtitle={
+            owner ? (
+              <div>
+                <Content component={ContentVariants.small}>{owner}</Content>
               </div>
-            )}
-          </Td>
+            ) : undefined
+          }
+        />
+      </Td>
+      <Td dataLabel="Created">
+        {project.metadata.creationTimestamp ? (
+          <Timestamp date={new Date(project.metadata.creationTimestamp)} />
+        ) : (
+          'Unknown'
         )}
-        <Td
-          className="odh-project-table__action-column"
-          isActionCell
-          onMouseEnter={runAccessCheck}
-          onClick={runAccessCheck}
-        >
-          <ActionsColumn items={item} />
-        </Td>
-      </Tr>
-      {expandColumn ? (
-        <Tr isExpanded={!!expandColumn}>
-          <Td
-            colSpan={4}
-            style={{
-              borderTopWidth: 1,
-              borderTopStyle: 'solid',
-              borderTopColor: 'var(--pf-t--global--border--color--default)',
-            }}
-          >
-            <ExpandableRowContent>
-              <ProjectTableRowNotebookTable
-                obj={project}
-                notebookStates={notebookStates}
-                refresh={refresh}
-              />
-            </ExpandableRowContent>
-          </Td>
-        </Tr>
-      ) : null}
-    </Tbody>
+      </Td>
+      <Td
+        className="odh-project-table__action-column"
+        isActionCell
+        onMouseEnter={runAccessCheck}
+        onClick={runAccessCheck}
+      >
+        <ActionsColumn items={item} />
+      </Td>
+    </Tr>
   );
 };
 
