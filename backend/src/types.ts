dab8ccebf7cc9b4a10b0bf0f838c1b9e3375cebb
rsun19 <robertssun1234@gmail.com>
2025-12-12 10:06:05 -0500

    added mlflow to crd

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -51,6 +51,7 @@ export type DashboardConfig = K8sResourceCommon & {
       disableFeatureStore: boolean;
       genAiStudio: boolean;
       modelAsService: boolean;
+      mlflow: boolean;
     };
     // Intentionally disjointed from the CRD, we should move away from this code-wise now; CRD later
     // groupsConfig?: {
