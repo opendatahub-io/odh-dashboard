ae8f1970d08d6df4656d2e362ffb1e972e9ec9b9
Griffin Sullivan <48397354+Griffin-Sullivan@users.noreply.github.com>
2025-12-18 09:10:12 -0500

    Add Delete Tier modal (#5780)

--- packages/maas/frontend/src/app/pages/tiers/AllTiersPage.tsx
+++ packages/maas/frontend/src/app/pages/tiers/AllTiersPage.tsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 import { PageSection } from '@patternfly/react-core';
 import ApplicationsPage from '@odh-dashboard/internal/pages/ApplicationsPage';
 import { useTiers } from '~/app/api/tiers';
+import { Tier } from '~/app/types/tier';
 import TiersTable from './allTiers/TiersTable';
+import DeleteTierModal from './components/DeleteTierModal';
 
 const AllTiersPage: React.FC = () => {
   const tiers = useTiers();
+  const [deleteTier, setDeleteTier] = React.useState<Tier | undefined>(undefined);
+
   return (
     <ApplicationsPage
       title="Tiers"
@@ -14,8 +18,16 @@ const AllTiersPage: React.FC = () => {
       empty={false}
     >
       <PageSection isFilled>
-        <TiersTable tiers={tiers} />
+        <TiersTable tiers={tiers} onDeleteTier={(tier) => setDeleteTier(tier)} />
       </PageSection>
+      {deleteTier && (
+        <DeleteTierModal
+          tier={deleteTier}
+          onClose={() => {
+            setDeleteTier(undefined);
+          }}
+        />
+      )}
     </ApplicationsPage>
   );
 };
