9c0b7dd3b255022ec5fa4d665d8dd9da107e2ff5
rsun19 <robertssun1234@gmail.com>
2025-12-23 14:47:02 -0500

    added experiment page override utils

--- frontend/src/pages/pipelines/global/mlflowExperiments/utils.ts
+++ frontend/src/pages/pipelines/global/mlflowExperiments/utils.ts
@@ -1,4 +1,6 @@
 export const removeQuery = '.du-bois-light-breadcrumb, .du-bois-dark-breadcrumb, header, aside';
+export const experimentsPagePaddingQuery = 'main > div';
+export const headingQuery = 'h2';
 export const mainElementQuery = 'main';
 
 export const isElementNode = (node: Node): node is Element => node.nodeType === Node.ELEMENT_NODE;
@@ -15,6 +17,25 @@ export const applyHiddenStylesToElement = (element: Element): void => {
 export const applyOverrideStylesToMainElement = (mainElement: HTMLElement): void => {
   mainElement.style.setProperty('margin', '0', 'important');
   mainElement.style.setProperty('border-radius', '0', 'important');
+};
+
+export const applyOverrideStylesInExperimentsPage = (
+  experimentsPagePaddingElement: Element | null,
+  headingElement: Element | null,
+): void => {
+  if (experimentsPagePaddingElement && isHTMLElement(experimentsPagePaddingElement)) {
+    experimentsPagePaddingElement.style.setProperty('padding-left', '0', 'important');
+    experimentsPagePaddingElement.style.setProperty('padding-right', '0', 'important');
+  }
+  if (headingElement && isHTMLElement(headingElement)) {
+    headingElement.style.setProperty('display', 'none', 'important');
+  }
+};
+
+export const overrideElementsInExperimentsPage = (doc: Document | Element): void => {
+  const experimentsPagePaddingElement = doc.querySelector(experimentsPagePaddingQuery);
+  const headingElement = doc.querySelector(headingQuery);
+  applyOverrideStylesInExperimentsPage(experimentsPagePaddingElement, headingElement);
 };
 
 export const hideElements = (doc: Document | Element): void => {
