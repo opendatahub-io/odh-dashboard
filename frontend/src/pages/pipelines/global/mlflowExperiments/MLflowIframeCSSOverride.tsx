0c9bd9aa8989d9cdebb5a8ca10a8164f93674ff2
Robert Sun <robertssun1234@gmail.com>
2025-12-23 13:31:58 -0500

    fix crashes in mlflow embedded page (#5816)
    * fixed crashes in embedded page
    
    * added listener for modified elements
    
    * modified code to account for attribute changes
    
    * added additional checking in mutation observer

--- frontend/src/pages/pipelines/global/mlflowExperiments/MLflowIframeCSSOverride.tsx
+++ frontend/src/pages/pipelines/global/mlflowExperiments/MLflowIframeCSSOverride.tsx
@@ -9,21 +9,36 @@ const MLflowIframeCSSOverride: React.FC<MLflowIframeCSSOverrideProps> = ({ child
 
   const isElementNode = (node: Node): node is Element => node.nodeType === Node.ELEMENT_NODE;
 
+  const isHTMLElement = (element: Element): element is HTMLElement =>
+    'style' in element && typeof element.style === 'object';
+
   const removeQuery = '.du-bois-light-breadcrumb, .du-bois-dark-breadcrumb, header, aside';
 
   useEffect(() => {
     let observer: MutationObserver | null = null;
 
-    const removeElements = (doc: Document | Element) => {
-      const elementsToRemove = doc.querySelectorAll(removeQuery);
-      elementsToRemove.forEach((element) => element.remove());
+    const applyHiddenStylesToElement = (element: Element) => {
+      if (isHTMLElement(element)) {
+        element.style.setProperty('display', 'none', 'important');
+      }
+    };
+
+    const hideElements = (doc: Document | Element) => {
+      const elementsToHide = doc.querySelectorAll(removeQuery);
+      elementsToHide.forEach((element) => {
+        applyHiddenStylesToElement(element);
+      });
+    };
+
+    const applyOverrideStylesToMainElement = (mainElement: HTMLElement) => {
+      mainElement.style.setProperty('margin', '0', 'important');
+      mainElement.style.setProperty('border-radius', '0', 'important');
     };
 
     const overrideMainElementStyles = (doc: Document | Element) => {
       const mainElement = doc.querySelector('main');
       if (mainElement) {
-        mainElement.style.setProperty('margin', '0', 'important');
-        mainElement.style.setProperty('border-radius', '0', 'important');
+        applyOverrideStylesToMainElement(mainElement);
       }
     };
 
@@ -33,18 +48,21 @@ const MLflowIframeCSSOverride: React.FC<MLflowIframeCSSOverrideProps> = ({ child
         if (iframe?.contentDocument) {
           const doc = iframe.contentDocument;
 
-          removeElements(doc);
+          hideElements(doc);
           overrideMainElementStyles(doc);
 
           observer = new MutationObserver((mutations) => {
             mutations.forEach((mutation) => {
               mutation.addedNodes.forEach((node) => {
                 if (isElementNode(node)) {
+                  // Check if the node itself matches our selectors
                   if (node.matches(removeQuery)) {
-                    node.remove();
-                  } else {
-                    removeElements(node);
+                    applyHiddenStylesToElement(node);
+                  } else if (node.matches('main') && isHTMLElement(node)) {
+                    applyOverrideStylesToMainElement(node);
                   }
+                  // Also check children within the node
+                  hideElements(node);
                   overrideMainElementStyles(node);
                 }
               });
