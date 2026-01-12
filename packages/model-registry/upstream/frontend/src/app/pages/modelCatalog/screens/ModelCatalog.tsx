6080036ef7493372ce012592f9046b44358e75f4
Mike Turley <mike.turley@alum.cs.umass.edu>
2026-01-12 14:19:40 -0500

    Update @odh-dashboard/model-registry: Performance filters for catalog landing page (#2075) (resolved conflicts)
    Upstream commit: f55b1d022bbb27784fc4f4d37c33bbe99bdf9d1f

--- packages/model-registry/upstream/frontend/src/app/pages/modelCatalog/screens/ModelCatalog.tsx
+++ packages/model-registry/upstream/frontend/src/app/pages/modelCatalog/screens/ModelCatalog.tsx
@@ -7,10 +7,6 @@ import ScrollViewOnMount from '~/app/shared/components/ScrollViewOnMount';
 import ModelCatalogFilters from '~/app/pages/modelCatalog/components/ModelCatalogFilters';
 import { ModelCatalogContext } from '~/app/context/modelCatalog/ModelCatalogContext';
 import { hasFiltersApplied } from '~/app/pages/modelCatalog/utils/modelCatalogUtils';
-import {
-  ModelCatalogNumberFilterKey,
-  ModelCatalogStringFilterKey,
-} from '~/concepts/modelCatalog/const';
 import { CategoryName } from '~/app/modelCatalogTypes';
 import ModelCatalogSourceLabelSelectorNavigator from './ModelCatalogSourceLabelSelectorNavigator';
 import ModelCatalogAllModelsView from './ModelCatalogAllModelsView';
@@ -19,7 +15,14 @@ import { useSearchParams } from 'react-router-dom';
 
 const ModelCatalog: React.FC = () => {
   const [searchTerm, setSearchTerm] = React.useState('');
-  const { selectedSourceLabel, filterData, setFilterData, updateSelectedSourceLabel } = React.useContext(ModelCatalogContext);
+  const {
+    selectedSourceLabel,
+    filterData,
+    updateSelectedSourceLabel,
+    clearAllFilters,
+    performanceViewEnabled,
+    clearBasicFiltersAndResetPerformanceToDefaults,
+  } = React.useContext(ModelCatalogContext);
   const filtersApplied = hasFiltersApplied(filterData);
   const isAllModelsView =
     selectedSourceLabel === CategoryName.allModels && !searchTerm && !filtersApplied;
@@ -43,20 +46,16 @@ const ModelCatalog: React.FC = () => {
     setSearchTerm('');
   }, []);
 
-  const resetAllFilters = React.useCallback(() => {
-    Object.values(ModelCatalogStringFilterKey).forEach((filterKey) => {
-      setFilterData(filterKey, []);
-    });
-
-    Object.values(ModelCatalogNumberFilterKey).forEach((filterKey) => {
-      setFilterData(filterKey, undefined);
-    });
-  }, [setFilterData]);
-
   const handleFilterReset = React.useCallback(() => {
     setSearchTerm('');
-    resetAllFilters();
-  }, [resetAllFilters]);
+    // When performance view is enabled, clear basic filters but reset
+    // performance filters to defaults instead of clearing them completely
+    if (performanceViewEnabled) {
+      clearBasicFiltersAndResetPerformanceToDefaults();
+    } else {
+      clearAllFilters();
+    }
+  }, [clearAllFilters, performanceViewEnabled, clearBasicFiltersAndResetPerformanceToDefaults]);
 
   return (
     <>
