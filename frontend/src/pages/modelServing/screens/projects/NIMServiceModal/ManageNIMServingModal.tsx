1a1be83a1a9675358645b6b85a0bd2822df2c110
mtalvi <mtalvi@redhat.com>
2026-01-13 12:24:05 +0200

    replica helper text

--- frontend/src/pages/modelServing/screens/projects/NIMServiceModal/ManageNIMServingModal.tsx
+++ frontend/src/pages/modelServing/screens/projects/NIMServiceModal/ManageNIMServingModal.tsx
@@ -454,8 +454,10 @@ const ManageNIMServingModal: React.FC<ManageNIMServingModalProps> = ({
               <KServeAutoscalerReplicaSection
                 data={createDataInferenceService}
                 setData={setCreateDataInferenceService}
-                infoContent="Consider network traffic and failover scenarios when specifying the number of model
-                server replicas."
+                infoContent="A replica is an independent instance of your model server.
+                Multiple replicas improve availability and handle higher traffic loads. 
+                Consider network traffic and failover scenarios when specifying the number of model server replicas.
+                More replicas enhance fault tolerance but use additional resources."
               />
             </StackItem>
             <DeploymentHardwareProfileSection
