51f606cd2646a3fd64f2dc64342ff75e3b4283fd
paulovmr <832830+paulovmr@users.noreply.github.com>
2026-01-14 10:24:19 -0300

    Update @odh-dashboard/notebooks: fix: fix port configuration form styling in Add Image modal (#809)
    Upstream commit: c4b18d55977a17120ac32bef21b79ecb11a11324

--- packages/notebooks/upstream/workspaces/frontend/src/app/pages/WorkspaceKinds/Form/image/WorkspaceKindFormImagePort.tsx
+++ packages/notebooks/upstream/workspaces/frontend/src/app/pages/WorkspaceKinds/Form/image/WorkspaceKindFormImagePort.tsx
@@ -3,7 +3,6 @@ import {
   FormFieldGroupExpandable,
   FormFieldGroupHeader,
 } from '@patternfly/react-core/dist/esm/components/Form';
-import { Grid, GridItem } from '@patternfly/react-core/dist/esm/layouts/Grid';
 import { TextInput } from '@patternfly/react-core/dist/esm/components/TextInput';
 import { WorkspaceKindImagePort } from '~/app/types';
 import ThemeAwareFormGroupWrapper from '~/shared/components/ThemeAwareFormGroupWrapper';
@@ -37,55 +36,45 @@ export const WorkspaceKindFormImagePort: React.FC<WorkspaceKindFormImagePortProp
         />
       }
     >
-      <Grid hasGutter md={6}>
-        <GridItem span={3}>
-          <ThemeAwareFormGroupWrapper label="ID" isRequired fieldId="port-id">
-            <TextInput
-              name="id"
-              isRequired
-              type="text"
-              value={currentPort.id}
-              onChange={(_, val) => setPorts([{ ...currentPort, id: val }])}
-              id="port-id"
-            />
-          </ThemeAwareFormGroupWrapper>
-        </GridItem>
-        <GridItem span={3}>
-          <ThemeAwareFormGroupWrapper label="Display Name" isRequired fieldId="port-display-name">
-            <TextInput
-              name="displayName"
-              isRequired
-              type="text"
-              value={currentPort.displayName}
-              onChange={(_, val) => setPorts([{ ...currentPort, displayName: val }])}
-              id="port-display-name"
-            />
-          </ThemeAwareFormGroupWrapper>
-        </GridItem>
-        <GridItem span={3}>
-          <ThemeAwareFormGroupWrapper label="Port" isRequired fieldId="port-number">
-            <TextInput
-              name="port"
-              isRequired
-              type="number"
-              value={currentPort.port}
-              onChange={(_, val) => setPorts([{ ...currentPort, port: Number(val) }])}
-              id="port-number"
-            />
-          </ThemeAwareFormGroupWrapper>
-        </GridItem>
-        <GridItem span={3}>
-          <ThemeAwareFormGroupWrapper label="Protocol" isRequired fieldId="port-protocol">
-            <TextInput
-              name="displayName"
-              isDisabled
-              type="text"
-              value={currentPort.protocol}
-              id="port-protocol"
-            />
-          </ThemeAwareFormGroupWrapper>
-        </GridItem>
-      </Grid>
+      <ThemeAwareFormGroupWrapper label="ID" isRequired fieldId="port-id">
+        <TextInput
+          name="id"
+          isRequired
+          type="text"
+          value={currentPort.id}
+          onChange={(_, val) => setPorts([{ ...currentPort, id: val }])}
+          id="port-id"
+        />
+      </ThemeAwareFormGroupWrapper>
+      <ThemeAwareFormGroupWrapper label="Display Name" isRequired fieldId="port-display-name">
+        <TextInput
+          name="displayName"
+          isRequired
+          type="text"
+          value={currentPort.displayName}
+          onChange={(_, val) => setPorts([{ ...currentPort, displayName: val }])}
+          id="port-display-name"
+        />
+      </ThemeAwareFormGroupWrapper>
+      <ThemeAwareFormGroupWrapper label="Port" isRequired fieldId="port-number">
+        <TextInput
+          name="port"
+          isRequired
+          type="number"
+          value={currentPort.port}
+          onChange={(_, val) => setPorts([{ ...currentPort, port: Number(val) }])}
+          id="port-number"
+        />
+      </ThemeAwareFormGroupWrapper>
+      <ThemeAwareFormGroupWrapper label="Protocol" isRequired fieldId="port-protocol">
+        <TextInput
+          name="protocol"
+          isDisabled
+          type="text"
+          value={currentPort.protocol}
+          id="port-protocol"
+        />
+      </ThemeAwareFormGroupWrapper>
     </FormFieldGroupExpandable>
   );
 };
