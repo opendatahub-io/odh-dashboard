dbe204eb5df0628b41a6c35051617133a6a28dcc
Juntao Wang <37624318+DaoDaoNoCode@users.noreply.github.com>
2026-01-23 16:46:36 -0500

    Permission Assignment - Add assignment button, routes and page and remove the inline adding (#6076)
    * Permission Assignment - Add assignment button, routes and page and remove the inline adding
    
    * Address comments from code rabbit

--- frontend/src/pages/projects/ProjectViewRoutes.tsx
+++ frontend/src/pages/projects/ProjectViewRoutes.tsx
@@ -12,6 +12,7 @@ import ProjectView from './screens/projects/ProjectView';
 import ProjectDetailsContextProvider from './ProjectDetailsContext';
 import SpawnerPage from './screens/spawner/SpawnerPage';
 import EditSpawnerPage from './screens/spawner/EditSpawnerPage';
+import ProjectPermissionsAssignRoles from './projectPermissions/ProjectPermissionsAssignRoles';
 
 const ProjectViewRoutes: React.FC = () => {
   const [modelMetricsEnabled] = useModelMetricsEnabled();
@@ -24,6 +25,7 @@ const ProjectViewRoutes: React.FC = () => {
         <Route index element={<ProjectDetails />} />
         <Route path="spawner" element={<SpawnerPage />} />
         <Route path="spawner/:notebookName" element={<EditSpawnerPage />} />
+        <Route path="permissions/assign" element={<ProjectPermissionsAssignRoles />} />
         {modelMetricsEnabled && (
           <>
             <Route path="metrics/model" element={<ProjectInferenceExplainabilityWrapper />}>
