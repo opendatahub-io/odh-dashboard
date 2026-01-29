4a4ced604fec5085ca8ec5443a9869bbf2dee0fc
Tobias Klumpp <tklumpp@redhat.com>
2026-01-29 17:15:24 +0000

    Remove shadow around guardrails alert

--- packages/gen-ai/frontend/src/app/Chatbot/components/chatbotConfiguration/GuardrailsNotConfiguredAlert.tsx
+++ packages/gen-ai/frontend/src/app/Chatbot/components/chatbotConfiguration/GuardrailsNotConfiguredAlert.tsx
@@ -2,7 +2,7 @@ import { Alert } from '@patternfly/react-core';
 import React from 'react';
 
 export const GuardrailsNotConfiguredAlert: () => React.JSX.Element = () => (
-  <Alert variant="warning" title="Guardrails unavailable">
+  <Alert variant="warning" title="Guardrails unavailable" isInline>
     Guardrails are not configured for this cluster. You can continue with the playground
     configuration, but guardrails will be disabled. Contact a cluster administrator to add
     guardrails.
