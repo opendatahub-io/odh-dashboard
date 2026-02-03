cd3ee36541d4405e3d7088b7962e8be6470fa170
rsun19 <robertssun1234@gmail.com>
2026-02-03 12:07:57 -0500

    refactored theming

--- frontend/src/app/ThemeContext.tsx
+++ frontend/src/app/ThemeContext.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import { useBrowserStorage } from '#~/components/browserStorage/BrowserStorageContext';
+import { MLFLOW_DARK_MODE_KEY } from '#~/pages/pipelines/global/mlflowExperiments/utils';
 
 type ThemeContextProps = {
   theme: string;
   setAllThemes: (themeName: string) => void;
-  mlflowTheme: boolean;
-  setOdhTheme: (theme: string) => void;
 };
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
@@ -18,19 +17,20 @@ type ThemeProviderProps = {
 };
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
   const [odhTheme, setOdhTheme] = useBrowserStorage<string>('odh.dashboard.ui.theme', 'light');
-  const [mlflowTheme, setMlflowTheme] = useBrowserStorage<boolean>(
-    '_mlflow_dark_mode_toggle_enabled',
-    odhTheme === 'dark',
+  const [, setMlflowTheme] = useBrowserStorage<boolean>(MLFLOW_DARK_MODE_KEY, odhTheme === 'dark');
+  console.log('odhTheme', odhTheme);
+
+  const setAllThemes = React.useCallback(
+    (theme: string) => {
+      setMlflowTheme(theme === 'dark');
+      setOdhTheme(theme);
+    },
+    [setMlflowTheme, setOdhTheme],
   );
 
-  const setAllThemes = (theme: string) => {
-    setMlflowTheme(theme === 'dark');
-    setOdhTheme(theme);
-  };
-
   const contextValue = React.useMemo(
-    () => ({ theme: odhTheme, setAllThemes, mlflowTheme, setOdhTheme }),
-    [odhTheme, mlflowTheme, setAllThemes, setOdhTheme],
+    () => ({ theme: odhTheme, setAllThemes }),
+    [odhTheme, setAllThemes],
   );
 
   return <ThemeContext.Provider value={contextValue}>{children}</ThemeContext.Provider>;
--- frontend/src/pages/pipelines/global/mlflowExperiments/MLflowIframe.tsx
+++ frontend/src/pages/pipelines/global/mlflowExperiments/MLflowIframe.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
 import { useThemeContext } from '#~/app/ThemeContext';
 import { useMlflowPathSync } from './useMlflowPathSync';
+import { MLFLOW_DARK_MODE_KEY } from './utils';
 
 const MlflowIframe = React.forwardRef<HTMLIFrameElement>((_, ref) => {
   const { iframeRef, initIframeSrc } = useMlflowPathSync(ref);
-  const { theme: odhTheme, mlflowTheme, setOdhTheme } = useThemeContext();
+  const { theme } = useThemeContext();
 
+  // Sync ODH theme to MLflow localStorage (ODH is source of truth)
   React.useEffect(() => {
-    if (mlflowTheme !== (odhTheme === 'dark')) {
-      setOdhTheme(mlflowTheme ? 'dark' : 'light');
+    const mlflowValue = localStorage.getItem(MLFLOW_DARK_MODE_KEY);
+    const mlflowDarkMode = mlflowValue === 'true';
+    const odhDarkMode = theme === 'dark';
+
+    if (mlflowDarkMode !== odhDarkMode) {
+      localStorage.setItem(MLFLOW_DARK_MODE_KEY, JSON.stringify(odhDarkMode));
     }
-  }, [odhTheme, mlflowTheme, setOdhTheme]);
+  }, [theme]);
 
   return (
     <iframe
