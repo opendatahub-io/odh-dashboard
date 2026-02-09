5dc3a57f08e10decc27759ac29911b531af7f81e
Premved Dhote <premveddhote@gmail.com>
2026-02-09 15:36:23 +0530

    Add ESLint rule to restrict direct PatternFly Modal imports

--- packages/eslint-config/package-restrictions.js
+++ packages/eslint-config/package-restrictions.js
@@ -9,6 +9,12 @@ module.exports = {
             group: ['@odh-dashboard/internal/pages/*', '!@odh-dashboard/internal/pages/projects'],
             message: 'Create reusable components or migrate old code from `frontend` to a package.',
           },
+          {
+            group: ['@patternfly/react-core'],
+            importNames: ['Modal', 'ModalBody', 'ModalHeader', 'ModalFooter', 'ModalVariant'],
+            message:
+              'Avoid using the PatternFly Modal directly. Use generic modal wrapper instead.',
+          },
         ],
       },
     ],
