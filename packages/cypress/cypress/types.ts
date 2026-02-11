ca0497c461fc643fd3550b277d6f84d5b5c752a0
Tobias Klumpp <tklumpp@redhat.com>
2026-02-11 13:13:51 +0000

    Move user stories to correct level

--- packages/cypress/cypress/types.ts
+++ packages/cypress/cypress/types.ts
@@ -482,6 +482,15 @@ export type GenAiTestData = {
   hardwareProfileDeploymentSize: string;
   configMapName: string;
   playgroundServiceName: string;
+  // Filter test values
+  filterByNameValue?: string;
+  filterByKeywordValue?: string;
+  filterByUseCaseValue?: string;
+  // Expected values for verification
+  expectedModelDisplayName?: string;
+  expectedUseCase?: string;
+  expectedStatus?: string;
+  enableAiAsset?: boolean;
 };
 
 export type ModelCatalogSourceTestData = {
