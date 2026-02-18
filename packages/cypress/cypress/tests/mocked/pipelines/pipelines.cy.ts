7a5a95dcd35536b6d79586c3ab2b2f58f4f6a762
Tobias Klumpp <tklumpp@redhat.com>
2026-02-18 11:28:57 +0000

    Fix failing test

--- packages/cypress/cypress/tests/mocked/pipelines/pipelines.cy.ts
+++ packages/cypress/cypress/tests/mocked/pipelines/pipelines.cy.ts
@@ -1102,17 +1102,17 @@ describe('Pipelines', () => {
     // Open the "Upload new version" modal
     pipelinesGlobal.findUploadVersionButton().click();
 
-    // Fill out the "Upload new version" modal
+    // Fill out the "Upload new version" modal and submit
     pipelineVersionImportModal.shouldBeOpen();
     pipelineVersionImportModal.selectPipelineByName('Test pipeline');
     pipelineVersionImportModal.fillVersionName('Argo workflow version');
     pipelineVersionImportModal.fillVersionDescription('Argo workflow version description');
     pipelineVersionImportModal.uploadPipelineYaml(argoWorkflowPipeline);
+    pipelineVersionImportModal.submit();
 
-    // Verify error appears and submit button is disabled
+    // Verify error appears
     pipelineVersionImportModal.findImportModalError().should('exist');
     pipelineVersionImportModal.findImportModalError().contains('Unsupported pipeline version');
-    pipelineVersionImportModal.findSubmitButton().should('be.disabled');
   });
 
   it('imports a new pipeline version by url', () => {
