6cf309aa6ee5c728896b6e2416beafef8a223c88
Emily Samoylov <esamoylo@redhat.com>
2026-02-23 16:25:50 -0500

    Page layout updates

---
+++ packages/model-serving/src/components/deploymentWizard/yaml/EnterYAMLEditModal.tsx
@@ -0,0 +1,33 @@
+import ContentModal from '@odh-dashboard/internal/components/modals/ContentModal';
+import React from 'react';
+
+type EnterYAMLEditModalProps = {
+  onClose: () => void;
+  onConfirm: () => void;
+};
+
+export const EnterYAMLEditModal: React.FC<EnterYAMLEditModalProps> = ({ onClose, onConfirm }) => {
+  return (
+    <ContentModal
+      onClose={onClose}
+      title="Switch to manual YAML editor?"
+      contents="By entering manual edit mode, you will be able to modifying the YAML directly. However, you will no longer be able to use the guided form, and any manual changes will be lost."
+      titleIconVariant="warning"
+      variant="small"
+      buttonActions={[
+        {
+          label: 'Switch to manual YAML editor',
+          onClick: onConfirm,
+          variant: 'danger',
+          dataTestId: 'switch-to-manual-yaml-editor',
+        },
+        {
+          label: 'Cancel',
+          onClick: onClose,
+          variant: 'link',
+          dataTestId: 'cancel-switch-to-manual-yaml-editor',
+        },
+      ]}
+    />
+  );
+};
