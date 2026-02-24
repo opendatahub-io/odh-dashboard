ed03b571475ced83c8c6e247d2b81b7d530d8f09
Gustavo Murcia <gmurcia@redhat.com>
2026-02-24 16:30:44 -0500

    chore(autorag): Initial UI for FileExplorer
    https://issues.redhat.com/browse/RHOAIENG-48911

--- packages/autorag/frontend/src/app/pages/MainPage.tsx
+++ packages/autorag/frontend/src/app/pages/MainPage.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '@patternfly/react-core';
 import ApplicationsPage from '@odh-dashboard/internal/pages/ApplicationsPage';
+import FileExplorer from '~/app/components/common/FileExplorer/FileExplorer.tsx';
 
 const MainPage: React.FC = () => {
   const loadError = undefined;
   const loaded = true;
+
+  const [isFileExplorerOpen, setIsFileExplorerOpen] = useState<boolean>(false);
 
   return (
     <ApplicationsPage
@@ -11,12 +15,21 @@ const MainPage: React.FC = () => {
       description={
         <p>Automatically configure and optimize your Retrieval-Augmented Generation workflows.</p>
       }
-      empty
+      empty={false}
       loadError={loadError}
       loaded={loaded}
       provideChildrenPadding
       removeChildrenTopPadding
-    />
+    >
+      <Button key="confirm" variant="primary" onClick={() => setIsFileExplorerOpen(true)}>
+        Open FileExplorer
+      </Button>
+      <FileExplorer
+        isOpen={isFileExplorerOpen}
+        onClose={() => setIsFileExplorerOpen(false)}
+        onSelect={(files) => null /* eslint-disable-line @typescript-eslint/no-unused-vars */}
+      />
+    </ApplicationsPage>
   );
 };
 
