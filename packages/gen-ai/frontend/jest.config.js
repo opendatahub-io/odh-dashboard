974215687480e6f1f2e82ec74032d0b06bc9d93e
Tobias Klumpp <tklumpp@redhat.com>
2026-02-26 14:16:20 +0000

    Specify module import alias

--- packages/gen-ai/frontend/jest.config.js
+++ packages/gen-ai/frontend/jest.config.js
@@ -19,6 +19,7 @@ module.exports = {
     '\\.(css|less|sass|scss)$': '<rootDir>/config/transform.style.js',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/config/transform.file.js',
+    '#~/(.*)': '<rootDir>/../../../frontend/src/$1',
     '~/(.*)': '<rootDir>/src/$1',
   },
 
