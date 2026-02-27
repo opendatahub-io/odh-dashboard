707af8d67e79d6a655c44d5cbf9ef1badc1df003
katieperry4 <katelynnperry99@gmail.com>
2026-02-27 12:09:17 -0500

    adding maas tab back

---
+++ packages/gen-ai/frontend/src/app/AIAssets/AIAssetsMaaSTabWrapper.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+type Props = { children?: React.ReactNode };
+
+const AIAssetsMaaSTabWrapper: React.FC<Props> = ({ children }) => <>{children}</>;
+
+export default AIAssetsMaaSTabWrapper;
