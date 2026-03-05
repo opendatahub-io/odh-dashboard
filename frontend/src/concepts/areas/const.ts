b3d19a372eeee7486afa6eafa67a957f4ad1b409
Nick Gagan <ngagan@redhat.com>
2026-03-05 13:05:01 -0500

    fix: extra externalModels

--- frontend/src/concepts/areas/const.ts
+++ frontend/src/concepts/areas/const.ts
@@ -7,7 +7,7 @@ export const techPreviewFlags = {
   automl: false,
   autorag: false,
   modelAsService: false,
-  externalModels: false,
+  aiAssetExternalModels: false,
   maasApiKeys: false,
   mlflow: false,
   projectRBAC: true,
