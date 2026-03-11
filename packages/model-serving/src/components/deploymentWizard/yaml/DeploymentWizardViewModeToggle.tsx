8f779d4309e134fb072d1163d8e1d716b79ac269
Emily Samoylov <esamoylo@redhat.com>
2026-03-11 16:01:34 -0400

    Namespace check and microcopy

--- packages/model-serving/src/components/deploymentWizard/yaml/DeploymentWizardViewModeToggle.tsx
+++ packages/model-serving/src/components/deploymentWizard/yaml/DeploymentWizardViewModeToggle.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Label, ToggleGroup, ToggleGroupItem } from '@patternfly/react-core';
+import { Flex, FlexItem, Label, ToggleGroup, ToggleGroupItem } from '@patternfly/react-core';
 import { ModelDeploymentWizardViewMode } from '../ModelDeploymentWizard';
 
 type DeploymentWizardViewModeToggleProps = {
@@ -7,39 +7,34 @@ type DeploymentWizardViewModeToggleProps = {
   setViewMode: (mode: ModelDeploymentWizardViewMode) => void;
 };
 
-// The label text color doesn't inherit the toggled state text color updates (it inverts)
-// `currentColor` tracks the toggled state text color so make the label text match.
-const techPreviewLabelStyle: React.CSSProperties & { '--pf-v6-c-label--Color'?: string } = {
-  marginLeft: '4px',
-  '--pf-v6-c-label--Color': 'currentColor',
-};
-
 export const DeploymentWizardViewModeToggle: React.FC<DeploymentWizardViewModeToggleProps> = ({
   viewMode,
   setViewMode,
 }) => (
-  <ToggleGroup aria-label="Deployment view mode">
-    <ToggleGroupItem
-      data-testid="form-view"
-      text="Form"
-      buttonId="form-view"
-      isSelected={viewMode === 'form'}
-      onChange={() => setViewMode('form')}
-      isDisabled={viewMode === 'yaml-edit'}
-    />
-    <ToggleGroupItem
-      data-testid="yaml-view"
-      text={
-        <>
-          YAML
-          <Label isCompact color="yellow" variant="outline" style={techPreviewLabelStyle}>
-            Tech preview
-          </Label>
-        </>
-      }
-      buttonId="yaml-view"
-      isSelected={viewMode !== 'form'}
-      onChange={() => (viewMode === 'form' ? setViewMode('yaml-preview') : undefined)}
-    />
-  </ToggleGroup>
+  <Flex>
+    <FlexItem>
+      <Label isCompact color="yellow" variant="outline">
+        YAML feature is in tech preview
+      </Label>
+    </FlexItem>
+    <FlexItem>
+      <ToggleGroup aria-label="Deployment view mode">
+        <ToggleGroupItem
+          data-testid="form-view"
+          text="Form"
+          buttonId="form-view"
+          isSelected={viewMode === 'form'}
+          onChange={() => setViewMode('form')}
+          isDisabled={viewMode === 'yaml-edit'}
+        />
+        <ToggleGroupItem
+          data-testid="yaml-view"
+          text="YAML"
+          buttonId="yaml-view"
+          isSelected={viewMode !== 'form'}
+          onChange={() => (viewMode === 'form' ? setViewMode('yaml-preview') : undefined)}
+        />
+      </ToggleGroup>
+    </FlexItem>
+  </Flex>
 );
