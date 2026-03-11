f95947a86568a163deef483c4df5b6475bfb38f9
Griffin-Sullivan <gsulliva@redhat.com>
2026-03-11 09:58:05 -0400

    Update MaaS API Key mocks for 3.4 redesign

--- packages/maas/bff/internal/constants/api_routes.go
+++ packages/maas/bff/internal/constants/api_routes.go
@@ -9,8 +9,8 @@ const (
 	TierCreatePath = ApiPathPrefix + "/tier"
 
 	// API Keys routes
-	APIKeysListPath   = ApiPathPrefix + "/api-keys"
-	APIKeyByIDPath    = ApiPathPrefix + "/api-key/:id"
-	APIKeyCreatePath  = ApiPathPrefix + "/api-key"
-	APIKeysDeletePath = ApiPathPrefix + "/api-keys"
+	APIKeyCreatePath     = ApiPathPrefix + "/api-keys"
+	APIKeySearchPath     = ApiPathPrefix + "/api-keys/search"
+	APIKeyBulkRevokePath = ApiPathPrefix + "/api-keys/bulk-revoke"
+	APIKeyByIDPath       = ApiPathPrefix + "/api-keys/:id"
 )
