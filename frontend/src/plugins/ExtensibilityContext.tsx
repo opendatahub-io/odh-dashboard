6968a4b8a22f31f3be2cf1a1893b9ccca6bd90d9
manaswinidas <dasmanaswini10@gmail.com>
2026-03-13 12:56:22 +0530

    Fix ExtensibilityContextProvider blocking all rendering until MF remotes load

--- frontend/src/plugins/ExtensibilityContext.tsx
+++ frontend/src/plugins/ExtensibilityContext.tsx
@@ -4,11 +4,7 @@ import { PluginStore } from '@odh-dashboard/plugin-core';
 import { useAppExtensions } from './useAppExtensions';
 
 export const ExtensibilityContextProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
-  const [appExtensions, loaded] = useAppExtensions();
-  // create the plugin store
-  const store = React.useMemo(
-    () => (loaded ? new PluginStore(appExtensions) : null),
-    [appExtensions, loaded],
-  );
-  return store ? <PluginStoreProvider store={store}>{children}</PluginStoreProvider> : null;
+  const [appExtensions] = useAppExtensions();
+  const store = React.useMemo(() => new PluginStore(appExtensions), [appExtensions]);
+  return <PluginStoreProvider store={store}>{children}</PluginStoreProvider>;
 };
