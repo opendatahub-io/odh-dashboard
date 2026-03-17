714d64f2df03270704831eccb63329a52384b0b0
Gustavo Murcia <gmurcia@redhat.com>
2026-03-17 18:01:58 -0400

    chore: make fmt

--- packages/autorag/bff/internal/repositories/s3.go
+++ packages/autorag/bff/internal/repositories/s3.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	ErrSecretNotFound     = errors.New("secret not found")
+	ErrSecretNotFound       = errors.New("secret not found")
 	ErrMissingRequiredField = errors.New("missing required field")
 )
 
