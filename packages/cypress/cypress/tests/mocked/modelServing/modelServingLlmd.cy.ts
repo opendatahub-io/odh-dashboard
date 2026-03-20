f192d04eec14eda10fefd47d70e74b64a9022fc4
Ashley McEntee <123661468+ashley-o0o@users.noreply.github.com>
2026-03-20 13:24:13 -0400

    Revert llmd test

--- packages/cypress/cypress/tests/mocked/modelServing/modelServingLlmd.cy.ts
+++ packages/cypress/cypress/tests/mocked/modelServing/modelServingLlmd.cy.ts
@@ -70,7 +70,6 @@ const initIntercepts = ({
       genAiStudio: true,
       modelAsService: true, // Enable MaaS for testing
       disableLLMd: false,
-      vLLMDeploymentOnMaaS: true,
     }),
   );
   cy.interceptOdh('GET /api/components', null, []);
@@ -349,7 +348,6 @@ describe('Model Serving LLMD', () => {
         .click();
       modelServingWizard.findExistingConnectionValue().should('have.value', 'test-s3-secret');
       modelServingWizard.findModelTypeSelectOption(ModelTypeLabel.GENERATIVE).click();
-      modelServingWizard.findLegacyModeCheckbox().should('not.be.checked');
       modelServingWizard.findLocationPathInput().should('exist').type('test-model/');
       modelServingWizard.findNextButton().should('be.enabled').click();
 
@@ -507,10 +505,6 @@ describe('Model Serving LLMD', () => {
       modelServingWizardEdit.findSaveConnectionCheckbox().should('be.checked');
       modelServingWizardEdit.findSaveConnectionCheckbox().click();
       modelServingWizardEdit.findSaveConnectionCheckbox().should('not.be.checked');
-      modelServingWizardEdit
-        .findLegacyModeCheckbox()
-        .should('not.be.checked')
-        .should('be.disabled');
       modelServingWizardEdit.findNextButton().should('be.enabled').click();
 
       // Step 2: Model deployment
