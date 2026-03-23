7c16bba231811905b9810544376bd31a55569055
John Haran <jharan@redhat.com>
2026-03-23 11:06:02 +0000

    update to ensure Vector stores tab only visible when dev flag enabled

--- packages/gen-ai/frontend/src/odh/extensions.ts
+++ packages/gen-ai/frontend/src/odh/extensions.ts
@@ -136,7 +136,7 @@ const extensions: (NavExtension | RouteExtension | AreaExtension | AIAssetsTabEx
   {
     type: 'gen-ai.ai-assets/tab',
     flags: {
-      required: [PLUGIN_GEN_AI],
+      required: [PLUGIN_GEN_AI, AI_ASSET_VECTOR_STORES],
     },
     properties: {
       id: 'vectorstores',
