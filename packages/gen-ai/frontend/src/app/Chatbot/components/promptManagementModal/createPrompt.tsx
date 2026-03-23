bc5deffaf0112d17dbccb41f06d0fc81ca124105
danpierce1 <4040112+danpierce1@users.noreply.github.com>
2026-03-23 10:06:44 +0000

    spacing for save prompt inputs

--- packages/gen-ai/frontend/src/app/Chatbot/components/promptManagementModal/createPrompt.tsx
+++ packages/gen-ai/frontend/src/app/Chatbot/components/promptManagementModal/createPrompt.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, Button, TextInput, TextArea, Title } from '@patternfly/react-core';
+import { Flex, FlexItem, Button, TextInput, TextArea, Title } from '@patternfly/react-core';
 import { usePlaygroundStore } from '~/app/Chatbot/store/usePlaygroundStore';
 import { useCreatePrompt } from './usePromptQueries';
 
@@ -37,21 +37,35 @@ export default function CreatePrompt({ onClose }: { onClose: () => void }): Reac
 
   return (
     <Flex direction={{ default: 'column' }}>
-      <Title headingLevel="h6">Name</Title>
-      <TextInput
-        value={dirtyPrompt?.name}
-        onChange={(_event, value) => handleChange('name', value)}
-      />
-      <Title headingLevel="h6">Prompt</Title>
-      <TextArea
-        value={dirtyPrompt?.template}
-        onChange={(_event, value) => handleChange('template', value)}
-      />
-      <Title headingLevel="h6">Commit Message</Title>
-      <TextInput
-        value={dirtyPrompt?.commit_message}
-        onChange={(_event, value) => handleChange('commit_message', value)}
-      />
+      <FlexItem spacer={{ default: 'spacerMd' }}>
+        <Title headingLevel="h6" style={{ paddingBottom: 'var(--pf-t--global--spacer--xs)' }}>
+          Name
+        </Title>
+        <TextInput
+          value={dirtyPrompt?.name}
+          onChange={(_event, value) => handleChange('name', value)}
+        />
+      </FlexItem>
+      <FlexItem spacer={{ default: 'spacerMd' }}>
+        <Title headingLevel="h6" style={{ paddingBottom: 'var(--pf-t--global--spacer--xs)' }}>
+          Prompt
+        </Title>
+        <TextArea
+          value={dirtyPrompt?.template}
+          resizeOrientation="vertical"
+          rows={12}
+          onChange={(_event, value) => handleChange('template', value)}
+        />
+      </FlexItem>
+      <FlexItem spacer={{ default: 'spacerMd' }}>
+        <Title headingLevel="h6" style={{ paddingBottom: 'var(--pf-t--global--spacer--xs)' }}>
+          Commit Message
+        </Title>
+        <TextInput
+          value={dirtyPrompt?.commit_message}
+          onChange={(_event, value) => handleChange('commit_message', value)}
+        />
+      </FlexItem>
       <Flex
         justifyContent={{ default: 'justifyContentSpaceBetween' }}
         style={{ width: '100%', paddingTop: 'var(--pf-t--global--spacer--md)' }}
