b5cf206ba16eb5305c2b53c4158103e6597a9d2e
Katelynn Perry <134058286+katieperry4@users.noreply.github.com>
2026-03-23 15:22:07 -0400

    Add subscription list page (#6789)
    * add subscription list page
    
    * pr comments and added cypress tests
    
    * fix data-test-id

---
+++ packages/maas/frontend/src/app/types/subscriptions.ts
@@ -0,0 +1,46 @@
+export type MaaSSubscription = {
+  name: string;
+  namespace: string;
+  phase: string;
+  priority?: number;
+  owner: OwnerSpec;
+  modelRefs: ModelSubscriptionRef[];
+  tokenMetadata?: TokenMetadata;
+  creationTimestamp: string;
+};
+
+export type ModelSubscriptionRef = {
+  name: string;
+  namespace: string;
+  tokenRateLimits: TokenRateLimit[];
+  tokenRateLimitRef?: string;
+  billingRate?: BillingRate;
+};
+
+export type BillingRate = {
+  perToken: string;
+};
+
+export type TokenRateLimit = {
+  limit: number;
+  window: string;
+};
+
+export type OwnerSpec = {
+  groups: GroupReference[];
+};
+
+export type GroupReference = {
+  name: string;
+};
+
+export type TokenMetadata = {
+  organizationId: string;
+  costCenter: string;
+  labels?: Record<string, string>;
+};
+
+export type MaaSSubscriptionListResponse = {
+  object: string;
+  data: MaaSSubscription[];
+};
