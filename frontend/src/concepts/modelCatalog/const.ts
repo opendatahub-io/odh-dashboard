eb7185e6efb3980c4ddd82e9a22155ca350601a4
Philip Colares Carneiro <philip.colares@gmail.com>
2026-03-23 11:55:19 +0000

    clean concepts
    Signed-off-by: Philip Colares Carneiro <philip.colares@gmail.com>

--- frontend/src/concepts/modelCatalog/const.ts
+++ frontend/src/concepts/modelCatalog/const.ts
@@ -1,13 +1,5 @@
-import {
-  ModelRegistryCustomProperty,
-  ModelRegistryMetadataType,
-} from '#~/concepts/modelRegistry/types';
-
 export const MODEL_CATALOG_SOURCES_CONFIGMAP = 'model-catalog-sources';
 export const MODEL_CATALOG_UNMANAGED_SOURCES_CONFIGMAP = 'model-catalog-unmanaged-sources';
-export const MAX_SHOWN_MODELS = 4;
-export const MIN_CARD_WIDTH = 225;
-export const FEATURED_LABEL = 'featured';
 
 export enum ReservedILabLabel {
   LabBase = 'lab-base',
@@ -16,9 +8,3 @@ export enum ReservedILabLabel {
 }
 
 export const RESERVED_ILAB_LABELS: ReservedILabLabel[] = Object.values(ReservedILabLabel);
-
-export const EMPTY_CUSTOM_PROPERTY_STRING: ModelRegistryCustomProperty = {
-  // eslint-disable-next-line camelcase
-  string_value: '',
-  metadataType: ModelRegistryMetadataType.STRING,
-};
