2884fdec5188104797813e0971b6ce659ffa7020
Ashley McEntee <123661468+ashley-o0o@users.noreply.github.com>
2026-03-25 14:05:41 -0400

    Update delete llmInferenceService

--- packages/llmd-serving/src/api/LLMdDeployment.ts
+++ packages/llmd-serving/src/api/LLMdDeployment.ts
@@ -1,12 +1,23 @@
 import { k8sDeleteResource, K8sStatus } from '@openshift/dynamic-plugin-sdk-utils';
-import { LLMInferenceServiceModel, type LLMdDeployment } from '../types';
+import {
+  LLMInferenceServiceConfigModel,
+  LLMInferenceServiceModel,
+  type LLMdDeployment,
+} from '../types';
 
 export const deleteDeployment = async (deployment: LLMdDeployment): Promise<void> => {
+  const { name, namespace } = deployment.model.metadata;
+
+  const hasMatchingConfig = deployment.model.spec.baseRefs?.some((ref) => ref.name === name);
+  if (hasMatchingConfig) {
+    await k8sDeleteResource<typeof LLMInferenceServiceConfigModel, K8sStatus>({
+      model: LLMInferenceServiceConfigModel,
+      queryOptions: { name, ns: namespace },
+    });
+  }
+
   await k8sDeleteResource<typeof LLMInferenceServiceModel, K8sStatus>({
     model: LLMInferenceServiceModel,
-    queryOptions: {
-      name: deployment.model.metadata.name,
-      ns: deployment.model.metadata.namespace,
-    },
+    queryOptions: { name, ns: namespace },
   });
 };
