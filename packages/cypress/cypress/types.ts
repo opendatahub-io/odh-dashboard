9875407e5c3eae3acebce6f0b0f09223c536aaae
Taj010 <rsheen003@gmail.com>
2026-03-26 12:11:22 -0400

    Add URI origin flow

--- packages/cypress/cypress/types.ts
+++ packages/cypress/cypress/types.ts
@@ -521,6 +521,11 @@ export type ModelRegistryTestData = {
   ociTransferJobFailedNotification: string;
   ociDestinationRegistry: string;
   ociDestinationUri: string;
+
+  // OCI Register and Store — URI origin variant
+  ociUriModelName: string;
+  ociUriJobName: string;
+  ociUriOriginUri: string;
 };
 
 export type ManageRegistryPermissionsTestData = {
