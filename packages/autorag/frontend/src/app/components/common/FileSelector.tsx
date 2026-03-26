4279b2c1caf8d76bd9dc24eb99fe459ff57dcac9
Daniel Duong <danielduong@ibm.com>
2026-03-25 20:41:26 -0400

    feat(autorag/eval): add animations

--- packages/autorag/frontend/src/app/components/common/FileSelector.tsx
+++ packages/autorag/frontend/src/app/components/common/FileSelector.tsx
@@ -9,12 +9,9 @@ import { TypeaheadSelect } from 'mod-arch-shared';
 import { TypeaheadSelectProps } from 'mod-arch-shared/dist/components/TypeaheadSelect';
 import React, { useState } from 'react';
 
-// handle slide + fade in/out animations (implement later, not now)
-
 interface FileSelectorProps<Files extends Array<string>> {
   id: string;
   files: Files;
-  // controlled or uncontrolled
   selected?: Files[number];
   onSelect: (selected: string) => void;
   onUpload: (
@@ -45,44 +42,70 @@ function FileSelector<Files extends Array<string>>(
   };
 
   return (
-    <div className="pf-v6-c-multiple-file-upload">
-      <TypeaheadSelect
-        selectOptions={props.files.map((file) => ({ content: file, value: file }))}
-        selected={props.selected ?? selected}
-        onSelect={(_event, _selection) => {
-          const selection = String(_selection);
-          props.onSelect(selection);
-          setSelected(selection);
+    <div className="pf-v6-c-multiple-file-upload pf-v6-u-display-block">
+      <div style={{ position: 'relative', zIndex: '1' }}>
+        <TypeaheadSelect
+          selectOptions={props.files.map((file) => ({ content: file, value: file }))}
+          selected={props.selected ?? selected}
+          onSelect={(_event, _selection) => {
+            const selection = String(_selection);
+            props.onSelect(selection);
+            setSelected(selection);
+          }}
+          allowClear
+          onClearSelection={() => {
+            props.onSelect('');
+            setSelected('');
+          }}
+          {...props.typeaheadProps}
+        />
+      </div>
+      <FileUpload
+        id={props.id}
+        style={{
+          maxHeight: hideUpload ? '0' : '6rem',
+          padding: hideUpload ? '0' : undefined,
+          marginTop: hideUpload ? '0' : 'var(--pf-t--global--spacer--sm)',
+          border: hideUpload ? '0' : undefined,
+
+          translate: hideUpload ? '0 -2rem' : '0 0',
+          opacity: hideUpload ? '0' : '1',
+          visibility: hideUpload ? 'hidden' : 'visible',
+
+          transition: 'max-height, padding, marginTop, border, translate, opacity, visibility',
+          transitionDuration: hideUpload
+            ? 'var(--pf-t--global--motion--duration--slide-out--default)'
+            : 'var(--pf-t--global--motion--duration--slide-in--default)',
+          transitionTimingFunction: 'cubic-bezier(.4, 0, .2, 1)',
         }}
-        allowClear
-        onClearSelection={() => {
-          props.onSelect('');
-          setSelected('');
+        onTransitionEnd={(event) => {
+          if (
+            event.propertyName === 'visibility' &&
+            event.currentTarget.style.visibility === 'hidden'
+          ) {
+            setUploadedFile(undefined);
+          }
         }}
-        {...props.typeaheadProps}
-      />
-      {!hideUpload && (
-        <FileUpload
-          id={props.id}
-          browseButtonText="Upload"
-          filenamePlaceholder="Drag and drop a file or upload"
-          isDisabled={!!uploadedFile && !uploadStatus}
-          filename={uploadedFile?.name}
-          onFileInputChange={handleFileChange}
-          {...props.fileUploadProps}
-        >
-          {!!uploadedFile && (
-            <FileUploadHelperText>
-              <Progress
-                value={uploadProgress}
-                variant={uploadStatus}
-                measureLocation={ProgressMeasureLocation.outside}
-                aria-label="File upload progress"
-              />
-            </FileUploadHelperText>
-          )}
-        </FileUpload>
-      )}
+        browseButtonText="Upload"
+        filenamePlaceholder="Drag and drop a file or upload"
+        hidden={hideUpload}
+        isDisabled={!!uploadedFile && !uploadStatus}
+        filename={uploadedFile?.name}
+        onFileInputChange={handleFileChange}
+        onClearClick={() => setUploadedFile(undefined)}
+        {...props.fileUploadProps}
+      >
+        {!!uploadedFile && (
+          <FileUploadHelperText>
+            <Progress
+              value={uploadProgress}
+              variant={uploadStatus}
+              measureLocation={ProgressMeasureLocation.outside}
+              aria-label="File upload progress"
+            />
+          </FileUploadHelperText>
+        )}
+      </FileUpload>
     </div>
   );
 }
