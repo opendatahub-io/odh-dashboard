266a2ba3f97ce3fb2ec30c138c5f1777cb66b9d7
Emily Samoylov <esamoylo@redhat.com>
2026-03-27 11:08:38 -0400

    Add model-serving-api api service

---
+++ backend/src/routes/api/service/model-serving/index.ts
@@ -0,0 +1,38 @@
+import { KubeFastifyInstance } from '../../../../types';
+import { registerProxy } from '../../../../utils/proxy';
+
+/**
+ * Proxy the model-serving-api service.
+ * Current endpoints:
+ * GET `/api/v1/gateways?namespace={ns}`
+ * Example response body:
+ * ```json
+ * {
+ *   "gateways": [
+ *     {
+ *       "name": "gateway-1",
+ *       "namespace": "namespace-1",
+ *       "listener": "http",
+ *       "status": "Ready"
+ *     }
+ *   ]
+ * }
+ * ```
+ */
+export default async (fastify: KubeFastifyInstance): Promise<void> => {
+  await registerProxy(fastify, {
+    prefix: '/',
+    rewritePrefix: '/',
+    authorize: true,
+    tls: true,
+    service: {
+      name: 'model-serving-api',
+      namespace: 'opendatahub',
+      port: 443,
+    },
+    local: {
+      // kubectl port-forward -n opendatahub svc/model-serving-api 9443:443
+      port: 9443,
+    },
+  });
+};
