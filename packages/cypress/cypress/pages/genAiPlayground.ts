9829bd68aa6d3cdc3b8bd9a9f12fd1e9bdfe0d3d
Emmanuel Ikeola <eikeola@redhat.com>
2026-03-31 15:00:16 +0100

    simplify gen-ai test navaigation

--- packages/cypress/cypress/pages/genAiPlayground.ts
+++ packages/cypress/cypress/pages/genAiPlayground.ts
@@ -1,35 +1,7 @@
 class GenAiPlayground {
   navigate(projectName: string) {
-    cy.intercept({
-      method: 'GET',
-      pathname: '/gen-ai/api/v1/lsd/status',
-      query: { namespace: projectName },
-    }).as('getLsdStatus');
-    cy.intercept({
-      method: 'GET',
-      pathname: '/gen-ai/api/v1/aaa/models',
-      query: { namespace: projectName },
-    }).as('getProjectModels');
-    cy.intercept({
-      method: 'GET',
-      pathname: '/gen-ai/api/v1/config',
-      query: { namespace: projectName },
-    }).as('getProjectConfig');
-    cy.intercept({
-      method: 'GET',
-      pathname: '/gen-ai/api/v1/aaa/vectorstores',
-    }).as('getAaeVectorStores');
-    cy.intercept({
-      method: 'GET',
-      pathname: '/gen-ai/api/v1/lsd/vectorstores',
-    }).as('getLsdVectorStores');
     cy.visit(`/gen-ai-studio/playground/${projectName}`);
     cy.url().should('include', `/gen-ai-studio/playground/${projectName}`);
-    cy.wait('@getLsdStatus').its('response.statusCode').should('eq', 200);
-    cy.wait('@getProjectModels').its('response.statusCode').should('eq', 200);
-    cy.wait('@getProjectConfig').its('response.statusCode').should('eq', 200);
-    cy.wait('@getAaeVectorStores');
-    cy.wait('@getLsdVectorStores');
   }
 
   findEmptyState() {
