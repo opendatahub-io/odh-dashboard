1e6811cd01e630af545ea8b1b4f765240989c658
pnaik1 <pnaik@redhat.com>
2026-04-01 16:04:36 +0530

    add CR discovery for dashboardNamespace

--- packages/eval-hub/frontend/src/odh/EvalHubWrapper.tsx
+++ packages/eval-hub/frontend/src/odh/EvalHubWrapper.tsx
@@ -5,8 +5,11 @@ import {
   ModularArchContextProvider,
   NotificationContextProvider,
   BrowserStorageContextProvider,
+  useSettings,
 } from 'mod-arch-core';
+import { Alert, Bullseye, Spinner } from '@patternfly/react-core';
 import { URL_PREFIX } from '~/app/utilities/const';
+import { AppContext } from '~/app/context/AppContext';
 import AppRoutes from '~/app/AppRoutes';
 import { registerMlflowEmbeddedRemote } from './registerMlflowEmbeddedRemote';
 
@@ -18,13 +21,44 @@ const modularArchConfig: ModularArchConfig = {
   BFF_API_VERSION: 'v1',
 };
 
+const EvalHubWrapperContent: React.FC = () => {
+  const { configSettings, userSettings, loaded, loadError } = useSettings();
+
+  const contextValue = React.useMemo(
+    () => (configSettings && userSettings ? { config: configSettings, user: userSettings } : null),
+    [configSettings, userSettings],
+  );
+
+  if (loadError) {
+    return (
+      <Bullseye>
+        <Alert variant="danger" isInline title="Failed to load settings">
+          {loadError.message}
+        </Alert>
+      </Bullseye>
+    );
+  }
+  if (!loaded || !contextValue) {
+    return (
+      <Bullseye>
+        <Spinner />
+      </Bullseye>
+    );
+  }
+  return (
+    <AppContext.Provider value={contextValue}>
+      <BrowserStorageContextProvider>
+        <NotificationContextProvider>
+          <AppRoutes />
+        </NotificationContextProvider>
+      </BrowserStorageContextProvider>
+    </AppContext.Provider>
+  );
+};
+
 const EvalHubWrapper: React.FC = () => (
   <ModularArchContextProvider config={modularArchConfig}>
-    <BrowserStorageContextProvider>
-      <NotificationContextProvider>
-        <AppRoutes />
-      </NotificationContextProvider>
-    </BrowserStorageContextProvider>
+    <EvalHubWrapperContent />
   </ModularArchContextProvider>
 );
 
