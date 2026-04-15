d7e30b8fc18f103e39fc1daf14b55a30db78a67a
Claudia Alphonse <claudialphonse@gmail.com>
2026-04-15 22:50:16 +0530

    Fix(RHOAIENG-57847): added column width fix and ability to add multiple unit time tokens (#7245)
    * fix: added column width fix and ability to add multiple unit time tokens
    
    * review:code rabbit review change

--- packages/maas/frontend/src/app/shared/AddModelsModal.tsx
+++ packages/maas/frontend/src/app/shared/AddModelsModal.tsx
@@ -237,8 +237,8 @@ const AddModelsModal: React.FC<AddModelsModalProps> = ({
                   <Th sort={getSortParams('name')}>Model name</Th>
                   <Th sort={getSortParams('namespace')}>Project</Th>
                   <Th sort={getSortParams('modelId')}>Model ID</Th>
-                  <Th>Subscriptions</Th>
-                  <Th>Policies</Th>
+                  <Th width={20}>Subscriptions</Th>
+                  <Th width={20}>Policies</Th>
                   <Th screenReaderText="Actions" />
                 </Tr>
               </Thead>
@@ -258,7 +258,7 @@ const AddModelsModal: React.FC<AddModelsModalProps> = ({
                       </Td>
                       <Td dataLabel="Project">{ref.namespace}</Td>
                       <Td dataLabel="Model ID">{ref.modelRef.name}</Td>
-                      <Td dataLabel="Subscriptions">
+                      <Td dataLabel="Subscriptions" modifier="breakWord">
                         {selected && fromSubscription && (
                           <Label color="green" isCompact>
                             {currentSelectionLabel}
@@ -270,7 +270,7 @@ const AddModelsModal: React.FC<AddModelsModalProps> = ({
                         {(fromSubscription ? !selected && subs.length === 0 : subs.length === 0) &&
                           'None'}
                       </Td>
-                      <Td dataLabel="Policies">
+                      <Td dataLabel="Policies" modifier="breakWord">
                         {selected && !fromSubscription && (
                           <Label color="green" isCompact>
                             {currentSelectionLabel}
