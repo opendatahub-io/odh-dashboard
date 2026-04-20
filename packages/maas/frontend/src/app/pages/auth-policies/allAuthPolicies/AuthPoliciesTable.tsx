0170033a2568f0c91aace8cb113c4e530ef1dab3
Claudia Alphonse <claudialphonse@gmail.com>
2026-04-21 00:37:53 +0530

    Fix(RHOAIENG-58427):Added toolbar with search based on policy name and description (#7268)
    * feat:added toolbar with search based on policy name and description and filtering based on phase
    
    * feat:added cypress test cases for filtering
    
    * review:removed filtering by phase to match consistency with subscription filtering

--- packages/maas/frontend/src/app/pages/auth-policies/allAuthPolicies/AuthPoliciesTable.tsx
+++ packages/maas/frontend/src/app/pages/auth-policies/allAuthPolicies/AuthPoliciesTable.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Table from '@odh-dashboard/internal/components/table/Table';
+import DashboardEmptyTableView from '@odh-dashboard/internal/concepts/dashboard/DashboardEmptyTableView';
 import { MaaSAuthPolicy } from '~/app/types/subscriptions';
 import { authPoliciesColumns } from './columns';
 import AuthPoliciesTableRow from './AuthPoliciesTableRow';
@@ -8,12 +9,14 @@ type AuthPoliciesTableProps = {
   authPolicies: MaaSAuthPolicy[];
   setDeleteAuthPolicy: (authPolicy: MaaSAuthPolicy) => void;
   toolbarContent?: React.ReactElement;
+  onClearFilters: () => void;
 };
 
 const AuthPoliciesTable: React.FC<AuthPoliciesTableProps> = ({
   authPolicies,
   setDeleteAuthPolicy,
   toolbarContent,
+  onClearFilters,
 }) => (
   <Table
     data-testid="auth-policies-table"
@@ -27,7 +30,9 @@ const AuthPoliciesTable: React.FC<AuthPoliciesTableProps> = ({
         setDeleteAuthPolicy={setDeleteAuthPolicy}
       />
     )}
+    emptyTableView={<DashboardEmptyTableView onClearFilters={onClearFilters} />}
     toolbarContent={toolbarContent}
+    onClearFilters={onClearFilters}
   />
 );
 
