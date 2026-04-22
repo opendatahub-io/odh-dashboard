ff85941db60005438179dd20f69f6ae6e4a4050f
Claudia Alphonse <claudialphonse@gmail.com>
2026-04-22 19:45:19 +0530

    review:added content changes for tooltip

--- packages/model-training/src/const.ts
+++ packages/model-training/src/const.ts
@@ -3,4 +3,4 @@ export const TRAINER_STATUS_ANNOTATION = 'trainer.opendatahub.io/trainerStatus';
 export const KUEUE_QUEUE_LABEL = 'kueue.x-k8s.io/queue-name';
 export const RAY_CLUSTER_SELECTOR_LABEL = 'ray.io/cluster';
 export const PAUSE_RAY_JOB_TOOLTIP_CONTENT =
-  'Pause is not available for Ray jobs without a dedicated cluster';
+  'Pause is only available for Ray jobs with a dedicated Ray cluster.';
