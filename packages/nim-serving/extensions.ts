6f7b7bfbe617784b494d6474209511921016d069
Yulia Krimerman <juliapiterova@hotmail.com>
2026-04-30 12:31:51 -0400

    rebase

--- packages/nim-serving/extensions.ts
+++ packages/nim-serving/extensions.ts
@@ -5,8 +5,16 @@ import type {
 // Allow this import as it consists of types and enums only.
 // eslint-disable-next-line no-restricted-syntax
 import { SupportedArea } from '@odh-dashboard/internal/concepts/areas/types';
+import type { ModelServingPlatformWatchDeploymentsExtension } from '@odh-dashboard/model-serving/extension-points';
+import type { NIMDeployment } from './src/deployments';
 
-const extensions: (AreaExtension | ProjectDetailsSettingsCardExtension)[] = [
+export const NIM_ID = 'nvidia-nim';
+
+const extensions: (
+  | AreaExtension
+  | ProjectDetailsSettingsCardExtension
+  | ModelServingPlatformWatchDeploymentsExtension<NIMDeployment>
+)[] = [
   {
     type: 'app.area',
     properties: {
@@ -24,6 +32,16 @@ const extensions: (AreaExtension | ProjectDetailsSettingsCardExtension)[] = [
     },
     flags: {
       required: [SupportedArea.NIM_WIZARD],
+    },
+  },
+  {
+    type: 'model-serving.platform/watch-deployments',
+    properties: {
+      platform: NIM_ID,
+      watch: () => import('./src/deployments').then((m) => m.useWatchDeployments),
+    },
+    flags: {
+      required: [SupportedArea.NIM_MODEL],
     },
   },
 ];
