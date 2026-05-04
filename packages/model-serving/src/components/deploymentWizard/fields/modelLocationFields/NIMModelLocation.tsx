73dadd4bb88574d5ea1308448fff5bd47e881fe2
Emily Samoylov <esamoylo@redhat.com>
2026-05-04 15:02:55 -0400

    Fix flake

--- packages/model-serving/src/components/deploymentWizard/fields/modelLocationFields/NIMModelLocation.tsx
+++ packages/model-serving/src/components/deploymentWizard/fields/modelLocationFields/NIMModelLocation.tsx
@@ -4,16 +4,3 @@ export const NIMModelLocationOption = {
   key: NIMModelLocationKey,
   label: 'NVIDIA NIM',
 };
-
-export type NIMModel = {
-  name: string;
-  namespace: string;
-  tag?: string;
-  metadata: {
-    displayName?: string;
-    shortDescription?: string;
-    tags?: string[];
-    latestTag?: string;
-    updatedDate?: string;
-  };
-};
