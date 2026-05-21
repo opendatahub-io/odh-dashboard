9f29a23c2c8545cb916f3c7abc74cb9834ad254d
Conor O'Malley <conormomalley@gmail.com>
2026-05-21 10:48:41 +0100

    add toolcalling flag

--- packages/model-registry/upstream/frontend/src/odh/ModelCatalogWrapper.tsx
+++ packages/model-registry/upstream/frontend/src/odh/ModelCatalogWrapper.tsx
@@ -10,15 +10,22 @@ import {
 import { ThemeProvider, Theme } from 'mod-arch-kubeflow';
 import { Bullseye } from '@patternfly/react-core';
 import useFetchDscStatus from '@odh-dashboard/internal/concepts/areas/useFetchDscStatus';
+import { useAppContext } from '@odh-dashboard/internal/app/AppContext';
 import { BFF_API_VERSION, URL_PREFIX } from '~/app/utilities/const';
 import { AppContext } from '~/app/context/AppContext';
 import ModelCatalogRoutes from '~/app/pages/modelCatalog/ModelCatalogRoutes';
 import { ModelRegistrySelectorContextProvider } from '~/app/context/ModelRegistrySelectorContext';
 import NotificationListener from '~/odh/components/NotificationListener';
 import OdhDevFeatureFlagOverridesProvider from '~/odh/components/OdhDevFeatureFlagOverridesProvider';
+import { TempDevFeature } from '~/app/hooks/useTempDevFeatureAvailable';
 
 const ModelCatalogWrapperContent: React.FC = () => {
   const { configSettings, userSettings, loaded, loadError } = useSettings();
+  const { dashboardConfig } = useAppContext();
+  const crdOverrides = React.useMemo(() => {
+    const { toolCalling } = dashboardConfig.spec.dashboardConfig;
+    return toolCalling ? { [TempDevFeature.ToolCallingConfiguration]: true } : {};
+  }, [dashboardConfig]);
   const appContextValue = React.useMemo(
     () => (configSettings && userSettings ? { config: configSettings, user: userSettings } : null),
     [configSettings, userSettings],
@@ -33,7 +40,7 @@ const ModelCatalogWrapperContent: React.FC = () => {
     <AppContext.Provider value={appContextValue}>
       <ThemeProvider theme={Theme.Patternfly}>
         <BrowserStorageContextProvider>
-          <OdhDevFeatureFlagOverridesProvider crdOverrides={{}}>
+          <OdhDevFeatureFlagOverridesProvider crdOverrides={crdOverrides}>
             <NotificationContextProvider>
               {/* TODO: TECH DEBT - Remove NotificationListener once midstream uses mod-arch-core NotificationContext */}
               <NotificationListener>
