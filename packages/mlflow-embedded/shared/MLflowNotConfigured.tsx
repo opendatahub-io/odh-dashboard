e57fb5e470b066aa612a56e5a9cc239dd48d6248
Taj010 <rsheen003@gmail.com>
2026-05-27 15:58:49 -0400

    address comment - adjust the svg image call

--- packages/mlflow-embedded/shared/MLflowNotConfigured.tsx
+++ packages/mlflow-embedded/shared/MLflowNotConfigured.tsx
@@ -8,16 +8,7 @@ import {
 // eslint-disable-next-line @odh-dashboard/no-restricted-imports
 import WhosMyAdministrator from '@odh-dashboard/internal/components/WhosMyAdministrator';
 import { MLFLOW_NOT_CONFIGURED_TITLE, MLFLOW_NOT_CONFIGURED_MESSAGE } from './const';
-import supportIconSvg from '../images/Icon-Red_Hat-Support-A-Black-RGB.svg';
-
-const SupportIcon: React.FC = () => (
-  <span
-    style={{ display: 'inline-flex', width: 60, height: 60, color: 'inherit' }}
-    // eslint-disable-next-line react/no-danger
-    dangerouslySetInnerHTML={{ __html: supportIconSvg }}
-    aria-hidden
-  />
-);
+import SupportIcon from '../icons/SupportIcon';
 
 const MLflowNotConfigured: React.FC = () => (
   <EmptyState
