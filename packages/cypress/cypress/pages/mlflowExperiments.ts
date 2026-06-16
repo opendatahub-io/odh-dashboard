11175516b40cbab3ecb43bcf16bd24f52583d1d4
Sri Sai Sowjanya Darna <sdarna@redhat.com>
2026-06-16 09:29:15 -0400

    cherry pick mlflow and prompt e2e fixes (#8127)
    Co-authored-by: Nana Nosirova <10577112+nananosirova@users.noreply.github.com>

--- packages/cypress/cypress/pages/mlflowExperiments.ts
+++ packages/cypress/cypress/pages/mlflowExperiments.ts
@@ -39,7 +39,7 @@ class MlflowExperiments {
 
   findNavItem() {
     return appChrome.findNavItem({
-      name: 'Experiments (MLflow)',
+      name: 'Experiments',
       rootSection: 'Develop & train',
     });
   }
@@ -113,7 +113,10 @@ class MlflowExperiments {
   }
 
   findCreateExperimentButton() {
-    return cy.findByTestId('create-experiment-table-empty-state-button', { timeout: 30000 });
+    return cy.get(
+      '[data-testid="create-experiment-table-empty-state-button"], [data-testid="create-experiment-button"]',
+      { timeout: 30000 },
+    );
   }
 
   findExperimentInTable(name: string) {
@@ -181,7 +184,7 @@ class MlflowExperiments {
   }
 
   findCompareRunsHeading() {
-    return cy.contains('Comparing');
+    return cy.findByTestId('mlflow-breadcrumb-active');
   }
 
   findCompareRunsVisualizations() {
