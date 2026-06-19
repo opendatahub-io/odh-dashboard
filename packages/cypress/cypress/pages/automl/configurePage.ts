6e5ab7686d78f04ad133d0990df6a95f7fa95ba5
Jeffrey Ho <jefho@redhat.com>
2026-06-19 12:24:02 -0400

    fix(automl): enable managed pipelines on test DSPA

--- packages/cypress/cypress/pages/automl/configurePage.ts
+++ packages/cypress/cypress/pages/automl/configurePage.ts
@@ -1,7 +1,6 @@
 import { automlExperimentsPage } from './experimentsPage';
 import { automlResultsPage } from './resultsPage';
 import { HTPASSWD_CLUSTER_ADMIN_USER } from '../../utils/e2eUsers';
-import { uploadManagedAutoMLPipelines } from '../../utils/autoXPipelines';
 import { waitForDspaReady } from '../../utils/oc_commands/dspa';
 import type { AutomlTestData } from '../../types';
 
@@ -172,9 +171,6 @@ class AutomlConfigurePage {
     cy.step('Login and wait for pipeline server');
     cy.visitWithLogin('/', HTPASSWD_CLUSTER_ADMIN_USER);
     waitForDspaReady(projectName);
-
-    cy.step('Upload managed AutoML pipelines to pipeline server');
-    uploadManagedAutoMLPipelines(projectName);
 
     cy.step('Navigate to AutoML experiments page');
     automlExperimentsPage.visit(projectName);
