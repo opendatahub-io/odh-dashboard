9a206955f865350f7144cd5bc8bc65078140f7a4
katieperry4 <katelynnperry99@gmail.com>
2026-06-29 11:59:01 -0400

    remove other flag references

--- packages/maas/frontend/src/app/pages/auth-policies/allAuthPolicies/AuthPoliciesTable.tsx
+++ packages/maas/frontend/src/app/pages/auth-policies/allAuthPolicies/AuthPoliciesTable.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Table, DashboardEmptyTableView } from '@odh-dashboard/ui-core';
-import { DashboardConfigContext } from '@odh-dashboard/plugin-core';
 import { MaaSAuthPolicy } from '~/app/types/subscriptions';
 import { authPoliciesColumns } from './columns';
 import AuthPoliciesTableRow from './AuthPoliciesTableRow';
@@ -19,33 +18,28 @@ const AuthPoliciesTable: React.FC<AuthPoliciesTableProps> = ({
   toolbarContent,
   onClearFilters,
   returnTo,
-}) => {
-  const dashboardConfig = React.useContext(DashboardConfigContext);
-  const isIARedesign = !!dashboardConfig?.dashboardConfig.maasSettingsIaRedesign;
-
-  return (
-    <Table
-      data-testid="auth-policies-table"
-      enablePagination
-      disableRowRenderSupport
-      isExpandable={isIARedesign}
-      data={authPolicies}
-      columns={authPoliciesColumns}
-      rowRenderer={(authPolicy: MaaSAuthPolicy, rowIndex: number) => (
-        <AuthPoliciesTableRow
-          key={authPolicy.name}
-          authPolicy={authPolicy}
-          rowIndex={rowIndex}
-          columns={authPoliciesColumns}
-          setDeleteAuthPolicy={setDeleteAuthPolicy}
-          returnTo={returnTo}
-        />
-      )}
-      emptyTableView={<DashboardEmptyTableView onClearFilters={onClearFilters} />}
-      toolbarContent={toolbarContent}
-      onClearFilters={onClearFilters}
-    />
-  );
-};
+}) => (
+  <Table
+    data-testid="auth-policies-table"
+    enablePagination
+    disableRowRenderSupport
+    isExpandable
+    data={authPolicies}
+    columns={authPoliciesColumns}
+    rowRenderer={(authPolicy: MaaSAuthPolicy, rowIndex: number) => (
+      <AuthPoliciesTableRow
+        key={authPolicy.name}
+        authPolicy={authPolicy}
+        rowIndex={rowIndex}
+        columns={authPoliciesColumns}
+        setDeleteAuthPolicy={setDeleteAuthPolicy}
+        returnTo={returnTo}
+      />
+    )}
+    emptyTableView={<DashboardEmptyTableView onClearFilters={onClearFilters} />}
+    toolbarContent={toolbarContent}
+    onClearFilters={onClearFilters}
+  />
+);
 
 export default AuthPoliciesTable;
