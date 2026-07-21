bc1a7a468209e579944290af7bed1d5d952ff2aa
paulovmr <832830+paulovmr@users.noreply.github.com>
2026-07-21 11:30:10 -0300

    RHOAIENG-72366: Move template hooks to @odh-dashboard/model-serving/shared
    Signed-off-by: paulovmr <832830+paulovmr@users.noreply.github.com>

--- frontend/src/pages/modelServing/customServingRuntimes/useTemplateOrder.ts
+++ frontend/src/pages/modelServing/customServingRuntimes/useTemplateOrder.ts
@@ -1,11 +1,6 @@
-import * as React from 'react';
-import useFetch, {
-  FetchOptions,
-  FetchStateObject,
-  NotReadyError,
-} from '@odh-dashboard/ui-core/hooks/useFetch';
+import { FetchOptions, FetchStateObject } from '@odh-dashboard/ui-core/hooks/useFetch';
+import { useTemplateOrder as useTemplateOrderBase } from '@odh-dashboard/model-serving/shared';
 import { getDashboardConfigTemplateOrder } from '#~/api';
-import useCustomServingRuntimesEnabled from '#~/pages/modelServing/customServingRuntimes/useCustomServingRuntimesEnabled';
 import { getDashboardConfigTemplateOrderBackend } from '#~/services/dashboardService';
 
 const useTemplateOrder = (
@@ -13,36 +8,12 @@ const useTemplateOrder = (
   adminPanel?: boolean,
   fetchOptions?: Partial<FetchOptions>,
 ): FetchStateObject<string[]> => {
-  const customServingRuntimesEnabled = useCustomServingRuntimesEnabled();
+  // TODO: Remove backend workaround when we migrate admin panel to Passthrough API
+  const fetcher = adminPanel
+    ? getDashboardConfigTemplateOrderBackend
+    : getDashboardConfigTemplateOrder;
 
-  const getTemplateOrder = React.useCallback(() => {
-    if (!namespace) {
-      return Promise.reject(new Error('No namespace provided'));
-    }
-
-    if (!customServingRuntimesEnabled) {
-      return Promise.reject(new NotReadyError('Custom serving runtime is not enabled'));
-    }
-
-    // TODO: Remove this when we migrate admin panel to Passthrough API
-    if (adminPanel) {
-      return getDashboardConfigTemplateOrderBackend(namespace).catch((e) => {
-        if (e.statusObject?.code === 404) {
-          throw new Error('Dashboard config template order is not configured.');
-        }
-        throw e;
-      });
-    }
-
-    return getDashboardConfigTemplateOrder(namespace).catch((e) => {
-      if (e.statusObject?.code === 404) {
-        throw new Error('Dashboard config template order is not configured.');
-      }
-      throw e;
-    });
-  }, [namespace, customServingRuntimesEnabled, adminPanel]);
-
-  return useFetch<string[]>(getTemplateOrder, [], fetchOptions);
+  return useTemplateOrderBase(namespace, fetcher, fetchOptions);
 };
 
 export default useTemplateOrder;
