72d1c62bd2410456eaa3bc8227cf40186dffb2ec
Kanishka Panwar <kanishka03p@gmail.com>
2026-07-22 15:27:22 +0530

    fix(RHOAIENG-62527): tighten Ray dashboard URL RBAC to named gets

--- dashboard-operator/internal/controller/dashboard_reconciler.go
+++ dashboard-operator/internal/controller/dashboard_reconciler.go
@@ -268,6 +268,8 @@ func (r *DashboardReconciler) reconcileSidecar(
 		allResources = append(allResources, rendered...)
 	}
 
+	remapRayDashboardGatewayRBAC(allResources)
+
 	deployer := deploy.NewDeployer(
 		deploy.WithFieldOwner("dashboard-operator"),
 		deploy.WithLabel(labels.PlatformPartOf, strings.ToLower(v1alpha1.DashboardKind)),
@@ -370,6 +372,8 @@ func (r *DashboardReconciler) reconcileStandalone(
 		}
 		allResources = append(allResources, rendered...)
 	}
+
+	remapRayDashboardGatewayRBAC(allResources)
 
 	deployer := deploy.NewDeployer(
 		deploy.WithFieldOwner("dashboard-operator"),
