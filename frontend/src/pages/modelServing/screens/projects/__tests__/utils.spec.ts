3b4ad25d10cfbaf9b24cb18e71acd700affccbfd
paulovmr <832830+paulovmr@users.noreply.github.com>
2026-08-03 10:37:36 -0300

    moving tests
    Signed-off-by: paulovmr <832830+paulovmr@users.noreply.github.com>

--- frontend/src/pages/modelServing/screens/projects/__tests__/utils.spec.ts
+++ frontend/src/pages/modelServing/screens/projects/__tests__/utils.spec.ts
@@ -1,10 +1,5 @@
 import type { PersistentVolumeClaimKind } from '@odh-dashboard/k8s-core';
-import {
-  ServingRuntimePlatform,
-  getModelPathFromUri,
-  isPVCUri,
-  getPVCNameFromURI,
-} from '@odh-dashboard/model-serving/shared';
+import { ServingRuntimePlatform } from '@odh-dashboard/model-serving/shared';
 import type { ServingRuntimeKind } from '@odh-dashboard/model-serving/shared';
 import { mockProjectK8sResource } from '@odh-dashboard/k8s-core/__mocks__/mockProjectK8sResource';
 import { mockPVCK8sResource } from '@odh-dashboard/k8s-core/__mocks__/mockPVCK8sResource';
@@ -577,42 +572,5 @@ describe('getPVCFromURI', () => {
       mockPVCK8sResource({ name: 'pvc-2', uid: 'pvc-2-uid' }),
     ];
     expect(getPVCFromURI(uri, pvcs)).toBeUndefined();
-  });
-});
-
-describe('getModelPathFromUri', () => {
-  it('should return the model path', () => {
-    const uri = 'pvc://pvc-1/model-path';
-    expect(getModelPathFromUri(uri)).toEqual('model-path');
-  });
-  it('should return an empty string if the URI is not a valid URI', () => {
-    const uri = 'not a uri';
-    expect(getModelPathFromUri(uri)).toEqual('');
-  });
-});
-
-describe('isPVCUri', () => {
-  it('should return true if the URI is a PVC URI', () => {
-    const uri = 'pvc://pvc-1/model-path';
-    expect(isPVCUri(uri)).toEqual(true);
-  });
-  it('should return false if the URI is not a PVC URI', () => {
-    const uri = 'not a uri';
-    expect(isPVCUri(uri)).toEqual(false);
-  });
-});
-
-describe('getPVCNameFromURI', () => {
-  it('should return the PVC name from the URI', () => {
-    const uri = 'pvc://pvc-1/model-path';
-    expect(getPVCNameFromURI(uri)).toEqual('pvc-1');
-  });
-  it('should return an empty string if the URI is not a valid URI', () => {
-    const uri = 'not a uri';
-    expect(getPVCNameFromURI(uri)).toEqual('');
-  });
-  it('should return an empty string if the URI is not a PVC URI', () => {
-    const uri = 'http://pvc-1/model-path';
-    expect(getPVCNameFromURI(uri)).toEqual('');
   });
 });
