4dfe3642472d140575b95ca1eed28d124f9d381a
Dipanshu Gupta <dipgupta@redhat.com>
2026-08-19 15:25:55 +0530

    fix: hybrid pnpm+npm upstream Docker builds and webpack resolve aliases
    Upstream subtree frontends stay npm islands while workspace packages use pnpm.
    Add MUI peer aliases, PatternFly CSS includes for upstream prod builds, and
    Jest fixes for observability and echarts under pnpm.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,6 @@
-module.exports = require('@odh-dashboard/eslint-config').recommendedReactTypescript(__dirname);
+const base = require('@odh-dashboard/eslint-config').recommendedReactTypescript(__dirname);
+
+module.exports = {
+  ...base,
+  ignorePatterns: [...(base.ignorePatterns ?? []), '**/upstream/**'],
+};
