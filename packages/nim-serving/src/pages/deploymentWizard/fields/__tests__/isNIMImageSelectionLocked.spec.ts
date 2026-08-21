123949de1a7e025ab2c6db189d2ede09bd2fa499
Yulia Krimerman <juliapiterova@hotmail.com>
2026-08-21 14:58:03 -0400

    working version

---
+++ packages/nim-serving/src/pages/deploymentWizard/fields/__tests__/isNIMImageSelectionLocked.spec.ts
@@ -0,0 +1,47 @@
+import { isNIMImageSelectionLocked } from '../NIMImageField';
+
+describe('isNIMImageSelectionLocked', () => {
+  it('should lock image selection when editing with a valid catalog image', () => {
+    expect(
+      isNIMImageSelectionLocked(
+        true,
+        { repository: 'nvcr.io/nim/test/test-model', tag: '1.0.0' },
+        false,
+      ),
+    ).toBe(true);
+  });
+
+  it('should unlock image selection when repository is missing', () => {
+    expect(isNIMImageSelectionLocked(true, { repository: '', tag: '1.0.0' }, false)).toBe(false);
+  });
+
+  it('should unlock image selection when tag is missing', () => {
+    expect(
+      isNIMImageSelectionLocked(
+        true,
+        { repository: 'nvcr.io/nim/test/test-model', tag: '' },
+        false,
+      ),
+    ).toBe(false);
+  });
+
+  it('should unlock image selection when the image is not found in the catalog', () => {
+    expect(
+      isNIMImageSelectionLocked(
+        true,
+        { repository: 'nvcr.io/nim/test/legacy-model', tag: '9.9.9' },
+        true,
+      ),
+    ).toBe(false);
+  });
+
+  it('should not lock image selection when not editing', () => {
+    expect(
+      isNIMImageSelectionLocked(
+        false,
+        { repository: 'nvcr.io/nim/test/test-model', tag: '1.0.0' },
+        false,
+      ),
+    ).toBe(false);
+  });
+});
