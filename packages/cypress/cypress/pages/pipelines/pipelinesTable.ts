6d1b764f1353a242eb141763372ead9592ffd588
Nana Nosirova <10577112+nananosirova@users.noreply.github.com>
2026-08-21 05:31:14 -0400

    Make pipeline e2e tests handle the MLflow integration section

--- packages/cypress/cypress/pages/pipelines/pipelinesTable.ts
+++ packages/cypress/cypress/pages/pipelines/pipelinesTable.ts
@@ -168,6 +168,10 @@ class PipelinesTable {
     return cy.findByTestId(this.testId);
   }
 
+  findPipelineLinkByName(name: string, timeout = 60000) {
+    return this.find().contains('a', name, { timeout });
+  }
+
   findRows() {
     return this.find().find('tbody tr');
   }
--- packages/cypress/cypress/utils/s3Buckets.ts
+++ packages/cypress/cypress/utils/s3Buckets.ts
@@ -1,3 +1,12 @@
 import type { AWSS3Buckets } from '../types';
 
 export const AWS_BUCKETS: AWSS3Buckets = Cypress.env('AWS_PIPELINES');
+
+export const parseS3Endpoint = (endpoint: string): { host: string; scheme: string } => {
+  try {
+    const url = new URL(endpoint);
+    return { host: url.host, scheme: url.protocol.replace(':', '') };
+  } catch {
+    return { host: endpoint.replace(/\/+$/, ''), scheme: 'https' };
+  }
+};
