3e2e2aaccd7c2c1c70d9e5d42a6b496a8283c7c3
Emily Samoylov <esamoylo@redhat.com>
2026-09-01 14:47:01 -0400

    empty string edge case

--- packages/nim-serving/src/pages/clusterStorage/clusterStorage.ts
+++ packages/nim-serving/src/pages/clusterStorage/clusterStorage.ts
@@ -4,4 +4,4 @@ export const NIM_PVC_ANNOTATION = 'dashboard.opendatahub.io/nim-pvc';
 export const NIM_PVC_SUBPATH_ANNOTATION = 'dashboard.opendatahub.io/nim-subpath';
 
 export const isNIMPVC = (pvc: PersistentVolumeClaimKind): boolean =>
-  !!pvc.metadata.annotations?.[NIM_PVC_ANNOTATION];
+  !!pvc.metadata.annotations && Object.hasOwn(pvc.metadata.annotations, NIM_PVC_ANNOTATION);
