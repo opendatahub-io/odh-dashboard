58861c7f69015aaded670d1bfdb2cbcf10a1a56d
Dipanshu Gupta <dipgupta@redhat.com>
2026-09-04 20:28:43 +0530

    fix: complete pnpm migration follow-up

--- packages/cypress/cypress/utils/__tests__/filterVirtualModuleCoverage.test.ts
+++ packages/cypress/cypress/utils/__tests__/filterVirtualModuleCoverage.test.ts
@@ -1,10 +1,11 @@
 import { filterVirtualModuleCoverage } from '../filterVirtualModuleCoverage';
 
 describe('filterVirtualModuleCoverage', () => {
-  it('removes data: URL coverage entries', () => {
+  it('removes virtual module coverage entries', () => {
     const filtered = filterVirtualModuleCoverage({
       '/app/src/foo.ts': { path: '/app/src/foo.ts' },
       'data:text/javascript,inline': { path: 'data:text/javascript,inline' },
+      '__module_federation/runtime': { path: '__module_federation/runtime' },
     });
 
     expect(Object.keys(filtered)).to.deep.equal(['/app/src/foo.ts']);
