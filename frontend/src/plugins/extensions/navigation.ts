d7b341c6721e417d98be7f5ca5682f724499c6af
Taj010 <rsheen003@gmail.com>
2026-09-08 14:00:00 -0400

    adjust and address comments

--- frontend/src/plugins/extensions/navigation.ts
+++ frontend/src/plugins/extensions/navigation.ts
@@ -73,9 +73,6 @@ const extensions: (NavExtension | TabRoutePageExtension)[] = [
   },
   {
     type: 'app.tab-route/page',
-    flags: {
-      required: [SupportedArea.AGENT_OPS],
-    },
     properties: {
       id: 'agents-tab-page',
       title: 'Agents',
