c67ae58310486d0e83558892f33c1f0b373228ec
Michal Stokluska <mstoklus@redhat.com>
2026-09-10 15:26:15 +0200

    feat(data-registry): add collections details page (#9634)
    * feat(data-registry): add collections details page
    
    * chore(data-registry): address comments
    
    * chore(data-registry): fix the collection details page owner and update by and address some coderabbit comments

--- packages/data-registry/frontend/src/app/pages/TableDetailView.tsx
+++ packages/data-registry/frontend/src/app/pages/TableDetailView.tsx
@@ -19,7 +19,7 @@ import { Link } from 'react-router-dom';
 import { AssetResponse } from '~/app/types';
 import SchemaColumnsTable from '~/app/components/SchemaColumnsTable';
 import ConnectionRefLink from '~/app/components/ConnectionRefLink';
-import { browseUrl } from '~/app/utilities/routes';
+import { collectionDetailUrl } from '~/app/utilities/routes';
 import {
   getFormatBadge,
   getUnstructuredFormatLabel,
@@ -99,7 +99,9 @@ const TableDetailView: React.FC<TableDetailViewProps> = ({ asset, project }) => 
                 <DescriptionListDescription data-testid="asset-collection">
                   {asset.collection ? (
                     project ? (
-                      <Link to={browseUrl(project)}>{asset.collection}</Link>
+                      <Link to={collectionDetailUrl(project, asset.collection)}>
+                        {asset.collection}
+                      </Link>
                     ) : (
                       asset.collection
                     )
