82a372bb697c6e29ce098b059879e673aedaddab
Emily Samoylov <esamoylo@redhat.com>
2026-09-14 11:35:25 -0400

    nim deploy e2e

--- packages/cypress/cypress/types.ts
+++ packages/cypress/cypress/types.ts
@@ -344,6 +344,20 @@ export type DataScienceProjectData = {
   deploymentMethod: 'llm-inference-service-llmd' | 'llm-inference-service-simple-vllm' | 'legacy';
 };
 
+export type NIMProjectScopedTestData = {
+  projectNamePrefix: string;
+  modelNamePrefix: string;
+  modelDescription: string;
+  nimImageNameWithGpu: string;
+  nimImageNameWithoutGpu: string;
+  nimModelId: string;
+  hardwareProfileName: string;
+  hardwareProfileYamlPath: string;
+  pvcNamePrefix: string;
+  pvcSizeGi: number;
+  tokenDisplayName: string;
+};
+
 export type RoutingTestData = DataScienceProjectData & {
   routingConfigName: string;
   routingConfigFixture: string;
