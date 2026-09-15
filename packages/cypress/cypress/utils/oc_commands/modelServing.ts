056d61ed3a753e53a7ceca8b58f9f591e60e0139
sridarna <sdarna@redhat.com>
2026-09-15 14:57:44 -0400

    update checkInferenceServiceState function to 5 sec wait time as documented

--- packages/cypress/cypress/utils/oc_commands/modelServing.ts
+++ packages/cypress/cypress/utils/oc_commands/modelServing.ts
@@ -289,7 +289,7 @@ export const checkInferenceServiceState = (
         cy.log(errorMessage);
         throw new Error(errorMessage);
       } else {
-        return cy.wait(50000).then(() => {
+        return cy.wait(5000).then(() => {
           failOnDeploymentStatus(serviceName);
           return checkState();
         });
